176ea43713fc4644463471721e82ae92b7552352
Akhila Raju <akhila@berkeley.edu>
2018-10-03 14:50:16 -0400

    Update CrystallizedState fields as per spec update.

--- beaconchain/src/main/java/net/consensys/beaconchain/state/CrystallizedState.java
+++ beaconchain/src/main/java/net/consensys/beaconchain/state/CrystallizedState.java
@@ -12,13 +12,14 @@ public class CrystallizedState {
 
   private CrosslinkRecord[] crosslink_records;
   private Hash dynasty_seed;
-  private Int32[] penalized_in_wp;
+  private Hash validator_set_delta_hash_chain;
+  private Int32[] deposits_penalized_in_period;
   private Int64 current_dynasty;
   private Int64 dynasty_start;
   private Int64 justified_streak;
   private Int64 last_finalized_slot;
   private Int64 last_justified_slot;
-  private Int64 last_state_recalc;
+  private Int64 last_state_recalculation;
   private ListOfValidators validators;
   private ShardAndCommittee[][] shard_and_committee_for_slots;
 
