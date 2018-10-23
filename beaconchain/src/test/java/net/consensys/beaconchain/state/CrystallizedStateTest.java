33801326aca57b303b630440dcebd203910a8da3
Akhila Raju <akhila@berkeley.edu>
2018-10-23 13:56:22 -0400

    WIP

--- beaconchain/src/test/java/net/consensys/beaconchain/state/CrystallizedStateTest.java
+++ beaconchain/src/test/java/net/consensys/beaconchain/state/CrystallizedStateTest.java
@@ -1,6 +1,11 @@
 package net.consensys.beaconchain.state;
 
+//import net.consensys.beaconchain.datastructures.ValidatorRecord;
+import net.consensys.beaconchain.state.CrystallizedState.CrystallizedStateOperators;
 import org.junit.Test;
+
+
+import org.web3j.abi.datatypes.generated.Bytes3;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,30 +14,52 @@ public class CrystallizedStateTest {
 
   /** Validates int to Bytes3 conversion. */
   @Test
-  public void toBytes3() {
-    assertEquals(true, true);
+  public void toBytes3Test() {
+
+    int seed = 212420;
+    byte[] bytes = {(byte) 3, (byte) 829, (byte) 212420};
+
+    Bytes3 expected = new Bytes3(bytes);
+    Bytes3 actual = CrystallizedStateOperators.toBytes3(seed);
+
+    assertEquals(expected, actual);
   }
 
 
-  /** Validate fromBytes3. */
+  /** Validate Bytes3 to int conversion. */
   @Test
-  public void fromBytes3() {
-    assertEquals(true, true);
+  public void fromBytes3Test() {
+
+    byte[] src = new byte[3];
+    int pos = 0;
+
+    int expected = 30;
+    int actual = CrystallizedStateOperators.fromBytes3(src, pos);
+
+    assertEquals(expected, actual);
   }
 
 
-  /** Validate shuffle. */
-  @Test
-  public void shuffle() {
-    assertEquals(true, true);
-  }
-
-
-  /** Validate split. */
-  @Test
-  public void split() {
-    assertEquals(true, true);
-  }
+//  /** Validate shuffle. */
+//  @Test
+//  public void shuffleTest() {
+//
+//    ValidatorRecord[] expected = new ValidatorRecord[];
+//    ValidatorRecord[] actual = CrystallizedStateOperators.shuffle();
+//
+//    assertEquals(expected, actual);
+//  }
+//
+//
+//  /** Validate split. */
+//  @Test
+//  public void splitTest() {
+//
+//    ValidatorRecord[][] expected = new ValidatorRecord[][];
+//    ValidatorRecord[][] actual = CrystallizedStateOperators.split();
+//
+//    assertEquals(expected, actual);
+//  }
 
 
 }
