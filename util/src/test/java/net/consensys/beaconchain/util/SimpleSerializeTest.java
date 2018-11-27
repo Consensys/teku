c0f66e43c6edbb23a7d4a2a4e12cc5088aeb76fb
Joseph Delong <joseph@delong.me>
2018-11-27 15:51:53 -0600

    add basic test for bytes32

--- util/src/test/java/net/consensys/beaconchain/util/SimpleSerializeTest.java
+++ util/src/test/java/net/consensys/beaconchain/util/SimpleSerializeTest.java
@@ -1,6 +1,9 @@
 package net.consensys.beaconchain.util;
 
-import static org.junit.Assert.assertEquals;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.assertArrayEquals;
+
+import net.consensys.beaconchain.util.bytes.Bytes32;
 
 import org.junit.Test;
 
@@ -10,6 +13,12 @@ public class SimpleSerializeTest {
 
   @Test
   public void serializeBytes32Test() {
-    assertEquals(true, false);
+    Bytes32 bytes32 = Bytes32.wrap("55555555555555555555555555555555".getBytes(UTF_8));
+
+    byte[] serialized = SimpleSerialize.serialize(bytes32);
+
+    byte[] comparator = "55555555555555555555555555555555".getBytes(UTF_8);
+
+    assertArrayEquals(serialized, comparator);
   }
 }
