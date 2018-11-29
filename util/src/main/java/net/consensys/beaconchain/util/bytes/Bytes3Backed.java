d38ed44b22061a337efef458beacdb16ddbff856
Akhila Raju <akhila@berkeley.edu>
2018-11-29 13:47:12 -0500

    Made BytesBacked a generic type rather than create a new interface for Bytes3Backed.

--- util/src/main/java/net/consensys/beaconchain/util/bytes/Bytes3Backed.java
+++
@@ -1,9 +0,0 @@
-package net.consensys.beaconchain.util.bytes;
-
-/**
- * Base interface for a value whose content is stored with exactly 3 bytes.
- */
-public interface Bytes3Backed extends BytesBacked {
-  @Override
-  Bytes3 bytes();
-}
