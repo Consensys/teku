efd2ad63451bc80bf548e0da8155b95d3ae5180a
jonny rhea <jonathan.rhea@gmail.com>
2018-12-04 23:26:48 -0600

    resolves #40 added basic skeleton classes and a main

---
+++ beaconchain/src/main/java/net/consensys/beaconchain/controllers/NodeController.java
@@ -0,0 +1,33 @@
+package net.consensys.beaconchain.controllers;
+
+
+public class NodeController {
+
+    public NodeController(){
+
+    }
+
+    public void init(){
+        // initialize/register all services
+
+        // PoWchain Service
+
+        // Blockchain Service
+
+        // Validator Service
+
+        // P2P Service
+
+        // RPC Service
+    }
+
+    public void start(){
+        // start all services
+
+
+    }
+
+    public void stop(){
+        // stop all services
+    }
+}
---
+++ beaconchain/src/main/java/net/consensys/beaconchain/controllers/ValidatorController.java
@@ -0,0 +1,22 @@
+package net.consensys.beaconchain.controllers;
+
+
+public class ValidatorController {
+
+    public ValidatorController(){
+
+    }
+
+    public void init(){
+        // instatiate validator
+        // register event listeners
+    }
+
+    public void start(){
+        // Begin attesting/proposing
+    }
+
+    public void stop(){
+
+    }
+}
