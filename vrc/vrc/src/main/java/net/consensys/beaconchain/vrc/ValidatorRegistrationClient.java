d90709b6eb8cb9e25236416e0c98c7c6497c706f
jonny rhea <jonathan.rhea@gmail.com>
2018-12-06 11:24:27 -0600

    added TODO's and comments to answer questions about what needs to be implemented in the VRC and to clarify the code that is simulating registration events

--- vrc/vrc/src/main/java/net/consensys/beaconchain/vrc/ValidatorRegistrationClient.java
+++ vrc/vrc/src/main/java/net/consensys/beaconchain/vrc/ValidatorRegistrationClient.java
@@ -5,6 +5,8 @@ import java.util.Random;
 
 import com.google.common.eventbus.EventBus;
 
+// TODO:  This class needs to utilize a Web3 provider to listen to the PoWChain
+// for validator registrations.
 public class ValidatorRegistrationClient {
 
     private final EventBus eventBus;
@@ -14,6 +16,8 @@ public class ValidatorRegistrationClient {
     }
 
     public void listenForValidators(){
+        // TODO:  This code will be removed when we have the code 
+        // wired to listen for real validator registrations. 
         while(true){
             try {
                 Thread.sleep(1000);
@@ -24,12 +28,15 @@ public class ValidatorRegistrationClient {
 
             byte[] array = new byte[7];
             new Random().nextBytes(array);
-            String generatedString = new String(array, Charset.forName("UTF-8"));
+            String generatedString = new String(array, Charset.forName("US-ASCII"));
+
+            // let listeners know that a new validator has registered
             validatorRegistered(generatedString);
         }
     }
 
     public void validatorRegistered(String validatorInfo){
+        // TODO: pass in real validator information that the beacon chain needs to know about
         ValidatorRegisteredEvent vrcEvent = new ValidatorRegisteredEvent();
         vrcEvent.setName(validatorInfo);
         this.eventBus.post(vrcEvent);
