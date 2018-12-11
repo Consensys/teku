494b5df878a2bcb52d86e0608df52e71d8976f5e
Akhila Raju <akhila@berkeley.edu>
2018-12-11 13:07:05 -0500

    Added DepositData class.

--- beaconchain/src/main/java/net/consensys/beaconchain/datastructures/BeaconChainOperations/Deposit.java
+++ beaconchain/src/main/java/net/consensys/beaconchain/datastructures/BeaconChainOperations/Deposit.java
@@ -20,19 +20,9 @@ public class Deposit {
 
   private Hash[] merkle_branch;
   private UInt64 merkle_tree_index;
-
-  //     # Deposit data
-  //    'deposit_data': {
-  //        # Deposit parameters
-  //        'deposit_parameters': DepositParametersRecord,
-  //        # Value in Gwei
-  //        'value': 'uint64',
-  //        # Timestamp from deposit contract
-  //        'timestamp': 'uint64',
-  //    },
+  private DepositData deposit_data;
 
   public Deposit() {
 
   }
-
 }
