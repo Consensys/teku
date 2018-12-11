a967ca59273afaeb18c89ae8f816f1874e66a49b
Akhila Raju <akhila@berkeley.edu>
2018-12-11 13:07:05 -0500

    Added DepositData class.

---
+++ beaconchain/src/main/java/net/consensys/beaconchain/datastructures/BeaconChainOperations/DepositData.java
@@ -0,0 +1,15 @@
+package net.consensys.beaconchain.datastructures.BeaconChainOperations;
+
+import net.consensys.beaconchain.util.uint.UInt64;
+
+public class DepositData {
+
+  private DepositParameters depositParameters;
+  private UInt64 value;
+  private UInt64 timestamp;
+
+  public DepositData() {
+
+  }
+
+}
