b5691b13efc507b24c8b326c75935a03c7a9742c
Akhila Raju <akhila.raju@consensys.net>
2018-12-17 21:55:04 -0600

    Updated Deposit fields

--- artemis/src/main/java/tech/pegasys/artemis/datastructures/BeaconChainOperations/DepositData.java
+++ artemis/src/main/java/tech/pegasys/artemis/datastructures/BeaconChainOperations/DepositData.java
@@ -17,7 +17,7 @@ import tech.pegasys.artemis.util.uint.UInt64;
 
 public class DepositData {
 
-  private DepositParameters depositParameters;
+  private DepositInput depositInput;
   private UInt64 value;
   private UInt64 timestamp;
 
