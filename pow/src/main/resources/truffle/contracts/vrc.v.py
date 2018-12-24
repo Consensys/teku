0700be83e2976e5564cc1196a257e1b5a755e4bb
Joseph Delong <joseph@delong.me>
2018-12-24 17:41:21 -0600

    update vrc contract to reflect Eth2 spec

--- pow/src/main/resources/truffle/contracts/vrc.v.py
+++ pow/src/main/resources/truffle/contracts/vrc.v.py
@@ -1,30 +1,30 @@
-## compiled with v0.1.0-beta.4 ##
+## compiled with v0.1.0-beta.6 ##
 
 MIN_DEPOSIT: constant(uint256) = 1  # ETH
 MAX_DEPOSIT: constant(uint256) = 32  # ETH
 GWEI_PER_ETH: constant(uint256) = 1000000000  # 10**9
 CHAIN_START_FULL_DEPOSIT_THRESHOLD: constant(uint256) = 16384  # 2**14
 DEPOSIT_CONTRACT_TREE_DEPTH: constant(uint256) = 32
+TWO_TO_POWER_OF_TREE_DEPTH: constant(uint256) = 4294967296  # 2**32
 SECONDS_PER_DAY: constant(uint256) = 86400
 
 Eth1Deposit: event({previous_receipt_root: bytes32, data: bytes[2064], deposit_count: uint256})
 ChainStart: event({receipt_root: bytes32, time: bytes[8]})
 
-
-receipt_tree: bytes32[uint256]
+receipt_tree: map(uint256, bytes32)
 deposit_count: uint256
 full_deposit_count: uint256
 
 @payable
 @public
-def deposit(deposit_parameters: bytes[2048]):
+def deposit(deposit_input: bytes[2048]):
     assert msg.value >= as_wei_value(MIN_DEPOSIT, "ether")
     assert msg.value <= as_wei_value(MAX_DEPOSIT, "ether")
 
-    index: uint256 = self.deposit_count + 2**DEPOSIT_CONTRACT_TREE_DEPTH
+    index: uint256 = self.deposit_count + TWO_TO_POWER_OF_TREE_DEPTH
     msg_gwei_bytes8: bytes[8] = slice(concat("", convert(msg.value / GWEI_PER_ETH, bytes32)), start=24, len=8)
     timestamp_bytes8: bytes[8] = slice(concat("", convert(block.timestamp, bytes32)), start=24, len=8)
-    deposit_data: bytes[2064] = concat(msg_gwei_bytes8, timestamp_bytes8, deposit_parameters)
+    deposit_data: bytes[2064] = concat(msg_gwei_bytes8, timestamp_bytes8, deposit_input)
 
     log.Eth1Deposit(self.receipt_tree[1], deposit_data, self.deposit_count)
 
