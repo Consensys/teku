13c24a2563b729946b74e5cac1830683659b9f5e
lakshmikanth <kanth.explorer@gmail.com>
2019-01-06 20:49:28 +0800

    ran spotlessApply to comply with formatting standards

--- artemis/src/test/java/tech/pegasys/artemis/state/util/ValidatorsUtilTest.java
+++ artemis/src/test/java/tech/pegasys/artemis/state/util/ValidatorsUtilTest.java
@@ -1,6 +1,22 @@
+/*
+ * Copyright 2019 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package tech.pegasys.artemis.state.util;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
+import static tech.pegasys.artemis.Constants.ACTIVE_PENDING_EXIT;
+import static tech.pegasys.artemis.Constants.EXITED_WITHOUT_PENALTY;
+
 import tech.pegasys.artemis.datastructures.beaconchainstate.ValidatorRecord;
 import tech.pegasys.artemis.datastructures.beaconchainstate.Validators;
 import tech.pegasys.artemis.ethereum.core.Hash;
@@ -8,9 +24,7 @@ import tech.pegasys.artemis.util.uint.UInt64;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.*;
-import static tech.pegasys.artemis.Constants.ACTIVE_PENDING_EXIT;
-import static tech.pegasys.artemis.Constants.EXITED_WITHOUT_PENALTY;
+import org.junit.Test;
 
 public class ValidatorsUtilTest {
 
