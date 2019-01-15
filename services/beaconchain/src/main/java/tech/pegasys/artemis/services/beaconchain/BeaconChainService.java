63579807886a11ed40650b00dd3e63a8457ad0db
Jonny Rhea <jonathan.rhea@gmail.com>
2019-01-15 17:05:34 -0600

    created StateTreeMananger, added simulated block and attestation events (#199)

--- services/beaconchain/src/main/java/tech/pegasys/artemis/services/beaconchain/BeaconChainService.java
+++ services/beaconchain/src/main/java/tech/pegasys/artemis/services/beaconchain/BeaconChainService.java
@@ -15,42 +15,31 @@ package tech.pegasys.artemis.services.beaconchain;
 
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
-import java.util.Date;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import tech.pegasys.artemis.Constants;
-import tech.pegasys.artemis.datastructures.beaconchainblocks.BeaconBlock;
 import tech.pegasys.artemis.pow.event.ChainStartEvent;
 import tech.pegasys.artemis.pow.event.ValidatorRegistrationEvent;
 import tech.pegasys.artemis.services.ServiceInterface;
-import tech.pegasys.artemis.state.BeaconState;
-import tech.pegasys.artemis.state.StateTransition;
+import tech.pegasys.artemis.state.StateTreeManager;
 
 public class BeaconChainService implements ServiceInterface {
 
   private EventBus eventBus;
-  private BeaconState state;
-  private StateTransition stateTransition;
   private ScheduledExecutorService scheduler;
   private static final Logger LOG = LogManager.getLogger();
-  private LinkedBlockingQueue<BeaconBlock> unprocessedBlocks;
+  private StateTreeManager stateTreeManager;
 
-  public BeaconChainService() {
-    this.state = new BeaconState();
-    this.stateTransition = new StateTransition();
-    this.unprocessedBlocks = new LinkedBlockingQueue<BeaconBlock>();
-  }
+  public BeaconChainService() {}
 
   @Override
   public void init(EventBus eventBus) {
     this.eventBus = eventBus;
     this.scheduler = Executors.newScheduledThreadPool(1);
+    this.stateTreeManager = new StateTreeManager(this.eventBus);
     this.eventBus.register(this);
   }
 
@@ -77,28 +66,5 @@ public class BeaconChainService implements ServiceInterface {
   public void onValidatorRegistered(ValidatorRegistrationEvent event) {
     LOG.info("Validator Registration Event detected");
     // LOG.info("   Validator Number: " + validatorRegisteredEvent.getInfo());
-  }
-
-  @Subscribe
-  public void onNewSlot(Date date) {
-    LOG.info("****** New Slot at: " + date + " ******");
-    Optional<BeaconBlock> block = unprocessedBlocks.stream().findFirst();
-    if (block.isPresent()) {
-      try {
-        stateTransition.initiate(this.state, block.get());
-      } catch (NoSuchElementException e) {
-        LOG.warn(e.toString());
-      }
-    }
-  }
-
-  // TODO: This event should accept a BeaconBlock.  Make the change once our datastructures are
-  // moved from artemis.jar
-  @Subscribe
-  public void onNewBlock(String beaconBlock) {
-    LOG.info("New Beacon Block Event detected");
-    BeaconBlock block = new BeaconBlock();
-    block.setSlot(this.state.getSlot());
-    unprocessedBlocks.add(block);
   }
 }
