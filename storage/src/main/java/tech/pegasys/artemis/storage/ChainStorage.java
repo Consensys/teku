632a91ef50d16075d6e1971d6f3f082d696e77f3
Steven J Schroeder <steven.schroeder@consensys.net>
2019-01-31 12:19:17 -0800

    Add Cava map (in-memory) kv store to chainstore.

--- storage/src/main/java/tech/pegasys/artemis/storage/ChainStorage.java
+++ storage/src/main/java/tech/pegasys/artemis/storage/ChainStorage.java
@@ -15,27 +15,35 @@ package tech.pegasys.artemis.storage;
 
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
+import net.consensys.cava.kv.MapKeyValueStore;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import tech.pegasys.artemis.datastructures.blocks.BeaconBlock;
 import tech.pegasys.artemis.datastructures.operations.Attestation;
 
 public class ChainStorage {
+  private MapKeyValueStore unprocessedStore;
+  private final String blockKey = "BLOCK";
+  private final String attestaionKey = "ATTESTATION";
   private final EventBus eventBus;
   private static final Logger LOG = LogManager.getLogger();
 
   public ChainStorage(EventBus eventBus) {
     this.eventBus = eventBus;
     this.eventBus.register(this);
+
+    this.unprocessedStore = new MapKeyValueStore();
   }
 
   @Subscribe
   public void onNewBlock(BeaconBlock block) {
     LOG.info("ChainStore - New Beacon Block Event detected");
+    /*unprocessedStore.putAsync(Bytes.wrap(blockKey.getBytes(Charset.defaultCharset())), null);*/
   }
 
   @Subscribe
   public void onNewAttestation(Attestation attestation) {
     LOG.info("ChainStore - New Attestation Event detected");
+    /*unprocessedStore.putAsync(Bytes.wrap(attestaionKey.getBytes(Charset.defaultCharset())), attestation.toBytes());*/
   }
 }
