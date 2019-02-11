3c68f4a6602b6b05b9fd2093f7624ad71d3b6e7d
jonny rhea <jonathan.rhea@gmail.com>
2019-02-11 12:30:52 -0600

    updated process_penalties_and_exits to 0.1 spec

---
+++ ethereum/statetransition/src/test/java/tech/pegasys/artemis/statetransition/util/BeaconStateUtilTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2019 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.artemis.statetransition.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.google.common.primitives.UnsignedLong;
+import net.consensys.cava.junit.BouncyCastleExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(BouncyCastleExtension.class)
+class BeaconStateUtilTest {
+  @Test
+  void minReturnsMin() {
+    UnsignedLong actual = BeaconStateUtil.min(UnsignedLong.valueOf(13L), UnsignedLong.valueOf(12L));
+    UnsignedLong expected = UnsignedLong.valueOf(12L);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void maxReturnsMax() {
+    UnsignedLong actual = BeaconStateUtil.max(UnsignedLong.valueOf(13L), UnsignedLong.valueOf(12L));
+    UnsignedLong expected = UnsignedLong.valueOf(13L);
+    assertEquals(expected, actual);
+  }
+}
