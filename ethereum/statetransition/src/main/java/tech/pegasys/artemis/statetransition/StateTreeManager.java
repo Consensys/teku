e3f341811b3f909b465119aab267dd39402d8c00
Jonny Rhea <jonathan.rhea@gmail.com>
2019-02-21 08:34:47 -0600

    slot scheduling is activated after onChainStart (#364)

--- ethereum/statetransition/src/main/java/tech/pegasys/artemis/statetransition/StateTreeManager.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/artemis/statetransition/StateTreeManager.java
@@ -53,34 +53,39 @@ public class StateTreeManager {
   @Subscribe
   public void onChainStarted(ChainStartEvent event) {
     LOG.info("ChainStart Event Detected");
+    Boolean result = false;
     try {
       this.state =
           BeaconStateUtil.get_initial_beacon_state(
               randomDeposits(100),
               UnsignedLong.valueOf(Constants.GENESIS_SLOT),
               new Eth1Data(Bytes32.ZERO, Bytes32.ZERO));
+      result = true;
+
     } catch (IllegalStateException e) {
       LOG.fatal("IllegalStateException thrown in StateTreeManager.java.");
       LOG.fatal(e.toString());
+    } finally {
+      this.eventBus.post(result);
     }
   }
 
   @Subscribe
   public void onValidatorRegistered(ValidatorRegistrationEvent event) {
     LOG.info("Validator Registration Event detected");
-    // LOG.info("   Validator Number: " + validatorRegisteredEvent.getInfo());
+    LOG.info("   Validator Number: " + event.getResponse().log.toString());
   }
 
   @Subscribe
   public void onNewSlot(Date date) {
     requireNonNull(state);
     LOG.info("****** New Slot at: " + date + " ******");
-    // TODO: get canonical state
-    this.state = BeaconState.deepCopy(state);
     Optional<BeaconBlock> block = this.storage.getUnprocessedBlock();
     if (block.isPresent()) {
       LOG.info("Unprocessed block retrieved.");
       try {
+        // get state corresponding to the block
+        this.state = BeaconState.deepCopy(state);
         stateTransition.initiate(this.state, block.get());
         this.storage.addProcessedBlock(block.get().getState_root(), block.get());
       } catch (NoSuchElementException e) {
