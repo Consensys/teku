aa01bd3873c7a418e84b1c927159dc70c7b4173d
jonny rhea <jonathan.rhea@gmail.com>
2019-02-25 09:09:33 -0600

    more fixes to block generation

--- networking/p2p/src/main/java/tech/pegasys/artemis/networking/p2p/MockP2PNetwork.java
+++ networking/p2p/src/main/java/tech/pegasys/artemis/networking/p2p/MockP2PNetwork.java
@@ -14,6 +14,7 @@
 package tech.pegasys.artemis.networking.p2p;
 
 import com.google.common.eventbus.EventBus;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
@@ -21,6 +22,7 @@ import net.consensys.cava.bytes.Bytes32;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import tech.pegasys.artemis.datastructures.blocks.BeaconBlock;
+import tech.pegasys.artemis.datastructures.operations.Deposit;
 import tech.pegasys.artemis.datastructures.state.BeaconState;
 import tech.pegasys.artemis.datastructures.util.DataStructureUtil;
 import tech.pegasys.artemis.networking.p2p.api.P2PNetwork;
@@ -95,6 +97,7 @@ public class MockP2PNetwork implements P2PNetwork {
       BeaconBlock block = BeaconBlock.createGenesis(state_root);
       Bytes32 parent_root = HashTreeUtil.hash_tree_root(block.toBytes());
       this.eventBus.post(block);
+      ArrayList<Deposit> deposits = DataStructureUtil.newDeposits(100);
       while (true) {
         Random random = new Random();
         long n = 1000L * Integer.toUnsignedLong(random.nextInt(7) + 6);
@@ -103,7 +106,8 @@ public class MockP2PNetwork implements P2PNetwork {
         state.incrementSlot();
         // Block Processing
         state_root = HashTreeUtil.hash_tree_root(state.toBytes());
-        block = DataStructureUtil.newBeaconBlock(state.getSlot(), parent_root, state_root);
+        block =
+            DataStructureUtil.newBeaconBlock(state.getSlot(), parent_root, state_root, deposits);
         parent_root = HashTreeUtil.hash_tree_root(block.toBytes());
         this.eventBus.post(block);
 
