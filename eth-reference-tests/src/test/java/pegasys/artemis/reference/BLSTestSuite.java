12b155ba5a0301b60760d01e348789bcd33c3f4d
Ben Edgington <ben@benjaminion.xyz>
2019-03-08 17:50:35 +0100

    Update to lastest (pre-release) official BLS tests (#435)

--- eth-reference-tests/src/test/java/pegasys/artemis/reference/BLSTestSuite.java
+++ eth-reference-tests/src/test/java/pegasys/artemis/reference/BLSTestSuite.java
@@ -39,22 +39,13 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 /*
  * The "official" BLS reference test data is from https://github.com/ethereum/eth2.0-tests/
- *
- * TODO: As of 2019-02-26 there are some issues with the reference data
- * The point compression used is broken. See https://github.com/ethereum/eth2.0-tests/issues/20
- * This leads to extensive and tedious work-arounds in the below. Test 06 is too hard to fix
- * Specifically:
- *   > The c flag is missing
- *   > The a flag is inverted and is at the wrong bit position
- *   > The compressed coordinate ordering is inverted: should be [Im, Re], but is [Re, Im]
- *   > The a flag doesn't always match the Y-coordinate, which matters when interpreting
- *     compressed input data, such as for the aggregation tests.
- * Also
- *   > Test case 07 input data is malformed.
- * Finally, note that the spec and test data will likely be changing.
  */
 
 class BLSTestSuite {
+
+  // TODO: reinstate the official tests once they have been updated
+  // private static String testFile = "**/bls/test_bls.yml";
+  private static String testFile = "**/test_bls_tmp.yml";
 
   @ParameterizedTest(name = "{index}. message hash to G2 uncompressed {0} -> {1}")
   @MethodSource("readMessageHashG2Uncompressed")
@@ -80,36 +71,10 @@ class BLSTestSuite {
     G2Point actual = G2Point.hashToG2(message, domain);
     Bytes48 xReExpected = Bytes48.leftPad(Bytes.fromHexString(output.get(0)));
     Bytes48 xImExpected = Bytes48.leftPad(Bytes.fromHexString(output.get(1)));
-
-    /*
-    // TODO the test should look like this when the test data have been fixed
-
     Bytes expectedBytes = Bytes.concatenate(xReExpected, xImExpected);
     Bytes actualBytes = actual.toBytesCompressed();
 
     assertEquals(expectedBytes, actualBytes);
-    */
-
-    // TODO Remove the following when the test data are fixed
-
-    byte[] xReBytes = new byte[48];
-    byte[] xImBytes = new byte[48];
-    actual.ecp2Point().getX().getA().toBytes(xReBytes);
-    actual.ecp2Point().getX().getB().toBytes(xImBytes);
-    Bytes48 xReActual = Bytes48.leftPad(Bytes48.wrap(xReBytes));
-    Bytes48 xImActual = Bytes48.leftPad(Bytes48.wrap(xImBytes));
-
-    // Mask the faulty flag bits
-    xReExpected =
-        xReExpected.and(
-            Bytes48.fromHexString(
-                "0x"
-                    + "1fffffffffffffffffffffffffffffff"
-                    + "ffffffffffffffffffffffffffffffff"
-                    + "ffffffffffffffffffffffffffffffff"));
-
-    assertEquals(xReExpected, xReActual);
-    assertEquals(xImExpected, xImActual);
   }
 
   @ParameterizedTest(name = "{index}. private to public key {0} -> {1}")
@@ -118,13 +83,8 @@ class BLSTestSuite {
     SecretKey privateKey = SecretKey.fromBytes(Bytes48.leftPad(Bytes.fromHexString(input)));
 
     PublicKey publicKeyActual = new PublicKey(privateKey);
+    PublicKey publicKeyExpected = PublicKey.fromBytesCompressed(Bytes.fromHexString(output));
 
-    // TODO: Remove workaround when test data are fixed
-    // We'd like to do this, but need to fix up the flags
-    //   PublicKey publicKeyExpected = PublicKey.fromBytes(Bytes.fromHexString(output));
-    byte[] tmp = Bytes.fromHexString(output).toArray();
-    tmp[0] |= (byte) 0xa0;
-    PublicKey publicKeyExpected = PublicKey.fromBytesCompressed(tmp);
     assertEquals(publicKeyExpected, publicKeyActual);
   }
 
@@ -143,23 +103,9 @@ class BLSTestSuite {
             .signature()
             .g2Point()
             .toBytesCompressed();
+    Bytes signatureExpectedBytes = Bytes.fromHexString(output);
 
-    // TODO: remove the following temporary fixes when the test data is good
-
-    // Swap Re & Im to match broken test data
-    byte[] tmp =
-        Bytes.concatenate(signatureActualBytes.slice(48, 48), signatureActualBytes.slice(0, 48))
-            .toArray();
-
-    // Ignore the flags entirely until the data are fixed
-    byte[] expectedBytes = Bytes.fromHexString(output).toArray();
-    expectedBytes[0] &= (byte) 0x1f;
-    tmp[48] &= (byte) 0x1f;
-
-    Bytes signatureActual = Bytes.wrap(tmp);
-    Bytes signatureExpected = Bytes.wrap(expectedBytes);
-
-    assertEquals(signatureExpected, signatureActual);
+    assertEquals(signatureExpectedBytes, signatureActualBytes);
   }
 
   @ParameterizedTest(name = "{index}. aggregate sig {0} -> {1}")
@@ -168,43 +114,31 @@ class BLSTestSuite {
 
     ArrayList<Signature> signatures = new ArrayList<>();
     for (String sig : input) {
-      signatures.add(new Signature(fixInput(sig)));
+      signatures.add(Signature.fromBytesCompressed(Bytes.fromHexString(sig)));
     }
 
-    Signature aggregateSignatureActual = Signature.aggregate(signatures);
-
-    Bytes aggregateSignatureActualBytes = aggregateSignatureActual.g2Point().toBytesCompressed();
-    // TODO: We need to swap real and imaginary parts to match the input data
-    aggregateSignatureActualBytes =
-        Bytes.concatenate(
-            aggregateSignatureActualBytes.slice(48, 48),
-            aggregateSignatureActualBytes.slice(0, 48));
-    byte[] tmpActual = aggregateSignatureActualBytes.toArray();
-
+    Bytes aggregateSignatureActualBytes =
+        Signature.aggregate(signatures).g2Point().toBytesCompressed();
     Bytes aggregateSignatureExpectedBytes = Bytes.fromHexString(output);
-    byte[] tmpExpected = aggregateSignatureExpectedBytes.toArray();
 
-    // TODO: Ignore flags for now
-    tmpActual[48] &= (byte) 0x1f;
-    tmpExpected[0] &= (byte) 0x1f;
-
-    // TODO: Reinstate this test when teh test data are fixed.
-    // Some tests pass, but the majority fail. I believe this is due to the test data being
-    // generated with incorrect Y value. We could run all 8 possibilities, but it's not worth
-    // it for now.
-    // assertEquals(Bytes.wrap(tmpExpected), Bytes.wrap(tmpActual));
+    assertEquals(aggregateSignatureExpectedBytes, aggregateSignatureActualBytes);
   }
 
-  /* The input data yml is malformed for this case - it needs a "- " before "input"
-    @ParameterizedTest(name = "{index}. aggregate pub keys {0} -> {1}")
-    @MethodSource("readAggregatePubKeys")
-    void testAggregatePubKeys(ArrayList<String> input, String output) {
+  @ParameterizedTest(name = "{index}. aggregate pub keys {0} -> {1}")
+  @MethodSource("readAggregatePubKeys")
+  void testAggregatePubKeys(ArrayList<String> input, String output) {
 
-      // TODO: See above
-      ;
-
+    ArrayList<PublicKey> publicKeys = new ArrayList<>();
+    for (String pubkey : input) {
+      publicKeys.add(PublicKey.fromBytesCompressed(Bytes.fromHexString(pubkey)));
     }
-  */
+
+    Bytes aggregatePublicKeyActualBytes =
+        PublicKey.aggregate(publicKeys).g1Point().toBytesCompressed();
+
+    Bytes aggregatePublicKeyExpectedBytes = Bytes.fromHexString(output);
+    assertEquals(aggregatePublicKeyExpectedBytes, aggregatePublicKeyActualBytes);
+  }
 
   @MustBeClosed
   private static Stream<Arguments> findTests(String glob, String tcase) throws IOException {
@@ -221,32 +155,32 @@ class BLSTestSuite {
 
   @MustBeClosed
   private static Stream<Arguments> readMessageHashG2Uncompressed() throws IOException {
-    return findTests("**/bls/test_bls.yml", "case01_message_hash_G2_uncompressed");
+    return findTests(testFile, "case01_message_hash_G2_uncompressed");
   }
 
   @MustBeClosed
   private static Stream<Arguments> readMessageHashG2Compressed() throws IOException {
-    return findTests("**/bls/test_bls.yml", "case02_message_hash_G2_compressed");
+    return findTests(testFile, "case02_message_hash_G2_compressed");
   }
 
   @MustBeClosed
   private static Stream<Arguments> readPrivateToPublicKey() throws IOException {
-    return findTests("**/bls/test_bls.yml", "case03_private_to_public_key");
+    return findTests(testFile, "case03_private_to_public_key");
   }
 
   @MustBeClosed
   private static Stream<Arguments> readSignMessages() throws IOException {
-    return findTests("**/bls/test_bls.yml", "case04_sign_messages");
+    return findTests(testFile, "case04_sign_messages");
   }
 
   @MustBeClosed
   private static Stream<Arguments> readAggregateSig() throws IOException {
-    return findTests("**/bls/test_bls.yml", "case06_aggregate_sigs");
+    return findTests(testFile, "case06_aggregate_sigs");
   }
 
   @MustBeClosed
   private static Stream<Arguments> readAggregatePubKeys() throws IOException {
-    return findTests("**/bls/test_bls.yml", "case07_aggregate_pubkeys");
+    return findTests(testFile, "case07_aggregate_pubkeys");
   }
 
   @SuppressWarnings({"unchecked", "rawtypes"})
@@ -287,33 +221,5 @@ class BLSTestSuite {
     y.reduce();
 
     return new G2Point(new ECP2(x, y));
-  }
-
-  // Temporary function for creating a G2 point from broken input data
-  private static G2Point fixInput(String pointData) {
-
-    Bytes foo = Bytes.fromHexString(pointData);
-    // Invert the Re, Im ordering
-    byte[] tmp = Bytes.concatenate(foo.slice(48, 48), foo.slice(0, 48)).toArray();
-
-    boolean aflag = ((tmp[48] & (byte) 0x80) == 0);
-    tmp[0] &= (byte) 0x1f;
-    tmp[0] = (byte) (tmp[0] | (byte) 0x80 | (byte) (aflag ? 0x20 : 0x00));
-    tmp[48] &= (byte) 0x1f;
-    G2Point point = G2Point.fromBytesCompressed(Bytes.wrap(tmp));
-
-    /*
-        // Check we got the right branch of the sqrt per the spec
-        FP2 yOrg = new FP2(point.ecp2Point().getY());
-        FP2 yNeg = new FP2(yOrg);
-        yNeg.neg();
-
-        if ((BIG.comp(yOrg.getB(), yNeg.getB()) < 0) || ((BIG.comp(yOrg.getB(), yNeg.getB()) == 0) && (BIG.comp(yOrg.getA(), yNeg.getA()) < 0))) {
-          System.out.println("Wrong branch!!!");
-          point = new G2Point(new ECP2(point.ecp2Point().getX(), yNeg));
-        }
-    */
-
-    return point;
   }
 }
