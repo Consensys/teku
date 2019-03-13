7fda3ca2fb4892b6a5c1677a827e1ffcd214aba8
jonny rhea <jonathan.rhea@gmail.com>
2019-03-12 23:47:56 -0500

    created the validator coordinator to manage validators and make room to integrate the new p2p stuff

--- validator/client/src/main/java/tech/pegasys/artemis/validator/client/CommitteeAssignmentTuple.java
+++ validator/client/src/main/java/tech/pegasys/artemis/validator/client/CommitteeAssignmentTuple.java
@@ -11,7 +11,7 @@
  * specific language governing permissions and limitations under the License.
  */
 
-package tech.pegasys.artemis.validatorclient;
+package tech.pegasys.artemis.validator.client;
 
 import java.util.List;
 
