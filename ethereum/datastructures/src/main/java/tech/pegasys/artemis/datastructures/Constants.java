952b7f5a76640f916d122572be56a169e9e7775d
Akhila Raju <akhila@berkeley.edu>
2019-03-21 11:48:20 -0400

    Fixes constants

--- ethereum/datastructures/src/main/java/tech/pegasys/artemis/datastructures/Constants.java
+++ ethereum/datastructures/src/main/java/tech/pegasys/artemis/datastructures/Constants.java
@@ -14,97 +14,264 @@
 package tech.pegasys.artemis.datastructures;
 
 import com.google.common.primitives.UnsignedLong;
+import java.util.HashMap;
 import net.consensys.cava.bytes.Bytes;
 import net.consensys.cava.bytes.Bytes32;
 import tech.pegasys.artemis.util.bls.BLSSignature;
+import tech.pegasys.artemis.util.config.ArtemisConfiguration;
 
-public final class Constants {
-  // The constants below are correct as of spec v0.4.0
-
-  // Test Values
-  /// *
-  public static final int SHARD_COUNT = 8;
-  public static final int TARGET_COMMITTEE_SIZE = 4;
-  public static final int MIN_ATTESTATION_INCLUSION_DELAY = 1;
-  public static final int SLOTS_PER_EPOCH = 8;
-  public static final int LATEST_BLOCK_ROOTS_LENGTH = 64;
-  public static final int LATEST_RANDAO_MIXES_LENGTH = 64;
-  public static final int LATEST_ACTIVE_INDEX_ROOTS_LENGTH = 64;
-  public static final int LATEST_SLASHED_EXIT_LENGTH = 64;
-  // */
+public class Constants {
+  // The constants below are correct as of spec v0.1
+  public HashMap<String, Object> config;
 
   // Misc
-  // public static final int SHARD_COUNT = 1024; // 2^10 shards
-  // public static final int TARGET_COMMITTEE_SIZE = 128; // 2^7 validators
-  public static final int MAX_BALANCE_CHURN_QUOTIENT = 32; // 2^5
-  public static final UnsignedLong BEACON_CHAIN_SHARD_NUMBER = UnsignedLong.MAX_VALUE; // 2^64 - 1
-  public static final int MAX_INDICES_PER_SLASHABLE_VOTE = 4096; // 2^12 votes
-  public static final int MAX_EXIT_DEQUEUES_PER_EPOCH = 4; // 2^2 withdrawals
-  public static final int SHUFFLE_ROUND_COUNT = 90;
+  public int SHARD_COUNT;
+  public int TARGET_COMMITTEE_SIZE;
+  public long EJECTION_BALANCE;
+  public int MAX_BALANCE_CHURN_QUOTIENT;
+  public UnsignedLong BEACON_CHAIN_SHARD_NUMBER;
+  public int MAX_INDICES_PER_SLASHABLE_VOTE;
+  public int MAX_WITHDRAWALS_PER_EPOCH;
 
   // Deposit contract
-  // public static final String DEPOSIT_CONTRACT_ADDRESS = "0x0"; // This is TBD in the spec.
-  public static final int DEPOSIT_CONTRACT_TREE_DEPTH = 32; // 2^5
-
-  // Gwei values
-  public static final long MIN_DEPOSIT_AMOUNT = 1000000000L; // 2^0 * 1E9 Gwei
-  public static final long MAX_DEPOSIT_AMOUNT = 32000000000L; // 2^5 * 1E9 Gwei
-  // public static final long FORK_CHOICE_BALANCE_INCREMENT = 1000000000L; // 2^0 * 1E9 Gwei
-  public static final long EJECTION_BALANCE = 16000000000L; // 2^4 * 1E9 Gwei
+  public String DEPOSIT_CONTRACT_ADDRESS;
+  public int DEPOSIT_CONTRACT_TREE_DEPTH;
+  public long MIN_DEPOSIT_AMOUNT;
+  public long MAX_DEPOSIT_AMOUNT;
 
   // Initial values
-  public static int GENESIS_FORK_VERSION = 0;
-  public static final long GENESIS_SLOT = 4294967296L; // 2^32
-  public static final long GENESIS_EPOCH = slot_to_epoch(GENESIS_SLOT);
-  public static final long GENESIS_START_SHARD = 0;
-  public static UnsignedLong FAR_FUTURE_EPOCH = UnsignedLong.MAX_VALUE;
-  public static Bytes32 ZERO_HASH = Bytes32.ZERO; // TODO Verify this.
-  public static final BLSSignature EMPTY_SIGNATURE = BLSSignature.empty();
-  public static Bytes BLS_WITHDRAWAL_PREFIX_BYTE = Bytes.EMPTY; // TODO Verify this.
+  public int GENESIS_FORK_VERSION;
+  public long GENESIS_SLOT;
+  public long GENESIS_EPOCH;
+  public long GENESIS_START_SHARD;
+  public UnsignedLong FAR_FUTURE_EPOCH;
+  public Bytes32 ZERO_HASH;
+  public BLSSignature EMPTY_SIGNATURE;
+  public Bytes BLS_WITHDRAWAL_PREFIX_BYTE;
 
   // Time parameters
-  public static final int SECONDS_PER_SLOT = 6; // 6 seconds
-  // public static final int MIN_ATTESTATION_INCLUSION_DELAY = 4; // 2^2 slots
-  // public static final int SLOTS_PER_EPOCH = 64; // 2^6 slots
-  public static final int MIN_SEED_LOOKAHEAD = 1; // 2^0 epochs (6.4 minutes)
-  public static final int ACTIVATION_EXIT_DELAY = 4; // 2^2 epochs (25.6 minutes)
-  public static final int EPOCHS_PER_ETH1_VOTING_PERIOD = 16; // 2^4 epochs (~1.7 hours)
-  public static final int MIN_VALIDATOR_WITHDRAWABILITY_DELAY = 256; // 2^8 epochs (~27 hours)
+  public int SLOT_DURATION;
+  public int MIN_ATTESTATION_INCLUSION_DELAY;
+  public int EPOCH_LENGTH;
+  public int SEED_LOOKAHEAD;
+  public int ENTRY_EXIT_DELAY;
+  public int ETH1_DATA_VOTING_PERIOD;
+  public int MIN_VALIDATOR_WITHDRAWAL_EPOCHS;
 
   // State list lengths
-  // public static final int LATEST_BLOCK_ROOTS_LENGTH = 8192; // 2^13 slots (~13 hours)
-  // public static final int LATEST_RANDAO_MIXES_LENGTH = 8192; // 2^13 epochs (~36 days)
-  // public static final int LATEST_ACTIVE_INDEX_ROOTS_LENGTH = 8192; // 2^13 epochs (~36 days)
-  // public static final int LATEST_SLASHED_EXIT_LENGTH = 8192; // 2^13 epochs (~36 days)
+  public int LATEST_BLOCK_ROOTS_LENGTH;
+  public int LATEST_RANDAO_MIXES_LENGTH;
+  public int LATEST_INDEX_ROOTS_LENGTH;
+  public int LATEST_PENALIZED_EXIT_LENGTH;
 
   // Reward and penalty quotients
-  public static final int BASE_REWARD_QUOTIENT = 32; // 2^5
-  public static final int WHISTLEBLOWER_REWARD_QUOTIENT = 512; // 2^9
-  public static final int ATTESTATION_INCLUSION_REWARD_QUOTIENT = 8; // 2^3
-  public static final int INACTIVITY_PENALTY_QUOTIENT = 16777216; // 2^24
-  // public static final int MIN_PENALTY_QUOTIENT = 32; // 2^5
+  public int BASE_REWARD_QUOTIENT;
+  public int WHISTLEBLOWER_REWARD_QUOTIENT;
+  public int INCLUDER_REWARD_QUOTIENT;
+  public int INACTIVITY_PENALTY_QUOTIENT;
+
+  // Status flags
+  public int INITIATED_EXIT;
+  public int WITHDRAWABLE;
 
   // Max transactions per block
-  public static final int MAX_PROPOSER_SLASHINGS = 16; // 2^4
-  public static final int MAX_ATTESTER_SLASHINGS = 1; // 2^0
-  public static final int MAX_ATTESTATIONS = 128; // 2^7
-  public static final int MAX_DEPOSITS = 16; // 2^4
-  public static final int MAX_VOLUNTARY_EXITS = 16; // 2^4
-  public static final int MAX_TRANSFERS = 16; // 2^4
+  public int MAX_PROPOSER_SLASHINGS;
+  public int MAX_ATTESTER_SLASHINGS;
+  public int MAX_ATTESTATIONS;
+  public int MAX_DEPOSITS;
+  public int MAX_EXITS;
 
   // Signature domains
-  public static final int DOMAIN_DEPOSIT = 0;
-  public static final int DOMAIN_ATTESTATION = 1;
-  public static final int DOMAIN_PROPOSAL = 2;
-  public static final int DOMAIN_EXIT = 3;
-  public static final int DOMAIN_RANDAO = 4;
-  public static final int DOMAIN_TRANSFER = 5;
+  public int DOMAIN_DEPOSIT;
+  public int DOMAIN_ATTESTATION;
+  public int DOMAIN_PROPOSAL;
+  public int DOMAIN_EXIT;
+  public int DOMAIN_RANDAO;
 
   // Artemis specific
-  public static final String SIM_DEPOSIT_VALUE = "1000000000000000000";
-  public static final int DEPOSIT_DATA_SIZE = 512; //
+  public String SIM_DEPOSIT_VALUE;
+  public int DEPOSIT_DATA_SIZE;
 
-  private static long slot_to_epoch(long slot) {
-    return slot / Constants.SLOTS_PER_EPOCH;
+  public Constants(ArtemisConfiguration config) {
+
+    // Misc
+    this.SHARD_COUNT =
+        config.getShardCount() != Integer.MIN_VALUE ? config.getShardCount() : 1024; // 2^10 shards
+    this.TARGET_COMMITTEE_SIZE =
+        config.getTargetCommitteeSize() != Integer.MIN_VALUE
+            ? config.getTargetCommitteeSize()
+            : 128; // 2^7 validators
+    this.EJECTION_BALANCE =
+        config.getEjectionBalance() != Long.MIN_VALUE
+            ? config.getEjectionBalance()
+            : 16000000000L; // 2^4 * 1E9 Gwei
+    this.MAX_BALANCE_CHURN_QUOTIENT =
+        config.getMaxBalanceChurnQuotient() != Integer.MIN_VALUE
+            ? config.getMaxBalanceChurnQuotient()
+            : 32; // 2^5
+    this.BEACON_CHAIN_SHARD_NUMBER =
+        !config.getBeaconChainShardNumber().equals("")
+            ? (UnsignedLong) config.getBeaconChainShardNumber()
+            : UnsignedLong.MAX_VALUE; // 2^64 - 1
+    this.MAX_INDICES_PER_SLASHABLE_VOTE =
+        config.getMaxIndicesPerSlashableVote() != Integer.MIN_VALUE
+            ? config.getMaxIndicesPerSlashableVote()
+            : 4096; // 2^12 votes
+    this.MAX_WITHDRAWALS_PER_EPOCH =
+        config.getMaxWithdrawalsPerEpoch() != Integer.MIN_VALUE
+            ? config.getMaxWithdrawalsPerEpoch()
+            : 4; // 2^2 withdrawals
+
+    // Deposit contract
+    this.DEPOSIT_CONTRACT_ADDRESS =
+        !config.getDepositContractAddress().equals("")
+            ? config.getDepositContractAddress()
+            : "0x0"; // This is TBD in the spec.
+    this.DEPOSIT_CONTRACT_TREE_DEPTH =
+        config.getDepositContractTreeDepth() != Integer.MIN_VALUE
+            ? config.getDepositContractTreeDepth()
+            : 32; // 2^5
+    this.MIN_DEPOSIT_AMOUNT =
+        config.getMinDepositAmount() != Long.MIN_VALUE
+            ? config.getMinDepositAmount()
+            : 1000000000L; // 2^0 * 1E9 Gwei
+    this.MAX_DEPOSIT_AMOUNT =
+        config.getMaxDepositAmount() != Long.MIN_VALUE
+            ? config.getMaxDepositAmount()
+            : 32000000000L; // 2^5 * 1E9 Gwei
+
+    // Initial values
+    this.GENESIS_FORK_VERSION =
+        config.getGenesisForkVersion() != Integer.MIN_VALUE ? config.getGenesisForkVersion() : 0;
+    this.GENESIS_SLOT =
+        config.getGenesisSlot() != Long.MIN_VALUE ? config.getGenesisSlot() : 4294967296L; // 2^32
+    this.GENESIS_EPOCH =
+        config.getGenesisEpoch() != Long.MIN_VALUE
+            ? config.getGenesisEpoch()
+            : slot_to_epoch(GENESIS_SLOT);
+    this.GENESIS_START_SHARD =
+        config.getGenesisStartShard() != Integer.MIN_VALUE ? config.getGenesisStartShard() : 0;
+    this.FAR_FUTURE_EPOCH =
+        !config.getFarFutureEpoch().equals("")
+            ? (UnsignedLong) config.getFarFutureEpoch()
+            : UnsignedLong.MAX_VALUE;
+    this.ZERO_HASH =
+        !config.getZeroHash().equals("")
+            ? (Bytes32) config.getZeroHash()
+            : Bytes32.ZERO; // TODO Verify this.
+    this.EMPTY_SIGNATURE =
+        !config.getEmptySignature().equals("")
+            ? (BLSSignature) config.getEmptySignature()
+            : BLSSignature.empty();
+    this.BLS_WITHDRAWAL_PREFIX_BYTE =
+        !config.getBlsWithdrawalPrefixByte().equals("")
+            ? (Bytes) config.getBlsWithdrawalPrefixByte()
+            : Bytes.EMPTY; // TODO Verify this.
+
+    // Time parameters
+    this.SLOT_DURATION =
+        config.getSlotDuration() != Integer.MIN_VALUE ? config.getSlotDuration() : 6; // 6 seconds
+    this.MIN_ATTESTATION_INCLUSION_DELAY =
+        config.getMinAttestationInclusionDelay() != Integer.MIN_VALUE
+            ? config.getMinAttestationInclusionDelay()
+            : 4; // 2^2 slots
+    this.EPOCH_LENGTH =
+        config.getEpochLength() != Integer.MIN_VALUE ? config.getEpochLength() : 64; // 2^6 slots
+    this.SEED_LOOKAHEAD =
+        config.getSeedLookahead() != Integer.MIN_VALUE
+            ? config.getSeedLookahead()
+            : 1; // 2^0 epochs (6.4 minutes)
+    this.ENTRY_EXIT_DELAY =
+        config.getEntryExitDelay() != Integer.MIN_VALUE
+            ? config.getEntryExitDelay()
+            : 4; // 2^2 epochs (25.6 minutes)
+    this.ETH1_DATA_VOTING_PERIOD =
+        config.getEth1DataVotingPeriod() != Integer.MIN_VALUE
+            ? config.getEth1DataVotingPeriod()
+            : 16; // 2^4 epochs (~1.7 hours)
+    this.MIN_VALIDATOR_WITHDRAWAL_EPOCHS =
+        config.getMinValidatorWithdrawalEpochs() != Integer.MIN_VALUE
+            ? config.getMinValidatorWithdrawalEpochs()
+            : 256; // 2^8 epochs (~27 hours)
+
+    // State list lengths
+    this.LATEST_BLOCK_ROOTS_LENGTH =
+        config.getLatestBlockRootsLength() != Integer.MIN_VALUE
+            ? config.getLatestBlockRootsLength()
+            : 8192; // 2^13 slots (~13 hours)
+    this.LATEST_RANDAO_MIXES_LENGTH =
+        config.getLatestRandaoMixesLength() != Integer.MIN_VALUE
+            ? config.getLatestRandaoMixesLength()
+            : 8192; // 2^13 epochs (~36 days)
+    this.LATEST_INDEX_ROOTS_LENGTH =
+        config.getLatestIndexRootsLength() != Integer.MIN_VALUE
+            ? config.getLatestIndexRootsLength()
+            : 8192; // 2^13 epochs (~36 days)
+    this.LATEST_PENALIZED_EXIT_LENGTH =
+        config.getLatestPenalizedExitLength() != Integer.MIN_VALUE
+            ? config.getLatestPenalizedExitLength()
+            : 8192; // 2^13 epochs (~36 days)
+
+    // Reward and penalty quotients
+    this.BASE_REWARD_QUOTIENT =
+        config.getBaseRewardQuotient() != Integer.MIN_VALUE
+            ? config.getBaseRewardQuotient()
+            : 32; // 2^5
+    this.WHISTLEBLOWER_REWARD_QUOTIENT =
+        config.getWhistleblowerRewardQuotient() != Integer.MIN_VALUE
+            ? config.getWhistleblowerRewardQuotient()
+            : 512; // 2^9
+    this.INCLUDER_REWARD_QUOTIENT =
+        config.getIncluderRewardQuotient() != Integer.MIN_VALUE
+            ? config.getIncluderRewardQuotient()
+            : 8; // 2^3
+    this.INACTIVITY_PENALTY_QUOTIENT =
+        config.getInactivityPenaltyQuotient() != Integer.MIN_VALUE
+            ? config.getInactivityPenaltyQuotient()
+            : 16777216; // 2^24
+
+    // Status flags
+    this.INITIATED_EXIT =
+        config.getInitiatedExit() != Integer.MIN_VALUE ? config.getInitiatedExit() : 1;
+    this.WITHDRAWABLE =
+        config.getWithdrawable() != Integer.MIN_VALUE ? config.getWithdrawable() : 2;
+
+    // Max transactions per block
+    this.MAX_PROPOSER_SLASHINGS =
+        config.getMaxProposerSlashings() != Integer.MIN_VALUE
+            ? config.getMaxProposerSlashings()
+            : 16; // 2^4
+    this.MAX_ATTESTER_SLASHINGS =
+        config.getMaxAttesterSlashings() != Integer.MIN_VALUE
+            ? config.getMaxAttesterSlashings()
+            : 1; // 2^0
+    this.MAX_ATTESTATIONS =
+        config.getMaxAttestations() != Integer.MIN_VALUE ? config.getMaxAttestations() : 128; // 2^7
+    this.MAX_DEPOSITS =
+        config.getMaxDeposits() != Integer.MIN_VALUE ? config.getMaxDeposits() : 16; // 2^4
+    this.MAX_EXITS = config.getMaxExits() != Integer.MIN_VALUE ? config.getMaxExits() : 16; // 2^4
+
+    // Signature domains
+    this.DOMAIN_DEPOSIT =
+        config.getDomainDeposit() != Integer.MIN_VALUE ? config.getDomainDeposit() : 0;
+    this.DOMAIN_ATTESTATION =
+        config.getDomainAttestation() != Integer.MIN_VALUE ? config.getDomainAttestation() : 1;
+    this.DOMAIN_PROPOSAL =
+        config.getDomainProposal() != Integer.MIN_VALUE ? config.getDomainProposal() : 2;
+    this.DOMAIN_EXIT = config.getDomainExit() != Integer.MIN_VALUE ? config.getDomainExit() : 3;
+    this.DOMAIN_RANDAO =
+        config.getDomainRandao() != Integer.MIN_VALUE ? config.getDomainRandao() : 4;
+
+    // Artemis specific
+    this.SIM_DEPOSIT_VALUE =
+        !config.getSimDepositValue().equals("")
+            ? config.getSimDepositValue()
+            : "1000000000000000000";
+    this.DEPOSIT_DATA_SIZE =
+        config.getDepositDataSize() != Integer.MIN_VALUE ? config.getDepositDataSize() : 512;
+  }
+
+  private long slot_to_epoch(long slot) {
+    return slot / (long) config.get("EPOCH_LENGTH");
   }
 }
--- util/src/main/java/tech/pegasys/artemis/util/config/ArtemisConfiguration.java
+++ util/src/main/java/tech/pegasys/artemis/util/config/ArtemisConfiguration.java
@@ -83,6 +83,76 @@ public final class ArtemisConfiguration {
                 .collect(Collectors.toList()));
     builder.addLong(
         "node.networkID", 1L, "The identifier of the network (mainnet, testnet, sidechain)", null);
+
+    // Constants
+    // Misc
+    builder.addInteger("constants.SHARD_COUNT", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.TARGET_COMMITTEE_SIZE", Integer.MIN_VALUE, null, null);
+    builder.addLong("constants.EJECTION_BALANCE", Long.MIN_VALUE, null, null);
+    builder.addInteger("constants.MAX_BALANCE_CHURN_QUOTIENT", Integer.MIN_VALUE, null, null);
+    builder.addDefault("constants.BEACON_CHAIN_SHARD_NUMBER", "");
+    builder.addInteger("constants.MAX_INDICES_PER_SLASHABLE_VOTE", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.MAX_WITHDRAWALS_PER_EPOCH", Integer.MIN_VALUE, null, null);
+
+    // Deposit Contract
+    builder.addString("constants.DEPOSIT_CONTRACT_ADDRESS", "", null, null);
+    builder.addInteger("constants.DEPOSIT_CONTRACT_TREE_DEPTH", Integer.MIN_VALUE, null, null);
+    builder.addLong("constants.MIN_DEPOSIT_AMOUNT", Long.MIN_VALUE, null, null);
+    builder.addLong("constants.MAX_DEPOSIT_AMOUNT", Long.MIN_VALUE, null, null);
+
+    // Initial Values
+    builder.addInteger("constants.GENESIS_FORK_VERSION", Integer.MIN_VALUE, null, null);
+    builder.addLong("constants.GENESIS_SLOT", Long.MIN_VALUE, null, null);
+    builder.addLong("constants.GENESIS_EPOCH", Long.MIN_VALUE, null, null);
+    builder.addInteger("constants.GENESIS_START_SHARD", Integer.MIN_VALUE, null, null);
+    builder.addDefault("constants.FAR_FUTURE_EPOCH", "");
+    builder.addDefault("constants.ZERO_HASH", "");
+    builder.addDefault("constants.EMPTY_SIGNATURE", "");
+    builder.addDefault("constants.BLS_WITHDRAWAL_PREFIX_BYTE", "");
+
+    // Time parameters
+    builder.addInteger("constants.SLOT_DURATION", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.MIN_ATTESTATION_INCLUSION_DELAY", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.EPOCH_LENGTH", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.SEED_LOOKAHEAD", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.ENTRY_EXIT_DELAY", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.ETH1_DATA_VOTING_PERIOD", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.MIN_VALIDATOR_WITHDRAWAL_EPOCHS", Integer.MIN_VALUE, null, null);
+
+    // State list lengths
+    builder.addInteger("constants.LATEST_BLOCK_ROOTS_LENGTH", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.LATEST_RANDAO_MIXES_LENGTH", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.LATEST_INDEX_ROOTS_LENGTH", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.LATEST_PENALIZED_EXIT_LENGTH", Integer.MIN_VALUE, null, null);
+
+    // Reward and penalty quotients
+    builder.addInteger("constants.BASE_REWARD_QUOTIENT", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.WHISTLEBLOWER_REWARD_QUOTIENT", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.INCLUDER_REWARD_QUOTIENT", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.INACTIVITY_PENALTY_QUOTIENT", Integer.MIN_VALUE, null, null);
+
+    // Status flags
+    builder.addInteger("constants.INITIATED_EXIT", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.WITHDRAWABLE", Integer.MIN_VALUE, null, null);
+
+    // Max transactions per block
+    builder.addInteger("constants.MAX_PROPOSER_SLASHINGS", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.MAX_ATTESTER_SLASHINGS", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.MAX_ATTESTATIONS", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.MAX_DEPOSITS", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.MAX_EXITS", Integer.MIN_VALUE, null, null);
+
+    // Signature domains
+    builder.addInteger("constants.DOMAIN_DEPOSIT", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.DOMAIN_ATTESTATION", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.DOMAIN_PROPOSAL", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.DOMAIN_EXIT", Integer.MIN_VALUE, null, null);
+    builder.addInteger("constants.DOMAIN_RANDAO", Integer.MIN_VALUE, null, null);
+
+    // Artemis specific
+    builder.addString("constants.SIM_DEPOSIT_VALUE", "", null, null);
+    builder.addInteger("constants.DEPOSIT_DATA_SIZE", Integer.MIN_VALUE, null, null);
+
     return builder.toSchema();
   }
 
@@ -154,6 +224,208 @@ public final class ArtemisConfiguration {
   /** @return the total number of nodes on the network */
   public int getNumNodes() {
     return config.getInteger("sim.numNodes");
+  }
+
+  /** @return misc constants */
+  public int getShardCount() {
+    return config.getInteger("SHARD_COUNT");
+  }
+
+  public int getTargetCommitteeSize() {
+    return config.getInteger("TARGET_COMMITTEE_SIZE");
+  }
+
+  public long getEjectionBalance() {
+    return config.getLong("EJECTION_BALANCE");
+  }
+
+  public int getMaxBalanceChurnQuotient() {
+    return config.getInteger("MAX_BALANCE_CHURN_QUOTIENT");
+  }
+
+  public Object getBeaconChainShardNumber() {
+    return config.get("BEACON_CHAIN_SHARD_NUMBER");
+  }
+
+  public int getMaxIndicesPerSlashableVote() {
+    return config.getInteger("MAX_INDICES_PER_SLASHABLE_VOTE");
+  }
+
+  public int getMaxWithdrawalsPerEpoch() {
+    return config.getInteger("MAX_WITHDRAWALS_PER_EPOCH");
+  }
+
+  /** @return deposit contract constants */
+  public String getDepositContractAddress() {
+    return config.getString("DEPOSIT_CONTRACT_ADDRESS");
+  }
+
+  public int getDepositContractTreeDepth() {
+    return config.getInteger("DEPOSIT_CONTRACT_TREE_DEPTH");
+  }
+
+  public long getMinDepositAmount() {
+    return config.getInteger("MIN_DEPOSIT_AMOUNT");
+  }
+
+  public long getMaxDepositAmount() {
+    return config.getInteger("MAX_DEPOSIT_AMOUNT");
+  }
+
+  /** @return initial value constants */
+  public int getGenesisForkVersion() {
+    return config.getInteger("GENESIS_FORK_VERSION");
+  }
+
+  public long getGenesisSlot() {
+    return config.getInteger("GENESIS_SLOT");
+  }
+
+  public long getGenesisEpoch() {
+    return config.getInteger("GENESIS_EPOCH");
+  }
+
+  public int getGenesisStartShard() {
+    return config.getInteger("GENESIS_START_SHARD");
+  }
+
+  public Object getFarFutureEpoch() {
+    return config.getInteger("FAR_FUTURE_EPOCH");
+  }
+
+  public Object getZeroHash() {
+    return config.getInteger("ZERO_HASH");
+  }
+
+  public Object getEmptySignature() {
+    return config.getInteger("EMPTY_SIGNATURE");
+  }
+
+  public Object getBlsWithdrawalPrefixByte() {
+    return config.getInteger("BLS_WITHDRAWAL_PREFIX_BYTE");
+  }
+
+  /** @return time parameter constants */
+  public int getSlotDuration() {
+    return config.getInteger("SLOT_DURATION");
+  }
+
+  public int getMinAttestationInclusionDelay() {
+    return config.getInteger("MIN_ATTESTATION_INCLUSION_DELAY");
+  }
+
+  public int getEpochLength() {
+    return config.getInteger("EPOCH_LENGTH");
+  }
+
+  public int getSeedLookahead() {
+    return config.getInteger("SEED_LOOKAHEAD");
+  }
+
+  public int getEntryExitDelay() {
+    return config.getInteger("ENTRY_EXIT_DELAY");
+  }
+
+  public int getEth1DataVotingPeriod() {
+    return config.getInteger("ETH1_DATA_VOTING_PERIOD");
+  }
+
+  public int getMinValidatorWithdrawalEpochs() {
+    return config.getInteger("MIN_VALIDATOR_WITHDRAWAL_EPOCHS");
+  }
+
+  /** @return state list length constants */
+  public int getLatestBlockRootsLength() {
+    return config.getInteger("LATEST_BLOCK_ROOTS_LENGTH");
+  }
+
+  public int getLatestRandaoMixesLength() {
+    return config.getInteger("LATEST_RANDAO_MIXES_LENGTH");
+  }
+
+  public int getLatestIndexRootsLength() {
+    return config.getInteger("LATEST_INDEX_ROOTS_LENGTH");
+  }
+
+  public int getLatestPenalizedExitLength() {
+    return config.getInteger("LATEST_PENALIZED_EXIT_LENGTH");
+  }
+
+  /** @return reward and penalty quotient constants */
+  public int getBaseRewardQuotient() {
+    return config.getInteger("BASE_REWARD_QUOTIENT");
+  }
+
+  public int getWhistleblowerRewardQuotient() {
+    return config.getInteger("WHISTLEBLOWER_REWARD_QUOTIENT");
+  }
+
+  public int getIncluderRewardQuotient() {
+    return config.getInteger("INCLUDER_REWARD_QUOTIENT");
+  }
+
+  public int getInactivityPenaltyQuotient() {
+    return config.getInteger("INACTIVITY_PENALTY_QUOTIENT");
+  }
+
+  /** @return status flag constants */
+  public int getInitiatedExit() {
+    return config.getInteger("INITIATED_EXIT");
+  }
+
+  public int getWithdrawable() {
+    return config.getInteger("WITHDRAWABLE");
+  }
+
+  /** @return max transactions per block constants */
+  public int getMaxProposerSlashings() {
+    return config.getInteger("MAX_PROPOSER_SLASHINGS");
+  }
+
+  public int getMaxAttesterSlashings() {
+    return config.getInteger("MAX_ATTESTER_SLASHINGS");
+  }
+
+  public int getMaxAttestations() {
+    return config.getInteger("MAX_ATTESTATIONS");
+  }
+
+  public int getMaxDeposits() {
+    return config.getInteger("MAX_DEPOSITS");
+  }
+
+  public int getMaxExits() {
+    return config.getInteger("MAX_EXITS");
+  }
+
+  /** @return signature domain constants */
+  public int getDomainDeposit() {
+    return config.getInteger("DOMAIN_DEPOSIT");
+  }
+
+  public int getDomainAttestation() {
+    return config.getInteger("DOMAIN_ATTESTATION");
+  }
+
+  public int getDomainProposal() {
+    return config.getInteger("DOMAIN_PROPOSAL");
+  }
+
+  public int getDomainExit() {
+    return config.getInteger("DOMAIN_EXIT");
+  }
+
+  public int getDomainRandao() {
+    return config.getInteger("DOMAIN_RANDAO");
+  }
+
+  /** @return Artemis specific constants */
+  public String getSimDepositValue() {
+    return config.getString("SIM_DEPOSIT_VALUE");
+  }
+
+  public int getDepositDataSize() {
+    return config.getInteger("DEPOSIT_DATA_SIZE");
   }
 
   /** @return the list of static peers associated with this node */
