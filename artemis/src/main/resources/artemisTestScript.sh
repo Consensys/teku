364b72cd60625ec8fab73bf48b9b3a9c316f7b15
Steven J Schroeder <steven.schroeder@consensys.net>
2019-03-28 12:51:23 -0500

    Add useful prints, fix tabstop and expandtab.

--- artemis/src/main/resources/artemisTestScript.sh
+++ artemis/src/main/resources/artemisTestScript.sh
@@ -7,47 +7,52 @@ slotCounter=0
 
 tail -fn0 ./artemisTestOutput.txt | \
 while read line ; do
-	echo "$line"
+    echo "$line"
 
 
-	# Terminate After 16 Slots/2 Epochs
-	if [ $slotCounter -gt 16 ]
-	then
-		break
-	fi
-        if [[ $line =~ .*Slot\ Event.* ]]
-        then
-		((slotCounter++))
-        fi
+    # Terminate After 16 Slots/2 Epochs
+    if [ $slotCounter -gt 16 ]
+    then
+        break
+    fi
+    if [[ $line =~ .*Slot\ Event.* ]]
+    then
+        ((slotCounter++))
+    fi
 
 
-	# Handle Exceptions
-	# Fail on Exception
-	if [[ $line =~ .*Exception.* ]]
-	then
-		# Error
-		exit 1
-	fi
-	# Fail on Mismatch State Root
-	if [[ $line =~ .*Block\ state\ root\ does\ NOT\ match\ the\ calculated\ state\ root.* ]]
-	then
-		# Error
-		exit 2
-	fi
-	# Fail on Unable to Update Justified and Finalized Roots
-	if [[ $line =~ .*Can\'t\ update\ justified\ and\ finalized\ block\ roots.* ]]
-	then
-		# Error
-		exit 3
-	fi
-	# Fail on Unable to Update Block Using LMDGhost
-	if [[ $line =~ .*Can\'t\ update\ head\ block\ using\ LMDGhost.* ]]
-	then
-		# Error
-		exit 4
-	fi
+    # Handle Exceptions
+    # Fail on Exception
+    if [[ $line =~ .*Exception.* ]]
+    then
+        # Error
+        echo $'\nFAILED: Exception was encountered.\n'
+        exit 1
+    fi
+    # Fail on Mismatch State Root
+    if [[ $line =~ .*Block\ state\ root\ does\ NOT\ match\ the\ calculated\ state\ root.* ]]
+    then
+        # Error
+        echo $'\nFAILED: Block state root DID NOT match the expected.\n'
+        exit 2
+    fi
+    # Fail on Unable to Update Justified and Finalized Roots
+    if [[ $line =~ .*Can\'t\ update\ justified\ and\ finalized\ block\ roots.* ]]
+    then
+        # Error
+        echo $'\nFAILED: Failed to update justified or finalized block roots.\n'
+        exit 3
+    fi
+    # Fail on Unable to Update Block Using LMDGhost
+    if [[ $line =~ .*Can\'t\ update\ head\ block\ using\ LMDGhost.* ]]
+    then
+        # Error
+        echo $'\nFAILED: Failed to update block head using LMDGhost\n'
+        exit 4
+    fi
 done
 kill $artemisPid
 rm ./artemisTestOutput.txt
+echo $'\nSUCCESS\n'
 exit 0
 
