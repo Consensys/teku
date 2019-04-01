99d7fcc9eb08b6d76feb623a9f7de718d36737da
Cem Ozer <cemozer2018@u.northwestern.edu>
2019-04-01 16:11:01 -0400

    Change BeaconState, add BeaconBlockHeader

--- ethereum/datastructures/src/main/java/tech/pegasys/artemis/datastructures/Constants.java
+++ ethereum/datastructures/src/main/java/tech/pegasys/artemis/datastructures/Constants.java
@@ -48,7 +48,9 @@ public class Constants {
   public static int MIN_SEED_LOOKAHEAD = 1; // 2^0 epochs (6.4 minutes)
   public static int ACTIVATION_EXIT_DELAY = 4; // 2^2 epochs (25.6 minutes)
   public static int EPOCHS_PER_ETH1_VOTING_PERIOD = 16; // 2^4 epochs (~1.7 hours)
+  public static int SLOTS_PER_HISTORICAL_ROOT = 8192; // 2^4 epochs (~1.7 hours)
   public static int MIN_VALIDATOR_WITHDRAWABILITY_DELAY = 256; // 2^8 epochs (~27 hours)
+  public static int PERSISTENT_COMMITTEE_PERIOD = 2048; // 2^8 epochs (~27 hours)
 
   // Initial values
   public static int GENESIS_FORK_VERSION = 0;
@@ -61,7 +63,6 @@ public class Constants {
   public static Bytes BLS_WITHDRAWAL_PREFIX_BYTE = Bytes.EMPTY;
 
   // State list lengths
-  public static int LATEST_BLOCK_ROOTS_LENGTH = 8192; // 2^13 slots (~13 hours)
   public static int LATEST_RANDAO_MIXES_LENGTH = 8192; // 2^13 epochs (~36 days)
   public static int LATEST_ACTIVE_INDEX_ROOTS_LENGTH = 8192; // 2^13 epochs (~36 days)
   public static int LATEST_SLASHED_EXIT_LENGTH = 8192; // 2^13 epochs (~36 days)
