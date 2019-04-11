bad1baaa0b177a805b5c733d8e39ba890a8183a9
Akhila Raju <akhila.raju@consensys.net>
2019-04-11 15:46:59 -0400

    make get_beacon_proposer_index safe for next epoch. (#580)

--- validator/client/src/main/java/tech/pegasys/artemis/validator/client/ValidatorClient.java
+++ validator/client/src/main/java/tech/pegasys/artemis/validator/client/ValidatorClient.java
@@ -14,6 +14,7 @@
 package tech.pegasys.artemis.validator.client;
 
 import static tech.pegasys.artemis.datastructures.Constants.SLOTS_PER_EPOCH;
+import static tech.pegasys.artemis.datastructures.util.BeaconStateUtil.get_beacon_proposer_index;
 import static tech.pegasys.artemis.datastructures.util.BeaconStateUtil.get_crosslink_committees_at_slot;
 import static tech.pegasys.artemis.datastructures.util.BeaconStateUtil.get_current_epoch;
 import static tech.pegasys.artemis.datastructures.util.BeaconStateUtil.get_epoch_start_slot;
@@ -67,10 +68,8 @@ public class ValidatorClient {
       if (selected_committees.size() > 0) {
         List<Integer> validators = selected_committees.get(0).getCommittee();
         int shard = selected_committees.get(0).getShard().intValue();
-        List<Integer> first_committee_at_slot =
-            crosslink_committees.get(0).getCommittee(); // List[ValidatorIndex]
         boolean is_proposer =
-            first_committee_at_slot.get(slot % first_committee_at_slot.size()) == validator_index;
+            validator_index == get_beacon_proposer_index(state, UnsignedLong.valueOf(slot), registry_change);
 
         return Optional.of(new CommitteeAssignmentTuple(validators, shard, slot, is_proposer));
       }
