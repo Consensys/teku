1c609ba30f49b488838df020c4ad8aaaab1dc26a
jonny rhea <jonathan.rhea@gmail.com>
2019-04-24 17:08:27 -0500

    Joe hates spotlessApply

--- services/adapter/src/test/java/tech/pegasys/artemis/services/adapter/ServiceAdapterTest.java
+++ services/adapter/src/test/java/tech/pegasys/artemis/services/adapter/ServiceAdapterTest.java
@@ -13,14 +13,9 @@
 
 package tech.pegasys.artemis.services.adapter;
 
-import com.google.common.eventbus.Subscribe;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.web3j.protocol.core.methods.response.Log;
-import tech.pegasys.artemis.pow.contract.DepositContract;
-import tech.pegasys.artemis.pow.event.Deposit;
-import tech.pegasys.artemis.pow.event.Eth2Genesis;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.google.common.eventbus.Subscribe;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.charset.Charset;
@@ -29,8 +24,12 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.web3j.protocol.core.methods.response.Log;
+import tech.pegasys.artemis.pow.contract.DepositContract;
+import tech.pegasys.artemis.pow.event.Deposit;
+import tech.pegasys.artemis.pow.event.Eth2Genesis;
 
 public class ServiceAdapterTest {
 
