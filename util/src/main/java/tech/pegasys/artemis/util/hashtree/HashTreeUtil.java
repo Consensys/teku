21a46e52bf805ec47bb844b485b8fe0f9bc9b851
Steven J Schroeder <steven.schroeder@consensys.net>
2019-04-30 15:00:57 -0500

    Fixes to make merkleize and pack v0.5.1 compliant.

--- util/src/main/java/tech/pegasys/artemis/util/hashtree/HashTreeUtil.java
+++ util/src/main/java/tech/pegasys/artemis/util/hashtree/HashTreeUtil.java
@@ -16,16 +16,24 @@ package tech.pegasys.artemis.util.hashtree;
 import static java.nio.ByteOrder.LITTLE_ENDIAN;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import net.consensys.cava.bytes.Bytes;
 import net.consensys.cava.bytes.Bytes32;
 import net.consensys.cava.crypto.Hash;
-import net.consensys.cava.ssz.SSZ;
 
 /** This class is a collection of tree hash root convenience methods */
 public final class HashTreeUtil {
+
+  public static enum SSZTypes {BASIC, CONTAINER, TUPLE_BASIC, TUPLE_CONTAINER, LIST_BASIC, LIST_CONTAINER};
+  public static final int BYTES_PER_CHUNK = 32;
+
+  private static Bytes32 hash_tree_root(SSZTypes sszType, Bytes... bytes) {
+    // NOT YET IMPLEMENTED
+    // TODO - My plan is to wrap all the hash_tree_root calculation in this class so our types aren't as complex.
+    return Bytes32.ZERO;
+  }
 
   /**
    * Create the hash tree root of a set of values of basic SSZ types or tuples of basic types. Basic
@@ -39,16 +47,7 @@ public final class HashTreeUtil {
    *     Spec v0.5.1</a>
    */
   public static Bytes32 hash_tree_root_basic_type(Bytes... bytes) {
-    if (bytes.length == 1) {
-      if (bytes[0].size() > 32) {
-        return Hash.keccak256(bytes[0]);
-      } else {
-        return Bytes32.rightPad(bytes[0]);
-      }
-    } else {
-      Bytes hash = merkleHash(new ArrayList<>(Arrays.asList(bytes)));
-      return Bytes32.rightPad(hash);
-    }
+    return merkleize(pack(bytes));
   }
 
   /**
@@ -65,8 +64,50 @@ public final class HashTreeUtil {
     return mix_in_length(hash_tree_root_basic_type(bytes.toArray(new Bytes[0])), length);
   }
 
+  public static Bytes32 merkleize(List<Bytes32> sszChunks) {
+    // A balanced binary tree must have a power of two number of leaves.
+    // NOTE: is_power_of_two also serves as a zero size check here.
+    while(!is_power_of_two(sszChunks.size())) {
+      sszChunks.add(Bytes32.ZERO);
+    }
+
+    // Expand the size of the sszChunks list large enough to hold the entire tree.
+    sszChunks.addAll(0, Collections.nCopies(sszChunks.size(), Bytes32.ZERO));
+
+    // Iteratively calculate the root for each parent in the binary tree, starting at the leaves.
+    for(int inlineTreeIndex = sszChunks.size() / 2 - 1; inlineTreeIndex > 0; inlineTreeIndex--) {
+      sszChunks.set(inlineTreeIndex, Hash.keccak256(Bytes.concatenate(sszChunks.get(inlineTreeIndex * 2), sszChunks.get(inlineTreeIndex * 2 + 1))));
+    }
+
+    // Return the root element, which is at index 1. The math is easier this way.
+    return sszChunks.get(1);
+  }
+
+  public static List<Bytes32> pack(Bytes... sszValues) {
+    // Join all varags sszValues into one Bytes type
+    Bytes concatenatedBytes = Bytes.concatenate(sszValues);
+
+    // Pad so that concatenatedBytes length is divisible by BYTES_PER_CHUNK
+    int packingRemainder = concatenatedBytes.size() % BYTES_PER_CHUNK;
+    if(packingRemainder != 0) {
+      concatenatedBytes = Bytes.concatenate(concatenatedBytes, Bytes.wrap(new byte[packingRemainder]));
+    }
+    
+    // Wrap each BYTES_PER_CHUNK-byte value into a Bytes32
+    List<Bytes32> chunkifiedBytes = new ArrayList<>();
+    for(int chunk = 0; chunk < concatenatedBytes.size(); chunk += BYTES_PER_CHUNK) {
+      chunkifiedBytes.add(Bytes32.wrap(concatenatedBytes, chunk));
+    }
+
+    return chunkifiedBytes;
+  }
+
   public static Bytes32 mix_in_length(Bytes32 merkle_root, int length) {
     return Hash.keccak256(Bytes.concatenate(merkle_root, Bytes.ofUnsignedInt(length, LITTLE_ENDIAN)));
+  }
+
+  public static boolean is_power_of_two(int value) {
+    return value > 0 && (value & (value - 1)) == 0;
   }
 
   /**
@@ -75,6 +116,7 @@ public final class HashTreeUtil {
    * @param values a list of homogeneous values
    * @return the merkle hash of the list of values
    */
+  @Deprecated
   public static Bytes32 merkleHash(List<Bytes> values) {
     Bytes littleEndianLength = Bytes.ofUnsignedInt(values.size(), LITTLE_ENDIAN);
     Bytes32 valuesLength = Bytes32.rightPad(littleEndianLength);
