484a318f0b2da358f17e21510d8c3dda1fb0c395
Jonny Rhea <jonathan.rhea@gmail.com>
2019-04-30 13:31:23 -0500

    Fixed NullPointer error and removed rlpx (#613)

--- artemis/src/test/java/tech/pegasys/artemis/BeaconNodeIntegrationTest.java
+++ artemis/src/test/java/tech/pegasys/artemis/BeaconNodeIntegrationTest.java
@@ -38,6 +38,7 @@ class BeaconNodeIntegrationTest {
   void testThreeNodes() throws InterruptedException, JsonProcessingException {
     CommandLineArguments cliArgs = new CommandLineArguments();
     CommandLine commandLine = new CommandLine(cliArgs);
+    commandLine.parse("");
 
     ArtemisConfiguration config1 = ArtemisConfiguration.fromFile("../config/testConfig.0.toml");
     ArtemisConfiguration config2 = ArtemisConfiguration.fromFile("../config/testConfig.1.toml");
@@ -52,46 +53,6 @@ class BeaconNodeIntegrationTest {
     node3.start();
 
     Thread.sleep(10000);
-
-    P2PNetwork net1 = node1.p2pNetwork();
-    P2PNetwork net2 = node2.p2pNetwork();
-    P2PNetwork net3 = node3.p2pNetwork();
-
-    Bytes block = null;
-    for (P2PNetwork net : Arrays.asList(net1, net2, net3)) {
-      for (Object p : net.getPeers()) {
-        Peer peer = (Peer) p;
-        Bytes message = peer.peerGossip();
-        if (!Objects.isNull(message)) {
-          block = message;
-          break;
-        }
-      }
-    }
-    assertNotNull(block);
-    node1.stop();
-    node2.stop();
-    node3.stop();
-  }
-
-  @Test
-  void testThreeNodesWithRLPx() throws InterruptedException, JsonProcessingException {
-    CommandLineArguments cliArgs = new CommandLineArguments();
-    CommandLine commandLine = new CommandLine(cliArgs);
-
-    ArtemisConfiguration config1 = ArtemisConfiguration.fromFile("../config/rlpxConfig.0.toml");
-    ArtemisConfiguration config2 = ArtemisConfiguration.fromFile("../config/rlpxConfig.1.toml");
-    ArtemisConfiguration config3 = ArtemisConfiguration.fromFile("../config/rlpxConfig.2.toml");
-
-    BeaconNode node1 = new BeaconNode(commandLine, cliArgs, config1);
-    BeaconNode node2 = new BeaconNode(commandLine, cliArgs, config2);
-    BeaconNode node3 = new BeaconNode(commandLine, cliArgs, config3);
-
-    node2.start();
-    node3.start();
-    node1.start();
-
-    Thread.sleep(20000);
 
     P2PNetwork net1 = node1.p2pNetwork();
     P2PNetwork net2 = node2.p2pNetwork();
