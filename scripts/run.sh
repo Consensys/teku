b75de0df1eaa310f824458d8f0d21c03c1c3d7a3
James Towle <jalextowle@gmail.com>
2019-05-02 07:28:46 -0500

    Update the `run` script to be more general (#618)
    * Renamed demo.sh to run.sh
    
    * Refactored run.sh to use functions from run_functions.sh
    
    * Implemented a lot of the logic to create configuration files
    
    * Added a usage statement
    
    * Created the create_config procedure which completely creates the new config files
    
    * Implemented the generalization of the tmux session
    
    * Added the config/runConfig configuration files to the gitignore
    
    * Generalized the run script further by creating more windows for larger numbers of nodes
    
    * Added comments and generally polished the script up

---
+++ scripts/run.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Get the working directory of this script file
+DIR=$(dirname $0)
+
+# Get the shell parameter that was provided
+NODES=$1
+
+# Source the functions from the utilities script
+source $DIR/utils.sh
+
+# If the shell parameters are invalid, print the usage statement and exit
+if [[ "$#" -ne 1 || "$NODES" -lt 1 || "$NODES" -gt 255 ]]
+then 
+  usage 
+  exit
+fi
+
+# Clean the demo directory
+clean demo
+
+# Create a list of all the peers for the configure node procedure to use
+COMBINATIONS=$(seq 19000 $((19000 + $NODES - 1)))
+PEERS=$(echo "$COMBINATIONS" | sed -E "s/^([0-9]+)/\"hob+tcp:\/\/abcf@localhost:\1\"/g")
+
+# Loop over all of the nodes to be created and configure them
+i=0
+while [ $i -lt $NODES ] 
+do
+  configure_node $i $NODES
+  i=$(($i + 1))
+done
+
+# Create a properly formatted tmux session for the simulation
+create_tmux_windows $NODES
