0cea3cfa13a1ba8a65fcc87e8602498654388d78
Cem Ozer <cemozer2018@u.northwestern.edu>
2019-06-28 16:40:25 -0400

    Abstract log coloring (#751)

--- util/src/main/java/tech/pegasys/artemis/util/alogger/ALogger.java
+++ util/src/main/java/tech/pegasys/artemis/util/alogger/ALogger.java
@@ -19,6 +19,22 @@ import org.apache.logging.log4j.Logger;
 
 public class ALogger {
 
+  public enum Color {
+    RED(0),
+    BLUE(1),
+    PURPLE(2),
+    WHITE(3),
+    GREEN(4);
+
+    private int identifier;
+
+    Color(int identifier) {
+      this.identifier = identifier;
+    }
+  }
+
+  private static final String resetCode = "\u001B[0m";
+
   private final Logger logger;
 
   public ALogger() {
@@ -33,9 +49,44 @@ public class ALogger {
     this.logger.log(level, message);
   }
 
-  public void log(Level info, String message, boolean printEnabled) {
+  public void log(Level level, String message, boolean printEnabled) {
     if (printEnabled) {
-      this.logger.log(info, message);
+      this.logger.log(level, message);
     }
+  }
+
+  public void log(Level level, String message, boolean printEnabled, Color color) {
+    log(level, addColor(message, color), printEnabled);
+  }
+
+  public void log(Level level, String message, Color color) {
+    this.logger.log(level, addColor(message, color));
+  }
+
+  private String findColor(Color color) {
+    String colorCode = "";
+    switch (color) {
+      case RED:
+        colorCode = "\u001B[31m";
+        break;
+      case BLUE:
+        colorCode = "\u001b[34;1m";
+        break;
+      case PURPLE:
+        colorCode = "\u001B[35m";
+        break;
+      case WHITE:
+        colorCode = "\033[1;30m";
+        break;
+      case GREEN:
+        colorCode = "\u001B[32m";
+        break;
+    }
+    return colorCode;
+  }
+
+  private String addColor(String message, Color color) {
+    String colorCode = findColor(color);
+    return colorCode + message + resetCode;
   }
 }
