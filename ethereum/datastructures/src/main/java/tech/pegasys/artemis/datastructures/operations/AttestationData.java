ea5052f3b3cfce003451e7a8092b8c5593442e70
decanus <dean.eigenmann@icloud.com>
2019-07-04 11:40:21 -0400

    attestation data

--- ethereum/datastructures/src/main/java/tech/pegasys/artemis/datastructures/operations/AttestationData.java
+++ ethereum/datastructures/src/main/java/tech/pegasys/artemis/datastructures/operations/AttestationData.java
@@ -20,6 +20,7 @@ import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 import org.apache.tuweni.ssz.SSZ;
 import tech.pegasys.artemis.datastructures.state.Crosslink;
+import tech.pegasys.artemis.datastructures.state.Checkpoint;
 import tech.pegasys.artemis.util.hashtree.HashTreeUtil;
 import tech.pegasys.artemis.util.hashtree.HashTreeUtil.SSZTypes;
 
@@ -28,60 +29,50 @@ public class AttestationData {
   // LMD GHOST vote
   private Bytes32 beacon_block_root;
 
-  // FFG vote
-  private UnsignedLong source_epoch;
-  private Bytes32 source_root;
-  private UnsignedLong target_epoch;
-  private Bytes32 target_root;
+  // FFG Vote
+  private Checkpoint source;
+  private Checkpoint target;
 
   // Crosslink vote
   private Crosslink crosslink;
 
   public AttestationData(
       Bytes32 beacon_block_root,
-      UnsignedLong source_epoch,
-      Bytes32 source_root,
-      UnsignedLong target_epoch,
-      Bytes32 target_root,
+      Checkpoint source,
+      Checkpoint target,
       Crosslink crosslink) {
     this.beacon_block_root = beacon_block_root;
-    this.source_epoch = source_epoch;
-    this.source_root = source_root;
-    this.target_epoch = target_epoch;
-    this.target_root = target_root;
+    this.source = source;
+    this.target = target;
     this.crosslink = crosslink;
   }
 
   public AttestationData(AttestationData attestationData) {
     this.beacon_block_root = attestationData.getBeacon_block_root();
-    this.source_epoch = attestationData.getSource_epoch();
-    this.source_root = attestationData.getSource_root();
-    this.target_epoch = attestationData.getTarget_epoch();
-    this.target_root = attestationData.getTarget_root();
+    this.source = attestationData.getSource();
+    this.target = attestationData.getTarget();
     this.crosslink = new Crosslink(attestationData.getCrosslink());
   }
 
+  // @todo figure out
   public static AttestationData fromBytes(Bytes bytes) {
     return SSZ.decode(
         bytes,
         reader ->
             new AttestationData(
                 Bytes32.wrap(reader.readFixedBytes(32)),
-                UnsignedLong.fromLongBits(reader.readUInt64()),
-                Bytes32.wrap(reader.readFixedBytes(32)),
-                UnsignedLong.fromLongBits(reader.readUInt64()),
-                Bytes32.wrap(reader.readFixedBytes(32)),
+                Checkpoint.fromBytes(reader.readBytes()),
+                Checkpoint.fromBytes(reader.readBytes()),
                 Crosslink.fromBytes(reader.readBytes())));
   }
 
+  // @todo figure out
   public Bytes toBytes() {
     return SSZ.encode(
         writer -> {
           writer.writeFixedBytes(32, beacon_block_root);
-          writer.writeUInt64(source_epoch.longValue());
-          writer.writeFixedBytes(32, source_root);
-          writer.writeUInt64(target_epoch.longValue());
-          writer.writeFixedBytes(32, target_root);
+          writer.writeBytes(source.toBytes());
+          writer.writeBytes(target.toBytes());
           writer.writeBytes(crosslink.toBytes());
         });
   }
@@ -89,7 +80,7 @@ public class AttestationData {
   @Override
   public int hashCode() {
     return Objects.hash(
-        beacon_block_root, source_epoch, source_root, target_epoch, target_root, crosslink);
+        beacon_block_root, source, target, crosslink);
   }
 
   @Override
@@ -108,10 +99,8 @@ public class AttestationData {
 
     AttestationData other = (AttestationData) obj;
     return Objects.equals(this.getBeacon_block_root(), other.getBeacon_block_root())
-        && Objects.equals(this.getSource_epoch(), other.getSource_epoch())
-        && Objects.equals(this.getSource_root(), other.getSource_root())
-        && Objects.equals(this.getTarget_epoch(), other.getTarget_epoch())
-        && Objects.equals(this.getTarget_root(), other.getTarget_root())
+        && Objects.equals(this.getSource(), other.getSource())
+        && Objects.equals(this.getTarget(), other.getTarget())
         && Objects.equals(this.getCrosslink(), other.getCrosslink());
   }
 
@@ -124,36 +113,20 @@ public class AttestationData {
     this.beacon_block_root = beacon_block_root;
   }
 
-  public UnsignedLong getSource_epoch() {
-    return source_epoch;
+  public Checkpoint getSource() {
+    return source;
   }
 
-  public void setSource_epoch(UnsignedLong source_epoch) {
-    this.source_epoch = source_epoch;
+  public void setSource(Checkpoint source) {
+    this.source = source;
   }
 
-  public Bytes32 getSource_root() {
-    return source_root;
+  public void setTarget(Checkpoint target) {
+    this.target = target;
   }
 
-  public void setSource_root(Bytes32 source_root) {
-    this.source_root = source_root;
-  }
-
-  public UnsignedLong getTarget_epoch() {
-    return target_epoch;
-  }
-
-  public void setTarget_epoch(UnsignedLong target_epoch) {
-    this.target_epoch = target_epoch;
-  }
-
-  public Bytes32 getTarget_root() {
-    return target_root;
-  }
-
-  public void setTarget_root(Bytes32 target_root) {
-    this.target_root = target_root;
+  public Checkpoint getTarget() {
+    return target;
   }
 
   public Crosslink getCrosslink() {
@@ -168,10 +141,8 @@ public class AttestationData {
     return HashTreeUtil.merkleize(
         Arrays.asList(
             HashTreeUtil.hash_tree_root(SSZTypes.TUPLE_OF_BASIC, beacon_block_root),
-            HashTreeUtil.hash_tree_root(SSZTypes.BASIC, SSZ.encodeUInt64(source_epoch.longValue())),
-            HashTreeUtil.hash_tree_root(SSZTypes.TUPLE_OF_BASIC, source_root),
-            HashTreeUtil.hash_tree_root(SSZTypes.BASIC, SSZ.encodeUInt64(target_epoch.longValue())),
-            HashTreeUtil.hash_tree_root(SSZTypes.TUPLE_OF_BASIC, target_root),
+            source.hash_tree_root(),
+            target.hash_tree_root(),
             crosslink.hash_tree_root()));
   }
 }
