5874e2a156c5f8688694c6ab8eefd23ce807444f
Joseph Delong <joseph@delong.me>
2019-08-06 11:23:43 -0500

    Updates to Test Framework (#814)
    * Implemented ssz_minimal_zero and MapObjectUtil object marshalling from YAML
    
    * spotlessApply
    
    * fix multitest
    
    * Update BLS reference tests for new format (#812)
    
    * Update hash function for BLS to SHA256 (#813)

--- eth-reference-tests/src/test/java/pegasys/artemis/reference/TestSuite.java
+++ eth-reference-tests/src/test/java/pegasys/artemis/reference/TestSuite.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -51,28 +50,33 @@ public abstract class TestSuite {
   private static Stream<Arguments> prepareTests(
       InputStream in, List<Pair<Class, List<String>>> objectPath) throws IOException {
     ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-    Map allTests = mapper.readerFor(Map.class).readValue(in);
-    List<Object> objects =
-        objectPath.stream()
-            .map(
-                e -> {
-                  Iterator<String> itr = e.getSecond().iterator();
-                  Object testObject = Map.copyOf(allTests);
-                  while (itr.hasNext()) {
-                    String param = itr.next();
-                    if (testObject.getClass().equals(ArrayList.class)) {
-                      testObject = ((ArrayList) testObject).get(Integer.valueOf(param));
-                    } else {
-                      testObject = ((Map) testObject).get(param);
-                    }
-                  }
-                  Class classType = e.getFirst();
-                  return MapObjectUtil.convertMapToTypedObject(classType, testObject);
-                })
-            .collect(Collectors.toList());
-
-    return Arrays.stream(new Arguments[] {Arguments.of(objects.toArray())});
+    List<Map> allTests = (ArrayList)((Map)mapper.readerFor(Map.class).readValue(in)).get("test_cases");
+    return allTests.stream().filter(map -> {
+        return objectPath.stream().allMatch(pair -> {
+            Iterator<String> itr = pair.getSecond().iterator();
+            Object testObject = Map.copyOf(map);
+            while (itr.hasNext()) {
+                String param = itr.next();
+                testObject =  ((Map)testObject).get(param);
+                if(testObject == null) return false;
+            }
+            return testObject != null;
+        });
+    }).map(map -> {
+        return objectPath.stream().map(pair -> {
+            Iterator<String> itr = pair.getSecond().iterator();
+            Object testObject = Map.copyOf(map);
+            while (itr.hasNext()) {
+                String param = itr.next();
+                testObject =  ((Map)testObject).get(param);
+            }
+            Class classType = pair.getFirst();
+            return MapObjectUtil.convertMapToTypedObject(classType, testObject);
+        }).collect(Collectors.toList());
+    }).map(objects -> Arguments.of(objects.toArray()));
   }
+
+
 
   @SuppressWarnings({"unchecked", "rawtypes"})
   public static Pair<Class, List<String>> getParams(Class classType, List<String> args) {
