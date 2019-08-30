117e8340d92fdd0fb09855f42ed265740bb8a2c0
Shahan Khatchadourian <shahan.k.code@gmail.com>
2019-08-29 20:37:20 -0400

    Final cleanup with spotless and added some serder tests.

--- util/src/test/java/tech/pegasys/artemis/util/bls/BLSPerformanceRunner.java
+++ util/src/test/java/tech/pegasys/artemis/util/bls/BLSPerformanceRunner.java
@@ -13,30 +13,25 @@
 
 package tech.pegasys.artemis.util.bls;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.tuweni.bytes.Bytes;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import tech.pegasys.artemis.util.alogger.ALogger;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ThreadFactory;
 import java.util.stream.Stream;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.ssz.SSZ;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import tech.pegasys.artemis.util.alogger.ALogger;
 
 public class BLSPerformanceRunner {
 
-  ALogger LOG = new ALogger(BLSPerformanceRunner.class.getName());
+  private ALogger LOG = new ALogger(BLSPerformanceRunner.class.getName());
 
   public BLSPerformanceRunner() {
     Configurator.setRootLevel(Level.INFO);
@@ -52,103 +47,143 @@ public class BLSPerformanceRunner {
     return end - start;
   }
 
-
   @ParameterizedTest()
   @MethodSource("singleAggregationCountOrder4")
-  void generateRandomSignature(Integer i) throws BLSException {
-    Long time = executeRun(() -> {
-      BLSSignature.random();
-    }, i);
-    LOG.log(Level.INFO, "Time for i:"+i+", time:"+time);
+  void generateRandomSignature(Integer i) {
+    Long time = executeRun(BLSSignature::random, i);
+    LOG.log(Level.INFO, "Time for i:" + i + ", time:" + time);
   }
-
-    @ParameterizedTest()
-    @MethodSource("singleAggregationCount")
-    void testAggregateManySignatures(Integer i) throws BLSException {
-        final BLSSignature signature = BLSSignature.random();
-        final List<BLSSignature> sigs = Collections.nCopies(i, signature);
-
-        Long time = executeRun(() -> {
-            try {
-                BLSSignature.aggregate(sigs);
-            } catch (BLSException e) {
-                LOG.log(Level.ERROR, "Exception"+e.toString());
-            }
-        }, 1);
-
-        LOG.log(Level.INFO, "Time for i:"+i+", time:"+time);
-    }
-
-    @ParameterizedTest()
-    @MethodSource("singleAggregationCountOrder4")
-    void testSigning(Integer i) throws BLSException {
-        BLSSignature signature = BLSSignature.random();
-        Bytes message1 = Bytes.wrap("Message One".getBytes(UTF_8));
-
-        // 1 & 2 sign message1; 3 & 4 sign message2
-        BLSKeyPair keyPair1 = BLSKeyPair.random();
-
-        Long time = executeRun(() -> {
-                BLSSignature signature1 = BLSSignature.sign(keyPair1, message1, 0);
-        }, i);
-        LOG.log(Level.INFO, "Time for i:"+i+", time:"+time);
-    }
-
-    @ParameterizedTest()
-    @MethodSource("singleAggregationCountOrder4")
-    void testSigsAndMessagesCheckSignature(Integer i) throws BLSException {
-        BLSSignature signature = BLSSignature.random();
-        Bytes message1 = Bytes.wrap("Message One".getBytes(UTF_8));
-        Bytes message2 = Bytes.wrap("Message Two".getBytes(UTF_8));
-
-        // 1 & 2 sign message1; 3 & 4 sign message2
-        BLSKeyPair keyPair1 = BLSKeyPair.random();
-        BLSKeyPair keyPair2 = BLSKeyPair.random();
-        BLSSignature signature1 = BLSSignature.sign(keyPair1, message1, 0);
-        BLSSignature signature2 = BLSSignature.sign(keyPair2, message1, 0);
-
-        BLSKeyPair keyPair3 = BLSKeyPair.random();
-        BLSKeyPair keyPair4 = BLSKeyPair.random();
-        BLSSignature signature3 = BLSSignature.sign(keyPair3, message2, 0);
-        BLSSignature signature4 = BLSSignature.sign(keyPair4, message2, 0);
-
-
-        // Aggregate keys 1 & 2, and keys 3 & 4
-        BLSPublicKey aggregatePublicKey12 =
-                BLSPublicKey.aggregate(Arrays.asList(keyPair1.getPublicKey(), keyPair2.getPublicKey()));
-
-        BLSPublicKey aggregatePublicKey34 =
-                BLSPublicKey.aggregate(Arrays.asList(keyPair3.getPublicKey(), keyPair4.getPublicKey()));
-
-        // Aggregate the signatures
-        BLSSignature aggregateSignature =
-                BLSSignature.aggregate(Arrays.asList(signature1, signature2, signature3, signature4));
-
-        Long time = executeRun(() -> {
-            try {
-                // Verify the aggregate signatures and keys
-                aggregateSignature.checkSignature(
-                        Arrays.asList(aggregatePublicKey12, aggregatePublicKey34),
-                        Arrays.asList(message1, message2), 0);
-            } catch (BLSException e) {
-                LOG.log(Level.ERROR, "Exception"+e.toString());
-            }
-        }, i);
-        LOG.log(Level.INFO, "Time for i:"+i+", time:"+time);
-    }
 
   @ParameterizedTest()
   @MethodSource("singleAggregationCount")
-  void testAggregationTime(Integer i) throws BLSException {
+  void testAggregateManySignatures(Integer i) {
+    final BLSSignature signature = BLSSignature.random();
+    final List<BLSSignature> sigs = Collections.nCopies(i, signature);
+
+    Long time =
+        executeRun(
+            () -> {
+              try {
+                BLSSignature.aggregate(sigs);
+              } catch (BLSException e) {
+                LOG.log(Level.ERROR, "Exception" + e.toString());
+              }
+            },
+            1);
+
+    LOG.log(Level.INFO, "Time for i:" + i + ", time:" + time);
+  }
+
+  @ParameterizedTest()
+  @MethodSource("singleAggregationCountOrder4")
+  void testSigning(Integer i) {
+    Bytes message1 = Bytes.wrap("Message One".getBytes(UTF_8));
+
+    // 1 & 2 sign message1; 3 & 4 sign message2
+    BLSKeyPair keyPair1 = BLSKeyPair.random();
+
+    Long time = executeRun(() -> BLSSignature.sign(keyPair1, message1, 0), i);
+    LOG.log(Level.INFO, "Time for i:" + i + ", time:" + time);
+  }
+
+  @ParameterizedTest()
+  @MethodSource("singleAggregationCountOrder4")
+  void testSigsAndMessagesCheckSignature(Integer i) throws BLSException {
+    Bytes message1 = Bytes.wrap("Message One".getBytes(UTF_8));
+    Bytes message2 = Bytes.wrap("Message Two".getBytes(UTF_8));
+
+    // 1 & 2 sign message1; 3 & 4 sign message2
+    BLSKeyPair keyPair1 = BLSKeyPair.random();
+    BLSKeyPair keyPair2 = BLSKeyPair.random();
+    BLSSignature signature1 = BLSSignature.sign(keyPair1, message1, 0);
+    BLSSignature signature2 = BLSSignature.sign(keyPair2, message1, 0);
+
+    BLSKeyPair keyPair3 = BLSKeyPair.random();
+    BLSKeyPair keyPair4 = BLSKeyPair.random();
+    BLSSignature signature3 = BLSSignature.sign(keyPair3, message2, 0);
+    BLSSignature signature4 = BLSSignature.sign(keyPair4, message2, 0);
+
+    // Aggregate keys 1 & 2, and keys 3 & 4
+    BLSPublicKey aggregatePublicKey12 =
+        BLSPublicKey.aggregate(Arrays.asList(keyPair1.getPublicKey(), keyPair2.getPublicKey()));
+
+    BLSPublicKey aggregatePublicKey34 =
+        BLSPublicKey.aggregate(Arrays.asList(keyPair3.getPublicKey(), keyPair4.getPublicKey()));
+
+    // Aggregate the signatures
+    BLSSignature aggregateSignature =
+        BLSSignature.aggregate(Arrays.asList(signature1, signature2, signature3, signature4));
+
+    Long time =
+        executeRun(
+            () -> {
+              try {
+                // Verify the aggregate signatures and keys
+                aggregateSignature.checkSignature(
+                    Arrays.asList(aggregatePublicKey12, aggregatePublicKey34),
+                    Arrays.asList(message1, message2),
+                    0);
+              } catch (BLSException e) {
+                LOG.log(Level.ERROR, "Exception" + e.toString());
+              }
+            },
+            i);
+    LOG.log(Level.INFO, "Time for i:" + i + ", time:" + time);
+  }
+
+  @ParameterizedTest()
+  @MethodSource("singleAggregationCount")
+  void testAggregationTime(Integer i) {
     BLSSignature signature = BLSSignature.random();
-    Long time = executeRun(() -> {
-      try {
-        BLSSignature.aggregate(Arrays.asList(signature));
-      } catch (BLSException e) {
-        LOG.log(Level.ERROR, "Exception"+e.toString());
-      }
-    }, i);
-    LOG.log(Level.INFO, "Time for i:"+i+", time:"+time);
+
+    Long time =
+        executeRun(
+            () -> {
+              try {
+                BLSSignature.aggregate(Collections.singletonList(signature));
+              } catch (BLSException e) {
+                LOG.log(Level.ERROR, "Exception" + e.toString());
+              }
+            },
+            i);
+    LOG.log(Level.INFO, "Time for i:" + i + ", time:" + time);
+  }
+
+  @ParameterizedTest()
+  @MethodSource("singleAggregationCount")
+  void testBLSPubKeyDeserialize(Integer i) {
+    Bytes emptyBytesSsz = SSZ.encode(writer -> writer.writeFixedBytes(Bytes.wrap(new byte[48])));
+
+    Long time = executeRun(() -> BLSPublicKey.fromBytes(emptyBytesSsz), i);
+    LOG.log(Level.INFO, "Time for i:" + i + ", time:" + time);
+  }
+
+  @ParameterizedTest()
+  @MethodSource("singleAggregationCount")
+  void testBLSPubKeySerialize(Integer i) {
+    BLSPublicKey emptyPublicKey = BLSPublicKey.empty();
+
+    Long time = executeRun(() -> emptyPublicKey.toBytes().toHexString(), i);
+    LOG.log(Level.INFO, "Time for i:" + i + ", time:" + time);
+  }
+
+  @ParameterizedTest()
+  @MethodSource("singleAggregationCount")
+  void testSignatureSerialize(Integer i) {
+    BLSSignature signature1 = BLSSignature.random();
+
+    Long time = executeRun(signature1::toBytes, i);
+    LOG.log(Level.INFO, "Time for i:" + i + ", time:" + time);
+  }
+
+  @ParameterizedTest()
+  @MethodSource("singleAggregationCount")
+  void testSignatureDeserialize(Integer i) {
+    BLSSignature signature1 = BLSSignature.random();
+    Bytes bytes = signature1.toBytes();
+
+    Long time = executeRun(() -> BLSSignature.fromBytes(bytes), i);
+    LOG.log(Level.INFO, "Time for i:" + i + ", time:" + time);
   }
 
   public static Stream<Arguments> singleAggregationCount() {
@@ -157,19 +192,17 @@ public class BLSPerformanceRunner {
     args.add(Arguments.of(100));
     args.add(Arguments.of(1000));
     args.add(Arguments.of(10000));
-    args.add(Arguments.of(100000));
-    args.add(Arguments.of(1000000));
+    //    args.add(Arguments.of(100000));
+    //    args.add(Arguments.of(1000000));
     return args.stream();
   }
 
-
-    public static Stream<Arguments> singleAggregationCountOrder4() {
-        ArrayList<Arguments> args = new ArrayList<>();
-        args.add(Arguments.of(10));
-        args.add(Arguments.of(100));
-        args.add(Arguments.of(1000));
-        args.add(Arguments.of(2000));
-        return args.stream();
-    }
-
+  public static Stream<Arguments> singleAggregationCountOrder4() {
+    ArrayList<Arguments> args = new ArrayList<>();
+    args.add(Arguments.of(10));
+    args.add(Arguments.of(100));
+    args.add(Arguments.of(1000));
+    //    args.add(Arguments.of(2000));
+    return args.stream();
+  }
 }
