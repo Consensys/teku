d47cfd3b1e9e071846b8dffdc29e40e52cd010aa
jonny rhea <jonathan.rhea@gmail.com>
2019-09-05 12:37:46 -0500

    mods to output genesis to json

--- ethereum/datastructures/src/test/java/tech/pegasys/artemis/datastructures/util/MockStartBeaconStateGeneratorTest.java
+++ ethereum/datastructures/src/test/java/tech/pegasys/artemis/datastructures/util/MockStartBeaconStateGeneratorTest.java
@@ -13,17 +13,34 @@
 
 package tech.pegasys.artemis.datastructures.util;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.google.common.primitives.UnsignedLong;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.bytes.Bytes32;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Test;
+import tech.pegasys.artemis.datastructures.Constants;
 import tech.pegasys.artemis.datastructures.operations.DepositData;
+import tech.pegasys.artemis.datastructures.operations.DepositWithIndex;
 import tech.pegasys.artemis.datastructures.state.BeaconStateWithCache;
 import tech.pegasys.artemis.datastructures.state.Validator;
 import tech.pegasys.artemis.util.bls.BLSKeyPair;
 import tech.pegasys.artemis.util.bls.BLSPublicKey;
+import tech.pegasys.artemis.util.bls.BLSSignature;
 
 class MockStartBeaconStateGeneratorTest {
   @Test
@@ -50,5 +67,71 @@ class MockStartBeaconStateGeneratorTest {
     final List<BLSPublicKey> expectedValidatorPublicKeys =
         validatorKeyPairs.stream().map(BLSKeyPair::getPublicKey).collect(Collectors.toList());
     assertEquals(expectedValidatorPublicKeys, actualValidatorPublicKeys);
+  }
+
+  @Test
+  public void newDepositFileGeneration() {
+
+
+    List<JSONObject> depositData = new ArrayList<>();
+    List<JSONObject> keys = new ArrayList<>();
+
+    JSONObject jsonObject = new JSONObject();
+
+
+    final UnsignedLong genesisTime = UnsignedLong.valueOf(1567630429);
+    final int validatorCount = 8;
+
+    final List<BLSKeyPair> validatorKeyPairs =
+            new MockStartValidatorKeyPairFactory().generateKeyPairs(0, validatorCount - 1);
+
+    validatorKeyPairs.forEach(blsKeyPair -> {
+      JSONObject keypair = new JSONObject();
+      keypair.put("public_key",blsKeyPair.getPublicKey().toBytes().toHexString());
+      keypair.put("private_key",blsKeyPair.getSecretKey().getSecretKey().toBytes().toHexString());
+      keys.add(keypair);
+    });
+
+    final List<DepositData> deposits =
+            new MockStartDepositGenerator().createDeposits(validatorKeyPairs);
+
+    deposits.forEach(deposit -> {
+      JSONObject data = new JSONObject();
+      //List<String> data = new ArrayList<>();
+      data.put("amount",deposit.getAmount());
+      data.put("public_key",deposit.getPubkey().toBytes().toHexString());
+      data.put("signature",deposit.getSignature().toBytes().toHexString());
+      data.put("withdrawal_credentials",deposit.getWithdrawal_credentials().toHexString());
+      depositData.add(data);
+    });
+
+    jsonObject.put("genesis_time",genesisTime);
+    jsonObject.put("validator_count",validatorCount);
+    jsonObject.put("deposits", depositData);
+    jsonObject.put("validator_credentials", keys);
+
+
+    final byte[] eth1BlockHashBytes = new byte[32];
+    Arrays.fill(eth1BlockHashBytes, (byte) 0x42);
+    Bytes32 block_hash = Bytes32.wrap(eth1BlockHashBytes);
+
+    jsonObject.put("block_hash", block_hash.toHexString());
+
+    UnsignedLong ETH1_TIMESTAMP =
+            UnsignedLong.valueOf(new BigInteger("2").pow(40));
+
+    jsonObject.put("eth1_timestamp", ETH1_TIMESTAMP);
+
+    MerkleTree<DepositWithIndex> merkleTree = DepositUtil.generateMerkleTree(deposits);
+
+    // Write JSON file
+    try (Writer file =
+                 Files.newBufferedWriter(Paths.get("../../interopDepositsAndKeys.json"), UTF_8)) {
+      file.write(jsonObject.toJSONString());
+      file.flush();
+
+    } catch (IOException e) {
+      System.out.println(e.toString());
+    }
   }
 }
