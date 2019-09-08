ed783756270819591396a93f307d4fc562b17627
Jonny Rhea <5555162+jrhea@users.noreply.github.com>
2019-09-08 16:17:49 -0400

    Mothra submodule (#866)
    * adding mothra as a submodule
    
    * adding mothra as a submodule
    
    * updates to script to read from ssz file
    
    * fixes to interop script and slot skip logic
    
    * checking in libs to prevent CI build from failing
    
    * fix to slot skip logic

---
+++ scripts/mothra-dist.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mkdir -p ../libs/release/deps && cp -f ../mothra/bin/*.jar ../libs/ && cp -f ../mothra/bin/*.dylib ../libs/ && cp -f ../mothra/bin/release/deps/libmothra.dylib ../libs/release/deps/
