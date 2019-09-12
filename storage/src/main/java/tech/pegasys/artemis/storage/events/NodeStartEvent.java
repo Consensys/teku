5dad741ac4e978ecb71940945b8d48eac1965e62
Adrian Sutton <adrian.sutton@consensys.net>
2019-09-11 21:35:10 -0400

    Move all interop genesis generation logic into the storage service instead of StateProcessor.  Only fire NodeStartEvent after all BeaconChainService components are setup and registered.

--- storage/src/main/java/tech/pegasys/artemis/storage/events/NodeStartEvent.java
+++ storage/src/main/java/tech/pegasys/artemis/storage/events/NodeStartEvent.java
@@ -13,16 +13,7 @@
 
 package tech.pegasys.artemis.storage.events;
 
-import tech.pegasys.artemis.datastructures.state.BeaconStateWithCache;
-
 public class NodeStartEvent {
-  private final BeaconStateWithCache state;
 
-  public NodeStartEvent(final BeaconStateWithCache state) {
-    this.state = state;
-  }
-
-  public BeaconStateWithCache getState() {
-    return state;
-  }
+  public NodeStartEvent() {}
 }
