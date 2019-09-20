1bc11ec345d0105eedc2e2f637d0c27ad2d20740
Shahan Khatchadourian <shahan.k.code@gmail.com>
2019-09-20 12:47:56 -0400

    Rpc messages (#904)
    Basic structures for RPC SSZ-ized messages, requests and responses.

---
+++ ethereum/datastructures/src/main/java/tech/pegasys/artemis/datastructures/networking/libp2p/rpc/BeaconBlocksMessageRequest.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2019 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.artemis.datastructures.networking.libp2p.rpc;
+
+import com.google.common.primitives.UnsignedLong;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.bytes.Bytes32;
+import org.apache.tuweni.ssz.SSZ;
+import tech.pegasys.artemis.util.SSZTypes.SSZContainer;
+import tech.pegasys.artemis.util.sos.SimpleOffsetSerializable;
+
+public final class BeaconBlocksMessageRequest implements SimpleOffsetSerializable, SSZContainer {
+
+  private final Bytes32 headBlockRoot;
+  private final UnsignedLong startSlot;
+  private final UnsignedLong count;
+  private final UnsignedLong step;
+
+  public BeaconBlocksMessageRequest(
+      Bytes32 headBlockRoot, UnsignedLong startSlot, UnsignedLong count, UnsignedLong step) {
+    this.headBlockRoot = headBlockRoot;
+    this.startSlot = startSlot;
+    this.step = step;
+    this.count = count;
+  }
+
+  @Override
+  public int getSSZFieldCount() {
+    return 4;
+  }
+
+  @Override
+  public List<Bytes> get_fixed_parts() {
+    List<Bytes> fixedPartsList =
+        new ArrayList<>(
+            List.of(
+                SSZ.encode(writer -> writer.writeFixedBytes(headBlockRoot)),
+                SSZ.encodeUInt64(startSlot.longValue()),
+                SSZ.encodeUInt64(count.longValue()),
+                SSZ.encodeUInt64(step.longValue())));
+    return fixedPartsList;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(headBlockRoot, startSlot, count, step);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (Objects.isNull(obj)) {
+      return false;
+    }
+
+    if (this == obj) {
+      return true;
+    }
+
+    if (!(obj instanceof BeaconBlocksMessageRequest)) {
+      return false;
+    }
+
+    BeaconBlocksMessageRequest other = (BeaconBlocksMessageRequest) obj;
+    return Objects.equals(this.headBlockRoot(), other.headBlockRoot())
+        && Objects.equals(this.startSlot(), other.startSlot())
+        && Objects.equals(this.count(), other.count())
+        && Objects.equals(this.step(), other.step());
+  }
+
+  public Bytes32 headBlockRoot() {
+    return headBlockRoot;
+  }
+
+  public UnsignedLong startSlot() {
+    return startSlot;
+  }
+
+  public UnsignedLong count() {
+    return count;
+  }
+
+  public UnsignedLong step() {
+    return step;
+  }
+}
---
+++ ethereum/datastructures/src/main/java/tech/pegasys/artemis/datastructures/networking/libp2p/rpc/BeaconBlocksMessageResponse.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2019 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.artemis.datastructures.networking.libp2p.rpc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.ssz.SSZ;
+import tech.pegasys.artemis.datastructures.blocks.BeaconBlock;
+import tech.pegasys.artemis.datastructures.util.SimpleOffsetSerializer;
+import tech.pegasys.artemis.util.SSZTypes.SSZContainer;
+import tech.pegasys.artemis.util.sos.SimpleOffsetSerializable;
+
+public final class BeaconBlocksMessageResponse implements SimpleOffsetSerializable, SSZContainer {
+
+  private final List<BeaconBlock> blocks;
+
+  public BeaconBlocksMessageResponse(List<BeaconBlock> blocks) {
+    this.blocks = blocks;
+  }
+
+  @Override
+  public int getSSZFieldCount() {
+    return 1;
+  }
+
+  @Override
+  public List<Bytes> get_fixed_parts() {
+    List<Bytes> fixedPartsList =
+        new ArrayList<>(
+            List.of(
+                SSZ.encode(
+                    writer ->
+                        writer.writeBytes(
+                            SimpleOffsetSerializer.serializeVariableCompositeList(blocks)))));
+    return fixedPartsList;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(blocks);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (Objects.isNull(obj)) {
+      return false;
+    }
+
+    if (this == obj) {
+      return true;
+    }
+
+    if (!(obj instanceof BeaconBlocksMessageResponse)) {
+      return false;
+    }
+
+    BeaconBlocksMessageResponse other = (BeaconBlocksMessageResponse) obj;
+    return Objects.equals(this.blocks(), other.blocks());
+  }
+
+  public List<BeaconBlock> blocks() {
+    return blocks;
+  }
+}
