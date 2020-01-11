644cbf75718c41e7eee0cfee28cb5b5a492f97b0
Shahan Khatchadourian <shahan.k.code@gmail.com>
2020-01-10 20:00:50 -0500

    werror fix

--- networking/eth2/src/main/java/tech/pegasys/artemis/networking/eth2/discovery/DiscoveryPeerSubscriberImpl.java
+++ networking/eth2/src/main/java/tech/pegasys/artemis/networking/eth2/discovery/DiscoveryPeerSubscriberImpl.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2020 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package tech.pegasys.artemis.networking.eth2.discovery;
 
 import com.google.common.eventbus.Subscribe;
@@ -11,13 +24,14 @@ public class DiscoveryPeerSubscriberImpl implements DiscoveryPeerSubscriber {
 
   Logger logger = LogManager.getLogger();
 
-  private P2PNetwork network;
+  private P2PNetwork<?> network;
 
-  public DiscoveryPeerSubscriberImpl(P2PNetwork network) {
+  public DiscoveryPeerSubscriberImpl(P2PNetwork<?> network) {
     this.network = network;
   }
 
   @Subscribe
+  @Override
   public void onDiscovery(DiscoveryPeer discoveryPeer) {
     String d = Base58.INSTANCE.encode(discoveryPeer.getNodeId().toArray());
     String connectString =
@@ -41,5 +55,4 @@ public class DiscoveryPeerSubscriberImpl implements DiscoveryPeerSubscriber {
           "connect failed with null, is this a mock? If so, the repo check expects a safe future to be handled, before being able to to test the mock");
     }
   }
-
 }
