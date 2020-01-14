7210b514f997662b85007b74e24d174fff2284b2
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-01-14 20:19:23 +0300

    No need to parallelize very lightweight task

--- ethereum/datastructures/src/main/java/tech/pegasys/artemis/datastructures/util/ValidatorsUtil.java
+++ ethereum/datastructures/src/main/java/tech/pegasys/artemis/datastructures/util/ValidatorsUtil.java
@@ -73,12 +73,7 @@ public class ValidatorsUtil {
   public static List<Integer> get_active_validator_indices(BeaconState state, UnsignedLong epoch) {
     List<Validator> validators = state.getValidators();
     return IntStream.range(0, validators.size())
-        .parallel()
-        .flatMap(
-            index ->
-                is_active_validator(validators.get(index), epoch)
-                    ? IntStream.of(index)
-                    : IntStream.empty())
+        .filter(index -> is_active_validator(validators.get(index), epoch))
         .boxed()
         .collect(Collectors.toList());
   }
