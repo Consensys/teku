8541794803ce871d80d11417255fc3e96b6fc59d
Adrian Sutton <adrian.sutton@consensys.net>
2020-01-22 09:00:48 +1000

    Add runAfterDelay and runAsync to SafeFuture. (#1103)

--- util/src/test/java/tech/pegasys/artemis/util/async/SafeFutureTest.java
+++ util/src/test/java/tech/pegasys/artemis/util/async/SafeFutureTest.java
@@ -15,11 +15,19 @@ package tech.pegasys.artemis.util.async;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
+import java.util.concurrent.Executor;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LoggerContext;
@@ -30,6 +38,7 @@ import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.apache.logging.log4j.core.layout.PatternLayout;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 class SafeFutureTest {
 
@@ -307,6 +316,65 @@ class SafeFutureTest {
 
     composedFuture.complete("Composed");
     assertThat(result).isCompletedWithValue("Success");
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void runAsync_shouldExecuteActionWithExecutorAndReturnResult() {
+    final SafeFuture<String> actionResult = new SafeFuture<>();
+    final Executor executor = mock(Executor.class);
+    final Supplier<SafeFuture<String>> action = mock(Supplier.class);
+    when(action.get()).thenReturn(actionResult);
+
+    final SafeFuture<String> result = SafeFuture.runAsync(action, executor);
+
+    final ArgumentCaptor<Runnable> captor = ArgumentCaptor.forClass(Runnable.class);
+    verify(executor).execute(captor.capture());
+    final Runnable executedRunnable = captor.getValue();
+
+    executedRunnable.run();
+    verify(action).get();
+    assertThat(result).isNotDone();
+
+    actionResult.complete("Yay");
+    assertThat(result).isCompletedWithValue("Yay");
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void runAsync_shouldExecuteActionWithExecutorAndReturnExceptionalResult() {
+    final SafeFuture<String> actionResult = new SafeFuture<>();
+    final Executor executor = mock(Executor.class);
+    final Supplier<SafeFuture<String>> action = mock(Supplier.class);
+    when(action.get()).thenReturn(actionResult);
+
+    final SafeFuture<String> result = SafeFuture.runAsync(action, executor);
+
+    final ArgumentCaptor<Runnable> captor = ArgumentCaptor.forClass(Runnable.class);
+    verify(executor).execute(captor.capture());
+    final Runnable executedRunnable = captor.getValue();
+
+    executedRunnable.run();
+    verify(action).get();
+    assertThat(result).isNotDone();
+
+    final RuntimeException exception = new RuntimeException("Nope");
+    actionResult.completeExceptionally(exception);
+    assertExceptionallyCompletedWith(result, exception);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void runAsync_shouldCompleteExceptionallyWhenExecutorFails() {
+    final Executor executor = mock(Executor.class);
+    final Supplier<SafeFuture<String>> action = mock(Supplier.class);
+    final RuntimeException exception = new RuntimeException("Nope");
+    doThrow(exception).when(executor).execute(any());
+
+    final SafeFuture<String> result = SafeFuture.runAsync(action, executor);
+
+    verify(action, never()).get();
+    assertExceptionallyCompletedWith(result, exception);
   }
 
   private CountingNoOpAppender startCountingReportedUnhandledExceptions() {
