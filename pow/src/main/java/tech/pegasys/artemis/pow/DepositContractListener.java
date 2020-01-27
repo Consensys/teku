380ce0f7acb04fc8d5972421a34a0d3307564f90
Adrian Sutton <adrian.sutton@consensys.net>
2020-01-28 09:50:17 +1000

    Remove debug statement

--- pow/src/main/java/tech/pegasys/artemis/pow/DepositContractListener.java
+++ pow/src/main/java/tech/pegasys/artemis/pow/DepositContractListener.java
@@ -20,8 +20,6 @@ import com.google.common.primitives.UnsignedLong;
 import io.reactivex.Flowable;
 import io.reactivex.disposables.Disposable;
 import java.util.Optional;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.web3j.abi.EventEncoder;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
@@ -33,7 +31,6 @@ import tech.pegasys.artemis.pow.contract.DepositContract.DepositEventEventRespon
 import tech.pegasys.artemis.pow.event.Deposit;
 
 public class DepositContractListener {
-  private static final Logger LOG = LogManager.getLogger();
   private final Disposable subscriptionNewDeposit;
   private DepositContract contract;
   private volatile Optional<EthBlock.Block> cachedBlock = Optional.empty();
@@ -60,7 +57,6 @@ public class DepositContractListener {
 
   private Flowable<Deposit> getBlockTimestamp(
       final Web3j web3j, final DepositEventEventResponse event) {
-    LOG.debug("Getting timestamp for deposit event in block {}", event.log.getBlockNumber());
     return getBlockByHash(web3j, event.log.getBlockHash())
         .map(block -> new Deposit(event, UnsignedLong.valueOf(block.getTimestamp())));
   }
