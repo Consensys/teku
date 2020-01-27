4fb813d6691e452424084d3f752c4e15c6e2fdce
Adrian Sutton <adrian.sutton@consensys.net>
2020-01-28 09:34:07 +1000

    Cache the block data.

--- pow/src/main/java/tech/pegasys/artemis/pow/DepositContractListener.java
+++ pow/src/main/java/tech/pegasys/artemis/pow/DepositContractListener.java
@@ -19,12 +19,15 @@ import com.google.common.eventbus.EventBus;
 import com.google.common.primitives.UnsignedLong;
 import io.reactivex.Flowable;
 import io.reactivex.disposables.Disposable;
+import java.util.Optional;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.web3j.abi.EventEncoder;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.request.EthFilter;
+import org.web3j.protocol.core.methods.response.EthBlock;
+import org.web3j.protocol.core.methods.response.EthBlock.Block;
 import tech.pegasys.artemis.pow.contract.DepositContract;
 import tech.pegasys.artemis.pow.contract.DepositContract.DepositEventEventResponse;
 import tech.pegasys.artemis.pow.event.Deposit;
@@ -33,6 +36,7 @@ public class DepositContractListener {
   private static final Logger LOG = LogManager.getLogger();
   private final Disposable subscriptionNewDeposit;
   private DepositContract contract;
+  private volatile Optional<EthBlock.Block> cachedBlock = Optional.empty();
 
   public DepositContractListener(Web3j web3j, EventBus eventBus, DepositContract contract) {
     this.contract = contract;
@@ -57,10 +61,24 @@ public class DepositContractListener {
   private Flowable<Deposit> getBlockTimestamp(
       final Web3j web3j, final DepositEventEventResponse event) {
     LOG.debug("Getting timestamp for deposit event in block {}", event.log.getBlockNumber());
-    return web3j
-        .ethGetBlockByHash(event.log.getBlockHash(), false)
-        .flowable()
-        .map(block -> new Deposit(event, UnsignedLong.valueOf(block.getBlock().getTimestamp())));
+    return getBlockByHash(web3j, event.log.getBlockHash())
+        .map(block -> new Deposit(event, UnsignedLong.valueOf(block.getTimestamp())));
+  }
+
+  private Flowable<Block> getBlockByHash(final Web3j web3j, final String blockHash) {
+    return cachedBlock
+        .filter(block -> block.getHash().equals(blockHash))
+        .map(Flowable::just)
+        .orElseGet(
+            () ->
+                web3j
+                    .ethGetBlockByHash(blockHash, false)
+                    .flowable()
+                    .map(
+                        blockResponse -> {
+                          cachedBlock = Optional.of(blockResponse.getBlock());
+                          return blockResponse.getBlock();
+                        }));
   }
 
   public DepositContract getContract() {
