6f26efdefe41c9d51792d72eb3c0a5d85f7cc750
mbaxter <meredith.baxter@consensys.net>
2020-01-30 10:11:49 -0500

    [BC-202] Fetch unknown ancestor blocks (#1118)

---
+++ sync/src/integration-test/java/tech/pegasys/artemis/sync/BlockPropagationIntegrationTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2020 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.artemis.sync;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.common.primitives.UnsignedLong;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.tuweni.bytes.Bytes32;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import tech.pegasys.artemis.data.BlockProcessingRecord;
+import tech.pegasys.artemis.datastructures.blocks.SignedBeaconBlock;
+import tech.pegasys.artemis.networking.eth2.Eth2NetworkFactory;
+import tech.pegasys.artemis.statetransition.events.BlockProposedEvent;
+import tech.pegasys.artemis.util.Waiter;
+import tech.pegasys.artemis.util.bls.BLSKeyGenerator;
+import tech.pegasys.artemis.util.bls.BLSKeyPair;
+import tech.pegasys.artemis.util.config.Constants;
+
+public class BlockPropagationIntegrationTest {
+  private final List<BLSKeyPair> validatorKeys = BLSKeyGenerator.generateKeyPairs(3);
+  private final Eth2NetworkFactory networkFactory = new Eth2NetworkFactory();
+
+  @AfterEach
+  public void tearDown() {
+    networkFactory.stopAll();
+  }
+
+  @Test
+  public void shouldFetchUnknownAncestorsOfPropagatedBlock() throws Exception {
+    UnsignedLong currentSlot = UnsignedLong.valueOf(Constants.GENESIS_SLOT);
+
+    // Setup node 1
+    SyncingNodeManager node1 = SyncingNodeManager.create(networkFactory, validatorKeys);
+    node1.chainUtil().setSlot(currentSlot);
+
+    // Add some blocks to node1, which node 2 will need to fetch
+    final List<SignedBeaconBlock> blocksToFetch = new ArrayList<>();
+    for (int i = 0; i < 3; i++) {
+      currentSlot = currentSlot.plus(UnsignedLong.ONE);
+      final BlockProcessingRecord record =
+          node1.chainUtil().createAndImportBlockAtSlot(currentSlot);
+      blocksToFetch.add(record.getBlock());
+    }
+
+    // Setup node 2
+    SyncingNodeManager node2 = SyncingNodeManager.create(networkFactory, validatorKeys);
+
+    // Connect networks
+    Waiter.waitFor(node1.network().connect(node2.network().getNodeAddress()));
+    // Wait for connections to get set up
+    Waiter.waitFor(
+        () -> {
+          assertThat(node1.network().getPeerCount()).isEqualTo(1);
+          assertThat(node2.network().getPeerCount()).isEqualTo(1);
+        });
+    // TODO: debug this - we shouldn't have to wait here
+    Thread.sleep(2000);
+
+    // Update slot so that blocks can be imported
+    currentSlot = currentSlot.plus(UnsignedLong.ONE);
+    node1.setSlot(currentSlot);
+    node2.setSlot(currentSlot);
+
+    // Propagate new block
+    final SignedBeaconBlock newBlock = node1.chainUtil().createBlockAtSlot(currentSlot);
+    node1.eventBus().post(new BlockProposedEvent(newBlock));
+
+    // Verify that node2 fetches required blocks in response
+    Waiter.waitFor(
+        () -> {
+          for (SignedBeaconBlock block : blocksToFetch) {
+            final Bytes32 blockRoot = block.getMessage().hash_tree_root();
+            assertThat(node2.storageClient().getBlockByRoot(blockRoot)).isPresent();
+          }
+          // Last block should be imported as well
+          final Bytes32 newBlockRoot = newBlock.getMessage().hash_tree_root();
+          assertThat(node2.storageClient().getBlockByRoot(newBlockRoot)).isPresent();
+        });
+  }
+}
---
+++ sync/src/main/java/tech/pegasys/artemis/sync/FetchBlockTask.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2020 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.artemis.sync;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.tuweni.bytes.Bytes32;
+import tech.pegasys.artemis.datastructures.blocks.SignedBeaconBlock;
+import tech.pegasys.artemis.networking.eth2.Eth2Network;
+import tech.pegasys.artemis.networking.eth2.peers.Eth2Peer;
+import tech.pegasys.artemis.networking.p2p.peer.NodeId;
+import tech.pegasys.artemis.sync.FetchBlockTask.FetchBlockResult.Status;
+import tech.pegasys.artemis.util.async.SafeFuture;
+
+class FetchBlockTask {
+  private static final Logger LOG = LogManager.getLogger();
+  private static final Comparator<Eth2Peer> SHUFFLING_COMPARATOR =
+      Comparator.comparing(p -> Math.random());
+
+  private final Eth2Network eth2Network;
+  private final Bytes32 blockRoot;
+  private final Set<NodeId> queriedPeers = Collections.newSetFromMap(new ConcurrentHashMap<>());
+
+  private final AtomicInteger numberOfRuns = new AtomicInteger(0);
+  private final AtomicBoolean cancelled = new AtomicBoolean(false);
+
+  FetchBlockTask(final Eth2Network eth2Network, final Bytes32 blockRoot) {
+    this.eth2Network = eth2Network;
+    this.blockRoot = blockRoot;
+  }
+
+  public void cancel() {
+    cancelled.set(true);
+  }
+
+  public static FetchBlockTask create(final Eth2Network eth2Network, final Bytes32 blockRoot) {
+    return new FetchBlockTask(eth2Network, blockRoot);
+  }
+
+  public Bytes32 getBlockRoot() {
+    return blockRoot;
+  }
+
+  public int getNumberOfRetries() {
+    return Math.max(0, numberOfRuns.get() - 1);
+  }
+
+  public SafeFuture<FetchBlockResult> run() {
+    if (cancelled.get()) {
+      return SafeFuture.completedFuture(FetchBlockResult.createFailed(Status.CANCELLED));
+    }
+
+    final Optional<Eth2Peer> maybePeer =
+        eth2Network
+            .streamPeers()
+            .filter(p -> !queriedPeers.contains(p.getId()))
+            .min(
+                Comparator.comparing(Eth2Peer::getOutstandingRequests)
+                    .thenComparing(SHUFFLING_COMPARATOR));
+
+    if (maybePeer.isEmpty()) {
+      return SafeFuture.completedFuture(FetchBlockResult.createFailed(Status.NO_AVAILABLE_PEERS));
+    }
+    final Eth2Peer peer = maybePeer.get();
+
+    numberOfRuns.incrementAndGet();
+    queriedPeers.add(peer.getId());
+    return peer.requestBlockByRoot(blockRoot)
+        .thenApply(FetchBlockResult::createSuccessful)
+        .exceptionally(
+            err -> {
+              LOG.debug("Failed to fetch block " + blockRoot, err);
+              return FetchBlockResult.createFailed(Status.FETCH_FAILED);
+            });
+  }
+
+  static final class FetchBlockResult {
+    public enum Status {
+      SUCCESSFUL,
+      NO_AVAILABLE_PEERS,
+      CANCELLED,
+      FETCH_FAILED
+    }
+
+    private final Status status;
+    private final Optional<SignedBeaconBlock> block;
+
+    private FetchBlockResult(final Status status, final Optional<SignedBeaconBlock> block) {
+      this.status = status;
+      this.block = block;
+    }
+
+    public static FetchBlockResult createSuccessful(SignedBeaconBlock block) {
+      return new FetchBlockResult(Status.SUCCESSFUL, Optional.of(block));
+    }
+
+    public static FetchBlockResult createFailed(Status failureStatus) {
+      return new FetchBlockResult(failureStatus, Optional.empty());
+    }
+
+    public boolean isSuccessful() {
+      return status == Status.SUCCESSFUL;
+    }
+
+    public SignedBeaconBlock getBlock() throws NoSuchElementException {
+      return block.orElseThrow();
+    }
+
+    public Status getStatus() {
+      return status;
+    }
+  }
+}
