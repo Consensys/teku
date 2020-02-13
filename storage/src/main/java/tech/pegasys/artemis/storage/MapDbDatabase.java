c5a85a495b20a625e15307d4a6cf9ce37622212e
Meredith Baxter <meredith.baxter@consensys.net>
2020-02-13 11:03:17 -0500

    Prune hot blocks up to the slot prior to the latest finalized block
    We need to make sure that the state for the finalized block is preserved
    so that we are able to import blocks on top of the finalized state.

--- storage/src/main/java/tech/pegasys/artemis/storage/MapDbDatabase.java
+++ storage/src/main/java/tech/pegasys/artemis/storage/MapDbDatabase.java
@@ -13,7 +13,6 @@
 
 package tech.pegasys.artemis.storage;
 
-import static tech.pegasys.artemis.datastructures.util.BeaconStateUtil.compute_start_slot_at_epoch;
 import static tech.pegasys.artemis.util.alogger.ALogger.STDOUT;
 
 import com.google.common.primitives.UnsignedLong;
@@ -257,10 +256,10 @@ public class MapDbDatabase implements Database {
 
   private void pruneHotBlocks(final Checkpoint newFinalizedCheckpoint) {
     // TODO: Can we prune blocks from in the finalized epoch as well?
-    final UnsignedLong startOfFinalizedEpoch =
-        compute_start_slot_at_epoch(newFinalizedCheckpoint.getEpoch());
+    SignedBeaconBlock newlyFinalizedBlock = hotBlocksByRoot.get(newFinalizedCheckpoint.getRoot());
+    final UnsignedLong finalizedSlot = newlyFinalizedBlock.getSlot();
     final ConcurrentNavigableMap<UnsignedLong, Set<Bytes32>> toRemove =
-        hotRootsBySlotCache.headMap(startOfFinalizedEpoch);
+        hotRootsBySlotCache.headMap(finalizedSlot);
     LOG.trace("Pruning slots {} from non-finalized pool", toRemove::keySet);
     toRemove
         .values()
