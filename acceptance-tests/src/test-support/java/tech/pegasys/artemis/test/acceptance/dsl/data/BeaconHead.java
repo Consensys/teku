9451d96be0ce9357f556ba3f78f59414e463c971
Paul Harris <paul.harris@consensys.net>
2020-02-20 13:02:20 +1000

    acceptance test object needs jackson annotations
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- acceptance-tests/src/test-support/java/tech/pegasys/artemis/test/acceptance/dsl/data/BeaconHead.java
+++ acceptance-tests/src/test-support/java/tech/pegasys/artemis/test/acceptance/dsl/data/BeaconHead.java
@@ -13,6 +13,7 @@
 
 package tech.pegasys.artemis.test.acceptance.dsl.data;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.MoreObjects;
 import com.google.common.primitives.UnsignedLong;
@@ -29,7 +30,11 @@ public class BeaconHead {
   @JsonProperty("state_root")
   private final Bytes32 stateRoot;
 
-  public BeaconHead(final UnsignedLong slot, final Bytes32 blockRoot, final Bytes32 stateRoot) {
+
+  @JsonCreator
+  public BeaconHead(@JsonProperty("slot") final UnsignedLong slot,
+                    @JsonProperty("block_root") final Bytes32 blockRoot,
+                    @JsonProperty("state_root") final Bytes32 stateRoot) {
     this.slot = slot;
     this.blockRoot = blockRoot;
     this.stateRoot = stateRoot;
