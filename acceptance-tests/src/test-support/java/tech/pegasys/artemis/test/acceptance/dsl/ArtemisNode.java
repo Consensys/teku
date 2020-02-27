94ddf701b801f9b6447e6a9641ac74fb64e6e4d5
Paul Harris <paul.harris@consensys.net>
2020-02-28 08:26:26 +1000

    fix AT with config changes. this is a bit of a left-field thing with the config being hand written for testing.

--- acceptance-tests/src/test-support/java/tech/pegasys/artemis/test/acceptance/dsl/ArtemisNode.java
+++ acceptance-tests/src/test-support/java/tech/pegasys/artemis/test/acceptance/dsl/ArtemisNode.java
@@ -270,9 +270,12 @@ public class ArtemisNode extends Node {
 
       final Map<String, Object> beaconRestApi = getSection(BEACONRESTAPI_SECTION);
       beaconRestApi.put("portNumber", REST_API_PORT);
+      beaconRestApi.put("enableSwagger", false);
 
       final Map<String, Object> output = getSection(OUTPUT_SECTION);
       output.put("transitionRecordDir", ARTIFACTS_PATH + "transitions/");
+      output.put("logDir", ARTIFACTS_PATH + "logs/");
+      output.put("dataDir", ARTIFACTS_PATH + "data/");
     }
 
     public Config withDepositsFrom(final BesuNode eth1Node) {
