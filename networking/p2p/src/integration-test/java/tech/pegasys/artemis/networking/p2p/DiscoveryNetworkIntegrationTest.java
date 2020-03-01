eb9792ba6cd0747256a9a5640f5bdb46d72520db
Adrian Sutton <adrian.sutton@consensys.net>
2020-03-02 09:48:19 +1000

    Add discovery network integration test.

---
+++ networking/p2p/src/integration-test/java/tech/pegasys/artemis/networking/p2p/DiscoveryNetworkIntegrationTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2020 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.artemis.networking.p2p;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import tech.pegasys.artemis.network.p2p.DiscoveryNetworkFactory;
+import tech.pegasys.artemis.networking.p2p.peer.Peer;
+import tech.pegasys.artemis.util.Waiter;
+
+public class DiscoveryNetworkIntegrationTest {
+  private final DiscoveryNetworkFactory discoveryNetworkFactory = new DiscoveryNetworkFactory();
+
+  @Test
+  public void shouldConnectToStaticPeers() throws Exception {
+    final DiscoveryNetwork<Peer> network1 = discoveryNetworkFactory.builder().buildAndStart();
+    final DiscoveryNetwork<Peer> network2 =
+        discoveryNetworkFactory.builder().staticPeer(network1.getNodeAddress()).buildAndStart();
+    assertConnected(network1, network2);
+  }
+
+  @Test
+  public void shouldReconnectToStaticPeersAfterDisconnection() throws Exception {
+    final DiscoveryNetwork<Peer> network1 = discoveryNetworkFactory.builder().buildAndStart();
+    final DiscoveryNetwork<Peer> network2 =
+        discoveryNetworkFactory.builder().staticPeer(network1.getNodeAddress()).buildAndStart();
+    assertConnected(network1, network2);
+
+    // Peers disconnect
+    network1.getPeer(network2.getNodeId()).orElseThrow().disconnect();
+
+    // But are automatically reconnected
+    assertConnected(network1, network2);
+  }
+
+  @Test
+  public void shouldConnectToBootnodes() throws Exception {
+    final DiscoveryNetwork<Peer> network1 = discoveryNetworkFactory.builder().buildAndStart();
+    final DiscoveryNetwork<Peer> network2 =
+        discoveryNetworkFactory.builder().bootnode(network1.getEnr().orElseThrow()).buildAndStart();
+    assertConnected(network1, network2);
+  }
+
+  @Test
+  @Disabled("Discovery library still buggy")
+  public void shouldDiscoverPeers() throws Exception {
+    final DiscoveryNetwork<Peer> network1 = discoveryNetworkFactory.builder().buildAndStart();
+    final DiscoveryNetwork<Peer> network2 =
+        discoveryNetworkFactory.builder().bootnode(network1.getEnr().orElseThrow()).buildAndStart();
+    assertConnected(network1, network2);
+
+    // Only knows about network1, but should discovery network2
+    final DiscoveryNetwork<Peer> network3 =
+        discoveryNetworkFactory.builder().bootnode(network1.getEnr().orElseThrow()).buildAndStart();
+    assertConnected(network1, network3);
+    assertConnected(network2, network3);
+  }
+
+  private void assertConnected(
+      final DiscoveryNetwork<Peer> network1, final DiscoveryNetwork<Peer> network2) {
+    Waiter.waitFor(
+        () -> {
+          assertThat(network1.getPeer(network2.getNodeId())).isPresent();
+          assertThat(network2.getPeer(network1.getNodeId())).isPresent();
+        });
+  }
+}
