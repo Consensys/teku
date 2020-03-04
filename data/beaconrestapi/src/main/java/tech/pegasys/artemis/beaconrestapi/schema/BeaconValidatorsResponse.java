66ae4c2e7dbd8befa7af1b090c32ecd1e7f9b790
Sally MacFarlane <sally.macfarlane@consensys.net>
2020-03-04 11:09:31 +1000

    generalise
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- data/beaconrestapi/src/main/java/tech/pegasys/artemis/beaconrestapi/schema/BeaconValidatorsResponse.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/artemis/beaconrestapi/schema/BeaconValidatorsResponse.java
@@ -14,37 +14,34 @@
 package tech.pegasys.artemis.beaconrestapi.schema;
 
 import java.util.List;
-import tech.pegasys.artemis.datastructures.state.Validator;
 
 public class BeaconValidatorsResponse {
-  public final List<Validator> validatorList;
+  public final List<?> validatorList;
   private int totalSize;
   private int nextPageToken;
 
-  public BeaconValidatorsResponse(List<Validator> validatorList) {
-    this(validatorList, 20, 0);
+  public BeaconValidatorsResponse(List<?> list) {
+    this(list, 20, 0);
   }
 
-  public BeaconValidatorsResponse(
-      final List<Validator> validatorList, final int pageSize, final int pageToken) {
+  public BeaconValidatorsResponse(final List<?> list, final int pageSize, final int pageToken) {
     if (pageSize > 0 && pageToken >= 0) {
       int offset = pageToken * pageSize;
-      this.totalSize = validatorList.size();
-      if (offset >= validatorList.size()) {
+      this.totalSize = list.size();
+      if (offset >= list.size()) {
         this.validatorList = List.of();
         this.nextPageToken = 0;
         return;
       }
-      this.validatorList =
-          validatorList.subList(offset, Math.min(offset + pageSize, validatorList.size()));
-      if (totalSize == 0 || offset + pageSize >= validatorList.size()) {
+      this.validatorList = list.subList(offset, Math.min(offset + pageSize, list.size()));
+      if (totalSize == 0 || offset + pageSize >= list.size()) {
         this.nextPageToken = 0;
       } else {
         this.nextPageToken = pageToken + 1;
       }
     } else {
       this.validatorList = List.of();
-      this.totalSize = validatorList.size();
+      this.totalSize = list.size();
       this.nextPageToken = 0;
     }
   }
