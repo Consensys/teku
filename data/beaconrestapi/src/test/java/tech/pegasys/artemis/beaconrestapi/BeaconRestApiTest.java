e674ff1a9038376b0c1a3027755fd2b384488bd0
Sally MacFarlane <sally.macfarlane@consensys.net>
2020-03-05 12:56:30 +1000

    added /beacon/state_root endpoint
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- data/beaconrestapi/src/test/java/tech/pegasys/artemis/beaconrestapi/BeaconRestApiTest.java
+++ data/beaconrestapi/src/test/java/tech/pegasys/artemis/beaconrestapi/BeaconRestApiTest.java
@@ -27,6 +27,7 @@ import org.junit.jupiter.api.Test;
 import tech.pegasys.artemis.beaconrestapi.beaconhandlers.BeaconChainHeadHandler;
 import tech.pegasys.artemis.beaconrestapi.beaconhandlers.BeaconHeadHandler;
 import tech.pegasys.artemis.beaconrestapi.beaconhandlers.BeaconStateHandler;
+import tech.pegasys.artemis.beaconrestapi.beaconhandlers.BeaconStateRootHandler;
 import tech.pegasys.artemis.beaconrestapi.beaconhandlers.BeaconValidatorsHandler;
 import tech.pegasys.artemis.beaconrestapi.beaconhandlers.GenesisTimeHandler;
 import tech.pegasys.artemis.beaconrestapi.beaconhandlers.NodeSyncingHandler;
@@ -106,5 +107,10 @@ class BeaconRestApiTest {
   @Test
   public void RestApiShouldHaveBeaconValidatorsEndpoint() {
     verify(app).get(eq(BeaconValidatorsHandler.ROUTE), any(BeaconValidatorsHandler.class));
+  }
+
+  @Test
+  public void RestApiShouldHaveBeaconStateRootEndpoint() {
+    verify(app).get(eq(BeaconStateRootHandler.ROUTE), any(BeaconStateRootHandler.class));
   }
 }
