b31071ecba8bf6d60ddb75b9e925bb95d574c942
Sally MacFarlane <sally.macfarlane@consensys.net>
2020-03-12 13:59:50 +1000

    field names
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- data/provider/src/main/java/tech/pegasys/artemis/api/schema/ValidatorDuties.java
+++ data/provider/src/main/java/tech/pegasys/artemis/api/schema/ValidatorDuties.java
@@ -14,13 +14,14 @@
 package tech.pegasys.artemis.api.schema;
 
 public class ValidatorDuties {
-  public final Integer committeeIndex;
-  public final BLSPubKey publicKey;
-  public final Integer validatorIndex;
+  public final Integer attestation_committee_index;
+  public final BLSPubKey validator_pubkey;
+  public final Integer validator_index;
 
-  public ValidatorDuties(Integer committeeIndex, BLSPubKey publicKey, Integer validatorIndex) {
-    this.committeeIndex = committeeIndex;
-    this.publicKey = publicKey;
-    this.validatorIndex = validatorIndex;
+  public ValidatorDuties(
+      Integer attestation_committee_index, BLSPubKey validator_pubkey, Integer validator_index) {
+    this.attestation_committee_index = attestation_committee_index;
+    this.validator_pubkey = validator_pubkey;
+    this.validator_index = validator_index;
   }
 }
