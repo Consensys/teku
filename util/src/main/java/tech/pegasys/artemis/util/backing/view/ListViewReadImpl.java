878a03fc43d41d8c396845dd26c67b406d493014
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-03-12 14:31:26 +0300

    Temp commit

--- util/src/main/java/tech/pegasys/artemis/util/backing/view/ListViewReadImpl.java
+++ util/src/main/java/tech/pegasys/artemis/util/backing/view/ListViewReadImpl.java
@@ -1,17 +1,47 @@
 package tech.pegasys.artemis.util.backing.view;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Arrays;
 import tech.pegasys.artemis.util.backing.ContainerViewWrite;
 import tech.pegasys.artemis.util.backing.ListViewRead;
 import tech.pegasys.artemis.util.backing.ListViewWrite;
+import tech.pegasys.artemis.util.backing.VectorViewWrite;
 import tech.pegasys.artemis.util.backing.ViewRead;
 import tech.pegasys.artemis.util.backing.tree.TreeNode;
+import tech.pegasys.artemis.util.backing.type.BasicViewTypes;
+import tech.pegasys.artemis.util.backing.type.ContainerViewType;
 import tech.pegasys.artemis.util.backing.type.ListViewType;
+import tech.pegasys.artemis.util.backing.view.BasicViews.UInt64View;
 
 public class ListViewReadImpl<C extends ViewRead> implements ListViewRead<C> {
 
   private final ContainerViewWrite container;
-  private final AtomicInteger size;
+  private final int size;
+  private final ListViewType<C> type;
+  private final VectorViewWrite<C> vector;
+
+  @SuppressWarnings("unchecked")
+  public ListViewReadImpl(ListViewType<C> type, TreeNode node) {
+    ContainerViewType<ContainerViewWrite> containerViewType =
+        new ContainerViewType<>(
+            Arrays.asList(type.getCompatibleVectorType(), BasicViewTypes.UINT64_TYPE),
+            MutableContainerImpl::new);
+    this.type = type;
+    this.container = containerViewType.createFromBackingNode(node);
+    this.size = getSizeFromTree();
+    this.vector = (VectorViewWrite<C>) container.get(0);
+
+  }
+
+  private int getSizeFromTree() {
+    UInt64View sizeView = (UInt64View) container.get(1);
+    return sizeView.get().intValue();
+  }
+
+  @Override
+  public C get(int index) {
+    checkIndex(index);
+    return vector.get(index);
+  }
 
   @Override
   public ListViewWrite<C> createWritableCopy() {
@@ -20,21 +50,24 @@ public class ListViewReadImpl<C extends ViewRead> implements ListViewRead<C> {
 
   @Override
   public ListViewType<C> getType() {
-    return null;
+    return type;
   }
 
   @Override
   public int size() {
-    return 0;
-  }
-
-  @Override
-  public C get(int index) {
-    return null;
+    return size;
   }
 
   @Override
   public TreeNode getBackingNode() {
-    return null;
+    return container.getBackingNode();
+  }
+
+  protected void checkIndex(int index) {
+    if (index >= size()) {
+      throw new IndexOutOfBoundsException(
+          "Invalid index " + index + " for list with size " + size());
+    }
   }
 }
+
