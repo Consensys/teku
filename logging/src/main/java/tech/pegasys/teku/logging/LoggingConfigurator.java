413cf66e6050b880ed5f0f8e6b02d3163a5fb570
Christopher Hare <chris.hare@consensys.net>
2020-03-12 17:09:16 +1000

    Logging to both

--- logging/src/main/java/tech/pegasys/teku/logging/LoggingConfigurator.java
+++ logging/src/main/java/tech/pegasys/teku/logging/LoggingConfigurator.java
@@ -14,7 +14,15 @@
 package tech.pegasys.teku.logging;
 
 import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.Layout;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.ConsoleAppender;
+import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.LoggerConfig;
+import org.apache.logging.log4j.core.layout.PatternLayout;
 
 public class LoggingConfigurator {
 
@@ -22,10 +30,13 @@ public class LoggingConfigurator {
   public static final String STATUS_LOGGER_NAME = "teku-status-log";
 
   private static final String CONSOLE_APPENDER_NAME = "teku-console";
+  private static final String CONSOLE_FORMAT = "%d{HH:mm:ss.SSS} [%-5level] - %msg%n";
+  private static final boolean ADDITIVITY = true;
 
   private static LoggingDestination DESTINATION;
   private static boolean COLOR;
   private static boolean INCLUDE_EVENTS;
+  private static Level LOG_LEVEL = Level.INFO;
 
   public static boolean isColorEnabled() {
     return COLOR;
@@ -35,6 +46,8 @@ public class LoggingConfigurator {
     // TODO try the Status logger instead of sop
     System.out.println("Setting logging level to " + level.name());
     Configurator.setAllLevels("", level);
+
+    LOG_LEVEL = level;
   }
 
   public static void setDestination(final LoggingDestination destination) {
@@ -55,5 +68,29 @@ public class LoggingConfigurator {
 
     // TODO file appender
 
+    final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+    final Configuration config = ctx.getConfiguration();
+    final Layout<?> layout =
+        PatternLayout.newBuilder()
+            .withConfiguration(ctx.getConfiguration())
+            .withPattern(CONSOLE_FORMAT)
+            .build();
+    final Appender consoleAppender =
+        ConsoleAppender.newBuilder().setName(CONSOLE_APPENDER_NAME).setLayout(layout).build();
+
+    consoleAppender.start();
+    config.addAppender(consoleAppender);
+
+    final LoggerConfig eventConsoleLogger =
+        new LoggerConfig(EVENT_LOGGER_NAME, LOG_LEVEL, ADDITIVITY);
+    eventConsoleLogger.addAppender(consoleAppender, LOG_LEVEL, null);
+    config.addLogger(EVENT_LOGGER_NAME, eventConsoleLogger);
+
+    final LoggerConfig statusConsoleLogger =
+        new LoggerConfig(STATUS_LOGGER_NAME, LOG_LEVEL, ADDITIVITY);
+    eventConsoleLogger.addAppender(consoleAppender, LOG_LEVEL, null);
+    config.addLogger(STATUS_LOGGER_NAME, statusConsoleLogger);
+
+    ctx.updateLoggers();
   }
 }
