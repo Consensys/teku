243ac39cd1a2455f4ddccc2ff3648bdce2895758
Paul Harris <paul.harris@consensys.net>
2020-03-20 12:33:08 +1000

    Add PostAttestation handling, to submit a signed Attestation. (#1420)
    * Add PostAttestation handling, to submit a signed Attestation.
    
    Added minimal validation to at least ensure the signature is not zero.
    
    addresses #1357
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- data/beaconrestapi/src/main/java/tech/pegasys/artemis/beaconrestapi/BeaconRestApi.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/artemis/beaconrestapi/BeaconRestApi.java
@@ -54,6 +54,7 @@ import tech.pegasys.artemis.beaconrestapi.handlers.node.GetSyncing;
 import tech.pegasys.artemis.beaconrestapi.handlers.node.GetVersion;
 import tech.pegasys.artemis.beaconrestapi.handlers.validator.GetAttestation;
 import tech.pegasys.artemis.beaconrestapi.handlers.validator.GetNewBlock;
+import tech.pegasys.artemis.beaconrestapi.handlers.validator.PostAttestation;
 import tech.pegasys.artemis.beaconrestapi.handlers.validator.PostDuties;
 import tech.pegasys.artemis.provider.JsonProvider;
 import tech.pegasys.artemis.util.cli.VersionProvider;
@@ -188,6 +189,7 @@ public class BeaconRestApi {
     app.get(GetValidators.ROUTE, new GetValidators(provider, jsonProvider));
     app.get(GetNewBlock.ROUTE, new GetNewBlock(dataProvider, jsonProvider));
 
+    app.post(PostAttestation.ROUTE, new PostAttestation(dataProvider, jsonProvider));
     app.post(PostDuties.ROUTE, new PostDuties(provider, jsonProvider));
   }
 
