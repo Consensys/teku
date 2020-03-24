4c109c4402e017da6b47cae2c923afa588d9a390
Adrian Sutton <adrian.sutton@consensys.net>
2020-03-25 08:03:59 +1000

    Add create block API to ValidatorApiChannel (#1459)
    Switch ValidatorCoordinator and REST API over to using it.

--- validator/api/src/main/java/tech/pegasys/artemis/validator/api/ValidatorApiChannel.java
+++ validator/api/src/main/java/tech/pegasys/artemis/validator/api/ValidatorApiChannel.java
@@ -16,10 +16,16 @@ package tech.pegasys.artemis.validator.api;
 import com.google.common.primitives.UnsignedLong;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import tech.pegasys.artemis.datastructures.blocks.BeaconBlock;
 import tech.pegasys.artemis.util.async.SafeFuture;
 import tech.pegasys.artemis.util.bls.BLSPublicKey;
+import tech.pegasys.artemis.util.bls.BLSSignature;
 
 public interface ValidatorApiChannel {
   SafeFuture<List<ValidatorDuties>> getDuties(
       UnsignedLong epoch, Collection<BLSPublicKey> publicKeys);
+
+  SafeFuture<Optional<BeaconBlock>> createUnsignedBlock(
+      UnsignedLong slot, BLSSignature randaoReveal);
 }
