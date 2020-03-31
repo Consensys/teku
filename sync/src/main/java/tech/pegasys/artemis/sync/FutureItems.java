05eea4d890ece90fbcf46a247d865c68991196f0
mbaxter <meredith.baxter@consensys.net>
2020-03-31 10:52:18 -0400

    [Minor] Make LRUCache use LRU strategy (#1505)

--- sync/src/main/java/tech/pegasys/artemis/sync/FutureItems.java
+++ sync/src/main/java/tech/pegasys/artemis/sync/FutureItems.java
@@ -23,8 +23,8 @@ import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.function.Function;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import tech.pegasys.artemis.util.collections.LimitedSet;
-import tech.pegasys.artemis.util.collections.LimitedSet.Mode;
+import tech.pegasys.artemis.util.collections.ConcurrentLimitedSet;
+import tech.pegasys.artemis.util.collections.LimitStrategy;
 
 /** Holds items with slots that are in the future relative to our node's current slot */
 class FutureItems<T> {
@@ -75,6 +75,7 @@ class FutureItems<T> {
   }
 
   private Set<T> createNewSet() {
-    return LimitedSet.create(MAX_ITEMS_PER_SLOT, Mode.DROP_LEAST_RECENTLY_ACCESSED);
+    return ConcurrentLimitedSet.create(
+        MAX_ITEMS_PER_SLOT, LimitStrategy.DROP_LEAST_RECENTLY_ACCESSED);
   }
 }
