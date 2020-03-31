3cb163280da35a3f2ab1199ed662c9d852cecfcf
Paul Harris <paul.harris@consensys.net>
2020-03-31 11:12:07 +1000

    bitlist and bitvector serializing using SSZ. (#1500)
    * bitlist and bitvector serializing using SSZ.
    
    As long as they are byte aligned, this works nicely, otherwise they get forced up to the byte boundary and you'll get extra bits padded at the end.
    
    addresses #1479
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- data/provider/src/main/java/tech/pegasys/artemis/provider/BitvectorDeserializer.java
+++ data/provider/src/main/java/tech/pegasys/artemis/provider/BitvectorDeserializer.java
@@ -14,7 +14,6 @@
 package tech.pegasys.artemis.provider;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import java.io.IOException;
@@ -23,10 +22,9 @@ import tech.pegasys.artemis.util.SSZTypes.Bitvector;
 
 public class BitvectorDeserializer extends JsonDeserializer<Bitvector> {
   @Override
-  public Bitvector deserialize(JsonParser p, DeserializationContext ctxt)
-      throws IOException, JsonProcessingException {
-    Bytes bytes = Bytes.fromHexString(p.getValueAsString());
-    int length = bytes.bitLength();
-    return Bitvector.fromBytes(bytes, length);
+  public Bitvector deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+    Bytes data = Bytes.fromHexString(p.getValueAsString());
+    int length = data.bitLength() + 1;
+    return Bitvector.fromBytes(data, length);
   }
 }
