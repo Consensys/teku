98413b9f29f95c2847ec4aaeec78fbd688b280f0
Sally MacFarlane <sally.macfarlane@consensys.net>
2020-04-07 12:20:19 +1000

    added peers metric (#1543)
    * added peers metric
    
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- networking/p2p/src/main/java/tech/pegasys/artemis/networking/p2p/libp2p/PeerManager.java
+++ networking/p2p/src/main/java/tech/pegasys/artemis/networking/p2p/libp2p/PeerManager.java
@@ -28,6 +28,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hyperledger.besu.plugin.services.MetricsSystem;
 import org.jetbrains.annotations.NotNull;
+import tech.pegasys.artemis.metrics.ArtemisMetricCategory;
 import tech.pegasys.artemis.networking.p2p.connection.ReputationManager;
 import tech.pegasys.artemis.networking.p2p.libp2p.rpc.RpcHandler;
 import tech.pegasys.artemis.networking.p2p.network.PeerHandler;
@@ -59,7 +60,8 @@ public class PeerManager implements ConnectionHandler {
     this.reputationManager = reputationManager;
     this.peerHandlers = peerHandlers;
     this.rpcHandlers = rpcHandlers;
-    // TODO - add metrics
+    metricsSystem.createGauge(
+        ArtemisMetricCategory.NETWORK, "libp2p_peers", "Total number of peers", this::getPeerCount);
   }
 
   @Override
