497f41ec01bd826409d73b10e7c025a8291a4096
Sally MacFarlane <sally.macfarlane@consensys.net>
2020-04-08 16:33:54 +1000

    BEACONCHAIN -> BEACON
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- artemis/src/test/java/tech/pegasys/artemis/cli/BeaconNodeCommandTest.java
+++ artemis/src/test/java/tech/pegasys/artemis/cli/BeaconNodeCommandTest.java
@@ -176,7 +176,7 @@ public class BeaconNodeCommandTest {
       "--metrics-enabled", "false",
       "--metrics-port", "8008",
       "--metrics-interface", "127.0.0.1",
-      "--metrics-categories", "BEACONCHAIN,JVM,PROCESS",
+      "--metrics-categories", "BEACON,JVM,PROCESS",
       "--data-path", dataPath.toString(),
       "--data-storage-mode", "prune",
       "--rest-api-port", "5051",
@@ -226,7 +226,7 @@ public class BeaconNodeCommandTest {
         .setMetricsEnabled(false)
         .setMetricsPort(8008)
         .setMetricsInterface("127.0.0.1")
-        .setMetricsCategories(Arrays.asList("BEACONCHAIN", "JVM", "PROCESS"))
+        .setMetricsCategories(Arrays.asList("BEACON", "JVM", "PROCESS"))
         .setLogColorEnabled(true)
         .setLogDestination("default_of_both")
         .setLogFile("teku.log")
