7e1a82da9a26e70928bc49973140a31a0ca2a39e
Adrian Sutton <adrian.sutton@consensys.net>
2020-04-15 06:46:59 +1000

    Prune duties from previous slots (#1582)

--- validator/client/src/main/java/tech/pegasys/artemis/validator/client/DutyScheduler.java
+++ validator/client/src/main/java/tech/pegasys/artemis/validator/client/DutyScheduler.java
@@ -19,8 +19,6 @@ import static tech.pegasys.artemis.datastructures.util.BeaconStateUtil.compute_e
 import com.google.common.primitives.UnsignedLong;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 import org.apache.logging.log4j.LogManager;
@@ -34,37 +32,27 @@ import tech.pegasys.artemis.util.config.Constants;
 import tech.pegasys.artemis.validator.api.ValidatorApiChannel;
 import tech.pegasys.artemis.validator.api.ValidatorDuties;
 import tech.pegasys.artemis.validator.api.ValidatorTimingChannel;
-import tech.pegasys.artemis.validator.client.duties.AggregationDuty;
-import tech.pegasys.artemis.validator.client.duties.AttestationProductionDuty;
-import tech.pegasys.artemis.validator.client.duties.BlockProductionDuty;
-import tech.pegasys.artemis.validator.client.duties.Duty;
-import tech.pegasys.artemis.validator.client.duties.ValidatorDutyFactory;
+import tech.pegasys.artemis.validator.client.duties.ScheduledDuties;
 
 public class DutyScheduler implements ValidatorTimingChannel {
   private static final Logger LOG = LogManager.getLogger();
-  final AtomicReference<UnsignedLong> latestScheduledEpoch = new AtomicReference<>();
-  private final ConcurrentMap<UnsignedLong, BlockProductionDuty> blockProposalDuties =
-      new ConcurrentHashMap<>();
-  private final ConcurrentMap<UnsignedLong, AttestationProductionDuty> attestationProposalDuties =
-      new ConcurrentHashMap<>();
-  private final ConcurrentMap<UnsignedLong, AggregationDuty> aggregationDuties =
-      new ConcurrentHashMap<>();
+  private final AtomicReference<UnsignedLong> latestScheduledEpoch = new AtomicReference<>();
+  private final ScheduledDuties scheduledDuties;
   private final AsyncRunner asyncRunner;
   private final ValidatorApiChannel validatorApiChannel;
   private final ForkProvider forkProvider;
-  private final ValidatorDutyFactory dutyFactory;
   private final Map<BLSPublicKey, Validator> validators;
 
   public DutyScheduler(
       final AsyncRunner asyncRunner,
       final ValidatorApiChannel validatorApiChannel,
       final ForkProvider forkProvider,
-      final ValidatorDutyFactory dutyFactory,
+      final ScheduledDuties scheduledDuties,
       final Map<BLSPublicKey, Validator> validators) {
     this.asyncRunner = asyncRunner;
     this.validatorApiChannel = validatorApiChannel;
     this.forkProvider = forkProvider;
-    this.dutyFactory = dutyFactory;
+    this.scheduledDuties = scheduledDuties;
     this.validators = validators;
   }
 
@@ -130,7 +118,7 @@ public class DutyScheduler implements ValidatorTimingChannel {
   }
 
   private void scheduleBlockProduction(final Validator validator, final UnsignedLong slot) {
-    blockProposalDuties.put(slot, dutyFactory.createBlockProductionDuty(validator, slot));
+    scheduledDuties.scheduleBlockProduction(slot, validator);
   }
 
   private void scheduleAttestationDuties(
@@ -158,12 +146,11 @@ public class DutyScheduler implements ValidatorTimingChannel {
       final int attestationCommitteePosition,
       final Validator validator,
       final UnsignedLong slot) {
-    return attestationProposalDuties
-        .computeIfAbsent(slot, dutyFactory::createAttestationProductionDuty)
-        .addValidator(validator, attestationCommitteeIndex, attestationCommitteePosition);
+    return scheduledDuties.scheduleAttestationProduction(
+        slot, validator, attestationCommitteeIndex, attestationCommitteePosition);
   }
 
-  public void scheduleAggregation(
+  private void scheduleAggregation(
       final int attestationCommitteeIndex,
       final int validatorIndex,
       final Validator validator,
@@ -176,13 +163,12 @@ public class DutyScheduler implements ValidatorTimingChannel {
         .finish(
             slotSignature -> {
               if (CommitteeUtil.isAggregator(slotSignature, aggregatorModulo)) {
-                aggregationDuties
-                    .computeIfAbsent(slot, dutyFactory::createAggregationDuty)
-                    .addValidator(
-                        validatorIndex,
-                        slotSignature,
-                        attestationCommitteeIndex,
-                        unsignedAttestationFuture);
+                scheduledDuties.scheduleAggregationDuties(
+                    slot,
+                    validatorIndex,
+                    slotSignature,
+                    attestationCommitteeIndex,
+                    unsignedAttestationFuture);
               }
             },
             error -> LOG.error("Failed to schedule aggregation duties", error));
@@ -190,28 +176,16 @@ public class DutyScheduler implements ValidatorTimingChannel {
 
   @Override
   public void onBlockProductionDue(final UnsignedLong slot) {
-    performDutyForSlot(blockProposalDuties, slot);
+    scheduledDuties.produceBlock(slot);
   }
 
   @Override
   public void onAttestationCreationDue(final UnsignedLong slot) {
-    performDutyForSlot(attestationProposalDuties, slot);
+    scheduledDuties.produceAttestations(slot);
   }
 
   @Override
   public void onAttestationAggregationDue(final UnsignedLong slot) {
-    performDutyForSlot(aggregationDuties, slot);
-  }
-
-  public void performDutyForSlot(
-      final Map<UnsignedLong, ? extends Duty> duties, final UnsignedLong slot) {
-    final Duty duty = duties.remove(slot);
-    if (duty == null) {
-      return;
-    }
-    duty.performDuty()
-        .finish(
-            () -> LOG.trace("{} completed successfully", duty::describe),
-            error -> LOG.error(duty.describe() + " failed", error));
+    scheduledDuties.performAggregation(slot);
   }
 }
---
+++ validator/client/src/test/java/tech/pegasys/artemis/validator/client/duties/ScheduledDutiesTest.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2020 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.artemis.validator.client.duties;
+
+import static com.google.common.primitives.UnsignedLong.ONE;
+import static com.google.common.primitives.UnsignedLong.ZERO;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static tech.pegasys.artemis.util.async.FutureUtil.ignoreFuture;
+
+import com.google.common.primitives.UnsignedLong;
+import org.junit.jupiter.api.Test;
+import tech.pegasys.artemis.bls.BLSSignature;
+import tech.pegasys.artemis.util.async.SafeFuture;
+import tech.pegasys.artemis.validator.client.Validator;
+
+class ScheduledDutiesTest {
+
+  public static final UnsignedLong TWO = UnsignedLong.valueOf(2);
+  private final Validator validator = mock(Validator.class);
+  private final ValidatorDutyFactory dutyFactory = mock(ValidatorDutyFactory.class);
+
+  private final ScheduledDuties duties = new ScheduledDuties(dutyFactory);
+
+  @Test
+  public void shouldDiscardMissedBlockProductionDuties() {
+    final BlockProductionDuty duty0 = mockDuty(BlockProductionDuty.class);
+    final BlockProductionDuty duty1 = mockDuty(BlockProductionDuty.class);
+    final BlockProductionDuty duty2 = mockDuty(BlockProductionDuty.class);
+    when(dutyFactory.createBlockProductionDuty(ZERO, validator)).thenReturn(duty0);
+    when(dutyFactory.createBlockProductionDuty(ONE, validator)).thenReturn(duty1);
+    when(dutyFactory.createBlockProductionDuty(TWO, validator)).thenReturn(duty2);
+    duties.scheduleBlockProduction(ZERO, validator);
+    duties.scheduleBlockProduction(ONE, validator);
+    duties.scheduleBlockProduction(TWO, validator);
+
+    duties.produceBlock(ONE);
+    verify(duty1).performDuty();
+
+    // Duty from slot zero was dropped
+    duties.produceBlock(ZERO);
+    verify(duty0, never()).performDuty();
+
+    // But the duty for slot 2 is still performed as scheduled
+    duties.produceBlock(TWO);
+    verify(duty2).performDuty();
+  }
+
+  @Test
+  public void shouldDiscardMissedAttestationProductionDuties() {
+    final AttestationProductionDuty duty0 = mockDuty(AttestationProductionDuty.class);
+    final AttestationProductionDuty duty1 = mockDuty(AttestationProductionDuty.class);
+    final AttestationProductionDuty duty2 = mockDuty(AttestationProductionDuty.class);
+    when(dutyFactory.createAttestationProductionDuty(ZERO)).thenReturn(duty0);
+    when(dutyFactory.createAttestationProductionDuty(ONE)).thenReturn(duty1);
+    when(dutyFactory.createAttestationProductionDuty(TWO)).thenReturn(duty2);
+
+    ignoreFuture(duties.scheduleAttestationProduction(ZERO, validator, 0, 0));
+    ignoreFuture(duties.scheduleAttestationProduction(ONE, validator, 0, 0));
+    ignoreFuture(duties.scheduleAttestationProduction(TWO, validator, 0, 0));
+
+    duties.produceAttestations(ONE);
+    verify(duty1).performDuty();
+
+    // Duty from slot zero was dropped
+    duties.produceAttestations(ZERO);
+    verify(duty0, never()).performDuty();
+
+    // But the duty for slot 2 is still performed as scheduled
+    duties.produceAttestations(TWO);
+    verify(duty2).performDuty();
+  }
+
+  @Test
+  public void shouldDiscardMissedAggregationDuties() {
+    final AggregationDuty duty0 = mockDuty(AggregationDuty.class);
+    final AggregationDuty duty1 = mockDuty(AggregationDuty.class);
+    final AggregationDuty duty2 = mockDuty(AggregationDuty.class);
+    when(dutyFactory.createAggregationDuty(ZERO)).thenReturn(duty0);
+    when(dutyFactory.createAggregationDuty(ONE)).thenReturn(duty1);
+    when(dutyFactory.createAggregationDuty(TWO)).thenReturn(duty2);
+
+    duties.scheduleAggregationDuties(ZERO, 0, BLSSignature.empty(), 0, new SafeFuture<>());
+    duties.scheduleAggregationDuties(ONE, 0, BLSSignature.empty(), 0, new SafeFuture<>());
+    duties.scheduleAggregationDuties(TWO, 0, BLSSignature.empty(), 0, new SafeFuture<>());
+
+    duties.performAggregation(ONE);
+    verify(duty1).performDuty();
+
+    // Duty from slot zero was dropped
+    duties.performAggregation(ZERO);
+    verify(duty0, never()).performDuty();
+
+    // But the duty for slot 2 is still performed as scheduled
+    duties.performAggregation(TWO);
+    verify(duty2).performDuty();
+  }
+
+  private <T extends Duty> T mockDuty(final Class<T> dutyType) {
+    final T mockDuty = mock(dutyType);
+    when(mockDuty.performDuty()).thenReturn(new SafeFuture<>());
+    return mockDuty;
+  }
+}
