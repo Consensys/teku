cde2d2bdb66cfa2ab8fdb348fc4760f5e296bb0c
Meredith Baxter <meredith.baxter@consensys.net>
2020-04-15 11:19:07 -0400

    Add status logging around chain data initialization

--- storage/src/main/java/tech/pegasys/artemis/storage/client/StorageBackedRecentChainData.java
+++ storage/src/main/java/tech/pegasys/artemis/storage/client/StorageBackedRecentChainData.java
@@ -13,6 +13,8 @@
 
 package tech.pegasys.artemis.storage.client;
 
+import static tech.pegasys.teku.logging.StatusLogger.STATUS_LOG;
+
 import com.google.common.eventbus.EventBus;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
@@ -51,14 +53,14 @@ public class StorageBackedRecentChainData extends RecentChainData {
   }
 
   private SafeFuture<RecentChainData> initializeFromStorage() {
-    LOG.trace("Begin initializing ChainStorageClient from storage");
+    STATUS_LOG.beginInitializingChainData();
     return requestInitialStore()
         .thenApply(
             maybeStore -> {
               maybeStore.ifPresent(
                   (store) -> {
                     this.setStore(store);
-                    LOG.debug("Finish initializing ChainStorageClient from storage");
+                    STATUS_LOG.finishInitializingChainData();
                   });
               return this;
             });
