a37b75728ddf24ad9da06c6b3a9541fedb9bf672
Meredith Baxter <meredith.baxter@consensys.net>
2020-04-15 11:27:03 -0400

    Remove unused logger

--- storage/src/main/java/tech/pegasys/artemis/storage/client/StorageBackedRecentChainData.java
+++ storage/src/main/java/tech/pegasys/artemis/storage/client/StorageBackedRecentChainData.java
@@ -18,8 +18,6 @@ import static tech.pegasys.teku.logging.StatusLogger.STATUS_LOG;
 import com.google.common.eventbus.EventBus;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import tech.pegasys.artemis.storage.Store;
 import tech.pegasys.artemis.storage.api.FinalizedCheckpointChannel;
 import tech.pegasys.artemis.storage.api.StorageUpdateChannel;
@@ -28,7 +26,6 @@ import tech.pegasys.artemis.util.async.SafeFuture;
 import tech.pegasys.artemis.util.config.Constants;
 
 public class StorageBackedRecentChainData extends RecentChainData {
-  private static final Logger LOG = LogManager.getLogger();
   private final AsyncRunner asyncRunner;
 
   public StorageBackedRecentChainData(
