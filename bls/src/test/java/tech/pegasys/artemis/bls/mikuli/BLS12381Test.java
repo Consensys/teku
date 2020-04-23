8f598c2fa48e6e7e17fd44940daec20c97867085
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-04-23 21:03:08 +0300

    Add batch BLS verify related unit tests

--- bls/src/test/java/tech/pegasys/artemis/bls/mikuli/BLS12381Test.java
+++ bls/src/test/java/tech/pegasys/artemis/bls/mikuli/BLS12381Test.java
@@ -17,11 +17,18 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Streams;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import org.apache.milagro.amcl.BLS381.BIG;
 import org.apache.tuweni.bytes.Bytes;
 import org.junit.jupiter.api.Test;
+import tech.pegasys.artemis.bls.mikuli.BLS12381.BatchSemiAggregate;
 
 class BLS12381Test {
 
@@ -127,5 +134,133 @@ class BLS12381Test {
     Signature aggregatedSignature = BLS12381.aggregate(signatures);
 
     assertFalse(BLS12381.aggregateVerify(publicKeys, messages, aggregatedSignature));
+  }
+
+  @Test
+  void batchVerifyTest() {
+    List<KeyPair> keys =
+        IntStream.range(0, 8).mapToObj(KeyPair::random).collect(Collectors.toList());
+    List<List<PublicKey>> pubKeys =
+        keys.stream()
+            .map(k -> Collections.singletonList(k.publicKey()))
+            .collect(Collectors.toList());
+    List<Bytes> messages =
+        IntStream.range(0, 8)
+            .mapToObj(i -> Bytes.wrap(("Hey " + i).getBytes()))
+            .collect(Collectors.toList());
+    List<Signature> signatures =
+        Streams.zip(keys.stream(), messages.stream(), (k, m) -> BLS12381.sign(k.secretKey(), m))
+            .collect(Collectors.toList());
+
+    Bytes aggrSigMsg = messages.get(0);
+    Signature aggrSig =
+        BLS12381.aggregate(
+            keys.stream().limit(3).map(k -> BLS12381.sign(k.secretKey(), aggrSigMsg)));
+    List<PublicKey> aggrSigPubkeys =
+        keys.stream().limit(3).map(KeyPair::publicKey).collect(Collectors.toList());
+
+    int aggrIndex = pubKeys.size();
+    pubKeys.add(aggrSigPubkeys);
+    messages.add(aggrSigMsg);
+    signatures.add(aggrSig);
+
+    int invalidIndex = pubKeys.size();
+    pubKeys.add(pubKeys.get(0));
+    messages.add(messages.get(0));
+    signatures.add(Signature.random(0));
+
+    // positive simple case
+    for (int sigCnt = 0; sigCnt < 6; sigCnt++) {
+      List<BatchSemiAggregate> semiAggr =
+          IntStream.range(0, sigCnt)
+              .mapToObj(
+                  i ->
+                      BLS12381.prepareBatchVerify(
+                          i, pubKeys.get(i), messages.get(i), signatures.get(i)))
+              .collect(Collectors.toList());
+      assertTrue(BLS12381.completeBatchVerify(semiAggr));
+    }
+
+    // positive simple case with aggr sig
+    for (int aggrPos = 0; aggrPos < 4; aggrPos++) {
+      int finalAggrPos = aggrPos;
+      List<BatchSemiAggregate> semiAggr =
+          IntStream.range(0, 4)
+              .map(i -> i == finalAggrPos ? aggrIndex : i)
+              .mapToObj(
+                  i ->
+                      BLS12381.prepareBatchVerify(
+                          i, pubKeys.get(i), messages.get(i), signatures.get(i)))
+              .collect(Collectors.toList());
+      assertTrue(BLS12381.completeBatchVerify(semiAggr));
+    }
+
+    // positive prepareBatchVerify2 test
+    for (int sigCnt = 0; sigCnt < 8; sigCnt++) {
+      Stream<List<Integer>> pairsStream =
+          Lists.partition(IntStream.range(0, sigCnt).boxed().collect(Collectors.toList()), 2)
+              .stream();
+      List<BatchSemiAggregate> semiAggr =
+          pairsStream
+              .map(
+                  l -> {
+                    if (l.size() == 1) {
+                      return BLS12381.prepareBatchVerify(
+                          l.get(0),
+                          pubKeys.get(l.get(0)),
+                          messages.get(l.get(0)),
+                          signatures.get(l.get(0)));
+                    } else {
+                      return BLS12381.prepareBatchVerify2(
+                          l.get(0),
+                          pubKeys.get(l.get(0)),
+                          messages.get(l.get(0)),
+                          signatures.get(l.get(0)),
+                          pubKeys.get(l.get(1)),
+                          messages.get(l.get(1)),
+                          signatures.get(l.get(1)));
+                    }
+                  })
+              .collect(Collectors.toList());
+
+      assertTrue(BLS12381.completeBatchVerify(semiAggr));
+    }
+
+    // negative prepareBatchVerify2 test
+    for (int sigCnt = 0; sigCnt < 5; sigCnt++) {
+      int finalSigCnt = sigCnt;
+      Stream<List<Integer>> pairsStream =
+          Lists.partition(
+              IntStream.range(0, 5)
+                  .map(i -> i == finalSigCnt ? invalidIndex : i)
+                  .boxed()
+                  .collect(Collectors.toList()),
+              2)
+              .stream();
+      List<BatchSemiAggregate> semiAggr =
+          pairsStream
+              .map(
+                  l -> {
+                    if (l.size() == 1) {
+                      return BLS12381.prepareBatchVerify(
+                          l.get(0),
+                          pubKeys.get(l.get(0)),
+                          messages.get(l.get(0)),
+                          signatures.get(l.get(0)));
+                    } else {
+                      return BLS12381.prepareBatchVerify2(
+                          l.get(0),
+                          pubKeys.get(l.get(0)),
+                          messages.get(l.get(0)),
+                          signatures.get(l.get(0)),
+                          pubKeys.get(l.get(1)),
+                          messages.get(l.get(1)),
+                          signatures.get(l.get(1)));
+                    }
+                  })
+              .collect(Collectors.toList());
+
+      assertFalse(BLS12381.completeBatchVerify(semiAggr));
+    }
   }
 }
