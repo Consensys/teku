c04f323d9e30b22b302afeec0a51fad26d7dfcac
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-04-28 20:04:55 +0300

    Bump libp2p version to support the latest Noise spec updates. Add temporary flag to interop with Lighthouse until they update Noise

--- networking/p2p/src/main/java/tech/pegasys/artemis/networking/p2p/libp2p/LibP2PNetwork.java
+++ networking/p2p/src/main/java/tech/pegasys/artemis/networking/p2p/libp2p/LibP2PNetwork.java
@@ -111,6 +111,12 @@ public class LibP2PNetwork implements P2PNetwork<Peer> {
     // Setup peers
     peerManager = new PeerManager(metricsSystem, reputationManager, peerHandlers, rpcHandlers);
 
+    // temporary flag
+    // set true for Lighthouse compatibility
+    // set false for Prysm compatibility
+    // TODO remove when all clients adjust the same Noise spec
+    NoiseXXSecureChannel.setRustInteroperability(false);
+
     host =
         BuilderJKt.hostJ(
             Defaults.None,
