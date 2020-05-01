cd12a4737e85ba04ef295ca54a00ac21d8e0c546
Adrian Sutton <adrian.sutton@consensys.net>
2020-05-01 13:19:42 +1000

    Add network definition for schlesi.

--- util/src/main/java/tech/pegasys/artemis/util/config/NetworkDefinition.java
+++ util/src/main/java/tech/pegasys/artemis/util/config/NetworkDefinition.java
@@ -42,8 +42,20 @@ public class NetworkDefinition {
                       "https://github.com/eth2-clients/eth2-testnets/raw/master/prysm/Topaz(v0.11.1)/genesis.ssz")
                   .discoveryBootnodes(
                       "enr:-Ku4QAGwOT9StqmwI5LHaIymIO4ooFKfNkEjWa0f1P8OsElgBh2Ijb-GrD_-b9W4kcPFcwmHQEy5RncqXNqdpVo1heoBh2F0dG5ldHOIAAAAAAAAAACEZXRoMpAAAAAAAAAAAP__________gmlkgnY0gmlwhBLf22SJc2VjcDI1NmsxoQJxCnE6v_x2ekgY_uoE1rtwzvGy40mq9eD66XfHPBWgIIN1ZHCCD6A")
-                  .eth1DepositContractAddress(
-                      Eth1Address.fromHexString("0x5cA1e00004366Ac85f492887AAab12d0e6418876"))
+                  .eth1DepositContractAddress("0x5cA1e00004366Ac85f492887AAab12d0e6418876")
+                  .build())
+          .put(
+              "schlesi",
+              builder()
+                  .constants("mainnet")
+                  .snappyCompressionEnabled(true)
+                  .initialState("https://github.com/goerli/schlesi/raw/master/light/genesis.ssz")
+                  .discoveryBootnodes(
+                      "enr:-LK4QJ-6k6QytxOn7P9BdDZHXesHz3aaglpvo-VcTGc-rfr5H4DBzjQsjg6stZoy1H-p3yK21IISkJHe742QTVwRS_IEh2F0dG5ldHOIAAAAAAAAAACEZXRoMpCZJe_WAAAAAP__________gmlkgnY0gmlwhDMPd52Jc2VjcDI1NmsxoQINdLr6UY7y2CzshX4n_BbdYM1G40rpdEs84Mdoyv_ZyYN0Y3CCIyiDdWRwgiMo",
+                      "enr:-LK4QFO0gKFieMiNrUystSk5Xt7DmIgusloLudv-gH8Krjw9SsUDZRk---H-3hwvL9rMfsMcZwU6L5ezK2d1_dG0UgECh2F0dG5ldHOIAAAAAAAAAACEZXRoMpCZJe_WAAAAAP__________gmlkgnY0gmlwhDMPd52Jc2VjcDI1NmsxoQPNb3TG-iN0aGTagN4peO0SEkWKklJOvloWL0He8pnB_4N0Y3CCJRyDdWRwgiUc",
+                      "enr:-LK4QJS5Rn_kkA2MQpieVDUao5vkBj3kE15S_JJepGA9MNfndwHyfBWSjmAa5T_qvkGklrDiZXqlIAahXTm_eH_IXY8Ch2F0dG5ldHOIAAAAAAAAAACEZXRoMpCZJe_WAAAAAP__________gmlkgnY0gmlwhDMPd52Jc2VjcDI1NmsxoQOS1-hRSwsxLo2PH3RKtwWdjLdT1IMX2nqkQAlHs5E7LIN0Y3CCMsiDdWRwgi7g",
+                      "enr:-LK4QC08ftWworc3AQkYAtFSzUZpbSkRrgw74WrvKPFL3BbPBozhZx-gLHw8FeBzbi_0HDmZDWqZF-oF0b0W8Q8kHFELh2F0dG5ldHOIAQAAAAAAAACEZXRoMpCZJe_WAAAAAP__________gmlkgnY0gmlwhDMPd52Jc2VjcDI1NmsxoQJyLMVEG-_6ho3DR0iYvyEVbMyOJ4o2G-pIIEsNw80nn4N0Y3CCNLyDdWRwgjDU")
+                  .eth1DepositContractAddress("0xA15554BF93a052669B511ae29EA21f3581677ac5")
                   .build())
           .build();
 
@@ -150,8 +162,8 @@ public class NetworkDefinition {
       return this;
     }
 
-    public Builder eth1DepositContractAddress(final Eth1Address eth1Address) {
-      this.eth1DepositContractAddress = Optional.of(eth1Address);
+    public Builder eth1DepositContractAddress(final String eth1Address) {
+      this.eth1DepositContractAddress = Optional.of(Eth1Address.fromHexString(eth1Address));
       return this;
     }
 
