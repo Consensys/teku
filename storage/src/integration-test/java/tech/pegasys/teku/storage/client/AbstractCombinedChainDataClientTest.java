90ec46afef55aeafe14c1ea6587f7701fd1a5e78
Meredith Baxter <meredith.baxter@consensys.net>
2020-05-11 12:36:19 -0400

    Add CombinedChainData integration tests

---
+++ storage/src/integration-test/java/tech/pegasys/teku/storage/client/AbstractCombinedChainDataClientTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2020 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.storage.client;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static tech.pegasys.teku.datastructures.util.BeaconStateUtil.compute_start_slot_at_epoch;
+
+import com.google.common.primitives.UnsignedLong;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tech.pegasys.teku.bls.BLSKeyGenerator;
+import tech.pegasys.teku.bls.BLSKeyPair;
+import tech.pegasys.teku.core.ChainBuilder;
+import tech.pegasys.teku.datastructures.blocks.SignedBlockAndState;
+import tech.pegasys.teku.storage.InMemoryStorageSystem;
+
+public abstract class AbstractCombinedChainDataClientTest {
+
+  private static final List<BLSKeyPair> VALIDATOR_KEYS = BLSKeyGenerator.generateKeyPairs(2);
+
+  protected InMemoryStorageSystem storageSystem;
+  protected ChainUpdater chainUpdater;
+
+  @BeforeEach
+  public void setup() {
+    storageSystem = createStorageSystem();
+    chainUpdater =
+        new ChainUpdater(storageSystem.recentChainData(), ChainBuilder.create(VALIDATOR_KEYS));
+  }
+
+  protected abstract InMemoryStorageSystem createStorageSystem();
+
+  @Test
+  public void getStateAtSlot_shouldRetrieveLatestFinalizedState() {
+    final CombinedChainDataClient client = storageSystem.combinedChainDataClient();
+
+    final UnsignedLong finalizedEpoch = UnsignedLong.valueOf(2);
+    final UnsignedLong finalizedSlot = compute_start_slot_at_epoch(finalizedEpoch);
+
+    // Setup chain with finalized block
+    chainUpdater.initializeGenesis();
+    final SignedBlockAndState blockAtEpoch = chainUpdater.advanceChain(finalizedSlot);
+    final SignedBlockAndState finalizedBlock = chainUpdater.finalizeEpoch(finalizedEpoch);
+    chainUpdater.addNewBestBlock();
+
+    // Sanity check
+    assertThat(blockAtEpoch).isEqualTo(finalizedBlock);
+
+    assertThat(client.getStateAtSlot(finalizedSlot))
+        .isCompletedWithValue(Optional.of(blockAtEpoch.getState()));
+  }
+
+  @Test
+  public void getStateAtSlot_shouldRetrieveHeadState() {
+    final CombinedChainDataClient client = storageSystem.combinedChainDataClient();
+
+    final UnsignedLong finalizedEpoch = UnsignedLong.valueOf(2);
+    final UnsignedLong finalizedSlot = compute_start_slot_at_epoch(finalizedEpoch);
+
+    chainUpdater.initializeGenesis();
+    chainUpdater.advanceChain(finalizedSlot);
+    chainUpdater.finalizeEpoch(finalizedEpoch);
+    final SignedBlockAndState bestBlock = chainUpdater.addNewBestBlock();
+
+    assertThat(client.getStateAtSlot(bestBlock.getSlot()))
+        .isCompletedWithValue(Optional.of(bestBlock.getState()));
+  }
+
+  @Test
+  public void getStateAtSlot_shouldRetrieveRecentState() {
+    final CombinedChainDataClient client = storageSystem.combinedChainDataClient();
+
+    final UnsignedLong finalizedEpoch = UnsignedLong.valueOf(2);
+    final UnsignedLong finalizedSlot = compute_start_slot_at_epoch(finalizedEpoch);
+
+    chainUpdater.initializeGenesis();
+    chainUpdater.advanceChain(finalizedSlot);
+    chainUpdater.finalizeEpoch(finalizedEpoch);
+    final SignedBlockAndState recentBlock = chainUpdater.advanceChain();
+    final SignedBlockAndState bestBlock = chainUpdater.addNewBestBlock();
+    // Sanity check
+    assertThat(recentBlock.getSlot()).isLessThan(bestBlock.getSlot());
+
+    assertThat(client.getStateAtSlot(recentBlock.getSlot()))
+        .isCompletedWithValue(Optional.of(recentBlock.getState()));
+  }
+}
