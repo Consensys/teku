24e2a3444fcc9dfbfeae67808f0fbf8c378427a3
Nicolas MASSART <nicolas.massart@consensys.net>
2020-05-18 12:09:39 +0200

    add gh-pages branch content to docs/

---
+++ docs/generate-versions.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const fs = require('fs');
+const args = require('minimist')(process.argv.slice(2));
+
+const TAG = args.tag;
+const SPEC = args.spec
+
+const TARGET_SPEC = `${TAG == '' ? 'latest' : TAG}.json`;
+
+// copy of spec file and rename
+fs.copyFile(SPEC, TARGET_SPEC, (err) => {
+  if (err) throw err;
+  console.log(`${SPEC} copied to ${TARGET_SPEC}`);
+});
+
+const JSON_FILE = './versions.json';
+let versions = require(JSON_FILE);
+
+if(TAG != ''){
+  versions[TAG] = { spec: TAG, source: TAG };
+  versions['stable'] = { spec: TAG, source: TAG };
+  fs.writeFileSync(JSON_FILE, JSON.stringify(versions,null,1));
+}
