0f6103e79640003c176f0061d3b63f6c42e20f43
Meredith Baxter <meredith.baxter@consensys.net>
2020-05-18 15:38:35 -0400

    Add ability to "restart" InMemoryStorageSystem

--- storage/src/testFixtures/java/tech/pegasys/teku/storage/InMemoryStorageSystem.java
+++ storage/src/testFixtures/java/tech/pegasys/teku/storage/InMemoryStorageSystem.java
@@ -14,59 +14,82 @@
 package tech.pegasys.teku.storage;
 
 import com.google.common.eventbus.EventBus;
+import tech.pegasys.teku.storage.api.FinalizedCheckpointChannel;
+import tech.pegasys.teku.storage.api.StubFinalizedCheckpointChannel;
 import tech.pegasys.teku.storage.api.TrackingReorgEventChannel;
 import tech.pegasys.teku.storage.client.CombinedChainDataClient;
-import tech.pegasys.teku.storage.client.MemoryOnlyRecentChainData;
 import tech.pegasys.teku.storage.client.RecentChainData;
+import tech.pegasys.teku.storage.client.StorageBackedRecentChainData;
 import tech.pegasys.teku.storage.server.ChainStorageServer;
 import tech.pegasys.teku.storage.server.Database;
 import tech.pegasys.teku.storage.server.DatabaseFactory;
-import tech.pegasys.teku.storage.server.rocksdb.InMemoryRocksDbDatabase;
+import tech.pegasys.teku.storage.server.rocksdb.InMemoryRocksDbDatabaseFactory;
+import tech.pegasys.teku.storage.server.rocksdb.core.MockRocksDbInstance;
 import tech.pegasys.teku.util.config.StateStorageMode;
 
 public class InMemoryStorageSystem {
   private final EventBus eventBus;
   private final TrackingReorgEventChannel reorgEventChannel;
 
+  private final MockRocksDbInstance rocksDbInstance;
   private final RecentChainData recentChainData;
   private final CombinedChainDataClient combinedChainDataClient;
 
   public InMemoryStorageSystem(
       final EventBus eventBus,
       final TrackingReorgEventChannel reorgEventChannel,
+      final MockRocksDbInstance rocksDbInstance,
       final RecentChainData recentChainData,
       final CombinedChainDataClient combinedChainDataClient) {
     this.eventBus = eventBus;
     this.reorgEventChannel = reorgEventChannel;
+    this.rocksDbInstance = rocksDbInstance;
     this.recentChainData = recentChainData;
     this.combinedChainDataClient = combinedChainDataClient;
   }
 
   public static InMemoryStorageSystem createEmptyV3StorageSystem(
       final StateStorageMode storageMode) {
+    final MockRocksDbInstance rocksDbInstance =
+        InMemoryRocksDbDatabaseFactory.createEmptyV3RocksDbInstance();
+    return createV3StorageSystem(rocksDbInstance, storageMode);
+  }
+
+  private static InMemoryStorageSystem createV3StorageSystem(
+      final MockRocksDbInstance rocksDbInstance, final StateStorageMode storageMode) {
     try {
       final EventBus eventBus = new EventBus();
-      final Database database = InMemoryRocksDbDatabase.createEmptyV3(storageMode);
+      final Database database =
+          InMemoryRocksDbDatabaseFactory.createV3(rocksDbInstance, storageMode);
       final DatabaseFactory dbFactory = () -> database;
-      final ChainStorageServer chainStorageServer = ChainStorageServer.create(eventBus, dbFactory);
 
+      // Create and start storage server
+      final ChainStorageServer chainStorageServer = ChainStorageServer.create(eventBus, dbFactory);
+      chainStorageServer.start();
+
+      // Create recent chain data
+      final FinalizedCheckpointChannel finalizedCheckpointChannel =
+          new StubFinalizedCheckpointChannel();
       final TrackingReorgEventChannel reorgEventChannel = new TrackingReorgEventChannel();
       final RecentChainData recentChainData =
-          MemoryOnlyRecentChainData.builder()
-              .eventBus(eventBus)
-              .reorgEventChannel(reorgEventChannel)
-              .storageUpdateChannel(chainStorageServer)
-              .build();
+          StorageBackedRecentChainData.createImmediately(
+              chainStorageServer, finalizedCheckpointChannel, reorgEventChannel, eventBus);
 
+      // Create combined client
       final CombinedChainDataClient combinedChainDataClient =
           new CombinedChainDataClient(recentChainData, chainStorageServer);
 
-      chainStorageServer.start();
+      // Return storage system
       return new InMemoryStorageSystem(
-          eventBus, reorgEventChannel, recentChainData, combinedChainDataClient);
+          eventBus, reorgEventChannel, rocksDbInstance, recentChainData, combinedChainDataClient);
     } catch (Exception e) {
       throw new IllegalStateException("Unable to initialize storage system", e);
     }
+  }
+
+  public InMemoryStorageSystem restarted(final StateStorageMode storageMode) {
+    final MockRocksDbInstance restartedRocksDbInstance = rocksDbInstance.reopen();
+    return createV3StorageSystem(restartedRocksDbInstance, storageMode);
   }
 
   public RecentChainData recentChainData() {
