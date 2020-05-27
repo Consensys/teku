97e42177b895118d5a05088796488018286fdba4
Sally MacFarlane <sally.macfarlane@consensys.net>
2020-05-27 11:24:40 +1000

    refactoring and tests
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/BeaconRestApi.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/BeaconRestApi.java
@@ -13,14 +13,12 @@
 
 package tech.pegasys.teku.beaconrestapi;
 
-import static com.google.common.collect.Streams.stream;
 import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
 import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
 import static javax.servlet.http.HttpServletResponse.SC_NO_CONTENT;
+import static tech.pegasys.teku.beaconrestapi.HostWhitelistUtils.isHostAuthorized;
 
-import com.google.common.base.Splitter;
-import com.google.common.collect.Iterables;
 import com.google.common.io.Resources;
 import io.javalin.Javalin;
 import io.javalin.plugin.openapi.OpenApiOptions;
@@ -31,7 +29,6 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.Optional;
 import kotlin.text.Charsets;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
@@ -93,32 +90,11 @@ public class BeaconRestApi {
     app.before(
         (ctx) -> {
           String header = ctx.header("host");
-          Optional<String> optionalHost = getAndValidateHostHeader(header);
-          if (!configuration.getRestApiHostWhitelist().contains("*")
-              && (!optionalHost.isPresent()
-                  || !hostIsInWhitelist(configuration, optionalHost.get()))) {
+          if (!isHostAuthorized(configuration.getRestApiHostWhitelist(), header)) {
             ctx.status(SC_FORBIDDEN);
-            LOG.debug("Host not authorized " + optionalHost);
+            LOG.debug("Host not authorized " + header);
           }
         });
-  }
-
-  private Optional<String> getAndValidateHostHeader(final String hostHeader) {
-    final Iterable<String> splitHostHeader = Splitter.on(':').split(hostHeader);
-    final long hostPieces = stream(splitHostHeader).count();
-    if (hostPieces > 1) {
-      // If the host contains a colon, verify the host is correctly formed - host [ ":" port ]
-      if (hostPieces > 2 || !Iterables.get(splitHostHeader, 1).matches("\\d{1,5}+")) {
-        return Optional.empty();
-      }
-    }
-    return Optional.ofNullable(Iterables.get(splitHostHeader, 0));
-  }
-
-  private boolean hostIsInWhitelist(
-      final TekuConfiguration configuration, final String hostHeader) {
-    return configuration.getRestApiHostWhitelist().stream()
-        .anyMatch(whitelistEntry -> whitelistEntry.toLowerCase().equals(hostHeader.toLowerCase()));
   }
 
   private void addCustomErrorPages(final TekuConfiguration configuration) {
