6badcbbf7b7a43d3b279f0ca000cfe9d08cc34fc
Adrian Sutton <adrian.sutton@consensys.net>
2020-05-29 07:59:35 +1000

    Restore equals and hashcode.

--- ethereum/datastructures/src/main/java/tech/pegasys/teku/datastructures/networking/libp2p/rpc/EmptyMessage.java
+++ ethereum/datastructures/src/main/java/tech/pegasys/teku/datastructures/networking/libp2p/rpc/EmptyMessage.java
@@ -33,6 +33,22 @@ public class EmptyMessage implements RpcRequest, SimpleOffsetSerializable, SSZCo
   }
 
   @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return 0;
+  }
+
+  @Override
   public String toString() {
     return "EmptyMessage{}";
   }
