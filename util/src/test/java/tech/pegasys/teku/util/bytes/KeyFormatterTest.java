f6d989538248c64bdb0b761cf34880ca39b20575
Paul Harris <paul.harris@consensys.net>
2020-05-29 08:39:13 +1000

    spotless
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- util/src/test/java/tech/pegasys/teku/util/bytes/KeyFormatterTest.java
+++ util/src/test/java/tech/pegasys/teku/util/bytes/KeyFormatterTest.java
@@ -14,7 +14,6 @@
 package tech.pegasys.teku.util.bytes;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.apache.tuweni.bytes.Bytes;
