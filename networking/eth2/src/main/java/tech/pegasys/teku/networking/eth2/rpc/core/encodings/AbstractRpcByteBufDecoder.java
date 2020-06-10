30983e6d9cc4c80fe66ba6faf5ec2c332fed8d68
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-06-10 18:11:41 +0300

    Delegate refcounting to CompositeByteBuf as it may be retained with output ByteBuf.

--- networking/eth2/src/main/java/tech/pegasys/teku/networking/eth2/rpc/core/encodings/AbstractRpcByteBufDecoder.java
+++ networking/eth2/src/main/java/tech/pegasys/teku/networking/eth2/rpc/core/encodings/AbstractRpcByteBufDecoder.java
@@ -1,51 +1,53 @@
 package tech.pegasys.teku.networking.eth2.rpc.core.encodings;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.CompositeByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.util.ReferenceCounted;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import tech.pegasys.teku.networking.eth2.rpc.core.encodings.compression.exceptions.PayloadSmallerThanExpectedException;
 
 public abstract class AbstractRpcByteBufDecoder<TMessage>
     implements RpcByteBufDecoder<TMessage> {
 
-  private final List<ByteBuf> incompleteFrames = new ArrayList<>();
+  private CompositeByteBuf compositeByteBuf = Unpooled.compositeBuffer();
+
 
   public synchronized Optional<TMessage> decodeOneMessage(ByteBuf in) {
-    incompleteFrames.add(in.retainedSlice());
-    ByteBuf inBuf = Unpooled.wrappedBuffer(incompleteFrames.toArray(new ByteBuf[0]));
+    if (!in.isReadable()) {
+      return Optional.empty();
+    }
+    compositeByteBuf.addComponent(true, in.retainedSlice());
     try {
       Optional<TMessage> outBuf;
       while (true) {
-        int readerIndex = inBuf.readerIndex();
-        outBuf = decodeOneImpl(inBuf);
+        int readerIndex = compositeByteBuf.readerIndex();
+        outBuf = decodeOneImpl(compositeByteBuf);
         if (outBuf.isPresent()
-            || readerIndex == inBuf.readerIndex()
-            || inBuf.readableBytes() == 0) {
+            || readerIndex == compositeByteBuf.readerIndex()
+            || compositeByteBuf.readableBytes() == 0) {
           break;
         }
       }
       if (outBuf.isPresent()) {
-        incompleteFrames.forEach(ReferenceCounted::release);
-        incompleteFrames.clear();
-        in.skipBytes(in.readableBytes() - inBuf.readableBytes());
+        in.skipBytes(in.readableBytes() - compositeByteBuf.readableBytes());
+        compositeByteBuf.release();
+        compositeByteBuf = Unpooled.compositeBuffer();
       } else {
         in.skipBytes(in.readableBytes());
       }
       return outBuf;
     } catch (Throwable t) {
-      incompleteFrames.forEach(ReferenceCounted::release);
+      compositeByteBuf.release();
+      compositeByteBuf = Unpooled.compositeBuffer();
       throw t;
     }
   }
 
   @Override
   public void complete() {
-    if (incompleteFrames.isEmpty()) {
+    if (compositeByteBuf.isReadable()) {
       throw new PayloadSmallerThanExpectedException(
-          "Rpc stream complete, but unprocessed data left: " + incompleteFrames);
+          "Rpc stream complete, but unprocessed data left: " + compositeByteBuf.readableBytes());
     }
   }
 
