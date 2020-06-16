390b1d81e92187c1f04714a7f6aece0c980c0370
Jason Frame <jasonwframe@gmail.com>
2020-06-16 15:48:30 +1000

    Generate and register command (#2148)

--- teku/src/main/java/tech/pegasys/teku/cli/deposit/DepositGenerateAndRegisterCommand.java
+++ teku/src/main/java/tech/pegasys/teku/cli/deposit/DepositGenerateAndRegisterCommand.java
@@ -13,16 +13,27 @@
 
 package tech.pegasys.teku.cli.deposit;
 
+import static tech.pegasys.teku.logging.SubCommandLogger.SUB_COMMAND_LOG;
+
 import com.google.common.annotations.VisibleForTesting;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.jetbrains.annotations.NotNull;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Mixin;
 import picocli.CommandLine.Option;
+import tech.pegasys.teku.cli.deposit.GenerateAction.ValidatorKeys;
+import tech.pegasys.teku.util.async.SafeFuture;
 import tech.pegasys.teku.util.cli.PicoCliVersionProvider;
 
 @Command(
-    name = "generate",
-    description = "Generate validator keys",
+    name = "generate-and-register",
+    description =
+        "Register validators by generating new keys and sending deposit transactions to an Ethereum 1 node",
     mixinStandardHelpOptions = true,
     showDefaultValues = true,
     abbreviateSynopsis = true,
@@ -32,9 +43,10 @@ import tech.pegasys.teku.util.cli.PicoCliVersionProvider;
     optionListHeading = "%nOptions:%n",
     footerHeading = "%n",
     footer = "Teku is licensed under the Apache License 2.0")
-public class DepositGenerateCommand implements Runnable {
+public class DepositGenerateAndRegisterCommand implements Runnable {
   private final Consumer<Integer> shutdownFunction;
 
+  @Mixin private RegisterParams registerParams;
   @Mixin private GenerateParams generateParams;
 
   @Option(
@@ -44,17 +56,19 @@ public class DepositGenerateCommand implements Runnable {
       hidden = true)
   private boolean displayConfirmation = true;
 
-  public DepositGenerateCommand() {
+  public DepositGenerateAndRegisterCommand() {
     this.shutdownFunction =
-        System::exit; // required because web3j uses non-daemon threads which halts the program
+        System::exit; // required because web3j use non-daemon threads which halts the program
   }
 
   @VisibleForTesting
-  DepositGenerateCommand(
+  DepositGenerateAndRegisterCommand(
       final Consumer<Integer> shutdownFunction,
+      final RegisterParams registerParams,
       final GenerateParams generateParams,
       final boolean displayConfirmation) {
     this.shutdownFunction = shutdownFunction;
+    this.registerParams = registerParams;
     this.generateParams = generateParams;
     this.displayConfirmation = displayConfirmation;
   }
@@ -62,7 +76,28 @@ public class DepositGenerateCommand implements Runnable {
   @Override
   public void run() {
     final GenerateAction generateAction = generateParams.createGenerateAction(displayConfirmation);
-    generateAction.generateKeys();
+
+    try (final RegisterAction registerAction =
+        registerParams.createRegisterAction(displayConfirmation)) {
+      registerAction.displayConfirmation(generateParams.getValidatorCount());
+      final List<SafeFuture<TransactionReceipt>> transactionReceipts =
+          generateAction
+              .generateKeysStream()
+              .map(registerValidator(registerAction))
+              .collect(Collectors.toList());
+      SafeFuture.allOf(transactionReceipts.toArray(SafeFuture[]::new)).get(2, TimeUnit.MINUTES);
+    } catch (final Throwable t) {
+      SUB_COMMAND_LOG.sendDepositFailure(t);
+      shutdownFunction.accept(1);
+    }
     shutdownFunction.accept(0);
+  }
+
+  @NotNull
+  private Function<ValidatorKeys, SafeFuture<TransactionReceipt>> registerValidator(
+      final RegisterAction registerAction) {
+    return validatorKey ->
+        registerAction.sendDeposit(
+            validatorKey.getValidatorKey(), validatorKey.getWithdrawalKey().getPublicKey());
   }
 }
