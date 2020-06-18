7841b085a8ec8fdb7c2b602b60c8c59ae99bf2d6
Ben Edgington <ben@benjaminion.xyz>
2020-06-18 13:05:46 +0100

    Update comment

--- bls/src/main/java/tech/pegasys/teku/bls/hashToG2/JacobianPoint.java
+++ bls/src/main/java/tech/pegasys/teku/bls/hashToG2/JacobianPoint.java
@@ -145,7 +145,7 @@ final class JacobianPoint {
     FP2Immutable s1 = y1.mul(z2).mul(z2z2);
     FP2Immutable s2 = y2.mul(z1).mul(z1z1);
 
-    // Shortcut for x1 == x2 case. Either P == Q or P == -Q.
+    // Shortcut for equal X coordinates case. Either P == Q or P == -Q.
     if (u1.equals(u2)) {
       return s1.equals(s2) ? dbl() : INFINITY;
     }
