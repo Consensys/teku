bccc5afa1bd08f19425fe65556a1c47cf89a5580
Cem Ozer <cemozer2018@u.northwestern.edu>
2020-06-22 15:04:44 -0400

    Add altona network definition

--- util/src/main/java/tech/pegasys/teku/util/config/NetworkDefinition.java
+++ util/src/main/java/tech/pegasys/teku/util/config/NetworkDefinition.java
@@ -45,6 +45,15 @@ public class NetworkDefinition {
                   .eth1Endpoint("https://goerli.prylabs.net")
                   .startupTimeoutSeconds(120)
                   .build())
+          .put(
+              "altona",
+              builder()
+                  .constants("altona")
+                  .snappyCompressionEnabled(true)
+                  .eth1DepositContractAddress("0x16e82D77882A663454Ef92806b7DeCa1D394810f")
+                  .eth1Endpoint("https://goerli.prylabs.net")
+                  .startupTimeoutSeconds(120)
+                  .build())
           .build();
 
   private final String constants;
