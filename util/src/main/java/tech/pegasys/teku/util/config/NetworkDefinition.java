9c6cc1a3f36e89d9c01468ca5d4e5a8543e845c1
Adrian Sutton <adrian.sutton@consensys.net>
2020-06-28 12:33:20 +1000

    Add Altona genesis state and a bootnode we control (#2244)

--- util/src/main/java/tech/pegasys/teku/util/config/NetworkDefinition.java
+++ util/src/main/java/tech/pegasys/teku/util/config/NetworkDefinition.java
@@ -50,9 +50,11 @@ public class NetworkDefinition {
               builder()
                   .constants("altona")
                   .snappyCompressionEnabled(true)
+                  .initialStateFromClasspath("altona-genesis.ssz")
                   .eth1DepositContractAddress("0x16e82D77882A663454Ef92806b7DeCa1D394810f")
                   .startupTimeoutSeconds(120)
                   .discoveryBootnodes(
+                      "enr:-KG4QLkU_qJgdh-DhT5HtONGvuqwbbW99piCe27Bpk8oO_hvC0qEYgOSpmVfZ-8iJgW71HO23ajEFCNOCvEaqpcIXiUDhGV0aDKQ_co5sAAAASH__________4JpZIJ2NIJpcIQDE_KiiXNlY3AyNTZrMaEDbBoq33b6SAUh96EJZWNYOqYwjm78pVviDreLfK-aeUKDdGNwgiMog3VkcIIjKA",
                       "enr:-LK4QFtV7Pz4reD5a7cpfi1z6yPrZ2I9eMMU5mGQpFXLnLoKZW8TXvVubShzLLpsEj6aayvVO1vFx-MApijD3HLPhlECh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD6etXjAAABIf__________gmlkgnY0gmlwhDMPYfCJc2VjcDI1NmsxoQIerw_qBc9apYfZqo2awiwS930_vvmGnW2psuHsTzrJ8YN0Y3CCIyiDdWRwgiMo",
                       "enr:-LK4QPVkFd_MKzdW0219doTZryq40tTe8rwWYO75KDmeZM78fBskGsfCuAww9t8y3u0Q0FlhXOhjE1CWpx3SGbUaU80Ch2F0dG5ldHOIAAAAAAAAAACEZXRoMpD6etXjAAABIf__________gmlkgnY0gmlwhDMPRgeJc2VjcDI1NmsxoQNHu-QfNgzl8VxbMiPgv6wgAljojnqAOrN18tzJMuN8oYN0Y3CCIyiDdWRwgiMo")
                   .build())
