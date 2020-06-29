6727d6a45af71308c3b7fdc6b80951f058bfa5ab
Anton Nashatyrev <Nashatyrev@users.noreply.github.com>
2020-06-29 16:12:50 +0300

    Optimize memory:  keep BeaconState view caches via SoftReference (#2235)

--- eth-benchmark-tests/src/jmh/java/tech/pegasys/teku/benchmarks/ProfilingRun.java
+++ eth-benchmark-tests/src/jmh/java/tech/pegasys/teku/benchmarks/ProfilingRun.java
@@ -16,11 +16,12 @@ package tech.pegasys.teku.benchmarks;
 import static org.mockito.Mockito.mock;
 
 import com.google.common.eventbus.EventBus;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
-import org.apache.tuweni.bytes.Bytes;
-import org.apache.tuweni.bytes.Bytes32;
-import org.apache.tuweni.ssz.SSZ;
+import java.util.function.Consumer;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import tech.pegasys.teku.benchmarks.gen.BlockIO;
@@ -32,6 +33,8 @@ import tech.pegasys.teku.core.results.BlockImportResult;
 import tech.pegasys.teku.datastructures.blocks.SignedBeaconBlock;
 import tech.pegasys.teku.datastructures.state.BeaconState;
 import tech.pegasys.teku.datastructures.util.BeaconStateUtil;
+import tech.pegasys.teku.ssz.backing.CompositeViewRead;
+import tech.pegasys.teku.ssz.backing.ViewRead;
 import tech.pegasys.teku.statetransition.BeaconChainUtil;
 import tech.pegasys.teku.statetransition.blockimport.BlockImporter;
 import tech.pegasys.teku.statetransition.forkchoice.ForkChoice;
@@ -39,11 +42,12 @@ import tech.pegasys.teku.statetransition.util.StartupUtil;
 import tech.pegasys.teku.storage.client.MemoryOnlyRecentChainData;
 import tech.pegasys.teku.storage.client.RecentChainData;
 import tech.pegasys.teku.util.config.Constants;
-import tech.pegasys.teku.util.hashtree.HashTreeUtil;
-import tech.pegasys.teku.util.hashtree.HashTreeUtil.SSZTypes;
 
 /** The test to be run manually for profiling block imports */
 public class ProfilingRun {
+
+  List<BeaconState> statesList = new ArrayList<>();
+  public static Consumer<Object> blackHole = o -> {};
 
   @Disabled
   @Test
@@ -52,7 +56,8 @@ public class ProfilingRun {
     Constants.setConstants("mainnet");
     BeaconStateUtil.BLS_VERIFY_DEPOSIT = false;
 
-    int validatorsCount = 16 * 1024;
+    int validatorsCount = 32 * 1024;
+    int iterationBlockLimit = Integer.MAX_VALUE;
 
     String blocksFile =
         "/blocks/blocks_epoch_"
@@ -62,9 +67,7 @@ public class ProfilingRun {
             + ".ssz.gz";
 
     System.out.println("Generating keypairs...");
-    //    List<BLSKeyPair> validatorKeys = BLSKeyGenerator.generateKeyPairs(validatorsCount);
-    //    List<BLSKeyPair> validatorKeys =
-    // BlsKeyPairIO.createReaderWithDefaultSource().readAll(validatorsCount);
+
     List<BLSKeyPair> validatorKeys =
         BlsKeyPairIO.createReaderForResource("/bls-key-pairs/bls-key-pairs-200k-seed-0.txt.gz")
             .readAll(validatorsCount);
@@ -81,12 +84,18 @@ public class ProfilingRun {
       BlockImporter blockImporter = new BlockImporter(recentChainData, forkChoice, localEventBus);
 
       System.out.println("Start blocks import from " + blocksFile);
+      int blockCount = 0;
+
+      long totalS = 0;
       try (Reader blockReader = BlockIO.createResourceReader(blocksFile)) {
         for (SignedBeaconBlock block : blockReader) {
+          if (block.getSlot().intValue() == 65) {
+            totalS = System.currentTimeMillis();
+            blockCount = 0;
+          }
           long s = System.currentTimeMillis();
           localChain.setSlot(block.getSlot());
           BlockImportResult result = blockImporter.importBlock(block);
-          //        compareHashes(result.getBlockProcessingRecord().getPostState());
           System.out.println(
               "Imported block at #"
                   + block.getSlot()
@@ -94,93 +103,101 @@ public class ProfilingRun {
                   + (System.currentTimeMillis() - s)
                   + " ms: "
                   + result);
+          blockCount++;
+          if (blockCount > iterationBlockLimit) break;
+        }
+      }
+      long totalT = System.currentTimeMillis() - totalS;
+      System.out.printf("############# Total: %f.2 blocks/sec\n", blockCount / (totalT / 1000.0));
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    new ProfilingRun().importBlocksMemProfiling();
+  }
+
+  @Disabled
+  @Test
+  public void importBlocksMemProfiling() throws Exception {
+
+    Constants.setConstants("mainnet");
+    BeaconStateUtil.BLS_VERIFY_DEPOSIT = false;
+
+    int validatorsCount = 32 * 1024;
+
+    String blocksFile =
+        "/blocks/blocks_epoch_"
+            + Constants.SLOTS_PER_EPOCH
+            + "_validators_"
+            + validatorsCount
+            + ".ssz.gz";
+
+    System.out.println("Generating keypairs...");
+
+    List<BLSKeyPair> validatorKeys =
+        BlsKeyPairIO.createReaderForResource("/bls-key-pairs/bls-key-pairs-200k-seed-0.txt.gz")
+            .readAll(validatorsCount);
+
+    BeaconState initialState =
+        StartupUtil.createMockedStartInitialBeaconState(0, validatorKeys, false);
+    statesList.add(initialState);
+
+    while (true) {
+      EventBus localEventBus = mock(EventBus.class);
+      RecentChainData recentChainData = MemoryOnlyRecentChainData.create(localEventBus);
+      BeaconChainUtil localChain = BeaconChainUtil.create(recentChainData, validatorKeys, false);
+      recentChainData.initializeFromGenesis(initialState);
+      initialState = null;
+      ForkChoice forkChoice = new ForkChoice(recentChainData, new StateTransition());
+      BlockImporter blockImporter = new BlockImporter(recentChainData, forkChoice, localEventBus);
+
+      System.out.println("Start blocks import from " + blocksFile);
+      int counter = 1;
+      try (Reader blockReader = BlockIO.createResourceReader(blocksFile)) {
+        for (SignedBeaconBlock block : blockReader) {
+          long s = System.currentTimeMillis();
+          localChain.setSlot(block.getSlot());
+          BlockImportResult result = blockImporter.importBlock(block);
+          System.out.println(
+              "Imported block at #"
+                  + block.getSlot()
+                  + " in "
+                  + (System.currentTimeMillis() - s)
+                  + " ms: "
+                  + result);
+
+          if (--counter == 0) {
+            statesList.add(result.getBlockProcessingRecord().get().getPostState());
+
+            // recreate View validator caches for older state
+            //            traverseViewHierarchy(statesList.get(statesList.size() - 2), v ->
+            // blackHole.accept(v));
+
+            System.out.println("Press enter: ");
+            String line =
+                new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))
+                    .readLine();
+            try {
+              counter = Integer.parseInt(line);
+            } catch (NumberFormatException e) {
+              counter = 1;
+            }
+          }
         }
       }
     }
   }
 
-  void compareHashes(BeaconState s1) {
-    for (int i = 0; i < s1.size(); i++) {
-      Bytes32 hash = s1.get(i).hashTreeRoot();
-      System.out.println(i + ": " + hash);
+  private static void traverseViewHierarchy(Object view, Consumer<ViewRead> visitor) {
+    if (view instanceof ViewRead) {
+      visitor.accept((ViewRead) view);
+      if (view instanceof CompositeViewRead) {
+        CompositeViewRead<?> cView = (CompositeViewRead<?>) view;
+
+        for (int i = 0; i < cView.size(); i++) {
+          traverseViewHierarchy(cView.get(i), visitor);
+        }
+      }
     }
-    System.out.println("BS: " + s1.hash_tree_root());
-    System.out.println("BS: " + old_hash_tree_root(s1));
-    System.out.println("getEth1_data: " + s1.getEth1_data().hash_tree_root());
-    System.out.println("getEth1_data: " + s1.getEth1_data().hash_tree_root());
-    System.out.println("getValidators: " + s1.getValidators().hash_tree_root());
-    System.out.println(
-        "getValidators: "
-            + HashTreeUtil.hash_tree_root(
-                HashTreeUtil.SSZTypes.LIST_OF_COMPOSITE, s1.getValidators()));
-    System.out.println("getBlock_roots: " + s1.getBlock_roots().hash_tree_root());
-    System.out.println(
-        "getBlock_roots: "
-            + HashTreeUtil.hash_tree_root(SSZTypes.VECTOR_OF_COMPOSITE, s1.getBlock_roots()));
-
-    System.out.println("getHistorical_roots: " + s1.getHistorical_roots().hash_tree_root());
-    System.out.println(
-        "getHistorical_roots: "
-            + HashTreeUtil.hash_tree_root(SSZTypes.LIST_OF_COMPOSITE, s1.getHistorical_roots()));
-
-    System.out.println("getEth1_data_votes: " + s1.getEth1_data_votes().hash_tree_root());
-    System.out.println(
-        "getEth1_data_votes: "
-            + HashTreeUtil.hash_tree_root(SSZTypes.LIST_OF_COMPOSITE, s1.getEth1_data_votes()));
-
-    System.out.println("getBalances: " + s1.getBalances().hash_tree_root());
-    System.out.println(
-        "getBalances: "
-            + HashTreeUtil.hash_tree_root_list_ul(
-                s1.getBalances().map(Bytes.class, item -> SSZ.encodeUInt64(item.longValue()))));
-
-    System.out.println(
-        "getJustification_bits: "
-            + HashTreeUtil.hash_tree_root_bitvector(s1.getJustification_bits()));
-  }
-
-  public Bytes32 old_hash_tree_root(BeaconState s) {
-    return HashTreeUtil.merkleize(
-        Arrays.asList(
-            // Versioning
-            HashTreeUtil.hash_tree_root(
-                SSZTypes.BASIC, SSZ.encodeUInt64(s.getGenesis_time().longValue())),
-            HashTreeUtil.hash_tree_root(SSZTypes.BASIC, SSZ.encodeUInt64(s.getSlot().longValue())),
-            s.getFork().hash_tree_root(),
-
-            // History
-            s.getLatest_block_header().hash_tree_root(),
-            HashTreeUtil.hash_tree_root(SSZTypes.VECTOR_OF_COMPOSITE, s.getBlock_roots()),
-            HashTreeUtil.hash_tree_root(SSZTypes.VECTOR_OF_COMPOSITE, s.getState_roots()),
-            HashTreeUtil.hash_tree_root_list_bytes(s.getHistorical_roots()),
-
-            // Ethereum 1.0 chain data
-            s.getEth1_data().hash_tree_root(),
-            HashTreeUtil.hash_tree_root(SSZTypes.LIST_OF_COMPOSITE, s.getEth1_data_votes()),
-            HashTreeUtil.hash_tree_root(
-                SSZTypes.BASIC, SSZ.encodeUInt64(s.getEth1_deposit_index().longValue())),
-
-            // Validator registry
-            HashTreeUtil.hash_tree_root(SSZTypes.LIST_OF_COMPOSITE, s.getValidators()),
-            HashTreeUtil.hash_tree_root_list_ul(
-                s.getBalances().map(Bytes.class, item -> SSZ.encodeUInt64(item.longValue()))),
-
-            // Randomness
-            HashTreeUtil.hash_tree_root(SSZTypes.VECTOR_OF_COMPOSITE, s.getRandao_mixes()),
-
-            // Slashings
-            HashTreeUtil.hash_tree_root_vector_unsigned_long(s.getSlashings()),
-
-            // Attestations
-            HashTreeUtil.hash_tree_root(
-                SSZTypes.LIST_OF_COMPOSITE, s.getPrevious_epoch_attestations()),
-            HashTreeUtil.hash_tree_root(
-                SSZTypes.LIST_OF_COMPOSITE, s.getCurrent_epoch_attestations()),
-
-            // Finality
-            HashTreeUtil.hash_tree_root_bitvector(s.getJustification_bits()),
-            s.getPrevious_justified_checkpoint().hash_tree_root(),
-            s.getCurrent_justified_checkpoint().hash_tree_root(),
-            s.getFinalized_checkpoint().hash_tree_root()));
   }
 }
