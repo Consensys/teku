a2e17dc12d64adfe4ae151c0126fda58bf952b85
Meredith Baxter <meredith.baxter@consensys.net>
2020-06-30 19:24:13 -0400

    Fix DepositStorageTest - make sure we're running across db versions

---
+++ storage/src/testFixtures/java/tech/pegasys/teku/storage/storageSystem/StorageSystemArgumentsProvider.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2020 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.storage.storageSystem;
+
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import tech.pegasys.teku.storage.server.VersionedDatabaseFactory;
+import tech.pegasys.teku.util.config.StateStorageMode;
+
+public class StorageSystemArgumentsProvider implements ArgumentsProvider {
+  private final List<Long> stateStorageFrequencyOptions =
+      List.of(VersionedDatabaseFactory.DEFAULT_STORAGE_FREQUENCY);
+
+  @Override
+  public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+    final Map<String, StorageSystemSupplier> storageSystems = new HashMap<>();
+    for (StateStorageMode mode : StateStorageMode.values()) {
+      storageSystems.put(
+          "v3 (in-memory)", (dataPath) -> InMemoryStorageSystem.createEmptyV3StorageSystem(mode));
+      storageSystems.put(
+          "v3 (file-backed)",
+          (dataPath) -> FileBackedStorageSystem.createV3StorageSystem(dataPath, mode));
+      for (Long storageFrequency : stateStorageFrequencyOptions) {
+        storageSystems.put(
+            "v4 (in-memory)",
+            (dataPath) -> InMemoryStorageSystem.createEmptyV4StorageSystem(mode, storageFrequency));
+        storageSystems.put(
+            "v5 (in-memory)",
+            (dataPath) -> InMemoryStorageSystem.createEmptyV5StorageSystem(mode, storageFrequency));
+        storageSystems.put(
+            "v4 (file-backed)",
+            (dataPath) ->
+                FileBackedStorageSystem.createV4StorageSystem(dataPath, mode, storageFrequency));
+        storageSystems.put(
+            "v5 (file-backed)",
+            (dataPath) ->
+                FileBackedStorageSystem.createV5StorageSystem(dataPath, mode, storageFrequency));
+      }
+    }
+    return storageSystems.entrySet().stream()
+        .map((entry) -> Arguments.of("storage type: " + entry.getKey(), entry.getValue()));
+  }
+
+  @FunctionalInterface
+  public interface StorageSystemSupplier {
+    StorageSystem get(final Path dataDirectory);
+  }
+}
