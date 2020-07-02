35126d004b13b004995cb17fa16a5ca75ed50d60
Adrian Sutton <adrian.sutton@consensys.net>
2020-07-03 07:31:25 +1000

    Regenerate checkpoint states on demand (#2294)
    Reduces memory requirements during non-finalised periods.

--- storage/src/main/java/tech/pegasys/teku/storage/server/rocksdb/dataaccess/RocksDbHotDao.java
+++ storage/src/main/java/tech/pegasys/teku/storage/server/rocksdb/dataaccess/RocksDbHotDao.java
@@ -49,8 +49,6 @@ public interface RocksDbHotDao extends AutoCloseable {
   @MustBeClosed
   Stream<SignedBeaconBlock> streamHotBlocks();
 
-  Map<Checkpoint, BeaconState> getCheckpointStates();
-
   Map<UnsignedLong, VoteTracker> getVotes();
 
   HotUpdater hotUpdater();
@@ -67,17 +65,11 @@ public interface RocksDbHotDao extends AutoCloseable {
 
     void setLatestFinalizedState(final BeaconState state);
 
-    void addCheckpointState(final Checkpoint checkpoint, final BeaconState state);
-
-    void addCheckpointStates(Map<Checkpoint, BeaconState> checkpointStates);
-
     void addHotBlock(final SignedBeaconBlock block);
 
     void addVotes(final Map<UnsignedLong, VoteTracker> states);
 
     void addHotBlocks(final Map<Bytes32, SignedBeaconBlock> blocks);
-
-    void deleteCheckpointState(final Checkpoint checkpoint);
 
     void deleteHotBlock(final Bytes32 blockRoot);
 
