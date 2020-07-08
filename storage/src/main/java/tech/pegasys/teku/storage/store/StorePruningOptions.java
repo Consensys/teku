f2c7fdaec24a86460d2cab4d590e4a673ce67551
Adrian Sutton <adrian.sutton@consensys.net>
2020-07-09 08:26:32 +1000

    Reduce maximum number of blocks to hold in memory to reduce memory usage during non-finalization.

--- storage/src/main/java/tech/pegasys/teku/storage/store/StorePruningOptions.java
+++ storage/src/main/java/tech/pegasys/teku/storage/store/StorePruningOptions.java
@@ -18,7 +18,7 @@ import tech.pegasys.teku.util.config.Constants;
 public class StorePruningOptions {
   public static final int DEFAULT_STATE_CACHE_SIZE = Constants.SLOTS_PER_EPOCH * 5;
   // Max block size is about 20x smaller than the minimum state size
-  public static final int DEFAULT_BLOCK_CACHE_SIZE = DEFAULT_STATE_CACHE_SIZE * 10;
+  public static final int DEFAULT_BLOCK_CACHE_SIZE = DEFAULT_STATE_CACHE_SIZE * 2;
   public static final int DEFAULT_CHECKPOINT_STATE_CACHE_SIZE = 20;
 
   private final int stateCacheSize;
