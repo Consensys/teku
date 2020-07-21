39cce5a733588630f1cabc70217714ba50da4b06
Adrian Sutton <adrian.sutton@consensys.net>
2020-07-22 08:08:18 +1000

    Add network definition for Medalla testnet (#2417)

--- teku/src/test/java/tech/pegasys/teku/cli/options/NetworkOptionsTest.java
+++ teku/src/test/java/tech/pegasys/teku/cli/options/NetworkOptionsTest.java
@@ -33,7 +33,7 @@ public class NetworkOptionsTest extends AbstractBeaconNodeCommandTest {
   }
 
   @ParameterizedTest(name = "{0}")
-  @ValueSource(strings = {"mainnet", "minimal", "altona", "onyx"})
+  @ValueSource(strings = {"mainnet", "minimal", "altona", "onyx", "medalla"})
   public void useDefaultsFromNetworkDefinition(final String networkName) {
     final NetworkDefinition networkDefinition = NetworkDefinition.fromCliArg(networkName);
 
