70a0fc4ddb13740d70c7061bdd1a4f6e4ddaf32b
Adrian Sutton <adrian.sutton@consensys.net>
2020-08-21 11:36:18 +1000

    Update ignore list.

--- storage/src/testFixtures/java/tech/pegasys/teku/storage/store/StoreAssertions.java
+++ storage/src/testFixtures/java/tech/pegasys/teku/storage/store/StoreAssertions.java
@@ -39,6 +39,6 @@ public class StoreAssertions {
             "metricsSystem",
             "states",
             "stateAndBlockProvider",
-            "checkpointStateTaskQueue");
+            "checkpointStates");
   }
 }
