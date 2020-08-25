176251e27cb8943f34e469b1ec3af01a2d36e124
Lucas Saldanha <lucas.saldanha@consensys.net>
2020-08-26 10:59:23 +1200

    Validator client option to subscribe to remote websocket events (#2645)
    * Validator client option to subscribe to remove websocket events
    
    Signed-off-by: Lucas Saldanha <lucas.saldanha@consensys.net>

---
+++ validator/eventadapter/src/main/java/tech/pegasys/teku/validator/eventadapter/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package provides an adapter which maps events from the Beacon node into events of interest
+ * of a Validator node.
+ */
+package tech.pegasys.teku.validator.eventadapter;
