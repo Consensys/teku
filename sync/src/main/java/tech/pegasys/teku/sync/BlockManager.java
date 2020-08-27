6484b98330fad5d8b443258278af5a3b8bb875e3
Adrian Sutton <adrian.sutton@consensys.net>
2020-08-27 11:35:15 +1000

    Retry import if parent became known during import attempt.

--- sync/src/main/java/tech/pegasys/teku/sync/BlockManager.java
+++ sync/src/main/java/tech/pegasys/teku/sync/BlockManager.java
@@ -111,7 +111,7 @@ public class BlockManager extends Service implements SlotEventsChannel {
   }
 
   private void importBlock(final SignedBeaconBlock block) {
-    recentBlockFetcher.cancelRecentBlockRequest(block.getMessage().hash_tree_root());
+    recentBlockFetcher.cancelRecentBlockRequest(block.getRoot());
     if (!shouldImportBlock(block)) {
       return;
     }
@@ -123,7 +123,16 @@ public class BlockManager extends Service implements SlotEventsChannel {
               if (result.isSuccessful()) {
                 LOG.trace("Imported block: {}", block);
               } else if (result.getFailureReason() == FailureReason.UNKNOWN_PARENT) {
+                // Add to the pending pool so it is triggered once the parent is imported
                 pendingBlocks.add(block);
+                // Check if the parent was imported while we were trying to import this block
+                // and if so, remove from the pendingPool again and process now
+                // We must add the block to the pending pool before this check happens
+                // to avoid race conditions between performing the check and the parent importing.
+                if (recentChainData.containsBlock(block.getParent_root())) {
+                  pendingBlocks.remove(block);
+                  importBlock(block);
+                }
               } else if (result.getFailureReason() == FailureReason.BLOCK_IS_FROM_FUTURE) {
                 futureBlocks.add(block);
               } else {
