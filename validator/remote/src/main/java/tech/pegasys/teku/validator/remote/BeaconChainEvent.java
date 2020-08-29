d927b0c94e6906f3a3555feecb479e86eab08df4
Adrian Sutton <adrian.sutton@consensys.net>
2020-08-29 17:47:17 +1000

    Revert "Make errorprone happy."
    This reverts commit faac4b3425d725f39306dbc3f3dd6b5ea2585e8a.

--- validator/remote/src/main/java/tech/pegasys/teku/validator/remote/BeaconChainEvent.java
+++ validator/remote/src/main/java/tech/pegasys/teku/validator/remote/BeaconChainEvent.java
@@ -16,12 +16,13 @@ package tech.pegasys.teku.validator.remote;
 import static tech.pegasys.teku.validator.remote.BeaconChainEvent.REORG_OCCURRED;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import java.util.Objects;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "name")
-@JsonSubTypes({@JsonSubTypes.Type(value = BeaconChainReorgEvent.class, name = REORG_OCCURRED)})
+@JsonSubTypes({@Type(value = BeaconChainReorgEvent.class, name = REORG_OCCURRED)})
 public class BeaconChainEvent {
 
   public static final String ATTESTATION = "BroadcastAttestationEvent";
