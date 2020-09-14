453d098c2c06dfeb6c6fb0da2cf966bb2157ed05
Meredith Baxter <meredith.baxter@consensys.net>
2020-09-14 12:43:26 -0400

    Fix test - remove extra assertion

--- ethereum/statetransition/src/test/java/tech/pegasys/teku/statetransition/blockimport/BlockImporterTest.java
+++ ethereum/statetransition/src/test/java/tech/pegasys/teku/statetransition/blockimport/BlockImporterTest.java
@@ -174,7 +174,6 @@ public class BlockImporterTest {
                   currentSlotFinal, aggregatedAttestations);
             })
         .hasMessageContaining("signature");
-    assertWeakSubjectivityWasChecked();
   }
 
   @Test
