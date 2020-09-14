ec761188a8e5ecf34eff32b4cc96b7b300919ea7
Cem Ozer <cemozer2018@u.northwestern.edu>
2020-09-14 20:40:50 +0300

    Restore deleted tests

--- networking/p2p/src/test/java/tech/pegasys/teku/networking/p2p/DiscoveryNetworkTest.java
+++ networking/p2p/src/test/java/tech/pegasys/teku/networking/p2p/DiscoveryNetworkTest.java
@@ -13,22 +13,6 @@
 
 package tech.pegasys.teku.networking.p2p;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static tech.pegasys.teku.datastructures.util.BeaconStateUtil.compute_fork_digest;
-import static tech.pegasys.teku.util.config.Constants.ATTESTATION_SUBNET_COUNT;
-import static tech.pegasys.teku.util.config.Constants.FAR_FUTURE_EPOCH;
-import static tech.pegasys.teku.util.config.Constants.GENESIS_FORK_VERSION;
-
-import java.net.InetSocketAddress;
-import java.util.Collections;
-import java.util.Optional;
-import java.util.OptionalInt;
-import java.util.function.Predicate;
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 import org.hyperledger.besu.metrics.noop.NoOpMetricsSystem;
@@ -42,6 +26,7 @@ import tech.pegasys.teku.datastructures.util.DataStructureUtil;
 import tech.pegasys.teku.datastructures.util.SimpleOffsetSerializer;
 import tech.pegasys.teku.infrastructure.async.DelayedExecutorAsyncRunner;
 import tech.pegasys.teku.infrastructure.async.SafeFuture;
+import tech.pegasys.teku.infrastructure.async.Waiter;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.network.p2p.peer.SimplePeerSelectionStrategy;
 import tech.pegasys.teku.networking.p2p.connection.ConnectionManager;
@@ -54,6 +39,26 @@ import tech.pegasys.teku.networking.p2p.peer.Peer;
 import tech.pegasys.teku.ssz.SSZTypes.Bitvector;
 import tech.pegasys.teku.ssz.SSZTypes.Bytes4;
 import tech.pegasys.teku.storage.store.MemKeyValueStore;
+
+import java.net.InetSocketAddress;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.function.Predicate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static tech.pegasys.teku.datastructures.util.BeaconStateUtil.compute_fork_digest;
+import static tech.pegasys.teku.util.config.Constants.ATTESTATION_SUBNET_COUNT;
+import static tech.pegasys.teku.util.config.Constants.FAR_FUTURE_EPOCH;
+import static tech.pegasys.teku.util.config.Constants.GENESIS_FORK_VERSION;
 
 class DiscoveryNetworkTest {
   private final DataStructureUtil dataStructureUtil = new DataStructureUtil();
@@ -97,6 +102,46 @@ class DiscoveryNetworkTest {
   public void shouldReturnEnrFromDiscoveryService() {
     when(discoveryService.getEnr()).thenReturn(Optional.of("enr:-"));
     assertThat(discoveryNetwork.getEnr()).contains("enr:-");
+  }
+
+  @Test
+  @SuppressWarnings({"FutureReturnValueIgnored"})
+  public void shouldStopConnectionManagerBeforeNetworkAndDiscovery() {
+    final SafeFuture<Void> connectionStop = new SafeFuture<>();
+    doReturn(new SafeFuture<Void>()).when(discoveryService).stop();
+    doReturn(connectionStop).when(connectionManager).stop();
+
+    Waiter.waitFor(discoveryNetwork::stop);
+
+    verify(connectionManager).stop();
+    verify(discoveryService).updateCustomENRField(any(), any());
+    verify(discoveryService).getEnr();
+    verifyNoMoreInteractions(discoveryService);
+    verifyNoInteractions(p2pNetwork);
+
+    connectionStop.complete(null);
+    verify(p2pNetwork).stop();
+    verify(discoveryService).stop();
+  }
+
+  @Test
+  @SuppressWarnings({"FutureReturnValueIgnored"})
+  public void shouldStopNetworkAndDiscoveryWhenConnectionManagerStopFails() {
+    final SafeFuture<Void> connectionStop = new SafeFuture<>();
+    doReturn(new SafeFuture<Void>()).when(discoveryService).stop();
+    doReturn(connectionStop).when(connectionManager).stop();
+
+    Waiter.waitFor(discoveryNetwork::stop);
+
+    verify(connectionManager).stop();
+    verify(discoveryService).updateCustomENRField(any(), any());
+    verify(discoveryService).getEnr();
+    verifyNoMoreInteractions(discoveryService);
+    verifyNoInteractions(p2pNetwork);
+
+    connectionStop.completeExceptionally(new RuntimeException("Nope"));
+    verify(p2pNetwork).stop();
+    verify(discoveryService).stop();
   }
 
   @Test
