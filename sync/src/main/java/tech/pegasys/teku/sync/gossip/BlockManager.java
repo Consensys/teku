dcf0546bbe8ae2cf115a6598079ac758bcbe59f8
Adrian Sutton <adrian.sutton@consensys.net>
2020-10-08 07:17:56 +1000

    Reduce lines.

--- sync/src/main/java/tech/pegasys/teku/sync/gossip/BlockManager.java
+++ sync/src/main/java/tech/pegasys/teku/sync/gossip/BlockManager.java
@@ -163,11 +163,10 @@ public class BlockManager extends Service implements SlotEventsChannel {
               } else if (result.getFailureReason() == FailureReason.UNKNOWN_PARENT) {
                 // Add to the pending pool so it is triggered once the parent is imported
                 pendingBlocks.add(block);
-                // Check if the parent was imported while we were trying to import this block
-                // and if so, remove from the pendingPool again and process now
-                // We must add the block to the pending pool before this check happens
-                // to avoid race conditions between performing the check and the parent
-                // importing.
+                // Check if the parent was imported while we were trying to import this block and if
+                // so, remove from the pendingPool again and process now We must add the block to
+                // the pending pool before this check happens to avoid race conditions between
+                // performing the check and the parent importing.
                 if (recentChainData.containsBlock(block.getParent_root())) {
                   pendingBlocks.remove(block);
                   importBlock(block);
