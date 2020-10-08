ec848568b5dbf3fb8f40f00897ff5bcb8988ebd7
Meredith Baxter <meredith.baxter@consensys.net>
2020-10-08 10:57:56 -0400

    Revert "Remove validator tools"
    This reverts commit 12b2eb688e877b090798c9061039fd2c45bacf8f.

--- teku/src/main/java/tech/pegasys/teku/cli/BeaconNodeCommand.java
+++ teku/src/main/java/tech/pegasys/teku/cli/BeaconNodeCommand.java
@@ -48,6 +48,7 @@ import tech.pegasys.teku.cli.options.StoreOptions;
 import tech.pegasys.teku.cli.options.ValidatorClientOptions;
 import tech.pegasys.teku.cli.options.ValidatorOptions;
 import tech.pegasys.teku.cli.options.WeakSubjectivityOptions;
+import tech.pegasys.teku.cli.subcommand.DepositCommand;
 import tech.pegasys.teku.cli.subcommand.GenesisCommand;
 import tech.pegasys.teku.cli.subcommand.PeerCommand;
 import tech.pegasys.teku.cli.subcommand.SlashingProtectionCommand;
@@ -77,6 +78,7 @@ import tech.pegasys.teku.util.config.NetworkDefinition;
       AdminCommand.class,
       TransitionCommand.class,
       PeerCommand.class,
+      DepositCommand.class,
       GenesisCommand.class,
       SlashingProtectionCommand.class,
       DebugToolsCommand.class,
