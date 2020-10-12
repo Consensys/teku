f15fd861a1e218e0358dd30dbbff76e11b6b092b
Meredith Baxter <meredith.baxter@consensys.net>
2020-10-12 11:15:54 -0400

    Remove side-effects from atomic update

--- infrastructure/time/src/main/java/tech/pegasys/teku/infrastructure/time/Throttler.java
+++ infrastructure/time/src/main/java/tech/pegasys/teku/infrastructure/time/Throttler.java
@@ -13,13 +13,12 @@
 
 package tech.pegasys.teku.infrastructure.time;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 
 public class Throttler<TResource> {
-  // The wrapped resouce can be invoked at most once every throttling period
+  // The wrapped resource can be invoked at most once every throttling period
   private final TResource resource;
   private final UInt64 throttlingPeriod;
   private final AtomicReference<UInt64> lastInvoked = new AtomicReference<>(null);
@@ -35,16 +34,21 @@ public class Throttler<TResource> {
     }
   }
 
+  /**
+   * If the event at the current time should be throttled, does not update lastInvoked and returns
+   * false. Otherwise, updates lastInvoked and returns true.
+   *
+   * @param currentTime The current time
+   * @return True if lastInvoked was updated (event should not be throttled), false otherwise
+   */
   private boolean updateLastInvoked(final UInt64 currentTime) {
-    final AtomicBoolean updated = new AtomicBoolean(false);
-    lastInvoked.updateAndGet(
-        last -> {
-          if (last == null || last.plus(throttlingPeriod).isLessThanOrEqualTo(currentTime)) {
-            updated.set(true);
-            return currentTime;
-          }
-          return last;
-        });
-    return updated.get();
+    final UInt64 previousValue =
+        lastInvoked.getAndUpdate(
+            last -> shouldThrottleEventAtTime(last, currentTime) ? last : currentTime);
+    return !shouldThrottleEventAtTime(previousValue, currentTime);
+  }
+
+  private boolean shouldThrottleEventAtTime(final UInt64 lastInvoked, final UInt64 currentTime) {
+    return lastInvoked != null && lastInvoked.plus(throttlingPeriod).isGreaterThan(currentTime);
   }
 }
