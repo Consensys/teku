446d194073be21405ca75ee1d206030467483482
Cem Ozer <cemozer2018@u.northwestern.edu>
2020-10-13 16:28:16 -0400

    Run spotless

--- sync/src/testFixtures/java/tech/pegasys/teku/sync/SyncingNodeManager.java
+++ sync/src/testFixtures/java/tech/pegasys/teku/sync/SyncingNodeManager.java
@@ -13,7 +13,11 @@
 
 package tech.pegasys.teku.sync;
 
+import static tech.pegasys.teku.infrastructure.events.TestExceptionHandler.TEST_EXCEPTION_HANDLER;
+
 import com.google.common.eventbus.EventBus;
+import java.util.List;
+import java.util.function.Consumer;
 import org.hyperledger.besu.metrics.noop.NoOpMetricsSystem;
 import tech.pegasys.teku.bls.BLSKeyPair;
 import tech.pegasys.teku.core.StateTransition;
@@ -41,11 +45,6 @@ import tech.pegasys.teku.sync.singlepeer.SinglePeerSyncService;
 import tech.pegasys.teku.sync.singlepeer.SyncManager;
 import tech.pegasys.teku.util.time.channels.SlotEventsChannel;
 import tech.pegasys.teku.weaksubjectivity.WeakSubjectivityValidator;
-
-import java.util.List;
-import java.util.function.Consumer;
-
-import static tech.pegasys.teku.infrastructure.events.TestExceptionHandler.TEST_EXCEPTION_HANDLER;
 
 public class SyncingNodeManager {
   private final EventBus eventBus;
@@ -100,12 +99,7 @@ public class SyncingNodeManager {
     final FutureItems<SignedBeaconBlock> futureBlocks =
         FutureItems.create(SignedBeaconBlock::getSlot);
     BlockManager blockManager =
-        BlockManager.create(
-            eventBus,
-            pendingBlocks,
-            futureBlocks,
-            recentChainData,
-            blockImporter);
+        BlockManager.create(eventBus, pendingBlocks, futureBlocks, recentChainData, blockImporter);
 
     SyncManager syncManager =
         SyncManager.create(
