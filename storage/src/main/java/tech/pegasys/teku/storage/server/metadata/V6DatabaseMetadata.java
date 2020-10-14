762fffc2b843467af11be5e6c451311f5bae1d8f
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-10-14 14:24:03 +0300

    Rename package

--- storage/src/main/java/tech/pegasys/teku/storage/server/metadata/V6DatabaseMetadata.java
+++ storage/src/main/java/tech/pegasys/teku/storage/server/metadata/V6DatabaseMetadata.java
@@ -26,7 +26,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
 import tech.pegasys.teku.storage.server.rocksdb.RocksDbConfiguration;
-import tech.pegasys.teku.util.annotations.JsonExplicit;
+import tech.pegasys.teku.util.serialization.JsonExplicit;
 
 /**
  * Defines the configuration for a database. The configuration used when a database is created is
--- util/src/main/java/tech/pegasys/teku/util/serialization/JsonExplicit.java
+++ util/src/main/java/tech/pegasys/teku/util/serialization/JsonExplicit.java
@@ -11,7 +11,7 @@
  * specific language governing permissions and limitations under the License.
  */
 
-package tech.pegasys.teku.util.annotations;
+package tech.pegasys.teku.util.serialization;
 
 import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
