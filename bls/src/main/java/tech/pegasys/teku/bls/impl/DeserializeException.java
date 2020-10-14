92087a1c590d4e1611791af7675b1279305e9265
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-10-14 12:50:20 +0300

    Use dedicated DeserializeException when failed to deserialize G1/G2 points from bytes

---
+++ bls/src/main/java/tech/pegasys/teku/bls/impl/DeserializeException.java
@@ -0,0 +1,8 @@
+package tech.pegasys.teku.bls.impl;
+
+public class DeserializeException extends IllegalArgumentException {
+
+  public DeserializeException(String message) {
+    super(message);
+  }
+}
