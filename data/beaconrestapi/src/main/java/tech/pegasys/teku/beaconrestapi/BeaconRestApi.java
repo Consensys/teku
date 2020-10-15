e3271ad8f341fccb910719027976d9f29b0a77bb
Cem Ozer <cemozer2018@u.northwestern.edu>
2020-10-15 16:25:35 -0400

    Run spotless

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/BeaconRestApi.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/BeaconRestApi.java
@@ -13,6 +13,12 @@
 
 package tech.pegasys.teku.beaconrestapi;
 
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
+import static javax.servlet.http.HttpServletResponse.SC_NO_CONTENT;
+import static javax.servlet.http.HttpServletResponse.SC_SERVICE_UNAVAILABLE;
+import static tech.pegasys.teku.beaconrestapi.HostAllowlistUtils.isHostAuthorized;
+
 import com.google.common.base.Throwables;
 import com.google.common.io.Resources;
 import io.javalin.Javalin;
@@ -24,6 +30,9 @@ import io.javalin.plugin.openapi.jackson.JacksonModelConverterFactory;
 import io.javalin.plugin.openapi.ui.SwaggerOptions;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import java.io.IOException;
+import java.net.BindException;
+import java.nio.charset.Charset;
 import kotlin.text.Charsets;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
@@ -85,16 +94,6 @@ import tech.pegasys.teku.util.cli.VersionProvider;
 import tech.pegasys.teku.util.config.GlobalConfiguration;
 import tech.pegasys.teku.util.config.InvalidConfigurationException;
 import tech.pegasys.teku.validator.api.NodeSyncingException;
-
-import java.io.IOException;
-import java.net.BindException;
-import java.nio.charset.Charset;
-
-import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
-import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
-import static javax.servlet.http.HttpServletResponse.SC_NO_CONTENT;
-import static javax.servlet.http.HttpServletResponse.SC_SERVICE_UNAVAILABLE;
-import static tech.pegasys.teku.beaconrestapi.HostAllowlistUtils.isHostAuthorized;
 
 public class BeaconRestApi {
 
@@ -298,14 +297,13 @@ public class BeaconRestApi {
   private void addV1BeaconHandlers(final DataProvider dataProvider) {
     app.get(GetGenesis.ROUTE, new GetGenesis(dataProvider, jsonProvider));
     app.get(
-            tech.pegasys.teku.beaconrestapi.handlers.v1.beacon.GetStateRoot.ROUTE,
-            new tech.pegasys.teku.beaconrestapi.handlers.v1.beacon.GetStateRoot(
-                    dataProvider, jsonProvider));
+        tech.pegasys.teku.beaconrestapi.handlers.v1.beacon.GetStateRoot.ROUTE,
+        new tech.pegasys.teku.beaconrestapi.handlers.v1.beacon.GetStateRoot(
+            dataProvider, jsonProvider));
     app.get(GetStateFork.ROUTE, new GetStateFork(dataProvider, jsonProvider));
     app.get(
-            GetStateFinalityCheckpoints.ROUTE,
-            new GetStateFinalityCheckpoints(
-                    dataProvider, jsonProvider));
+        GetStateFinalityCheckpoints.ROUTE,
+        new GetStateFinalityCheckpoints(dataProvider, jsonProvider));
     app.get(GetStateValidator.ROUTE, new GetStateValidator(dataProvider, jsonProvider));
     app.get(GetStateValidators.ROUTE, new GetStateValidators(dataProvider, jsonProvider));
     app.post(
--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/GetStateFinalityCheckpoints.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/GetStateFinalityCheckpoints.java
@@ -13,6 +13,19 @@
 
 package tech.pegasys.teku.beaconrestapi.handlers.v1.beacon;
 
+import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
+import static javax.servlet.http.HttpServletResponse.SC_SERVICE_UNAVAILABLE;
+import static tech.pegasys.teku.beaconrestapi.RestApiConstants.PARAM_STATE_ID;
+import static tech.pegasys.teku.beaconrestapi.RestApiConstants.PARAM_STATE_ID_DESCRIPTION;
+import static tech.pegasys.teku.beaconrestapi.RestApiConstants.RES_BAD_REQUEST;
+import static tech.pegasys.teku.beaconrestapi.RestApiConstants.RES_INTERNAL_ERROR;
+import static tech.pegasys.teku.beaconrestapi.RestApiConstants.RES_NOT_FOUND;
+import static tech.pegasys.teku.beaconrestapi.RestApiConstants.RES_OK;
+import static tech.pegasys.teku.beaconrestapi.RestApiConstants.RES_SERVICE_UNAVAILABLE;
+import static tech.pegasys.teku.beaconrestapi.RestApiConstants.SERVICE_UNAVAILABLE;
+import static tech.pegasys.teku.beaconrestapi.RestApiConstants.TAG_V1_BEACON;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
@@ -21,6 +34,8 @@ import io.javalin.plugin.openapi.annotations.OpenApi;
 import io.javalin.plugin.openapi.annotations.OpenApiContent;
 import io.javalin.plugin.openapi.annotations.OpenApiParam;
 import io.javalin.plugin.openapi.annotations.OpenApiResponse;
+import java.util.Map;
+import java.util.Optional;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
@@ -38,60 +53,46 @@ import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.provider.JsonProvider;
 import tech.pegasys.teku.storage.client.ChainDataUnavailableException;
 
-import java.util.Map;
-import java.util.Optional;
-
-import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
-import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
-import static javax.servlet.http.HttpServletResponse.SC_SERVICE_UNAVAILABLE;
-import static tech.pegasys.teku.beaconrestapi.RestApiConstants.PARAM_STATE_ID;
-import static tech.pegasys.teku.beaconrestapi.RestApiConstants.PARAM_STATE_ID_DESCRIPTION;
-import static tech.pegasys.teku.beaconrestapi.RestApiConstants.RES_BAD_REQUEST;
-import static tech.pegasys.teku.beaconrestapi.RestApiConstants.RES_INTERNAL_ERROR;
-import static tech.pegasys.teku.beaconrestapi.RestApiConstants.RES_NOT_FOUND;
-import static tech.pegasys.teku.beaconrestapi.RestApiConstants.RES_OK;
-import static tech.pegasys.teku.beaconrestapi.RestApiConstants.RES_SERVICE_UNAVAILABLE;
-import static tech.pegasys.teku.beaconrestapi.RestApiConstants.SERVICE_UNAVAILABLE;
-import static tech.pegasys.teku.beaconrestapi.RestApiConstants.TAG_V1_BEACON;
-
 public class GetStateFinalityCheckpoints extends AbstractHandler implements Handler {
   private static final Logger LOG = LogManager.getLogger();
   public static final String ROUTE = "/eth/v1/beacon/states/:state_id/finality_checkpoints";
   private final ChainDataProvider chainDataProvider;
 
-  public GetStateFinalityCheckpoints(final DataProvider dataProvider, final JsonProvider jsonProvider) {
+  public GetStateFinalityCheckpoints(
+      final DataProvider dataProvider, final JsonProvider jsonProvider) {
     super(jsonProvider);
     this.chainDataProvider = dataProvider.getChainDataProvider();
   }
 
-  GetStateFinalityCheckpoints(final ChainDataProvider chainDataProvider, final JsonProvider jsonProvider) {
+  GetStateFinalityCheckpoints(
+      final ChainDataProvider chainDataProvider, final JsonProvider jsonProvider) {
     super(jsonProvider);
     this.chainDataProvider = chainDataProvider;
   }
 
   @OpenApi(
-          path = ROUTE,
-          method = HttpMethod.GET,
-          summary = "Get state finality checkpoints",
-          tags = {TAG_V1_BEACON},
-          description =
-                  "Returns finality checkpoints for state with given 'stateId'. In case finality is not yet achieved, checkpoint should return epoch 0 and ZERO_HASH as root.",
-          pathParams = {@OpenApiParam(name = PARAM_STATE_ID, description = PARAM_STATE_ID_DESCRIPTION)},
-          responses = {
-                  @OpenApiResponse(
-                          status = RES_OK,
-                          content = @OpenApiContent(from = GetStateRootResponse.class)),
-                  @OpenApiResponse(status = RES_BAD_REQUEST),
-                  @OpenApiResponse(status = RES_NOT_FOUND),
-                  @OpenApiResponse(status = RES_INTERNAL_ERROR),
-                  @OpenApiResponse(status = RES_SERVICE_UNAVAILABLE, description = SERVICE_UNAVAILABLE)
-          })
+      path = ROUTE,
+      method = HttpMethod.GET,
+      summary = "Get state finality checkpoints",
+      tags = {TAG_V1_BEACON},
+      description =
+          "Returns finality checkpoints for state with given 'stateId'. In case finality is not yet achieved, checkpoint should return epoch 0 and ZERO_HASH as root.",
+      pathParams = {@OpenApiParam(name = PARAM_STATE_ID, description = PARAM_STATE_ID_DESCRIPTION)},
+      responses = {
+        @OpenApiResponse(
+            status = RES_OK,
+            content = @OpenApiContent(from = GetStateRootResponse.class)),
+        @OpenApiResponse(status = RES_BAD_REQUEST),
+        @OpenApiResponse(status = RES_NOT_FOUND),
+        @OpenApiResponse(status = RES_INTERNAL_ERROR),
+        @OpenApiResponse(status = RES_SERVICE_UNAVAILABLE, description = SERVICE_UNAVAILABLE)
+      })
   @Override
   public void handle(@NotNull final Context ctx) throws Exception {
     final Map<String, String> pathParams = ctx.pathParamMap();
     try {
       final Optional<UInt64> maybeSlot =
-              chainDataProvider.stateParameterToSlot(pathParams.get(PARAM_STATE_ID));
+          chainDataProvider.stateParameterToSlot(pathParams.get(PARAM_STATE_ID));
       if (maybeSlot.isEmpty()) {
         ctx.status(SC_NOT_FOUND);
         return;
@@ -109,13 +110,15 @@ public class GetStateFinalityCheckpoints extends AbstractHandler implements Hand
   }
 
   private Optional<String> handleResult(Context ctx, final BeaconState response)
-          throws JsonProcessingException {
+      throws JsonProcessingException {
     boolean isFinalized = response.finalized_checkpoint.epoch.isGreaterThan(UInt64.ZERO);
-    FinalityCheckpointsResponse finalityCheckpointsResponse = new FinalityCheckpointsResponse(
+    FinalityCheckpointsResponse finalityCheckpointsResponse =
+        new FinalityCheckpointsResponse(
             isFinalized ? response.previous_justified_checkpoint : Checkpoint.EMPTY,
             isFinalized ? response.current_justified_checkpoint : Checkpoint.EMPTY,
-            isFinalized ? response.finalized_checkpoint : Checkpoint.EMPTY
-    );
-    return Optional.of(jsonProvider.objectToJSON(new GetStateFinalityCheckpointsResponse(finalityCheckpointsResponse)));
+            isFinalized ? response.finalized_checkpoint : Checkpoint.EMPTY);
+    return Optional.of(
+        jsonProvider.objectToJSON(
+            new GetStateFinalityCheckpointsResponse(finalityCheckpointsResponse)));
   }
 }
