2823e07756cb18f6adabeed0f6ff58ef9f6c1f0d
Paul Harris <paul.harris@consensys.net>
2020-10-16 13:41:29 +1000

    implement `/eth/v1/beacon/pool/attestations` POST
     - deprecated old `/validator/attestation` POST endpoint
    
     - switched validator client to use the new standard rest endpoint.
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/PostAttestationData.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/PostAttestationData.java
@@ -11,7 +11,7 @@
  * specific language governing permissions and limitations under the License.
  */
 
-package tech.pegasys.teku.beaconrestapi.handlers.v1.validator;
+package tech.pegasys.teku.beaconrestapi.handlers.v1.beacon;
 
 import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
 import static javax.servlet.http.HttpServletResponse.SC_OK;
