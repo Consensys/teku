49ea21897ebda0cd791f402fc1ce4a0bb1dfef5b
Paul Harris <paul.harris@consensys.net>
2020-10-23 13:13:03 +1000

    validator committee indexes need to be returned as string. (#3057)
    * validator committee indexes need to be returned as string.
    
    returning as integers resulted in a failed eth2-comply test.
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- data/serializer/src/main/java/tech/pegasys/teku/api/response/v1/beacon/EpochCommitteeResponse.java
+++ data/serializer/src/main/java/tech/pegasys/teku/api/response/v1/beacon/EpochCommitteeResponse.java
@@ -18,9 +18,11 @@ import static tech.pegasys.teku.api.schema.SchemaConstants.EXAMPLE_UINT64;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Objects;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
+import tech.pegasys.teku.infrastructure.unsigned.UInt64Util;
 
 public class EpochCommitteeResponse {
   @JsonProperty("index")
@@ -35,20 +37,21 @@ public class EpochCommitteeResponse {
   public final UInt64 slot;
 
   @JsonProperty("validators")
-  public final List<Integer> validators;
+  @ArraySchema(schema = @Schema(type = "string", example = EXAMPLE_UINT64))
+  public final List<UInt64> validators;
 
   public EpochCommitteeResponse(
       tech.pegasys.teku.datastructures.state.CommitteeAssignment committeeAssignment) {
     this.slot = committeeAssignment.getSlot();
     this.index = committeeAssignment.getCommitteeIndex();
-    this.validators = committeeAssignment.getCommittee();
+    this.validators = UInt64Util.intToUInt64List(committeeAssignment.getCommittee());
   }
 
   @JsonCreator
   public EpochCommitteeResponse(
       @JsonProperty("slot") final UInt64 slot,
       @JsonProperty("index") final UInt64 index,
-      @JsonProperty("validators") final List<Integer> validators) {
+      @JsonProperty("validators") final List<UInt64> validators) {
     this.slot = slot;
     this.index = index;
     this.validators = validators;
