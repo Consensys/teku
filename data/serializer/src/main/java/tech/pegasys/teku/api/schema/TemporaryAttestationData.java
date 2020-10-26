361b589cd5f635a982df2c7006d60f4a670f5608
Cem Ozer <cemozer2018@u.northwestern.edu>
2020-10-26 16:18:06 -0400

    Allow validator client to deserialize new and legacy createAttestationData endpoint

---
+++ data/serializer/src/main/java/tech/pegasys/teku/api/schema/TemporaryAttestationData.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2020 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.api.schema;
+
+import static tech.pegasys.teku.api.schema.SchemaConstants.DESCRIPTION_BYTES32;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.apache.tuweni.bytes.Bytes32;
+import tech.pegasys.teku.infrastructure.unsigned.UInt64;
+
+public class TemporaryAttestationData {
+  @Schema(type = "string", format = "uint64")
+  public final UInt64 slot;
+
+  @Schema(type = "string", format = "uint64")
+  public final UInt64 index;
+
+  @Schema(type = "string", format = "byte", description = DESCRIPTION_BYTES32)
+  public final Bytes32 beacon_block_root;
+
+  public final Checkpoint source;
+  public final Checkpoint target;
+
+  @JsonProperty("data")
+  public final AttestationData data;
+
+  @JsonCreator
+  public TemporaryAttestationData(
+      @JsonProperty("data") final AttestationData data,
+      @JsonProperty("slot") final UInt64 slot,
+      @JsonProperty("index") final UInt64 index,
+      @JsonProperty("beacon_block_root") final Bytes32 beacon_block_root,
+      @JsonProperty("source") final Checkpoint source,
+      @JsonProperty("target") final Checkpoint target) {
+    this.data = data;
+    this.slot = slot;
+    this.index = index;
+    this.beacon_block_root = beacon_block_root;
+    this.source = source;
+    this.target = target;
+  }
+
+  public AttestationData getAttestationData() {
+    if (data != null) {
+      return data;
+    }
+
+    return new AttestationData(slot, index, beacon_block_root, source, target);
+  }
+}
