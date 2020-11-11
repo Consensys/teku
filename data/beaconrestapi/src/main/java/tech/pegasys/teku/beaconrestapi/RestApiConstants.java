5cdcd7e82ad4eb1f20e4ed1f01ac117a8016dc12
Paul Harris <paul.harris@consensys.net>
2020-11-11 16:16:34 +1000

    Remove deprecated apis (#3168)
    * remove deprecated beacon endpoints
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/RestApiConstants.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/RestApiConstants.java
@@ -42,14 +42,10 @@ public class RestApiConstants {
   public static final String STATUS = "status";
 
   public static final String TAG_ADMIN = "Admin";
-  public static final String TAG_BEACON = "Beacon";
-  public static final String TAG_NODE = "Node";
-  public static final String TAG_NETWORK = "Network";
-  public static final String TAG_VALIDATOR = "Validator";
 
-  public static final String TAG_V1_NODE = "Node V1";
-  public static final String TAG_V1_VALIDATOR = "Validator V1";
-  public static final String TAG_V1_BEACON = "Beacon V1";
+  public static final String TAG_V1_NODE = "Node";
+  public static final String TAG_V1_VALIDATOR = "Validator";
+  public static final String TAG_V1_BEACON = "Beacon";
   public static final String TAG_CONFIG = "Config";
   public static final String TAG_EVENTS = "Events";
   public static final String TAG_VALIDATOR_REQUIRED = "Validator Required Api";
