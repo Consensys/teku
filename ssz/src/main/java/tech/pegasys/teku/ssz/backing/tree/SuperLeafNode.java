8d1784469949317723ec1b4db06d76b8541023c3
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-11-12 10:30:54 +0300

    Almost complete SuperLeafNode implementation

--- ssz/src/main/java/tech/pegasys/teku/ssz/backing/tree/SuperLeafNode.java
+++ ssz/src/main/java/tech/pegasys/teku/ssz/backing/tree/SuperLeafNode.java
@@ -1,14 +1,27 @@
 package tech.pegasys.teku.ssz.backing.tree;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static java.lang.Math.max;
 
+import com.google.common.base.Preconditions;
 import java.util.function.Function;
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 import org.apache.tuweni.bytes.MutableBytes;
+import org.apache.tuweni.crypto.Hash;
 import org.jetbrains.annotations.NotNull;
 import tech.pegasys.teku.ssz.backing.tree.TreeNodeImpl.LeafNodeImpl;
 
+/**
+ * Works for:
+ *   Vector[UInt64]
+ *   Vector[byte]
+ *   Vector[bytes32]
+ *
+ * Doesn't work for
+ *   Container
+ *   Vector[Container]
+ */
 public class SuperLeafNode implements TreeNode {
   private final int depth;
   private final Bytes data;
@@ -24,7 +37,25 @@ public class SuperLeafNode implements TreeNode {
 
   @Override
   public Bytes32 hashTreeRoot() {
-    throw new UnsupportedOperationException("TODO");
+    return hashTreeRoot(1, 0, totalLeafCount);
+  }
+
+  private Bytes32 hashTreeRoot(int curDepth, long fromNodeIdx, long toNodeIdx) {
+    if (fromNodeIdx >= dataLeafCount) {
+      return TreeUtil.ZERO_LEAVES[depth - curDepth].hashTreeRoot();
+    } else if (curDepth == depth) {
+      assert toNodeIdx - fromNodeIdx == 1;
+      if (fromNodeIdx + 1 < dataLeafCount) {
+        return Bytes32.wrap(data, (int) fromNodeIdx * 32);
+      } else {
+        return Bytes32.rightPad(data.slice((int) (fromNodeIdx * 32)));
+      }
+    } else {
+      long midNodeIdx = fromNodeIdx + (1 << (depth - curDepth));
+      assert midNodeIdx - fromNodeIdx == toNodeIdx - midNodeIdx;
+      return Hash.sha2_256(Bytes.wrap(hashTreeRoot(curDepth + 1, fromNodeIdx, midNodeIdx),
+          hashTreeRoot(curDepth + 1, midNodeIdx, toNodeIdx)));
+    }
   }
 
   @NotNull
@@ -52,19 +83,39 @@ public class SuperLeafNode implements TreeNode {
   public TreeNode updated(TreeUpdates newNodes) {
     int lastUpdatedIndex = (int) (newNodes.getGIndex(newNodes.size() - 1) - totalLeafCount);
     if (lastUpdatedIndex < 0) {
-      throw new IllegalArgumentException("Invalid last generalized index");
+      throw new UnsupportedOperationException("Not yet supported updating branch nodes");
     }
-    int newLeafCount = max(lastUpdatedIndex + 1, dataLeafCount);
-    MutableBytes newData = MutableBytes.create(32 * newLeafCount);
+    if (lastUpdatedIndex > totalLeafCount) {
+      throw new IndexOutOfBoundsException("Invalid general index");
+    }
+
+    TreeNode lastNewNode = newNodes.getNode(newNodes.size() - 1);
+    if (!(lastNewNode instanceof LeafNode)) {
+      throw new IllegalArgumentException("Can't update leaf node with branch node");
+    }
+    LeafNode lastNewLeafNode = (LeafNode) lastNewNode;
+    int newDataSize = max(data.size(), lastUpdatedIndex * 32 + lastNewLeafNode.getData().size());
+    int newLeafCount = (newDataSize + 31) / 32;
+    MutableBytes newData = MutableBytes.create(newDataSize);
     int updateIdx = 0;
     for (int leafIdx = 0; leafIdx < newLeafCount; leafIdx++) {
-      if (newNodes.getGIndex(updateIdx) - totalLeafCount == leafIdx) {
-        LeafNode node = (LeafNode) newNodes.getNode(updateIdx);
-        node.getData().copyTo(newData, leafIdx * 32);
+      if (updateIdx < newNodes.size() &&
+          newNodes.getGIndex(updateIdx) - totalLeafCount == leafIdx) {
+
+        TreeNode node = newNodes.getNode(updateIdx);
+        if (!(node instanceof LeafNode)) {
+          throw new IllegalArgumentException("Can't update leaf node with branch node");
+        }
+        LeafNode leafNode = (LeafNode) node;
+        if (leafIdx < lastUpdatedIndex && leafNode.getData().size() != 32) {
+          throw new IllegalArgumentException("Update nodes should be packed");
+        }
+        leafNode.getData().copyTo(newData, leafIdx * 32);
         updateIdx++;
+      } else {
+        data.slice(leafIdx * 32, 32).copyTo(newData, leafIdx * 32);
       }
     }
-    int leafIdx = 0;
-    return null;
+    return new SuperLeafNode(depth, newData);
   }
 }
