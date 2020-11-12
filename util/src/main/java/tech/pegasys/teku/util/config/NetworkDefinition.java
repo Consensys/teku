571faf2bb71dd65d0e6804c43c04e840f074a88e
Adrian Sutton <adrian.sutton@consensys.net>
2020-11-13 07:40:03 +1000

    Add pyrmont network definition. (#3205)

--- util/src/main/java/tech/pegasys/teku/util/config/NetworkDefinition.java
+++ util/src/main/java/tech/pegasys/teku/util/config/NetworkDefinition.java
@@ -89,6 +89,19 @@ public class NetworkDefinition {
                       // Prysm @protolambda
                       "enr:-LK4QAhU5smiLgU0AgrdFv8eCKmDPCBkXCMCIy8Aktaci5qvCYOsW98xVqJS6OoPWt4Sz_YoTdLQBWxd-RZ756vmGPMBh2F0dG5ldHOIAAAAAAAAAACEZXRoMpCXm69nAHAe0P__________gmlkgnY0gmlwhDTTDL2Jc2VjcDI1NmsxoQOmSJ0mKsQjab7Zralm1Hi0AEReZ2SEqYdKoOPmoA98DoN0Y3CCIyiDdWRwgiMo")
                   .build())
+          .put(
+              "pyrmont",
+              builder()
+                  .constants("pyrmont")
+                  .startupTimeoutSeconds(120)
+                  .eth1DepositContractAddress("0x2c539a95d2a3f9b10681D9c0dD7cCE37D40c7B79")
+                  .initialStateFromClasspath("pyrmont-genesis.ssz")
+                  .discoveryBootnodes(
+                      // @protolambda bootnode 1
+                      "enr:-Ku4QDuuQGbUpzWMW1IUZpvt3xUzZuEwm2CvHqWQ-EGGzWXPYNc-PZPIfm05R7W3YwEIGM2_2-Y3JHQuEiizbYlW-HoBh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhDQPSjiJc2VjcDI1NmsxoQM6yTQB6XGWYJbI7NZFBjp4Yb9AYKQPBhVrfUclQUobb4N1ZHCCIyg",
+                      // @protolambda bootnode 2
+                      "enr:-Ku4QAOnRymufUy7UbyxheWFbV9WAtt7BlvoixBz8-Xstb0oBui0ERAiBcsY5xDbE2YxvT7u6gwZPju9V_ecAAJMddUBh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhDaa13aJc2VjcDI1NmsxoQKdNQJvnohpf0VO0ZYCAJxGjT0uwJoAHbAiBMujGjK0SoN1ZHCCIyg")
+                  .build())
           .build();
 
   private final String constants;
