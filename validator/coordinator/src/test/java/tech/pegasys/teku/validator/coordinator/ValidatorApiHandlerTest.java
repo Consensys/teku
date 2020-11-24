e99e6dc0c938304dfe73b7b32fae3c07048d382a
Cem Ozer <cemozer2018@u.northwestern.edu>
2020-11-24 17:27:17 -0500

    Fix test compilation

--- validator/coordinator/src/test/java/tech/pegasys/teku/validator/coordinator/ValidatorApiHandlerTest.java
+++ validator/coordinator/src/test/java/tech/pegasys/teku/validator/coordinator/ValidatorApiHandlerTest.java
@@ -34,12 +34,10 @@ import static tech.pegasys.teku.infrastructure.unsigned.UInt64.ONE;
 import static tech.pegasys.teku.infrastructure.unsigned.UInt64.ZERO;
 
 import com.google.common.eventbus.EventBus;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import org.apache.tuweni.bytes.Bytes32;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import tech.pegasys.teku.api.ChainDataProvider;
 import tech.pegasys.teku.bls.BLSPublicKey;
 import tech.pegasys.teku.bls.BLSSignature;
 import tech.pegasys.teku.core.StateTransition;
@@ -78,6 +76,10 @@ import tech.pegasys.teku.validator.api.ProposerDuties;
 import tech.pegasys.teku.validator.api.SendSignedBlockResult;
 import tech.pegasys.teku.validator.coordinator.performance.DefaultPerformanceTracker;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 class ValidatorApiHandlerTest {
 
   private static final UInt64 EPOCH = UInt64.valueOf(13);
@@ -99,9 +101,12 @@ class ValidatorApiHandlerTest {
   private final EventBus eventBus = mock(EventBus.class);
   private final DefaultPerformanceTracker performanceTracker =
       mock(DefaultPerformanceTracker.class);
+  private final ChainDataProvider chainDataProvider =
+          mock(ChainDataProvider.class);
 
   private final ValidatorApiHandler validatorApiHandler =
       new ValidatorApiHandler(
+              chainDataProvider,
           chainDataClient,
           syncStateProvider,
           stateTransition,
