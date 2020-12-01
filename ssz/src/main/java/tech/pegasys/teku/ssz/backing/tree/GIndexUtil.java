0ab3a104c37e36049f1387538a4c7ce5becd7281
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-12-01 13:14:47 +0300

    Add more GIndexUtil tests

--- ssz/src/main/java/tech/pegasys/teku/ssz/backing/tree/GIndexUtil.java
+++ ssz/src/main/java/tech/pegasys/teku/ssz/backing/tree/GIndexUtil.java
@@ -13,12 +13,18 @@
 
 package tech.pegasys.teku.ssz.backing.tree;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static java.lang.Integer.min;
+
+import com.google.common.annotations.VisibleForTesting;
 
 /**
  * Util methods for binary tree generalized indexes manipulations See
  * https://github.com/ethereum/eth2.0-specs/blob/v1.0.0/ssz/merkle-proofs.md#generalized-merkle-tree-index
  * for more info on generalized indexes
+ *
+ * <p>Here the general index is represented by <code>long</code> which is treated as unsigned uint64
+ * Thus the only illegal generalized index value is <code>0</code>
  */
 public class GIndexUtil {
 
@@ -28,21 +34,41 @@ public class GIndexUtil {
     Right,
     Successor,
     Predecessor,
-    Same
+    Same;
+
+    /** <code>gIdxCompare(idx1, idx2) == gIdxCompare(idx2, idx1).inverse()</code> */
+    public NodeRelation inverse() {
+      switch (this) {
+        case Left:
+          return Right;
+        case Right:
+          return Left;
+        case Predecessor:
+          return Successor;
+        case Successor:
+          return Predecessor;
+        case Same:
+          return Same;
+        default:
+          throw new IllegalArgumentException("Unknown: " + this);
+      }
+    }
   }
 
   /**
    * The generalized index of either a root tree node or an index of a node relative to the node
-   * itself. Equal to <code>1</code>
+   * itself. Effectively this is <code>1L</code>
    */
   static final long SELF_G_INDEX = 1;
 
   /**
    * The generalized index (normally an index of non-existing node) of the leftmost possible node
+   * Effectively this is {@link Long#MIN_VALUE} or <code>0b10000...000L</code> in binary form
    */
   static final long LEFTMOST_G_INDEX = gIdxLeftmostFrom(SELF_G_INDEX);
   /**
    * The generalized index (normally an index of non-existing node) of the rightmost possible node
+   * Effectively this is <code>-1L</code> or <code>0b11111...111L</code> in binary form
    */
   static final long RIGHTMOST_G_INDEX = gIdxRightmostFrom(SELF_G_INDEX);
 
@@ -52,6 +78,7 @@ public class GIndexUtil {
    * @see #SELF_G_INDEX
    */
   public static boolean gIdxIsSelf(long generalizedIndex) {
+    checkGIndex(generalizedIndex);
     return generalizedIndex == SELF_G_INDEX;
   }
 
@@ -68,6 +95,8 @@ public class GIndexUtil {
    * </ul>
    */
   public static NodeRelation gIdxCompare(long idx1, long idx2) {
+    checkGIndex(idx1);
+    checkGIndex(idx2);
     long anchor1 = Long.highestOneBit(idx1);
     long anchor2 = Long.highestOneBit(idx2);
     int depth1 = Long.bitCount(anchor1 - 1);
@@ -97,7 +126,7 @@ public class GIndexUtil {
    * {@link #SELF_G_INDEX} would be 0
    */
   public static int gIdxGetDepth(long generalizedIndex) {
-    assert generalizedIndex >= 1;
+    checkGIndex(generalizedIndex);
     long anchor = Long.highestOneBit(generalizedIndex);
     return Long.bitCount(anchor - 1);
   }
@@ -138,32 +167,58 @@ public class GIndexUtil {
    * </ul>
    */
   public static long gIdxChildGIndex(long generalizedIndex, int childIdx, int childDepth) {
-    assert generalizedIndex >= 1;
-    assert childIdx < (1 << childDepth);
+    checkGIndex(generalizedIndex);
+    checkArgument(childDepth >= 0 && childDepth < 64);
+    checkArgument(childIdx >= 0);
+    checkArgument(
+        childIdx < (1 << childDepth), "Invalid child index %s, for depth %s", childIdx, childDepth);
+    checkArgument(gIdxGetDepth(generalizedIndex) + childDepth < 64);
     return (generalizedIndex << childDepth) | childIdx;
   }
 
   /**
    * Returns the generalized index (normally an index of non-existing node) of the leftmost possible
    * successor of this node
+   *
+   * <p>For example:
+   *
+   * <ul>
+   *   <li><code>gIdxLeftmostFrom(0b1100) == 0b110000000...00L</code>
+   *   <li><code>gIdxLeftmostFrom(0b1101) == 0b110100000...00L</code>
+   * </ul>
    */
   public static long gIdxLeftmostFrom(long fromGeneralizedIndex) {
-    assert fromGeneralizedIndex >= 1;
-    int nodeDepth = TreeUtil.treeDepth(fromGeneralizedIndex);
-    assert nodeDepth <= 63;
-    return fromGeneralizedIndex << (63 - nodeDepth);
+    checkGIndex(fromGeneralizedIndex);
+    long highestOneBit = Long.highestOneBit(fromGeneralizedIndex);
+    if (highestOneBit < 0) {
+      return fromGeneralizedIndex;
+    } else {
+      int nodeDepth = Long.bitCount(highestOneBit - 1);
+      return fromGeneralizedIndex << (63 - nodeDepth);
+    }
   }
 
   /**
    * Returns the generalized index (normally an index of non-existing node) of the rightmost
    * possible successor of this node
+   *
+   * <p>For example:
+   *
+   * <ul>
+   *   <li><code>gIdxRightmostFrom(0b1100) == 0b110011111...11L</code>
+   *   <li><code>gIdxRightmostFrom(0b1101) == 0b110111111...11L</code>
+   * </ul>
    */
   public static long gIdxRightmostFrom(long fromGeneralizedIndex) {
-    assert fromGeneralizedIndex >= 1;
-    int nodeDepth = TreeUtil.treeDepth(fromGeneralizedIndex);
-    assert nodeDepth <= 63;
-    int shiftN = 63 - nodeDepth;
-    return (fromGeneralizedIndex << shiftN) | ((1L << shiftN) - 1);
+    checkGIndex(fromGeneralizedIndex);
+    long highestOneBit = Long.highestOneBit(fromGeneralizedIndex);
+    if (highestOneBit < 0) {
+      return fromGeneralizedIndex;
+    } else {
+      int nodeDepth = Long.bitCount(highestOneBit - 1);
+      int shiftN = 63 - nodeDepth;
+      return (fromGeneralizedIndex << shiftN) | ((1L << shiftN) - 1);
+    }
   }
 
   /**
@@ -183,6 +238,9 @@ public class GIndexUtil {
    * </ul>
    */
   public static int gIdxGetChildIndex(long generalizedIndex, int childDepth) {
+    checkGIndex(generalizedIndex);
+    checkArgument(childDepth < 64);
+
     long anchor = Long.highestOneBit(generalizedIndex);
     int indexBitCount = Long.bitCount(anchor - 1);
     if (indexBitCount < childDepth) {
@@ -207,6 +265,9 @@ public class GIndexUtil {
    * </ul>
    */
   public static long gIdxGetRelativeGIndex(long generalizedIndex, int childDepth) {
+    checkGIndex(generalizedIndex);
+    checkArgument(childDepth < 64);
+
     long anchor = Long.highestOneBit(generalizedIndex);
     long pivot = anchor >>> childDepth;
     if (pivot == 0) {
@@ -214,5 +275,15 @@ public class GIndexUtil {
           "Generalized index " + generalizedIndex + " is upper than depth " + childDepth);
     }
     return (generalizedIndex & (pivot - 1)) | pivot;
+  }
+
+  @VisibleForTesting
+  static long gIdxGetParent(long generalizedIndex) {
+    checkGIndex(generalizedIndex);
+    return generalizedIndex >>> 1;
+  }
+
+  private static void checkGIndex(long index) {
+    checkArgument(index != 0, "Invalid index: %s", index);
   }
 }
--- ssz/src/test/java/tech/pegasys/teku/ssz/backing/tree/GIndexUtilTest.java
+++ ssz/src/test/java/tech/pegasys/teku/ssz/backing/tree/GIndexUtilTest.java
@@ -14,13 +14,27 @@
 package tech.pegasys.teku.ssz.backing.tree;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.LEFTMOST_G_INDEX;
 import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.NodeRelation.Left;
 import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.NodeRelation.Predecessor;
 import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.NodeRelation.Right;
 import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.NodeRelation.Same;
+import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.NodeRelation.Successor;
+import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.RIGHTMOST_G_INDEX;
+import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.SELF_G_INDEX;
+import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.gIdxChildGIndex;
 import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.gIdxCompare;
+import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.gIdxGetDepth;
+import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.gIdxGetParent;
+import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.gIdxIsSelf;
+import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.gIdxLeftGIndex;
+import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.gIdxLeftmostFrom;
+import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.gIdxRightGIndex;
+import static tech.pegasys.teku.ssz.backing.tree.GIndexUtil.gIdxRightmostFrom;
 
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -28,20 +42,127 @@ import tech.pegasys.teku.ssz.backing.tree.GIndexUtil.NodeRelation;
 
 public class GIndexUtilTest {
 
+  static final long INVALID_G_INDEX = 0L;
 
   static Stream<Arguments> compareCases() {
     return Stream.of(
-        Arguments.of(0b1L, 0b1L, Same),
+        Arguments.of(SELF_G_INDEX, SELF_G_INDEX, Same),
+        Arguments.of(Long.MAX_VALUE, Long.MAX_VALUE, Same),
+        Arguments.of(Long.MIN_VALUE, Long.MIN_VALUE, Same),
+        Arguments.of(LEFTMOST_G_INDEX, LEFTMOST_G_INDEX, Same),
+        Arguments.of(RIGHTMOST_G_INDEX, RIGHTMOST_G_INDEX, Same),
         Arguments.of(0b1L, 0b11L, Predecessor),
         Arguments.of(0b1L, 0b10L, Predecessor),
         Arguments.of(0b10L, 0b11L, Left),
-        Arguments.of(0b11L, 0b10L, Right)
-    );
+        Arguments.of(0b1011L, 0b110L, Left),
+        Arguments.of(0b1010L, 0b110L, Left),
+        Arguments.of(0b1010L, 0b111L, Left),
+        Arguments.of(0b1011L, 0b1100L, Left),
+        Arguments.of(0b1011L, 0b101L, Successor),
+        Arguments.of(0b10L, RIGHTMOST_G_INDEX, Left),
+        Arguments.of(0b11L, RIGHTMOST_G_INDEX, Predecessor),
+        Arguments.of(0b10L, LEFTMOST_G_INDEX, Predecessor),
+        Arguments.of(0b11L, LEFTMOST_G_INDEX, Right),
+        Arguments.of(0b1L, Long.MAX_VALUE, Predecessor),
+        Arguments.of(0b10L, Long.MAX_VALUE, Left),
+        Arguments.of(0b11L, Long.MAX_VALUE, Predecessor),
+        Arguments.of(LEFTMOST_G_INDEX, SELF_G_INDEX, Successor),
+        Arguments.of(LEFTMOST_G_INDEX, LEFTMOST_G_INDEX + 1, Left),
+        Arguments.of(INVALID_G_INDEX, 0b1L, null),
+        Arguments.of(0b1L, INVALID_G_INDEX, null));
   }
 
   @ParameterizedTest
   @MethodSource("compareCases")
   void testCompare(long idx1, long idx2, NodeRelation expected) {
-    assertThat(gIdxCompare(idx1, idx2)).isEqualTo(expected);
+    if (expected != null) {
+      assertThat(gIdxCompare(idx1, idx2)).isEqualTo(expected);
+      assertThat(gIdxCompare(idx2, idx1)).isEqualTo(expected.inverse());
+    } else {
+      assertThatThrownBy(() -> gIdxCompare(idx1, idx2));
+    }
+  }
+
+  @Test
+  void testIsSelf() {
+    assertThat(gIdxIsSelf(SELF_G_INDEX)).isTrue();
+    assertThat(gIdxIsSelf(0b10)).isFalse();
+    assertThat(gIdxIsSelf(LEFTMOST_G_INDEX)).isFalse();
+    assertThat(gIdxIsSelf(RIGHTMOST_G_INDEX)).isFalse();
+  }
+
+  @Test
+  void testGetDepth() {
+    assertThat(gIdxGetDepth(SELF_G_INDEX)).isEqualTo(0);
+    assertThat(gIdxGetDepth(0b10)).isEqualTo(1);
+    assertThat(gIdxGetDepth(0b11)).isEqualTo(1);
+    assertThat(gIdxGetDepth(0b100)).isEqualTo(2);
+    assertThat(gIdxGetDepth(0b101)).isEqualTo(2);
+    assertThat(gIdxGetDepth(0b111)).isEqualTo(2);
+    assertThat(gIdxGetDepth(LEFTMOST_G_INDEX)).isEqualTo(63);
+    assertThat(gIdxGetDepth(RIGHTMOST_G_INDEX)).isEqualTo(63);
+    assertThat(gIdxGetDepth(gIdxLeftmostFrom(0b10010))).isEqualTo(63);
+    assertThatThrownBy(() -> gIdxGetDepth(INVALID_G_INDEX));
+  }
+
+  @Test
+  void testConstants() {
+    assertThat(SELF_G_INDEX).isEqualTo(1L);
+    assertThat(LEFTMOST_G_INDEX).isEqualTo(Long.MIN_VALUE);
+    assertThat(RIGHTMOST_G_INDEX).isEqualTo(-1L);
+  }
+
+  @Test
+  void testLeftRightGIndex() {
+    assertThat(gIdxLeftGIndex(SELF_G_INDEX)).isEqualTo(0b10L);
+    assertThat(gIdxRightGIndex(SELF_G_INDEX)).isEqualTo(0b11L);
+    assertThat(gIdxLeftGIndex(gIdxGetParent(LEFTMOST_G_INDEX))).isEqualTo(LEFTMOST_G_INDEX);
+    assertThat(gIdxRightGIndex(gIdxGetParent(RIGHTMOST_G_INDEX))).isEqualTo(RIGHTMOST_G_INDEX);
+    assertThat(gIdxLeftGIndex(0b10L)).isEqualTo(0b100L);
+    assertThat(gIdxRightGIndex(0b10L)).isEqualTo(0b101L);
+    assertThat(gIdxLeftGIndex(0b11L)).isEqualTo(0b110L);
+    assertThat(gIdxRightGIndex(0b11L)).isEqualTo(0b111L);
+    assertThatThrownBy(() -> gIdxLeftGIndex(INVALID_G_INDEX));
+    assertThatThrownBy(() -> gIdxLeftGIndex(LEFTMOST_G_INDEX));
+    assertThatThrownBy(() -> gIdxLeftGIndex(RIGHTMOST_G_INDEX));
+  }
+
+  @Test
+  void testChildGIndex() {
+    assertThat(gIdxChildGIndex(SELF_G_INDEX, 0, 0)).isEqualTo(SELF_G_INDEX);
+    assertThat(gIdxChildGIndex(LEFTMOST_G_INDEX, 0, 0)).isEqualTo(LEFTMOST_G_INDEX);
+    assertThat(gIdxChildGIndex(RIGHTMOST_G_INDEX, 0, 0)).isEqualTo(RIGHTMOST_G_INDEX);
+    assertThat(gIdxChildGIndex(SELF_G_INDEX, 0, 1)).isEqualTo(0b10L);
+    assertThat(gIdxChildGIndex(SELF_G_INDEX, 1, 1)).isEqualTo(0b11L);
+    assertThat(gIdxChildGIndex(SELF_G_INDEX, 0, 2)).isEqualTo(0b100L);
+    assertThat(gIdxChildGIndex(SELF_G_INDEX, 1, 2)).isEqualTo(0b101L);
+    assertThat(gIdxChildGIndex(SELF_G_INDEX, 2, 2)).isEqualTo(0b110L);
+    assertThat(gIdxChildGIndex(SELF_G_INDEX, 3, 2)).isEqualTo(0b111L);
+    assertThat(gIdxChildGIndex(0b100L, 0, 2)).isEqualTo(0b10000L);
+    assertThat(gIdxChildGIndex(0b100L, 1, 2)).isEqualTo(0b10001L);
+    assertThat(gIdxChildGIndex(0b100L, 2, 2)).isEqualTo(0b10010L);
+    assertThat(gIdxChildGIndex(0b100L, 3, 2)).isEqualTo(0b10011L);
+
+    assertThatThrownBy(() -> gIdxChildGIndex(INVALID_G_INDEX, 0, 2));
+    assertThatThrownBy(() -> gIdxChildGIndex(SELF_G_INDEX, 4, 2));
+    assertThatThrownBy(() -> gIdxChildGIndex(LEFTMOST_G_INDEX, 0, 1));
+    assertThatThrownBy(() -> gIdxChildGIndex(RIGHTMOST_G_INDEX, 0, 1));
+    assertThatThrownBy(() -> gIdxChildGIndex(gIdxGetParent(LEFTMOST_G_INDEX), 0, 2));
+  }
+
+  @Test
+  void testLeftRightmost() {
+    assertThat(gIdxLeftmostFrom(SELF_G_INDEX)).isEqualTo(LEFTMOST_G_INDEX);
+    assertThat(gIdxRightmostFrom(SELF_G_INDEX)).isEqualTo(RIGHTMOST_G_INDEX);
+    assertThat(gIdxLeftmostFrom(0b10)).isEqualTo(LEFTMOST_G_INDEX);
+    assertThat(gIdxRightmostFrom(0b11)).isEqualTo(RIGHTMOST_G_INDEX);
+    assertThat(gIdxLeftmostFrom(0b11)).isEqualTo(0b11L << 62);
+    assertThat(gIdxRightmostFrom(0b10))
+        .isEqualTo(0b1011111111111111_1111111111111111_1111111111111111_1111111111111111L);
+
+    assertThat(gIdxLeftmostFrom(LEFTMOST_G_INDEX)).isEqualTo(LEFTMOST_G_INDEX);
+    assertThat(gIdxRightmostFrom(LEFTMOST_G_INDEX)).isEqualTo(LEFTMOST_G_INDEX);
+    assertThat(gIdxLeftmostFrom(RIGHTMOST_G_INDEX)).isEqualTo(RIGHTMOST_G_INDEX);
+    assertThat(gIdxRightmostFrom(RIGHTMOST_G_INDEX)).isEqualTo(RIGHTMOST_G_INDEX);
   }
 }
