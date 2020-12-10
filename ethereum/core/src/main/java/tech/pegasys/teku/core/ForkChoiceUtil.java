c3345676d82979ad9ee5c936c7e6203888e1daff
Adrian Sutton <adrian.sutton@consensys.net>
2020-12-10 16:24:58 +1000

    Remove unused variable.

--- ethereum/core/src/main/java/tech/pegasys/teku/core/ForkChoiceUtil.java
+++ ethereum/core/src/main/java/tech/pegasys/teku/core/ForkChoiceUtil.java
@@ -28,8 +28,6 @@ import java.util.NavigableMap;
 import java.util.Optional;
 import java.util.TreeMap;
 import javax.annotation.CheckReturnValue;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.apache.tuweni.bytes.Bytes32;
 import tech.pegasys.teku.core.lookup.IndexedAttestationProvider;
 import tech.pegasys.teku.core.results.BlockImportResult;
@@ -48,7 +46,6 @@ import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.protoarray.ForkChoiceStrategy;
 
 public class ForkChoiceUtil {
-  private static final Logger LOG = LogManager.getLogger();
 
   public static UInt64 get_slots_since_genesis(ReadOnlyStore store, boolean useUnixTime) {
     UInt64 time = useUnixTime ? UInt64.valueOf(Instant.now().getEpochSecond()) : store.getTime();
