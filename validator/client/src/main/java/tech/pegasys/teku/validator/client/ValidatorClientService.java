b421b5fa61dbcfb4dc89c7b48ee8e05df5cf0861
Paul Harris <paul.harris@consensys.net>
2021-01-11 13:47:59 +1000

    spotless
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- validator/client/src/main/java/tech/pegasys/teku/validator/client/ValidatorClientService.java
+++ validator/client/src/main/java/tech/pegasys/teku/validator/client/ValidatorClientService.java
@@ -16,8 +16,6 @@ package tech.pegasys.teku.validator.client;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Map;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.hyperledger.besu.plugin.services.MetricsSystem;
 import tech.pegasys.teku.bls.BLSPublicKey;
 import tech.pegasys.teku.core.signatures.LocalSlashingProtector;
