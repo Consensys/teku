3c09f9468b4da341eae5b6fc2dbdb0b6a708b0dc
Adrian Sutton <adrian.sutton@consensys.net>
2021-01-15 07:10:43 +1000

    Fix option in help text (#3461)

--- teku/src/main/java/tech/pegasys/teku/cli/options/ValidatorOptions.java
+++ teku/src/main/java/tech/pegasys/teku/cli/options/ValidatorOptions.java
@@ -41,7 +41,7 @@ public class ValidatorOptions {
       names = {"--validators-graffiti-file"},
       paramLabel = "<GRAFFITI FILE>",
       description =
-          "File to load graffiti value to include during block creation. Value gets converted to bytes and padded to Bytes32.  If file reading fails during block creation, teku will fall back to any value supplied via --graffiti.",
+          "File to load graffiti value to include during block creation. Value gets converted to bytes and padded to Bytes32.  If file reading fails during block creation, teku will fall back to any value supplied via --validators-graffiti.",
       arity = "1")
   private Path graffitiFile;
 
