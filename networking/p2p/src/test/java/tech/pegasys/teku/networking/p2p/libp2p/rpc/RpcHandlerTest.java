0d337ddaddb3341acb798834eedf512ccbeb3b54
Meredith Baxter <meredith.baxter@consensys.net>
2021-01-25 15:02:05 -0500

    Fix tests

--- networking/p2p/src/test/java/tech/pegasys/teku/networking/p2p/libp2p/rpc/RpcHandlerTest.java
+++ networking/p2p/src/test/java/tech/pegasys/teku/networking/p2p/libp2p/rpc/RpcHandlerTest.java
@@ -16,7 +16,6 @@ package tech.pegasys.teku.networking.p2p.libp2p.rpc;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -25,6 +24,7 @@ import static org.mockito.Mockito.when;
 import io.libp2p.core.Connection;
 import io.libp2p.core.Stream;
 import io.libp2p.core.StreamPromise;
+import io.libp2p.core.multistream.ProtocolBinding;
 import io.libp2p.core.mux.StreamMuxer.Session;
 import java.util.concurrent.CompletableFuture;
 import kotlin.Unit;
@@ -40,6 +40,7 @@ import tech.pegasys.teku.networking.p2p.rpc.RpcRequestHandler;
 import tech.pegasys.teku.networking.p2p.rpc.RpcStream;
 import tech.pegasys.teku.networking.p2p.rpc.StreamTimeoutException;
 
+@SuppressWarnings("unchecked")
 public class RpcHandlerTest {
 
   StubAsyncRunner asyncRunner = new StubAsyncRunner();
@@ -48,7 +49,7 @@ public class RpcHandlerTest {
 
   Connection connection = mock(Connection.class);
   Session session = mock(Session.class);
-  StreamPromise<Object> streamPromise =
+  StreamPromise<Controller> streamPromise =
       new StreamPromise<>(new CompletableFuture<>(), new CompletableFuture<>());
   CompletableFuture<Unit> closeFuture = new CompletableFuture<>();
   SafeFuture<Void> writeFuture = new SafeFuture<>();
@@ -61,7 +62,7 @@ public class RpcHandlerTest {
   @BeforeEach
   void init() {
     when(connection.muxerSession()).thenReturn(session);
-    when(session.createStream(anyList())).thenReturn(streamPromise);
+    when(session.createStream((ProtocolBinding<Controller>) any())).thenReturn(streamPromise);
     when(connection.closeFuture()).thenReturn(closeFuture);
 
     when(controller.getRpcStream()).thenReturn(rpcStream);
