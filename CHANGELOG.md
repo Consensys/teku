b6d1d87e6d05b39c6d8138a7c039b8a58e7583f3
Cem Ozer <cemozer2018@u.northwestern.edu>
2021-02-12 09:23:22 -0500

    Upgrade to BLST 0.3.2 (#3514)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@
   chain head block is imported. The `--Xvalidators-dependent-root-enabled` option can be used to switch to the new behaviour now for testing.
   Note: this should be applied to both the beacon node and validator client if running separately.
 - The `/teku/v1/beacon/states/:state_id` endpoint has been deprecated in favor of the standard API `/eth/v1/debug/beacon/states/:state_id` which now returns the state as SSZ when the `Accept: application/octet-stream` header is specified on the request.
+- Upgraded to use BLS implementation BLST version 0.3.2. 
 
 ## Current Releases
 For information on changes in released versions of Teku, see the [releases page](https://github.com/ConsenSys/teku/releases).
--- bls/src/test/java/tech/pegasys/teku/bls/BLSTest.java
+++ bls/src/test/java/tech/pegasys/teku/bls/BLSTest.java
@@ -20,7 +20,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -76,7 +75,7 @@ public abstract class BLSTest {
 
   @Test
   // The empty signature is not a valid signature
-  void succeedsWhenPassingEmptySignatureToAggregateSignaturesThrowsIllegalArgumentException() {
+  void passingEmptySignatureToAggregateSignaturesThrowsIllegalArgumentException() {
     BLSSignature signature1 = BLSTestUtil.randomSignature(1);
     BLSSignature signature2 = BLSSignature.empty();
     BLSSignature signature3 = BLSTestUtil.randomSignature(3);
@@ -325,9 +324,7 @@ public abstract class BLSTest {
 
   @Test
   void succeedsWhenWeCanSignAndVerifyWithValidDST() {
-    final Bytes DST =
-        Bytes.wrap(
-            "BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_".getBytes(StandardCharsets.US_ASCII));
+    final String DST = "BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_";
     final BLSKeyPair keyPair = BLSTestUtil.randomKeyPair(42);
     final Bytes message = Bytes.wrap("Hello, world!".getBytes(UTF_8));
     final BLSSignature signature = BLS.sign(keyPair.getSecretKey(), message, DST);
@@ -336,9 +333,7 @@ public abstract class BLSTest {
 
   @Test
   void verifyWithDifferentDSTFails() {
-    final Bytes DST =
-        Bytes.wrap(
-            "BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_".getBytes(StandardCharsets.US_ASCII));
+    final String DST = "BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_";
     final BLSKeyPair keyPair = BLSTestUtil.randomKeyPair(42);
     final Bytes message = Bytes.wrap("Hello, world!".getBytes(UTF_8));
     final BLSSignature signature = BLS.sign(keyPair.getSecretKey(), message, DST);
