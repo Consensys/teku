16b0391071af456a2fe9d78034b2780546a44a32
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2021-03-10 22:41:23 +0300

    Fix SszMutablePrimitiveCollection.setAll()

--- ssz/src/main/java/tech/pegasys/teku/ssz/backing/collections/SszMutablePrimitiveCollection.java
+++ ssz/src/main/java/tech/pegasys/teku/ssz/backing/collections/SszMutablePrimitiveCollection.java
@@ -35,7 +35,7 @@ public interface SszMutablePrimitiveCollection<
     clear();
     int idx = 0;
     for (ElementT newChild : newChildren) {
-      setElement(idx, newChild);
+      setElement(idx++, newChild);
     }
   }
 
