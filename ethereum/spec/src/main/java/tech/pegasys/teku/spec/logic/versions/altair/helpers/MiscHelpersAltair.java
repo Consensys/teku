a95b69af9c162dec48183e0e0f0ce9ae3dd8b99d
Meredith Baxter <meredith.baxter@consensys.net>
2021-03-18 14:01:25 -0400

    Add misc helpers

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/logic/versions/altair/helpers/MiscHelpersAltair.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/logic/versions/altair/helpers/MiscHelpersAltair.java
@@ -13,6 +13,56 @@
 
 package tech.pegasys.teku.spec.logic.versions.altair.helpers;
 
+import java.util.List;
+import tech.pegasys.teku.spec.constants.IncentivizationWeights;
+import tech.pegasys.teku.spec.constants.ParticipationFlags;
 import tech.pegasys.teku.spec.logic.common.helpers.MiscHelpers;
 
-public class MiscHelpersAltair extends MiscHelpers {}
+public class MiscHelpersAltair extends MiscHelpers {
+  private static final List<ParticipationIndexAndWeight> flagIndicesAndWeights =
+      List.of(
+          new ParticipationIndexAndWeight(
+              ParticipationFlags.TIMELY_HEAD_FLAG_INDEX, IncentivizationWeights.TIMELY_HEAD_WEIGHT),
+          new ParticipationIndexAndWeight(
+              ParticipationFlags.TIMELY_SOURCE_FLAG_INDEX,
+              IncentivizationWeights.TIMELY_SOURCE_WEIGHT),
+          new ParticipationIndexAndWeight(
+              ParticipationFlags.TIMELY_TARGET_FLAG_INDEX,
+              IncentivizationWeights.TIMELY_TARGET_WEIGHT));
+
+  public List<ParticipationIndexAndWeight> getFlagIndicesAndWeights() {
+    return flagIndicesAndWeights;
+  }
+
+  public byte addFlag(final byte participationFlags, final int flagIndex) {
+    final int flag = indexToFlag(flagIndex);
+    return (byte) (participationFlags | flag);
+  }
+
+  public boolean hasFlag(final byte participationFlags, final int flagIndex) {
+    final int flag = indexToFlag(flagIndex);
+    return (participationFlags & flag) == flag;
+  }
+
+  protected int indexToFlag(final int flagIndex) {
+    return 1 << flagIndex;
+  }
+
+  public static class ParticipationIndexAndWeight {
+    final int index;
+    final int weight;
+
+    private ParticipationIndexAndWeight(final int index, final int weight) {
+      this.index = index;
+      this.weight = weight;
+    }
+
+    public int getIndex() {
+      return index;
+    }
+
+    public int getWeight() {
+      return weight;
+    }
+  }
+}
