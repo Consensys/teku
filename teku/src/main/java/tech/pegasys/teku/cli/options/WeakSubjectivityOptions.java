d32c91e8d2344e2ca5b47685ca9cd31267785cea
Paul Harris <paul.harris@consensys.net>
2021-03-19 13:42:41 +1000

    Update --ws-checkpoint to accept a URL
    `--ws-checkpoint` now accepts either root:epoch, or a URL containing key `ws_checkpoint` (formatted as root:epoch) as part of a json payload.
    
    If the checkpoint passed starts with '0x', then it will be loaded as a literal checkpoint reference, otherwise it will attempt to load from url.
    
    If the checkpoint doesn't start in 0x, and it doesnt load from URL, teku will fail with a CLI error.
    
    Fixes #3531
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- teku/src/main/java/tech/pegasys/teku/cli/options/WeakSubjectivityOptions.java
+++ teku/src/main/java/tech/pegasys/teku/cli/options/WeakSubjectivityOptions.java
@@ -13,21 +13,27 @@
 
 package tech.pegasys.teku.cli.options;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.InputStream;
+import java.util.Optional;
 import picocli.CommandLine;
 import tech.pegasys.teku.cli.converter.CheckpointConverter;
 import tech.pegasys.teku.config.TekuConfiguration;
+import tech.pegasys.teku.infrastructure.io.resource.ResourceLoader;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.spec.datastructures.state.Checkpoint;
 
 public class WeakSubjectivityOptions {
-
   @CommandLine.Option(
-      converter = CheckpointConverter.class,
       names = {"--ws-checkpoint"},
-      paramLabel = "<BLOCK_ROOT>:<EPOCH_NUMBER>",
-      description = "A recent checkpoint within the weak subjectivity period.",
+      paramLabel = "<STRING>",
+      description =
+          "A recent checkpoint within the weak subjectivity period. "
+              + "Should be a string containing <BLOCK_ROOT>:<EPOCH_NUMBER> "
+              + "or a URL containing the field ws_checkpoint with the same information.",
       arity = "1")
-  private Checkpoint weakSubjectivityCheckpoint;
+  private String weakSubjectivityCheckpoint;
 
   @CommandLine.Option(
       names = {"--Xws-suppress-errors-until-epoch"},
@@ -41,12 +47,43 @@ public class WeakSubjectivityOptions {
   public TekuConfiguration.Builder configure(TekuConfiguration.Builder builder) {
     return builder.weakSubjectivity(
         wsBuilder -> {
-          if (weakSubjectivityCheckpoint != null) {
-            wsBuilder.weakSubjectivityCheckpoint(weakSubjectivityCheckpoint);
-          }
+          getWeakSubjectivityCheckpoint().map(wsBuilder::weakSubjectivityCheckpoint);
           if (suppressWSPeriodChecksUntilEpoch != null) {
             wsBuilder.suppressWSPeriodChecksUntilEpoch(suppressWSPeriodChecksUntilEpoch);
           }
         });
+  }
+
+  private Optional<Checkpoint> getWeakSubjectivityCheckpoint() {
+    if (weakSubjectivityCheckpoint != null) {
+      CheckpointConverter checkpointConverter = new CheckpointConverter();
+      try {
+        if (weakSubjectivityCheckpoint.startsWith("0x")) {
+          return Optional.of(checkpointConverter.convert(weakSubjectivityCheckpoint));
+        } else {
+          ObjectMapper mapper = new ObjectMapper();
+          final InputStream inputStream =
+              ResourceLoader.urlOrFile()
+                  .load(weakSubjectivityCheckpoint)
+                  .orElseThrow(
+                      () ->
+                          new IllegalArgumentException(
+                              "Failed to load resource URL " + weakSubjectivityCheckpoint));
+          final String checkpointString =
+              mapper.readTree(inputStream).get("ws_checkpoint").asText();
+          return Optional.of(checkpointConverter.convert(checkpointString));
+        }
+      } catch (JsonParseException e) {
+        throw new IllegalArgumentException(
+            "Invalid value for option '--ws-checkpoint': "
+                + "ws_checkpoint was not found at URL "
+                + weakSubjectivityCheckpoint,
+            e);
+      } catch (Exception ex) {
+        throw new CommandLine.PicocliException(
+            "Invalid value for option '--ws-checkpoint': " + ex.getMessage(), ex);
+      }
+    }
+    return Optional.empty();
   }
 }
