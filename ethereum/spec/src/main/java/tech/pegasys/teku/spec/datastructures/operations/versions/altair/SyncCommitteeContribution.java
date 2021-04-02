1f78dd78d1884a71b0b0f7033b6adae649e1df74
Adrian Sutton <adrian.sutton@consensys.net>
2021-04-02 13:43:06 +1000

    Add sync committee gossip data structures (#3815)

---
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/datastructures/operations/versions/altair/SyncCommitteeContribution.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2021 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.spec.datastructures.operations.versions.altair;
+
+import org.apache.tuweni.bytes.Bytes32;
+import tech.pegasys.teku.bls.BLSSignature;
+import tech.pegasys.teku.infrastructure.unsigned.UInt64;
+import tech.pegasys.teku.spec.datastructures.type.SszSignature;
+import tech.pegasys.teku.ssz.collections.SszBitvector;
+import tech.pegasys.teku.ssz.containers.Container5;
+import tech.pegasys.teku.ssz.containers.ContainerSchema5;
+import tech.pegasys.teku.ssz.primitive.SszBytes32;
+import tech.pegasys.teku.ssz.primitive.SszUInt64;
+import tech.pegasys.teku.ssz.tree.TreeNode;
+
+public class SyncCommitteeContribution
+    extends Container5<
+        SyncCommitteeContribution, SszUInt64, SszBytes32, SszUInt64, SszBitvector, SszSignature> {
+
+  protected SyncCommitteeContribution(
+      final ContainerSchema5<
+              SyncCommitteeContribution,
+              SszUInt64,
+              SszBytes32,
+              SszUInt64,
+              SszBitvector,
+              SszSignature>
+          schema) {
+    super(schema);
+  }
+
+  protected SyncCommitteeContribution(
+      final ContainerSchema5<
+              SyncCommitteeContribution,
+              SszUInt64,
+              SszBytes32,
+              SszUInt64,
+              SszBitvector,
+              SszSignature>
+          schema,
+      final TreeNode backingNode) {
+    super(schema, backingNode);
+  }
+
+  protected SyncCommitteeContribution(
+      final ContainerSchema5<
+              SyncCommitteeContribution,
+              SszUInt64,
+              SszBytes32,
+              SszUInt64,
+              SszBitvector,
+              SszSignature>
+          schema,
+      final SszUInt64 arg0,
+      final SszBytes32 arg1,
+      final SszUInt64 arg2,
+      final SszBitvector arg3,
+      final SszSignature arg4) {
+    super(schema, arg0, arg1, arg2, arg3, arg4);
+  }
+
+  public UInt64 getSlot() {
+    return getField0().get();
+  }
+
+  public Bytes32 getBeaconBlockRoot() {
+    return getField1().get();
+  }
+
+  public UInt64 getSubcommitteeIndex() {
+    return getField2().get();
+  }
+
+  public SszBitvector getAggregationBits() {
+    return getField3();
+  }
+
+  public BLSSignature getSignature() {
+    return getField4().getSignature();
+  }
+}
