f202bb1bf453ac00ff9c62b45bd3f39dd336f5ef
Adrian Sutton <adrian.sutton@consensys.net>
2021-05-17 15:00:38 +1000

    Spotless this time...

--- validator/coordinator/src/main/java/tech/pegasys/teku/validator/coordinator/performance/SyncCommitteePerformanceTracker.java
+++ validator/coordinator/src/main/java/tech/pegasys/teku/validator/coordinator/performance/SyncCommitteePerformanceTracker.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.NavigableMap;
 import java.util.Optional;
 import java.util.Set;
