c8426a7a433730dc6e95ce9698eda2106a692037
Adrian Sutton <adrian.sutton@consensys.net>
2021-05-29 15:40:30 +1000

    Spotless.

--- pow/src/main/java/tech/pegasys/teku/pow/TimeBasedEth1HeadTracker.java
+++ pow/src/main/java/tech/pegasys/teku/pow/TimeBasedEth1HeadTracker.java
@@ -18,7 +18,6 @@ import static com.google.common.base.Preconditions.checkState;
 import com.google.common.base.Throwables;
 import java.math.BigInteger;
 import java.time.Duration;
-import java.util.Objects;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.web3j.protocol.core.methods.response.EthBlock.Block;
