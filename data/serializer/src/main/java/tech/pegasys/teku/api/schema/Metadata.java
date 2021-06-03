f18feb8189b578b7268a5d7aac721bf010d72092
Paul Harris <paul.harris@consensys.net>
2021-06-03 10:21:54 +1000

    add syncnets to network identity metadata for altair (#4053)
    fixes #4052
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- data/serializer/src/main/java/tech/pegasys/teku/api/schema/Metadata.java
+++ data/serializer/src/main/java/tech/pegasys/teku/api/schema/Metadata.java
@@ -16,10 +16,12 @@ package tech.pegasys.teku.api.schema;
 import static tech.pegasys.teku.api.schema.SchemaConstants.PATTERN_UINT64;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Objects;
 import tech.pegasys.teku.spec.datastructures.networking.libp2p.rpc.metadata.MetadataMessage;
+import tech.pegasys.teku.spec.datastructures.networking.libp2p.rpc.metadata.versions.altair.MetadataMessageAltair;
 
 @Schema(
     description =
@@ -43,18 +45,39 @@ public class Metadata {
           "Bitvector representing the node's persistent attestation subnet subscriptions.")
   public final String attestationSubnetSubscriptions;
 
+  @JsonProperty("syncnets")
+  @Schema(
+      type = "string",
+      pattern = "^0x[a-fA-F0-9]{2,}$",
+      description =
+          "Bitvector representing the node's persistent attestation subnet subscriptions.")
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  public final String syncCommitteeSubscriptions;
+
   @JsonCreator
   public Metadata(
       @JsonProperty("seq_number") final String sequenceNumber,
-      @JsonProperty("attnets") final String attestationSubnetSubscriptions) {
+      @JsonProperty("attnets") final String attestationSubnetSubscriptions,
+      @JsonProperty("syncnets") final String syncCommitteeSubscriptions) {
     this.sequenceNumber = sequenceNumber;
     this.attestationSubnetSubscriptions = attestationSubnetSubscriptions;
+    this.syncCommitteeSubscriptions = syncCommitteeSubscriptions;
   }
 
   public Metadata(final MetadataMessage metadataMessage) {
     this.sequenceNumber = metadataMessage.getSeqNumber().toString();
     this.attestationSubnetSubscriptions =
         metadataMessage.getAttnets().sszSerialize().toHexString().toLowerCase();
+    if (metadataMessage instanceof MetadataMessageAltair) {
+      this.syncCommitteeSubscriptions =
+          ((MetadataMessageAltair) metadataMessage)
+              .getSyncnets()
+              .sszSerialize()
+              .toHexString()
+              .toLowerCase();
+    } else {
+      this.syncCommitteeSubscriptions = null;
+    }
   }
 
   @Override
@@ -63,11 +86,12 @@ public class Metadata {
     if (o == null || getClass() != o.getClass()) return false;
     final Metadata metadata = (Metadata) o;
     return Objects.equals(sequenceNumber, metadata.sequenceNumber)
-        && Objects.equals(attestationSubnetSubscriptions, metadata.attestationSubnetSubscriptions);
+        && Objects.equals(attestationSubnetSubscriptions, metadata.attestationSubnetSubscriptions)
+        && Objects.equals(syncCommitteeSubscriptions, metadata.syncCommitteeSubscriptions);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(sequenceNumber, attestationSubnetSubscriptions);
+    return Objects.hash(sequenceNumber, attestationSubnetSubscriptions, syncCommitteeSubscriptions);
   }
 }
