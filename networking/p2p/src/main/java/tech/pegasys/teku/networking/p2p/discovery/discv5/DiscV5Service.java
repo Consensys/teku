cad5eeec05649500d0804bcea29ac2ae58821986
Adrian Sutton <adrian.sutton@consensys.net>
2021-07-03 08:57:03 +1000

    Periodically ping bootnodes (#4131)
    Ensures we recover after a network outage that causes all discovery nodes to be dropped.

--- networking/p2p/src/main/java/tech/pegasys/teku/networking/p2p/discovery/discv5/DiscV5Service.java
+++ networking/p2p/src/main/java/tech/pegasys/teku/networking/p2p/discovery/discv5/DiscV5Service.java
@@ -13,9 +13,13 @@
 
 package tech.pegasys.teku.networking.p2p.discovery.discv5;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.units.bigints.UInt64;
 import org.ethereum.beacon.discovery.DiscoverySystem;
@@ -23,9 +27,12 @@ import org.ethereum.beacon.discovery.DiscoverySystemBuilder;
 import org.ethereum.beacon.discovery.schema.EnrField;
 import org.ethereum.beacon.discovery.schema.NodeRecord;
 import org.ethereum.beacon.discovery.schema.NodeRecordBuilder;
+import org.ethereum.beacon.discovery.schema.NodeRecordFactory;
 import org.ethereum.beacon.discovery.schema.NodeRecordInfo;
 import org.ethereum.beacon.discovery.schema.NodeStatus;
 import org.ethereum.beacon.discovery.storage.NewAddressHandler;
+import tech.pegasys.teku.infrastructure.async.AsyncRunner;
+import tech.pegasys.teku.infrastructure.async.Cancellable;
 import tech.pegasys.teku.infrastructure.async.SafeFuture;
 import tech.pegasys.teku.networking.p2p.discovery.DiscoveryConfig;
 import tech.pegasys.teku.networking.p2p.discovery.DiscoveryPeer;
@@ -38,43 +45,43 @@ import tech.pegasys.teku.spec.schemas.SchemaDefinitionsSupplier;
 import tech.pegasys.teku.storage.store.KeyValueStore;
 
 public class DiscV5Service extends Service implements DiscoveryService {
+  private static final Logger LOG = LogManager.getLogger();
   private static final String SEQ_NO_STORE_KEY = "local-enr-seqno";
+  private static final Duration BOOTNODE_REFRESH_DELAY = Duration.ofMinutes(2);
+  private final AsyncRunner asyncRunner;
   private final SchemaDefinitionsSupplier currentSchemaDefinitionsSupplier;
-
-  public static DiscoveryService create(
-      final DiscoveryConfig discoConfig,
-      final NetworkConfig p2pConfig,
-      final KeyValueStore<String, Bytes> kvStore,
-      final Bytes privateKey,
-      final SchemaDefinitionsSupplier currentSchemaDefinitionsSupplier) {
-    return new DiscV5Service(
-        discoConfig, p2pConfig, kvStore, privateKey, currentSchemaDefinitionsSupplier);
-  }
 
   private final DiscoverySystem discoverySystem;
   private final KeyValueStore<String, Bytes> kvStore;
+  private final List<NodeRecord> bootnodes;
+  private volatile Cancellable bootnodeRefreshTask;
 
-  private DiscV5Service(
+  public DiscV5Service(
+      final AsyncRunner asyncRunner,
       final DiscoveryConfig discoConfig,
       final NetworkConfig p2pConfig,
       final KeyValueStore<String, Bytes> kvStore,
       final Bytes privateKey,
       final SchemaDefinitionsSupplier currentSchemaDefinitionsSupplier) {
+    this.asyncRunner = asyncRunner;
     this.currentSchemaDefinitionsSupplier = currentSchemaDefinitionsSupplier;
     final String listenAddress = p2pConfig.getNetworkInterface();
     final int listenPort = p2pConfig.getListenPort();
     final String advertisedAddress = p2pConfig.getAdvertisedIp();
     final int advertisedPort = p2pConfig.getAdvertisedPort();
-    final List<String> bootnodes = discoConfig.getBootnodes();
     final UInt64 seqNo =
         kvStore.get(SEQ_NO_STORE_KEY).map(UInt64::fromBytes).orElse(UInt64.ZERO).add(1);
     final NewAddressHandler maybeUpdateNodeRecordHandler =
         maybeUpdateNodeRecord(p2pConfig.hasUserExplicitlySetAdvertisedIp());
-    discoverySystem =
+    this.bootnodes =
+        discoConfig.getBootnodes().stream()
+            .map(NodeRecordFactory.DEFAULT::fromEnr)
+            .collect(Collectors.toList());
+    this.discoverySystem =
         new DiscoverySystemBuilder()
             .listen(listenAddress, listenPort)
             .privateKey(privateKey)
-            .bootnodes(bootnodes.toArray(new String[0]))
+            .bootnodes(bootnodes)
             .localNodeRecord(
                 new NodeRecordBuilder()
                     .privateKey(privateKey)
@@ -103,11 +110,28 @@ public class DiscV5Service extends Service implements DiscoveryService {
 
   @Override
   protected SafeFuture<?> doStart() {
-    return SafeFuture.of(discoverySystem.start());
+    return SafeFuture.of(discoverySystem.start())
+        .thenRun(
+            () ->
+                this.bootnodeRefreshTask =
+                    asyncRunner.runWithFixedDelay(
+                        this::pingBootnodes,
+                        BOOTNODE_REFRESH_DELAY,
+                        error -> LOG.error("Failed to contact discovery bootnodes", error)));
+  }
+
+  private void pingBootnodes() {
+    bootnodes.forEach(
+        bootnode ->
+            SafeFuture.of(discoverySystem.ping(bootnode))
+                .finish(error -> LOG.debug("Bootnode {} is unresponsive", bootnode)));
   }
 
   @Override
   protected SafeFuture<?> doStop() {
+    final Cancellable refreshTask = this.bootnodeRefreshTask;
+    this.bootnodeRefreshTask = null;
+    refreshTask.cancel();
     discoverySystem.stop();
     return SafeFuture.completedFuture(null);
   }
