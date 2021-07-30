e5ff2b8f7583de08bbd4e8a44978040e6f72c5d9
Paul Harris <paul.harris@consensys.net>
2021-07-30 15:48:27 +1000

    use event topics to show contribution gossip
    Now that there is a contribution_and_proof http event, listen for it during an acceptance test to show that contribution_and_proof objects are coming from validators remote to the beacon node, indicating that the contributions are being successfully gossipped.
    
    fixes #4191
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

---
+++ acceptance-tests/src/testFixtures/java/tech/pegasys/teku/test/acceptance/dsl/Eth2EventHandler.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2021 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.test.acceptance.dsl;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Eth2EventHandler implements EventHandler {
+  private final List<PackedMessage> eventList = new ArrayList<>();
+
+  @Override
+  public void onOpen() {}
+
+  @Override
+  public void onClosed() {}
+
+  @Override
+  public void onMessage(final String event, final MessageEvent messageEvent) {
+    eventList.add(new PackedMessage(event, messageEvent));
+  }
+
+  public List<PackedMessage> getMessages() {
+    return eventList;
+  }
+
+  @Override
+  public void onComment(final String comment) {}
+
+  @Override
+  public void onError(final Throwable t) {}
+
+  public static class PackedMessage {
+    private String event;
+    private MessageEvent messageEvent;
+
+    public PackedMessage(final String event, final MessageEvent messageEvent) {
+      this.event = event;
+      this.messageEvent = messageEvent;
+    }
+
+    public String getEvent() {
+      return event;
+    }
+
+    public MessageEvent getMessageEvent() {
+      return messageEvent;
+    }
+  }
+}
