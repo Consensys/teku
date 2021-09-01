15c08235671368d272ba0e4bff62c355d5581da3
Adrian Sutton <adrian.sutton@consensys.net>
2021-09-02 07:13:00 +1000

    Add generic method to iterate while skipping levels.

---
+++ ssz/src/main/java/tech/pegasys/teku/ssz/schema/impl/IterationUtil.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2021 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.ssz.schema.impl;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static tech.pegasys.teku.ssz.tree.GIndexUtil.gIdxCompose;
+
+import org.apache.tuweni.bytes.Bytes32;
+import tech.pegasys.teku.ssz.tree.GIndexUtil;
+import tech.pegasys.teku.ssz.tree.TreeNode;
+
+public class IterationUtil {
+
+  public static void visitNodesToDepth(
+      final NodeVisitor nodeVisitor,
+      final int maxCompressionDepth,
+      final TreeNode rootNode,
+      final long rootGIndex,
+      final int depthToVisit) {
+    checkArgument(depthToVisit > 0, "Depth must be positive");
+    if (nodeVisitor.canSkipBranch(rootNode.hashTreeRoot(), rootGIndex)) {
+      return;
+    }
+    if (depthToVisit <= maxCompressionDepth) {
+      visitChildNodesAtDepth(nodeVisitor, rootNode, rootGIndex, depthToVisit);
+    } else {
+      visitIntermediateBranches(
+          nodeVisitor, maxCompressionDepth, rootNode, rootGIndex, depthToVisit);
+    }
+  }
+
+  private static void visitIntermediateBranches(
+      final NodeVisitor nodeVisitor,
+      final int maxCompressionDepth,
+      final TreeNode rootNode,
+      final long rootGIndex,
+      final int depthToVisit) {
+    // Max compression depth exceeded so will need to record some interim branch nodes
+    final int childCount = Math.toIntExact(1L << maxCompressionDepth);
+    final Bytes32[] childRoots = new Bytes32[childCount];
+    final int remainingDepth = depthToVisit - maxCompressionDepth;
+    for (int childIndex = 0; childIndex < childCount; childIndex++) {
+      final long childRelativeGIndex =
+          GIndexUtil.gIdxChildGIndex(GIndexUtil.SELF_G_INDEX, childIndex, maxCompressionDepth);
+      final TreeNode childNode = rootNode.get(childRelativeGIndex);
+      childRoots[childIndex] = childNode.hashTreeRoot();
+      visitNodesToDepth(
+          nodeVisitor,
+          maxCompressionDepth,
+          childNode,
+          gIdxCompose(rootGIndex, childRelativeGIndex),
+          remainingDepth);
+    }
+    nodeVisitor.onBranchNode(rootNode.hashTreeRoot(), rootGIndex, maxCompressionDepth, childRoots);
+  }
+
+  private static void visitChildNodesAtDepth(
+      final NodeVisitor nodeVisitor,
+      final TreeNode rootNode,
+      final long rootGIndex,
+      final int depthToVisit) {
+    final int childCount = Math.toIntExact(1L << depthToVisit);
+    final Bytes32[] childRoots = new Bytes32[childCount];
+    for (int childIndex = 0; childIndex < childCount; childIndex++) {
+      final long childRelativeGIndex =
+          GIndexUtil.gIdxChildGIndex(GIndexUtil.SELF_G_INDEX, childIndex, depthToVisit);
+      final TreeNode childNode = rootNode.get(childRelativeGIndex);
+      childRoots[childIndex] = childNode.hashTreeRoot();
+      nodeVisitor.onTargetDepthNode(childNode, gIdxCompose(rootGIndex, childRelativeGIndex));
+    }
+    nodeVisitor.onBranchNode(rootNode.hashTreeRoot(), rootGIndex, depthToVisit, childRoots);
+  }
+
+  public interface NodeVisitor {
+
+    /**
+     * Called prior to visiting a branch or its descendants to determine if the branch needs to be
+     * visited.
+     *
+     * @param root the hash tree root of the branch node
+     * @param gIndex the generalized index of the branch node
+     * @return true if the branch node and all its descendants can be skipped, false to iterate into
+     *     the descendants
+     */
+    boolean canSkipBranch(Bytes32 root, long gIndex);
+
+    /**
+     * Called when an intermediate branch node is visited. Multiple levels of branch nodes may be
+     * skipped to optimise iteration and storage, in which case the children
+     *
+     * @param root the hash tree root of the branch node
+     * @param gIndex the generalised index of the branch node
+     * @param depth the number of tree levels being skipped. ie the depth of the tree from the
+     *     branch node to the children
+     * @param children the non-empty children at the specified depth from the branch node.
+     */
+    void onBranchNode(Bytes32 root, long gIndex, int depth, Bytes32[] children);
+
+    /**
+     * Called when a descendant node at the requested depth is reached.
+     *
+     * @param node the node at the target depth
+     * @param gIndex the generalized index of the node
+     */
+    void onTargetDepthNode(TreeNode node, long gIndex);
+  }
+}
---
+++ ssz/src/test/java/tech/pegasys/teku/ssz/schema/impl/IterationUtilTest.java
@@ -0,0 +1,278 @@
+/*
+ * Copyright 2021 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.ssz.schema.impl;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+import static tech.pegasys.teku.ssz.tree.GIndexUtil.SELF_G_INDEX;
+
+import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.bytes.Bytes32;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.verification.VerificationMode;
+import tech.pegasys.teku.ssz.schema.impl.IterationUtil.NodeVisitor;
+import tech.pegasys.teku.ssz.tree.GIndexUtil;
+import tech.pegasys.teku.ssz.tree.LeafNode;
+import tech.pegasys.teku.ssz.tree.TreeNode;
+import tech.pegasys.teku.ssz.tree.TreeUtil;
+
+class IterationUtilTest {
+
+  private static final int UNLIMITED_COMPRESSION = Integer.MAX_VALUE;
+  private final NodeVisitor visitor = mock(NodeVisitor.class);
+  private final InOrder inOrder = inOrder(visitor);
+
+  @BeforeEach
+  void setUp() {
+    when(visitor.canSkipBranch(any(), anyLong())).thenReturn(false);
+  }
+
+  @Test
+  void shouldVisitBothChildrenOfBranchNode() {
+    final LeafNode left = LeafNode.create(Bytes.of(1));
+    final LeafNode right = LeafNode.create(Bytes.of(2));
+    final TreeNode rootNode = TreeUtil.createTree(List.of(left, right));
+    IterationUtil.visitNodesToDepth(visitor, UNLIMITED_COMPRESSION, rootNode, SELF_G_INDEX, 1);
+    inOrder.verify(visitor).onTargetDepthNode(left, GIndexUtil.LEFT_CHILD_G_INDEX);
+    inOrder.verify(visitor).onTargetDepthNode(right, GIndexUtil.RIGHT_CHILD_G_INDEX);
+    inOrder
+        .verify(visitor)
+        .onBranchNode(rootNode.hashTreeRoot(), SELF_G_INDEX, 1, roots(left, right));
+    inOrder.verifyNoMoreInteractions();
+  }
+
+  @Test
+  void shouldVisitChildrenAtSpecifiedDepth() {
+    final int depth = 5;
+    final List<TreeNode> children = createChildrenForDepth(depth);
+    final TreeNode rootNode = TreeUtil.createTree(children, depth);
+
+    IterationUtil.visitNodesToDepth(visitor, UNLIMITED_COMPRESSION, rootNode, SELF_G_INDEX, depth);
+
+    verifyChildrenVisited(children, depth);
+    inOrder
+        .verify(visitor)
+        .onBranchNode(rootNode.hashTreeRoot(), SELF_G_INDEX, depth, roots(children));
+    inOrder.verifyNoMoreInteractions();
+  }
+
+  @Test
+  void shouldReportGeneralisedIndexCorrectlyWhenRootNodeIsNotSelfIndex() {
+    final int rootGIndex = 12;
+    final LeafNode left = LeafNode.create(Bytes.of(1));
+    final LeafNode right = LeafNode.create(Bytes.of(2));
+    final TreeNode rootNode = TreeUtil.createTree(List.of(left, right));
+    IterationUtil.visitNodesToDepth(visitor, UNLIMITED_COMPRESSION, rootNode, rootGIndex, 1);
+    inOrder.verify(visitor).onTargetDepthNode(left, GIndexUtil.gIdxLeftGIndex(rootGIndex));
+    inOrder.verify(visitor).onTargetDepthNode(right, GIndexUtil.gIdxRightGIndex(rootGIndex));
+    inOrder
+        .verify(visitor)
+        .onBranchNode(rootNode.hashTreeRoot(), rootGIndex, 1, roots(left, right));
+    inOrder.verifyNoMoreInteractions();
+  }
+
+  @Test
+  void shouldVisitIntermediateBranchesWhenDepthExceedsMaxCompressionDepth_evenInterval() {
+    final int depth = 4;
+    final int maxCompressionDepth = 2;
+    final List<TreeNode> children = createChildrenForDepth(depth);
+    final TreeNode rootNode = TreeUtil.createTree(children, depth);
+
+    IterationUtil.visitNodesToDepth(visitor, maxCompressionDepth, rootNode, SELF_G_INDEX, depth);
+
+    // Visit branch nodes 5 levels deep
+    final List<TreeNode> branchNodes = getNodesAtDepth(rootNode, maxCompressionDepth);
+    // Children should be evenly split between intermediate branch nodes
+    final int childrenPerBranchNode = children.size() / branchNodes.size();
+    for (int i = 0; i < branchNodes.size(); i++) {
+      final TreeNode branchNode = branchNodes.get(i);
+      final long branchGIndex = GIndexUtil.gIdxChildGIndex(SELF_G_INDEX, i, maxCompressionDepth);
+      final int firstChildIndex = i * childrenPerBranchNode;
+      final List<TreeNode> branchChildren =
+          children.subList(firstChildIndex, firstChildIndex + childrenPerBranchNode);
+      // Visit children first
+      verifyChildrenVisited(branchGIndex, branchChildren, maxCompressionDepth);
+      // Then branch node itself
+      inOrder
+          .verify(visitor)
+          .onBranchNode(
+              branchNode.hashTreeRoot(), branchGIndex, maxCompressionDepth, roots(branchChildren));
+    }
+    // Then root node
+    inOrder
+        .verify(visitor)
+        .onBranchNode(
+            rootNode.hashTreeRoot(), SELF_G_INDEX, maxCompressionDepth, roots(branchNodes));
+  }
+
+  @Test
+  void shouldVisitIntermediateBranchesWhenDepthExceedsMaxCompressionDepth_unevenInterval() {
+    final int depth = 4;
+    final int maxCompressionDepth = 3;
+    final List<TreeNode> children = createChildrenForDepth(depth);
+    final TreeNode rootNode = TreeUtil.createTree(children, depth);
+
+    IterationUtil.visitNodesToDepth(visitor, maxCompressionDepth, rootNode, SELF_G_INDEX, depth);
+
+    // Visit branch nodes 5 levels deep
+    final List<TreeNode> branchNodes = getNodesAtDepth(rootNode, maxCompressionDepth);
+    // Children should be evenly split between intermediate branch nodes
+    final int childrenPerBranchNode = children.size() / branchNodes.size();
+    final int remainingDepth = depth - maxCompressionDepth;
+    for (int i = 0; i < branchNodes.size(); i++) {
+      final TreeNode branchNode = branchNodes.get(i);
+      final long branchGIndex = GIndexUtil.gIdxChildGIndex(SELF_G_INDEX, i, maxCompressionDepth);
+      final int firstChildIndex = i * childrenPerBranchNode;
+      final List<TreeNode> branchChildren =
+          children.subList(firstChildIndex, firstChildIndex + childrenPerBranchNode);
+      // Visit children first
+      verifyChildrenVisited(branchGIndex, branchChildren, remainingDepth);
+      // Then branch node itself
+      inOrder
+          .verify(visitor)
+          .onBranchNode(
+              branchNode.hashTreeRoot(), branchGIndex, remainingDepth, roots(branchChildren));
+    }
+    // Then root node
+    inOrder
+        .verify(visitor)
+        .onBranchNode(
+            rootNode.hashTreeRoot(), SELF_G_INDEX, maxCompressionDepth, roots(branchNodes));
+  }
+
+  @Test
+  void shouldNotVisitAnyNodesIfRootCanBeSkipped() {
+    final int depth = 5;
+    final List<TreeNode> children = createChildrenForDepth(depth);
+    final TreeNode rootNode = TreeUtil.createTree(children, depth);
+
+    when(visitor.canSkipBranch(rootNode.hashTreeRoot(), SELF_G_INDEX)).thenReturn(true);
+
+    IterationUtil.visitNodesToDepth(visitor, UNLIMITED_COMPRESSION, rootNode, SELF_G_INDEX, depth);
+
+    inOrder.verify(visitor).canSkipBranch(rootNode.hashTreeRoot(), SELF_G_INDEX);
+    inOrder.verifyNoMoreInteractions();
+  }
+
+  @Test
+  void shouldSkipIntermediateBranchesWhenPossible() {
+    final int depth = 4;
+    final int maxCompressionDepth = 2;
+    final List<TreeNode> children = createChildrenForDepth(depth);
+    final TreeNode rootNode = TreeUtil.createTree(children, depth);
+    final List<TreeNode> branchNodes = getNodesAtDepth(rootNode, maxCompressionDepth);
+    final Set<Integer> skippableBranchIndices = Set.of(0, 2);
+    // Can skip intermediate branches 0 and 2, but should store the others
+    skippableBranchIndices.forEach(
+        index ->
+            when(visitor.canSkipBranch(
+                    branchNodes.get(index).hashTreeRoot(),
+                    GIndexUtil.gIdxChildGIndex(SELF_G_INDEX, index, maxCompressionDepth)))
+                .thenReturn(true));
+
+    IterationUtil.visitNodesToDepth(visitor, maxCompressionDepth, rootNode, SELF_G_INDEX, depth);
+
+    // Visit branch nodes 5 levels deep
+
+    // Children should be evenly split between intermediate branch nodes
+    final int childrenPerBranchNode = children.size() / branchNodes.size();
+    for (int i = 0; i < branchNodes.size(); i++) {
+      final TreeNode branchNode = branchNodes.get(i);
+      final long branchGIndex = GIndexUtil.gIdxChildGIndex(SELF_G_INDEX, i, maxCompressionDepth);
+      final int firstChildIndex = i * childrenPerBranchNode;
+      final List<TreeNode> branchChildren =
+          children.subList(firstChildIndex, firstChildIndex + childrenPerBranchNode);
+      // Should skip all nodes under branches 0 and 2
+      final VerificationMode verificationMode =
+          skippableBranchIndices.contains(i) ? never() : times(1);
+      // Visit children first
+      verifyChildrenVisited(verificationMode, branchGIndex, branchChildren, maxCompressionDepth);
+      // Then branch node itself
+      inOrder
+          .verify(visitor, verificationMode)
+          .onBranchNode(
+              branchNode.hashTreeRoot(), branchGIndex, maxCompressionDepth, roots(branchChildren));
+    }
+    // Then root node
+    inOrder
+        .verify(visitor)
+        .onBranchNode(
+            rootNode.hashTreeRoot(), SELF_G_INDEX, maxCompressionDepth, roots(branchNodes));
+  }
+
+  private List<TreeNode> getNodesAtDepth(final TreeNode rootNode, final int depth) {
+    final List<TreeNode> nodes = new ArrayList<>();
+    final long childCount = 1L << depth;
+    for (long i = 0; i < childCount; i++) {
+      final long childGIndex = GIndexUtil.gIdxChildGIndex(SELF_G_INDEX, i, depth);
+      nodes.add(rootNode.get(childGIndex));
+    }
+    return nodes;
+  }
+
+  private void verifyChildrenVisited(final List<TreeNode> children, final int depth) {
+    verifyChildrenVisited(SELF_G_INDEX, children, depth);
+  }
+
+  private void verifyChildrenVisited(
+      final long rootGIndex, final List<TreeNode> children, final int depth) {
+    verifyChildrenVisited(times(1), rootGIndex, children, depth);
+  }
+
+  private void verifyChildrenVisited(
+      final VerificationMode verificationMode,
+      final long rootGIndex,
+      final List<TreeNode> children,
+      final int depth) {
+    for (int i = 0; i < children.size(); i++) {
+      final TreeNode child = children.get(i);
+      inOrder
+          .verify(visitor, verificationMode)
+          .onTargetDepthNode(child, GIndexUtil.gIdxChildGIndex(rootGIndex, i, depth));
+    }
+  }
+
+  private List<TreeNode> createChildrenForDepth(final int depth) {
+    final List<TreeNode> children = new ArrayList<>();
+    for (int i = 0; i < (int) 1L << depth; i++) {
+      children.add(LeafNode.create(Bytes.ofUnsignedInt(i, ByteOrder.BIG_ENDIAN)));
+    }
+    return children;
+  }
+
+  private Bytes32[] roots(final Collection<TreeNode> nodes) {
+    return roots(nodes.stream());
+  }
+
+  private Bytes32[] roots(final TreeNode... nodes) {
+    return roots(Stream.of(nodes));
+  }
+
+  private Bytes32[] roots(final Stream<TreeNode> nodes) {
+    return nodes.map(TreeNode::hashTreeRoot).toArray(Bytes32[]::new);
+  }
+}
