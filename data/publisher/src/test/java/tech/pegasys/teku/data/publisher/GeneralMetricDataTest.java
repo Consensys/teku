1766ff9d4f0a58cd01fde5f569c7d763fedbfd15
regio <regiom@gmail.com>
2021-09-15 14:20:46 +1000

    Refactoring unit tests

--- data/publisher/src/test/java/tech/pegasys/teku/data/publisher/GeneralMetricDataTest.java
+++ data/publisher/src/test/java/tech/pegasys/teku/data/publisher/GeneralMetricDataTest.java
@@ -14,23 +14,14 @@
 package tech.pegasys.teku.data.publisher;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import java.util.ArrayList;
-import org.hyperledger.besu.metrics.Observation;
-import org.hyperledger.besu.metrics.StandardMetricCategory;
-import org.hyperledger.besu.metrics.prometheus.PrometheusMetricsSystem;
 import org.junit.jupiter.api.Test;
-import tech.pegasys.teku.infrastructure.metrics.TekuMetricCategory;
 import tech.pegasys.teku.provider.JsonProvider;
 
 class GeneralMetricDataTest {
 
   private JsonProvider jsonProvider = new JsonProvider();
-
-  PrometheusMetricsSystem prometheusMock = mock(PrometheusMetricsSystem.class);
 
   @Test
   public void shouldSerializeObject() throws JsonProcessingException {
@@ -38,46 +29,5 @@ class GeneralMetricDataTest {
         new GeneralMetricData(1, 10L, "system", 11L, 12L, "teku", "21.8", 2, 3, 4);
     final String data = jsonProvider.objectToJSON(process);
     assertThat(process).isEqualTo(jsonProvider.jsonToObject(data, GeneralMetricData.class));
-  }
-
-  @Test
-  public void shouldSerializeObjectFromPrometheusMetrics() throws JsonProcessingException {
-    when(prometheusMock.streamObservations()).thenReturn(getMockObservations().stream());
-    final GeneralMetricData metricData =
-        new GeneralMetricData(1, 10L, "system", 10L, 10L, "Teku", "1.1", 1, 1, 1);
-    final String data = jsonProvider.objectToJSON(metricData);
-
-    assertThat(metricData).isEqualTo(jsonProvider.jsonToObject(data, GeneralMetricData.class));
-  }
-
-  @Test
-  public void shouldSerializeObjectFromPrometheusMetricsWhenNoDataIsFound()
-      throws JsonProcessingException {
-    when(prometheusMock.streamObservations()).thenReturn(new ArrayList<Observation>().stream());
-    final GeneralMetricData metricData =
-        new GeneralMetricData(1, 10L, "system", 10L, 10L, "Teku", "1.1", 1, 1, 1);
-    final String data = jsonProvider.objectToJSON(metricData);
-
-    assertThat(metricData).isEqualTo(jsonProvider.jsonToObject(data, GeneralMetricData.class));
-  }
-
-  private ArrayList<Observation> getMockObservations() {
-    ArrayList<Observation> list = new ArrayList<>();
-    Observation cpu =
-        new Observation(StandardMetricCategory.PROCESS, "cpu_seconds_total", 1.0, null);
-    Observation memory =
-        new Observation(StandardMetricCategory.JVM, "memory_pool_bytes_used", 1.0, null);
-    Observation clientVersion =
-        new Observation(TekuMetricCategory.BEACON, "teku_version", 1.0, null);
-    Observation activeValidators =
-        new Observation(TekuMetricCategory.BEACON, "current_active_validators", 1.0, null);
-    Observation liveValidators =
-        new Observation(TekuMetricCategory.BEACON, "current_live_validators", 1.0, null);
-    list.add(cpu);
-    list.add(memory);
-    list.add(clientVersion);
-    list.add(activeValidators);
-    list.add(liveValidators);
-    return list;
   }
 }
