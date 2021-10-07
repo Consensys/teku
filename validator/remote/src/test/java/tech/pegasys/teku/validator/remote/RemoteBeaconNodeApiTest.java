5e038de5283aa4aff86cad1b74a60f1d4a40035a
Paul Harris <paul.harris@consensys.net>
2021-10-07 14:01:48 +1000

    ensure http url is valid before attempting to use it in remote beacon node api.
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

---
+++ validator/remote/src/test/java/tech/pegasys/teku/validator/remote/RemoteBeaconNodeApiTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.validator.remote;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+
+import java.net.URI;
+import org.junit.jupiter.api.Test;
+import tech.pegasys.teku.infrastructure.async.AsyncRunner;
+import tech.pegasys.teku.infrastructure.async.StubAsyncRunner;
+import tech.pegasys.teku.service.serviceutils.ServiceConfig;
+import tech.pegasys.teku.spec.Spec;
+import tech.pegasys.teku.spec.TestSpecFactory;
+
+class RemoteBeaconNodeApiTest {
+  private final ServiceConfig serviceConfig = mock(ServiceConfig.class);
+  private final AsyncRunner asyncRunner = new StubAsyncRunner();
+  private final Spec spec = TestSpecFactory.createMinimalAltair();
+
+  @Test
+  void producesExceptionWhenInvalidUrlPassed() {
+    assertThatThrownBy(
+            () ->
+                RemoteBeaconNodeApi.create(
+                    serviceConfig, asyncRunner, new URI("notvalid"), spec, false, false))
+        .hasMessageContaining("Failed to convert remote api endpoint");
+  }
+}
