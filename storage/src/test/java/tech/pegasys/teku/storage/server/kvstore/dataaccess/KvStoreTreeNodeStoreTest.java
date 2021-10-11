4cec7402784d8d8267d11eabff8416265a0a9e8b
Adrian Sutton <adrian.sutton@consensys.net>
2021-10-11 14:11:17 +1000

    Add tests.

---
+++ storage/src/test/java/tech/pegasys/teku/storage/server/kvstore/dataaccess/KvStoreTreeNodeStoreTest.java
@@ -0,0 +1,116 @@
+package tech.pegasys.teku.storage.server.kvstore.dataaccess;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.apache.tuweni.bytes.Bytes32;
+import org.junit.jupiter.api.Test;
+import tech.pegasys.teku.spec.Spec;
+import tech.pegasys.teku.spec.TestSpecFactory;
+import tech.pegasys.teku.spec.util.DataStructureUtil;
+import tech.pegasys.teku.ssz.tree.TreeNodeSource.CompressedBranchInfo;
+import tech.pegasys.teku.storage.server.kvstore.KvStoreAccessor.KvStoreTransaction;
+import tech.pegasys.teku.storage.server.kvstore.schema.SchemaFinalizedTreeState;
+import tech.pegasys.teku.storage.server.kvstore.schema.V6TreeSchemaFinalized;
+
+class KvStoreTreeNodeStoreTest {
+
+  private final Spec spec = TestSpecFactory.createDefault();
+  private final DataStructureUtil dataStructureUtil = new DataStructureUtil(spec);
+  private final Set<Bytes32> knownBranchCache = new HashSet<>();
+  private final KvStoreTransaction transaction = mock(KvStoreTransaction.class);
+  private final SchemaFinalizedTreeState schema = new V6TreeSchemaFinalized(spec);
+
+  private final KvStoreTreeNodeStore store =
+      new KvStoreTreeNodeStore(knownBranchCache, transaction, schema);
+
+  @Test
+  void canSkipBranch_shouldSkipBranchWhenInKnownBranchCache() {
+    final Bytes32 root = dataStructureUtil.randomBytes32();
+    knownBranchCache.add(root);
+
+    assertThat(store.canSkipBranch(root, 3)).isTrue();
+    assertThat(store.getSkippedBranchNodeCount()).isEqualTo(1);
+    assertThat(store.getStoredBranchNodeCount()).isZero();
+    assertThat(store.getStoredLeafNodeCount()).isZero();
+  }
+
+  @Test
+  void canSkipBranch_shouldNotSkipBranchWhenNotInKnownBranchCache() {
+    final Bytes32 root = dataStructureUtil.randomBytes32();
+
+    assertThat(store.canSkipBranch(root, 3)).isFalse();
+    assertThat(store.getSkippedBranchNodeCount()).isZero();
+    assertThat(store.getStoredBranchNodeCount()).isZero();
+    assertThat(store.getStoredLeafNodeCount()).isZero();
+  }
+
+  @Test
+  void canSkipBranch_shouldSkipBranchWhenNewlyStored() {
+    final Bytes32 root = dataStructureUtil.randomBytes32();
+    store.storeBranchNode(root, 3, 1, new Bytes32[0]);
+
+    assertThat(store.canSkipBranch(root, 3)).isTrue();
+    assertThat(store.getSkippedBranchNodeCount()).isEqualTo(1);
+    assertThat(store.getStoredBranchNodeCount()).isEqualTo(1);
+    assertThat(store.getStoredLeafNodeCount()).isZero();
+  }
+
+  @Test
+  void storeBranchNode_shouldStoreBranchWhenNotAlreadyStored() {
+    final Bytes32 root = dataStructureUtil.randomBytes32();
+    final int depth = 2;
+    final Bytes32[] children = {
+      dataStructureUtil.randomBytes32(), dataStructureUtil.randomBytes32()
+    };
+
+    store.storeBranchNode(root, 5, depth, children);
+
+    verify(transaction)
+        .put(
+            schema.getColumnFinalizedStateMerkleTreeBranches(),
+            root,
+            new CompressedBranchInfo(depth, children));
+    assertThat(store.getStoredBranchNodeCount()).isEqualTo(1);
+  }
+
+  @Test
+  void storeBranchNode_shouldNotStoreBranchWhenAlreadyStored() {
+    final Bytes32 root = dataStructureUtil.randomBytes32();
+    final int depth = 2;
+    final Bytes32[] children = {
+      dataStructureUtil.randomBytes32(), dataStructureUtil.randomBytes32()
+    };
+
+    store.storeBranchNode(root, 5, depth, children);
+    store.storeBranchNode(root, 5, depth, children);
+
+    verify(transaction, times(1))
+        .put(
+            schema.getColumnFinalizedStateMerkleTreeBranches(),
+            root,
+            new CompressedBranchInfo(depth, children));
+
+    assertThat(store.getStoredBranchNodeCount()).isEqualTo(1);
+  }
+
+  @Test
+  void storeBranchNode_shouldNotStoreBranchWhenAlreadyKnown() {
+    final Bytes32 root = dataStructureUtil.randomBytes32();
+    knownBranchCache.add(root);
+
+    store.storeBranchNode(
+        root,
+        5,
+        2,
+        new Bytes32[] {dataStructureUtil.randomBytes32(), dataStructureUtil.randomBytes32()});
+
+    verifyNoInteractions(transaction);
+    assertThat(store.getStoredBranchNodeCount()).isZero();
+  }
+}
