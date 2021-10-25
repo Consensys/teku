c585975ea7ed7f937b2f46a9fb84a89fc71ab6b7
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-10-24 17:06:43 -0700

    Sync Committee added to types

--- light-client/src/main/java/tech/pegasys/teku/lightclient/client/types.java
+++ light-client/src/main/java/tech/pegasys/teku/lightclient/client/types.java
@@ -1,5 +1,11 @@
 package tech.pegasys.teku.lightclient.client;
 
+import tech.pegasys.teku.bls.BLSPublicKey;
+
 public class types {
+	
+	public static void SyncCommitteeFast(BLSPublicKey PublicKey) {
+		
+	}
 
 }
