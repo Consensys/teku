4947e1535b312f6d6d9869781bd39af31d516d61
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-10-26 17:40:13 -0700

    Added BeaconBlock Header static logic with root

--- light-client/src/main/java/tech/pegasys/teku/lightclient/client/types.java
+++ light-client/src/main/java/tech/pegasys/teku/lightclient/client/types.java
@@ -1,14 +1,28 @@
 package tech.pegasys.teku.lightclient.client;
 
+import tech.pegasys.teku.api.schema.Root;
 import tech.pegasys.teku.bls.BLSPublicKey;
 
 public class types {
+	
+	public static void BeaconBlockHeader(Root root) {
+		
+		
+	}
 	
 	public static void LightClientStoreFast() {
 		
 	}
 	
 	public static void LightClientSnapshotFast() {
+		
+//		export interface BeaconBlockHeader {
+//			  slot: Slot;
+//			  proposerIndex: ValidatorIndex;
+//			  parentRoot: Root;
+//			  stateRoot: Root;
+//			  bodyRoot: Root;
+//			}		
 		//Beacon block header
 		  //header: phase0.BeaconBlockHeader;
 		  // Sync committees corresponding to the header
