54c9e792f0ad0929e3e69b91c7d76493fb11b8be
Regio Michelin <regiom@gmail.com>
2021-11-10 13:12:16 +1100

    [Key manager] - List Keys

--- validator/client/src/test/java/tech/pegasys/teku/validator/client/restapi/apis/GetKeysTest.java
+++ validator/client/src/test/java/tech/pegasys/teku/validator/client/restapi/apis/GetKeysTest.java
@@ -16,24 +16,29 @@ package tech.pegasys.teku.validator.client.restapi.apis;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Supplier;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import tech.pegasys.teku.bls.BLSKeyPair;
 import tech.pegasys.teku.bls.BLSPublicKey;
 import tech.pegasys.teku.bls.BLSTestUtil;
 import tech.pegasys.teku.infrastructure.restapi.JsonTestUtil;
 import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiRequest;
+import tech.pegasys.teku.validator.client.KeyManager;
 
 class GetKeysTest {
+  final KeyManager keyManager = Mockito.mock(KeyManager.class);
+
   @Test
   void metadata_shouldProduceCorrectOpenApi() throws Exception {
-    final Supplier<List<BLSPublicKey>> validatorKeys = getList();
-    final GetKeys endpoint = new GetKeys(validatorKeys);
+    final GetKeys endpoint = new GetKeys(keyManager);
     final String json = JsonTestUtil.serializeEndpointMetadata(endpoint);
     final Map<String, Object> result = JsonTestUtil.parse(json);
 
@@ -45,31 +50,31 @@ class GetKeysTest {
 
   @Test
   void shouldListValidatorKeys() throws Exception {
-    final Supplier<List<BLSPublicKey>> validatorKeys = getList();
-    final GetKeys endpoint = new GetKeys(validatorKeys);
+    final Set<BLSPublicKey> validatorKeys = getList();
+    when(keyManager.getValidatorKeys()).thenReturn(validatorKeys);
+    final GetKeys endpoint = new GetKeys(keyManager);
     final RestApiRequest request = mock(RestApiRequest.class);
     endpoint.handle(request);
 
-    verify(request).respondOk(List.copyOf(validatorKeys.get()));
+    verify(request).respondOk(List.copyOf(validatorKeys));
   }
 
   @Test
   void shouldListEmpytValidatorKeys() throws Exception {
-    final Supplier<List<BLSPublicKey>> validatorKeys = Collections::emptyList;
-    final GetKeys endpoint = new GetKeys(validatorKeys);
+    final List<BLSPublicKey> validatorKeys = Collections.emptyList();
+    final GetKeys endpoint = new GetKeys(keyManager);
     final RestApiRequest request = mock(RestApiRequest.class);
     endpoint.handle(request);
 
-    verify(request).respondOk(List.copyOf(validatorKeys.get()));
+    verify(request).respondOk(List.copyOf(validatorKeys));
   }
 
-  private Supplier<List<BLSPublicKey>> getList() {
+  private Set<BLSPublicKey> getList() {
     BLSKeyPair keyPair1 = BLSTestUtil.randomKeyPair(1);
     BLSKeyPair keyPair2 = BLSTestUtil.randomKeyPair(2);
     BLSKeyPair keyPair3 = BLSTestUtil.randomKeyPair(3);
 
-    List<BLSPublicKey> publicKeys =
-        Arrays.asList(keyPair1.getPublicKey(), keyPair2.getPublicKey(), keyPair3.getPublicKey());
-    return () -> publicKeys;
+    return new HashSet<>(
+        Arrays.asList(keyPair1.getPublicKey(), keyPair2.getPublicKey(), keyPair3.getPublicKey()));
   }
 }
