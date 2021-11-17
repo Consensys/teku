e21d7df1dcf8d1ecfc21bda5f3bd2bd19699eb67
Enrico Del Fante <enrico.delfante@consensys.net>
2021-11-17 10:47:27 +0100

    remove LOG

--- validator/client/src/main/java/tech/pegasys/teku/validator/client/BeaconProposerPreparer.java
+++ validator/client/src/main/java/tech/pegasys/teku/validator/client/BeaconProposerPreparer.java
@@ -17,8 +17,6 @@ import static tech.pegasys.teku.infrastructure.logging.ValidatorLogger.VALIDATOR
 
 import java.util.Optional;
 import java.util.stream.Collectors;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.apache.tuweni.bytes.Bytes32;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.spec.Spec;
@@ -29,8 +27,6 @@ import tech.pegasys.teku.validator.api.ValidatorTimingChannel;
 import tech.pegasys.teku.validator.client.loader.OwnedValidators;
 
 public class BeaconProposerPreparer implements ValidatorTimingChannel {
-  private static final Logger LOG = LogManager.getLogger();
-
   private final ValidatorApiChannel validatorApiChannel;
   private final ValidatorIndexProvider validatorIndexProvider;
   private final OwnedValidators validators;
