0534a3fbe04bf3e4d7243200f774b4bfeebfd663
Enrico Del Fante <enrico.delfante@consensys.net>
2021-12-04 15:44:10 +0100

    fix to UT and IT

--- validator/client/src/integration-test/java/tech/pegasys/teku/validator/client/signer/ExternalSignerAltairIntegrationTest.java
+++ validator/client/src/integration-test/java/tech/pegasys/teku/validator/client/signer/ExternalSignerAltairIntegrationTest.java
@@ -93,6 +93,7 @@ public class ExternalSignerAltairIntegrationTest {
     this.client = client;
     final ValidatorConfig config =
         ValidatorConfig.builder()
+            .specProvider(spec)
             .validatorExternalSignerPublicKeySources(List.of(KEYPAIR.getPublicKey().toString()))
             .validatorExternalSignerUrl(new URL("http://127.0.0.1:" + client.getLocalPort()))
             .validatorExternalSignerTimeout(TIMEOUT)
