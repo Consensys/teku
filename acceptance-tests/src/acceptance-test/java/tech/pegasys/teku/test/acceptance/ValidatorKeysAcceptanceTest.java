315a478368f756f5a906d641d3d1c9413b6e3ad8
Paul Harris <paul.harris@consensys.net>
2021-12-09 11:59:39 +1000

    cleanup test a little, expand on the slashing-protection check for deleted item.
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- acceptance-tests/src/acceptance-test/java/tech/pegasys/teku/test/acceptance/ValidatorKeysAcceptanceTest.java
+++ acceptance-tests/src/acceptance-test/java/tech/pegasys/teku/test/acceptance/ValidatorKeysAcceptanceTest.java
@@ -19,7 +19,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Iterator;
-import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import tech.pegasys.teku.bls.BLSPublicKey;
@@ -38,7 +37,7 @@ public class ValidatorKeysAcceptanceTest extends AcceptanceTestBase {
       new DataStructureUtil(SpecFactory.create("less-swift"));
 
   @Test
-  void shouldAddValidatorToRunningClient(@TempDir final Path tempDir) throws Exception {
+  void shouldMaintainValidatorsInMutableClient(@TempDir final Path tempDir) throws Exception {
     final String networkName = "less-swift";
     final BesuNode eth1Node = createBesuNode();
     eth1Node.start();
@@ -64,26 +63,28 @@ public class ValidatorKeysAcceptanceTest extends AcceptanceTestBase {
 
     getValidatorListing(validatorClient, 0);
 
-    addValidatorsAndExpect(validatorClient, validatorKeystores, tempDir, Optional.of("imported"));
+    addValidatorsAndExpect(validatorClient, validatorKeystores, tempDir, "imported");
 
     validatorClient.waitForLogMessageContaining("Added validator");
     validatorClient.waitForLogMessageContaining("Published block");
     getValidatorListing(validatorClient, 8);
 
     // second add attempt would be duplicates
-    addValidatorsAndExpect(validatorClient, validatorKeystores, tempDir, Optional.of("duplicate"));
+    addValidatorsAndExpect(validatorClient, validatorKeystores, tempDir, "duplicate");
 
-    // a random key won't be found
+    // a random key won't be found, remove should give not_found
     removeValidatorAndCheckStatus(
         validatorClient, dataStructureUtil.randomPublicKey(), "not_found");
 
-    beaconNode.waitForLogMessageContaining("Epoch Event *** Epoch: 1");
+    // wait for epoch 1 to ensure the validator has attested, which will give slashing protection
+    // data
+    beaconNode.waitForLogMessageContaining("Epoch Event *** Epoch: 2");
     // remove a validator
     final BLSPublicKey removedPubkey = validatorKeystores.getPublicKeys().get(0);
     removeValidatorAndCheckStatus(validatorClient, removedPubkey, "deleted");
 
     // should only be 7 validators left
-    validatorClient.waitForLogMessageContaining("Deleted validator");
+    validatorClient.waitForLogMessageContaining("Removed validator");
     validatorClient.waitForLogMessageContaining("Published block");
     getValidatorListing(validatorClient, 7);
 
@@ -100,14 +101,14 @@ public class ValidatorKeysAcceptanceTest extends AcceptanceTestBase {
       final TekuValidatorNode validatorClient,
       final ValidatorKeystores validatorKeystores,
       final Path tempDir,
-      final Optional<String> status)
+      final String expectedStatus)
       throws IOException {
     final JsonNode addResult =
         jsonProvider
             .getObjectMapper()
             .readTree(validatorClient.addValidators(validatorKeystores, tempDir));
     assertThat(addResult.get("data").size()).isEqualTo(validatorKeystores.getValidatorCount());
-    status.ifPresent(expected -> checkStatus(addResult.get("data"), expected));
+    checkStatus(addResult.get("data"), expectedStatus);
   }
 
   private void removeValidatorAndCheckStatus(
@@ -119,6 +120,13 @@ public class ValidatorKeysAcceptanceTest extends AcceptanceTestBase {
         jsonProvider.getObjectMapper().readTree(validatorClient.removeValidator(publicKey));
     assertThat(removeResult.get("data").size()).isEqualTo(1);
     checkStatus(removeResult.get("data"), expectedStatus);
+    if (expectedStatus.equals("deleted") || expectedStatus.equals("not_active")) {
+      final JsonNode slashingProtection =
+          jsonProvider.getObjectMapper().readTree(removeResult.get("slashing_protection").asText());
+      final JsonNode slashingData = slashingProtection.get("data");
+      assertThat(slashingData.size()).isEqualTo(1);
+      assertThat(slashingData.get(0).get("pubkey").asText()).isEqualTo(publicKey.toString());
+    }
   }
 
   private void checkStatus(final JsonNode data, final String status) {
