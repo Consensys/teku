3b5d488f67fc5d2d4e45a4cb9a7d685945dcb700
Justin Traglia <jtraglia@pm.me>
2022-01-28 09:23:42 -0600

    Move exception handlers to `:infrastructure:async`
    `:infrastructure:execeptions` had a dependency on `:infrastructure:async` that
    would cause a circular dependency in some situations. For example, if
    `:infrastructure:metrics` included `:infrastructure:exceptions`:
    
    ```
    Circular dependency between the following tasks:
    :infrastructure:async:compileJava
    \--- :infrastructure:metrics:compileJava
         \--- :infrastructure:exceptions:compileJava
              \--- :infrastructure:async:compileJava (*)
    ```
    
    To resolve this problem, this commit moves the two exception handlers that used
    `async.SafeFuture` from `:infrastructure:execeptions:ExceptionUtil` to
    `:infrastructure:async:ExceptionHandlers`.
    
    Also, the `:infrastructure:async` dependency has been removed from
    `:infrastructure:execeptions` as it is no longer required.

---
+++ infrastructure/async/src/main/java/tech/pegasys/teku/infrastructure/async/ExceptionHandlers.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.infrastructure.async;
+
+import java.util.function.Consumer;
+
+public class ExceptionHandlers {
+
+  public static Runnable exceptionHandlingRunnable(
+      final Runnable runnable, final SafeFuture<?> target) {
+    return () -> {
+      try {
+        runnable.run();
+      } catch (final Throwable t) {
+        target.completeExceptionally(t);
+      }
+    };
+  }
+
+  public static <T> Consumer<T> exceptionHandlingConsumer(
+      final Consumer<T> consumer, final SafeFuture<?> target) {
+    return value -> {
+      try {
+        consumer.accept(value);
+      } catch (final Throwable t) {
+        target.completeExceptionally(t);
+      }
+    };
+  }
+}
---
+++ infrastructure/async/src/test/java/tech/pegasys/teku/infrastructure/async/ExceptionHandlersTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2020 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.infrastructure.async;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.util.function.Consumer;
+import org.junit.jupiter.api.Test;
+
+public class ExceptionHandlersTest {
+
+  @Test
+  void exceptionHandlingRunnable_shouldNotCompleteFutureWhenRunnableCompletesNormally() {
+    final SafeFuture<Void> future = new SafeFuture<>();
+    final Runnable delegate = mock(Runnable.class);
+    final Runnable runnable = ExceptionHandlers.exceptionHandlingRunnable(delegate, future);
+    runnable.run();
+
+    verify(delegate).run();
+    assertThat(future).isNotCompleted();
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  void exceptionHandlingConsumer_shouldPropagateExceptionInConsumer() {
+    final SafeFuture<Void> future = new SafeFuture<>();
+    final RuntimeException exception = new RuntimeException("oops");
+    final String value = "the value";
+    final Consumer<String> delegate = mock(Consumer.class);
+    doThrow(exception).when(delegate).accept(value);
+    final Consumer<String> consumer = ExceptionHandlers.exceptionHandlingConsumer(delegate, future);
+    consumer.accept(value);
+    verify(delegate).accept(value);
+    assertThat(future).isCompletedExceptionally();
+    assertThatThrownBy(future::join).hasRootCause(exception);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  void exceptionHandlingConsumer_shouldNotCompleteFutureWhenRunnableCompletesNormally() {
+    final SafeFuture<Void> future = new SafeFuture<>();
+    final String value = "the value";
+    final Consumer<String> delegate = mock(Consumer.class);
+    final Consumer<String> consumer = ExceptionHandlers.exceptionHandlingConsumer(delegate, future);
+    consumer.accept(value);
+
+    verify(delegate).accept(value);
+    assertThat(future).isNotCompleted();
+  }
+}
