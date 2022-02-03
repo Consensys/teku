Consensys/teku

commit 540bf51b8407fa80695960dd4d4d778ddf0f4496

Add additional validation for configuration/cli options (#4909)
Co-authored-by: Adrian Sutton <adrian.sutton@consensys.net>

networking/p2p/src/main/java/tech/pegasys/teku/networking/p2p/discovery/DiscoveryConfig.java
@@ -19,6 +19,8 @@ import static tech.pegasys.teku.networking.p2p.network.config.NetworkConfig.DEFA
 import java.util.Collections;
 import java.util.List;
 import java.util.OptionalInt;
+import tech.pegasys.teku.infrastructure.exceptions.InvalidConfigurationException;
+import tech.pegasys.teku.infrastructure.io.PortAvailability;
 
 public class DiscoveryConfig {
 
@@ -136,11 +138,19 @@ public class DiscoveryConfig {
     }
 
     public Builder listenUdpPort(final int listenUdpPort) {
+      if (!PortAvailability.isPortValid(listenUdpPort)) {
+        throw new InvalidConfigurationException(
+            String.format("Invalid listenUdpPort: %d", listenUdpPort));
+      }
       this.listenUdpPort = OptionalInt.of(listenUdpPort);
       return this;
     }
 
     public Builder listenUdpPortDefault(final int listenUdpPort) {
+      if (!PortAvailability.isPortValid(listenUdpPort)) {
+        throw new InvalidConfigurationException(
+            String.format("Invalid listenUdpPortDefault: %d", listenUdpPort));
+      }
       if (this.listenUdpPort.isEmpty()) {
         this.listenUdpPort = OptionalInt.of(listenUdpPort);
       }
@@ -149,12 +159,24 @@ public class DiscoveryConfig {
 
     public Builder advertisedUdpPort(final OptionalInt advertisedUdpPort) {
       checkNotNull(advertisedUdpPort);
+      if (advertisedUdpPort.isPresent()) {
+        if (!PortAvailability.isPortValid(advertisedUdpPort.getAsInt())) {
+          throw new InvalidConfigurationException(
+              String.format("Invalid advertisedUdpPort: " + advertisedUdpPort.getAsInt()));
+        }
+      }
       this.advertisedUdpPort = advertisedUdpPort;
       return this;
     }
 
     public Builder advertisedUdpPortDefault(final OptionalInt advertisedUdpPort) {
       checkNotNull(advertisedUdpPort);
+      if (advertisedUdpPort.isPresent()) {
+        if (!PortAvailability.isPortValid(advertisedUdpPort.getAsInt())) {
+          throw new InvalidConfigurationException(
+              String.format("Invalid advertisedUdpPortDefault: %d", advertisedUdpPort.getAsInt()));
+        }
+      }
       if (this.advertisedUdpPort.isEmpty()) {
         this.advertisedUdpPort = advertisedUdpPort;
       }
@@ -183,18 +205,28 @@ public class DiscoveryConfig {
 
     public Builder minPeers(final Integer minPeers) {
       checkNotNull(minPeers);
+      if (minPeers < 0) {
+        throw new InvalidConfigurationException(String.format("Invalid minPeers: %d", minPeers));
+      }
       this.minPeers = minPeers;
       return this;
     }
 
     public Builder maxPeers(final Integer maxPeers) {
       checkNotNull(maxPeers);
+      if (maxPeers < 0) {
+        throw new InvalidConfigurationException(String.format("Invalid maxPeers: %d", maxPeers));
+      }
       this.maxPeers = maxPeers;
       return this;
     }
 
     public Builder minRandomlySelectedPeers(final Integer minRandomlySelectedPeers) {
       checkNotNull(minRandomlySelectedPeers);
+      if (maxPeers < 0) {
+        throw new InvalidConfigurationException(
+            String.format("Invalid minRandomlySelectedPeers: %d", minRandomlySelectedPeers));
+      }
       this.minRandomlySelectedPeers = OptionalInt.of(minRandomlySelectedPeers);
       return this;
     }

validator/api/src/main/java/tech/pegasys/teku/validator/api/InteropConfig.java
@@ -79,6 +79,11 @@ public class InteropConfig {
     private InteropConfigBuilder() {}
 
     public InteropConfigBuilder interopGenesisTime(Integer interopGenesisTime) {
+      checkNotNull(interopGenesisTime);
+      if (interopGenesisTime < 0) {
+        throw new InvalidConfigurationException(
+            String.format("Invalid interopGenesisTime: %d", interopGenesisTime));
+      }
       this.interopGenesisTime = interopGenesisTime;
       return this;
     }
@@ -90,16 +95,29 @@ public class InteropConfig {
 
     public InteropConfigBuilder interopOwnedValidatorStartIndex(
         int interopOwnedValidatorStartIndex) {
+      if (interopOwnedValidatorStartIndex < 0) {
+        throw new InvalidConfigurationException(
+            String.format(
+                "Invalid interopOwnedValidatorStartIndex: %d", interopOwnedValidatorStartIndex));
+      }
       this.interopOwnedValidatorStartIndex = interopOwnedValidatorStartIndex;
       return this;
     }
 
     public InteropConfigBuilder interopOwnedValidatorCount(int interopOwnedValidatorCount) {
+      if (interopOwnedValidatorCount < 0) {
+        throw new InvalidConfigurationException(
+            String.format("Invalid interopOwnedValidatorCount: %d", interopOwnedValidatorCount));
+      }
       this.interopOwnedValidatorCount = interopOwnedValidatorCount;
       return this;
     }
 
     public InteropConfigBuilder interopNumberOfValidators(int interopNumberOfValidators) {
+      if (interopNumberOfValidators < 0) {
+        throw new InvalidConfigurationException(
+            String.format("Invalid interopNumberOfValidators: %d", interopNumberOfValidators));
+      }
       this.interopNumberOfValidators = interopNumberOfValidators;
       return this;
     }

data/beaconrestapi/build.gradle
@@ -8,6 +8,7 @@ dependencies {
     implementation project(':infrastructure:async')
     implementation project(':infrastructure:exceptions')
     implementation project(':infrastructure:http')
+    implementation project(':infrastructure:io')
     implementation project(':infrastructure:logging')
     implementation project(':infrastructure:version')
     implementation project(':networking:eth2')

data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/BeaconRestApiConfig.java
@@ -17,6 +17,8 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import tech.pegasys.teku.infrastructure.exceptions.InvalidConfigurationException;
+import tech.pegasys.teku.infrastructure.io.PortAvailability;
 import tech.pegasys.teku.spec.datastructures.eth1.Eth1Address;
 
 public class BeaconRestApiConfig {
@@ -134,6 +136,10 @@ public class BeaconRestApiConfig {
     private BeaconRestApiConfigBuilder() {}
 
     public BeaconRestApiConfigBuilder restApiPort(final int restApiPort) {
+      if (!PortAvailability.isPortValid(restApiPort)) {
+        throw new InvalidConfigurationException(
+            String.format("Invalid restApiPort: %d", restApiPort));
+      }
       this.restApiPort = restApiPort;
       return this;
     }
@@ -181,6 +187,10 @@ public class BeaconRestApiConfig {
     }
 
     public BeaconRestApiConfigBuilder maxPendingEvents(final int maxEventQueueSize) {
+      if (maxEventQueueSize < 0) {
+        throw new InvalidConfigurationException(
+            String.format("Invalid maxEventQueueSize: %d", maxEventQueueSize));
+      }
       this.maxPendingEvents = maxEventQueueSize;
       return this;
     }
@@ -192,6 +202,10 @@ public class BeaconRestApiConfig {
     }
 
     public BeaconRestApiConfigBuilder validatorThreads(final int validatorThreads) {
+      if (validatorThreads < 0) {
+        throw new InvalidConfigurationException(
+            String.format("Invalid validatorThreads: %d", validatorThreads));
+      }
       this.validatorThreads = validatorThreads;
       return this;
     }

ethereum/networks/build.gradle
@@ -2,6 +2,8 @@ dependencies {
     api project(':ethereum:spec')
 
     implementation project(':infrastructure:io')
+    implementation project(':infrastructure:exceptions')
+
     implementation 'org.apache.tuweni:tuweni-units'
 
     testImplementation 'tech.pegasys.discovery:discovery'

ethereum/networks/src/main/java/tech/pegasys/teku/networks/Eth2NetworkConfiguration.java
@@ -31,6 +31,7 @@ import java.util.List;
 import java.util.Optional;
 import org.apache.tuweni.bytes.Bytes32;
 import org.apache.tuweni.units.bigints.UInt256;
+import tech.pegasys.teku.infrastructure.exceptions.InvalidConfigurationException;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.spec.Spec;
 import tech.pegasys.teku.spec.SpecFactory;
@@ -271,11 +272,19 @@ public class Eth2NetworkConfiguration {
     }
 
     public Builder startupTargetPeerCount(final int startupTargetPeerCount) {
+      if (startupTargetPeerCount < 0) {
+        throw new InvalidConfigurationException(
+            String.format("Invalid startupTargetPeerCount: %d", startupTargetPeerCount));
+      }
       this.startupTargetPeerCount = startupTargetPeerCount;
       return this;
     }
 
     public Builder startupTimeoutSeconds(final int startupTimeoutSeconds) {
+      if (startupTimeoutSeconds < 0) {
+        throw new InvalidConfigurationException(
+            String.format("Invalid startupTimeoutSeconds: %d", startupTimeoutSeconds));
+      }
       this.startupTimeoutSeconds = startupTimeoutSeconds;
       return this;
     }
@@ -318,6 +327,11 @@ public class Eth2NetworkConfiguration {
     }
 
     public Builder safeSlotsToImportOptimistically(final int safeSlotsToImportOptimistically) {
+      if (safeSlotsToImportOptimistically < 0) {
+        throw new InvalidConfigurationException(
+            String.format(
+                "Invalid safeSlotsToImportOptimistically: %d", safeSlotsToImportOptimistically));
+      }
       this.safeSlotsToImportOptimistically = safeSlotsToImportOptimistically;
       return this;
     }

infrastructure/metrics/build.gradle
@@ -1,5 +1,9 @@
 dependencies {
     api 'org.hyperledger.besu:plugin-api'
+
+    implementation project(':infrastructure:exceptions')
+    implementation project(':infrastructure:io')
+
     implementation 'com.fasterxml.jackson.core:jackson-databind'
     implementation 'io.prometheus:simpleclient'
     implementation 'io.vertx:vertx-core'

infrastructure/metrics/src/main/java/tech/pegasys/teku/infrastructure/metrics/MetricsConfig.java
@@ -20,6 +20,8 @@ import java.util.List;
 import java.util.Set;
 import org.hyperledger.besu.metrics.StandardMetricCategory;
 import org.hyperledger.besu.plugin.services.metrics.MetricCategory;
+import tech.pegasys.teku.infrastructure.exceptions.InvalidConfigurationException;
+import tech.pegasys.teku.infrastructure.io.PortAvailability;
 
 public class MetricsConfig {
 
@@ -118,6 +120,10 @@ public class MetricsConfig {
     }
 
     public MetricsConfigBuilder metricsPort(int metricsPort) {
+      if (!PortAvailability.isPortValid(metricsPort)) {
+        throw new InvalidConfigurationException(
+            String.format("Invalid metricsPort: %d", metricsPort));
+      }
       this.metricsPort = metricsPort;
       return this;
     }
@@ -143,11 +149,19 @@ public class MetricsConfig {
     }
 
     public MetricsConfigBuilder metricsPublicationInterval(int metricsPublicationInterval) {
+      if (metricsPublicationInterval < 0) {
+        throw new InvalidConfigurationException(
+            String.format("Invalid metricsPublicationInterval: %d", metricsPublicationInterval));
+      }
       this.metricsPublicationInterval = metricsPublicationInterval;
       return this;
     }
 
     public MetricsConfigBuilder idleTimeoutSeconds(final int idleTimeoutSeconds) {
+      if (idleTimeoutSeconds < 0) {
+        throw new InvalidConfigurationException(
+            String.format("Invalid idleTimeoutSeconds: %d", idleTimeoutSeconds));
+      }
       this.idleTimeoutSeconds = idleTimeoutSeconds;
       return this;
     }

commit b8d39243c717e0d02e7e3a6d6dc91e86f053b10b

Fix two mistakes introduced in #4909
1. Replace `maxPeers` with `minRandomlySelectedPeers` the builder function for
   that field. It incorrectly checked the wrong value. This was most likely a
   copy-paste mistake.

2. Remove new call to `checkNotNull` in `interopGenesisTime`. This was probably
   added because I thought it could not hurt. I have since learned that not all
   builder functions should `checkNotNull`.

networking/p2p/src/main/java/tech/pegasys/teku/networking/p2p/discovery/DiscoveryConfig.java
@@ -223,7 +223,7 @@ public class DiscoveryConfig {
 
     public Builder minRandomlySelectedPeers(final Integer minRandomlySelectedPeers) {
       checkNotNull(minRandomlySelectedPeers);
-      if (maxPeers < 0) {
+      if (minRandomlySelectedPeers < 0) {
         throw new InvalidConfigurationException(
             String.format("Invalid minRandomlySelectedPeers: %d", minRandomlySelectedPeers));
       }

validator/api/src/main/java/tech/pegasys/teku/validator/api/InteropConfig.java
@@ -79,7 +79,6 @@ public class InteropConfig {
     private InteropConfigBuilder() {}
 
     public InteropConfigBuilder interopGenesisTime(Integer interopGenesisTime) {
-      checkNotNull(interopGenesisTime);
       if (interopGenesisTime < 0) {
         throw new InvalidConfigurationException(
             String.format("Invalid interopGenesisTime: %d", interopGenesisTime));