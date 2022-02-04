9ba69f2be65a13bee317ff6dd8889840b70a36c5
Adrian Sutton <adrian.sutton@consensys.net>
2022-02-04 11:37:17 +1000

    Add ability to use a builder when deserializing objects. (#4933)
    Avoids the need to make all deserializable objects mutable just to support the framework.

--- infrastructure/restapi/src/main/java/tech/pegasys/teku/infrastructure/restapi/types/OptionalDeserializableFieldDefinition.java
+++ infrastructure/restapi/src/main/java/tech/pegasys/teku/infrastructure/restapi/types/OptionalDeserializableFieldDefinition.java
@@ -19,16 +19,16 @@ import java.util.Optional;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
-public class OptionalDeserializableFieldDefinition<TObject, TField>
+public class OptionalDeserializableFieldDefinition<TObject, TBuilder, TField>
     extends OptionalSerializableFieldDefinition<TObject, TField>
-    implements DeserializableFieldDefinition<TObject> {
-  private final BiConsumer<TObject, Optional<TField>> setter;
+    implements DeserializableFieldDefinition<TObject, TBuilder> {
+  private final BiConsumer<TBuilder, Optional<TField>> setter;
   private final DeserializableTypeDefinition<TField> deserializableType;
 
   OptionalDeserializableFieldDefinition(
       final String name,
       final Function<TObject, Optional<TField>> getter,
-      final BiConsumer<TObject, Optional<TField>> setter,
+      final BiConsumer<TBuilder, Optional<TField>> setter,
       final DeserializableTypeDefinition<TField> type) {
     super(name, getter, type);
     this.setter = setter;
@@ -36,7 +36,7 @@ public class OptionalDeserializableFieldDefinition<TObject, TField>
   }
 
   @Override
-  public void readField(final TObject target, final JsonParser parser) throws IOException {
+  public void readField(final TBuilder target, final JsonParser parser) throws IOException {
     final Optional<TField> value = Optional.ofNullable(deserializableType.deserialize(parser));
     setter.accept(target, value);
   }
