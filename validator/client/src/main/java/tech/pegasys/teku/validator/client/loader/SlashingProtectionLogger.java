64ed1c3c28dfba446953489430fe2b17ef672a41
Adrian Sutton <adrian.sutton@consensys.net>
2022-02-21 10:39:24 +1000

    Errorprone.

--- validator/client/src/main/java/tech/pegasys/teku/validator/client/loader/SlashingProtectionLogger.java
+++ validator/client/src/main/java/tech/pegasys/teku/validator/client/loader/SlashingProtectionLogger.java
@@ -14,10 +14,12 @@
 package tech.pegasys.teku.validator.client.loader;
 
 import static tech.pegasys.teku.data.signingrecord.ValidatorSigningRecord.NEVER_SIGNED;
+import static tech.pegasys.teku.spec.config.SpecConfig.GENESIS_EPOCH;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
@@ -145,13 +147,15 @@ public class SlashingProtectionLogger implements ValidatorTimingChannel {
 
   private Function<ValidatorSigningRecord, Boolean> createOutdatedSigningRecordClassifier(
       final UInt64 currentSlot) {
-    return signingRecord ->
-        spec.computeEpochAtSlot(currentSlot)
-            .minusMinZero(
-                signingRecord.getAttestationTargetEpoch() == NEVER_SIGNED
-                    ? UInt64.ZERO
-                    : signingRecord.getAttestationTargetEpoch())
-            .isGreaterThan(SAFE_PROTECTION_EPOCHS_DELTA);
+    return signingRecord -> {
+      final UInt64 attestationTargetEpoch = signingRecord.getAttestationTargetEpoch();
+      return spec.computeEpochAtSlot(currentSlot)
+          .minusMinZero(
+              Objects.equals(attestationTargetEpoch, NEVER_SIGNED)
+                  ? GENESIS_EPOCH
+                  : attestationTargetEpoch)
+          .isGreaterThan(SAFE_PROTECTION_EPOCHS_DELTA);
+    };
   }
 
   @Override
