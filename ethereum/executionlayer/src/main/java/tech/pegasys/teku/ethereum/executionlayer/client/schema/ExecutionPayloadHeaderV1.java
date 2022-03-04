cd09d3284e5662627473aaf134678ef93e539127
Enrico Del Fante <enrico.delfante@consensys.net>
2022-03-04 13:24:15 +0100

    builder_getPayloadHeader

--- ethereum/executionlayer/src/main/java/tech/pegasys/teku/ethereum/executionlayer/client/schema/ExecutionPayloadHeaderV1.java
+++ ethereum/executionlayer/src/main/java/tech/pegasys/teku/ethereum/executionlayer/client/schema/ExecutionPayloadHeaderV1.java
@@ -13,4 +13,136 @@
 
 package tech.pegasys.teku.ethereum.executionlayer.client.schema;
 
-public class ExecutionPayloadHeaderV1 {}
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.google.common.base.MoreObjects;
+import java.util.Objects;
+import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.bytes.Bytes32;
+import org.apache.tuweni.units.bigints.UInt256;
+import tech.pegasys.teku.ethereum.executionlayer.client.serialization.Bytes32Deserializer;
+import tech.pegasys.teku.ethereum.executionlayer.client.serialization.BytesSerializer;
+import tech.pegasys.teku.infrastructure.bytes.Bytes20;
+import tech.pegasys.teku.infrastructure.unsigned.UInt64;
+import tech.pegasys.teku.spec.datastructures.execution.ExecutionPayloadHeader;
+import tech.pegasys.teku.spec.datastructures.execution.ExecutionPayloadHeaderSchema;
+
+public class ExecutionPayloadHeaderV1 extends ExecutionPayloadCommon {
+  @JsonSerialize(using = BytesSerializer.class)
+  @JsonDeserialize(using = Bytes32Deserializer.class)
+  public final Bytes32 transactionsRoot;
+
+  public ExecutionPayloadHeaderV1(
+      @JsonProperty("parentHash") Bytes32 parentHash,
+      @JsonProperty("feeRecipient") Bytes20 feeRecipient,
+      @JsonProperty("stateRoot") Bytes32 stateRoot,
+      @JsonProperty("receiptsRoot") Bytes32 receiptsRoot,
+      @JsonProperty("logsBloom") Bytes logsBloom,
+      @JsonProperty("prevRandao") Bytes32 prevRandao,
+      @JsonProperty("blockNumber") UInt64 blockNumber,
+      @JsonProperty("gasLimit") UInt64 gasLimit,
+      @JsonProperty("gasUsed") UInt64 gasUsed,
+      @JsonProperty("timestamp") UInt64 timestamp,
+      @JsonProperty("extraData") Bytes extraData,
+      @JsonProperty("baseFeePerGas") UInt256 baseFeePerGas,
+      @JsonProperty("blockHash") Bytes32 blockHash,
+      @JsonProperty("transactionsRoot") Bytes32 transactionsRoot) {
+    super(
+        parentHash,
+        feeRecipient,
+        stateRoot,
+        receiptsRoot,
+        logsBloom,
+        prevRandao,
+        blockNumber,
+        gasLimit,
+        gasUsed,
+        timestamp,
+        extraData,
+        baseFeePerGas,
+        blockHash);
+    checkNotNull(transactionsRoot, "transactionsRoot");
+    this.transactionsRoot = transactionsRoot;
+  }
+
+  public ExecutionPayloadHeader asInternalExecutionPayloadHeader(
+      ExecutionPayloadHeaderSchema executionPayloadHeaderSchema) {
+    return executionPayloadHeaderSchema.create(
+        parentHash,
+        feeRecipient,
+        stateRoot,
+        receiptsRoot,
+        logsBloom,
+        prevRandao,
+        blockNumber,
+        gasLimit,
+        gasUsed,
+        timestamp,
+        extraData,
+        baseFeePerGas,
+        blockHash,
+        transactionsRoot);
+  }
+
+  public static ExecutionPayloadHeaderV1 fromInternalExecutionPayloadHeader(
+      ExecutionPayloadHeader executionPayloadHeader) {
+    return new ExecutionPayloadHeaderV1(
+        executionPayloadHeader.getParentHash(),
+        executionPayloadHeader.getFeeRecipient(),
+        executionPayloadHeader.getStateRoot(),
+        executionPayloadHeader.getReceiptsRoot(),
+        executionPayloadHeader.getLogsBloom(),
+        executionPayloadHeader.getPrevRandao(),
+        executionPayloadHeader.getBlockNumber(),
+        executionPayloadHeader.getGasLimit(),
+        executionPayloadHeader.getGasUsed(),
+        executionPayloadHeader.getTimestamp(),
+        executionPayloadHeader.getExtraData(),
+        executionPayloadHeader.getBaseFeePerGas(),
+        executionPayloadHeader.getBlockHash(),
+        executionPayloadHeader.getTransactionsRoot());
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    if (!super.equals(o)) {
+      return false;
+    }
+    final ExecutionPayloadHeaderV1 that = (ExecutionPayloadHeaderV1) o;
+    return Objects.equals(transactionsRoot, that.transactionsRoot);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), transactionsRoot);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("parentHash", parentHash)
+        .add("feeRecipient", feeRecipient)
+        .add("stateRoot", stateRoot)
+        .add("receiptsRoot", receiptsRoot)
+        .add("logsBloom", logsBloom)
+        .add("prevRandao", prevRandao)
+        .add("blockNumber", blockNumber)
+        .add("gasLimit", gasLimit)
+        .add("gasUsed", gasUsed)
+        .add("timestamp", timestamp)
+        .add("extraData", extraData)
+        .add("baseFeePerGas", baseFeePerGas)
+        .add("blockHash", blockHash)
+        .add("transactionsRoot", transactionsRoot)
+        .toString();
+  }
+}
