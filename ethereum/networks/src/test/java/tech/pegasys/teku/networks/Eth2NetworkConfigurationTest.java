b04672c094cc38d2f39bba549c831e4680dd0457
Adrian Sutton <adrian.sutton@consensys.net>
2022-03-10 09:56:57 +1000

    Add kiln configuration

--- ethereum/networks/src/test/java/tech/pegasys/teku/networks/Eth2NetworkConfigurationTest.java
+++ ethereum/networks/src/test/java/tech/pegasys/teku/networks/Eth2NetworkConfigurationTest.java
@@ -25,6 +25,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import tech.pegasys.teku.networks.Eth2NetworkConfiguration.Builder;
 import tech.pegasys.teku.spec.networks.Eth2Network;
 
 public class Eth2NetworkConfigurationTest {
@@ -121,14 +122,15 @@ public class Eth2NetworkConfigurationTest {
 
   public static Stream<Arguments> getDefinedNetworks() {
     return Stream.of(
-        Arguments.of(Eth2Network.MAINNET, (NetworkDefinition) b -> b.applyMainnetNetworkDefaults()),
-        Arguments.of(Eth2Network.MINIMAL, (NetworkDefinition) b -> b.applyMinimalNetworkDefaults()),
-        Arguments.of(Eth2Network.PRATER, (NetworkDefinition) b -> b.applyPraterNetworkDefaults()),
+        Arguments.of(Eth2Network.MAINNET, (NetworkDefinition) Builder::applyMainnetNetworkDefaults),
+        Arguments.of(Eth2Network.MINIMAL, (NetworkDefinition) Builder::applyMinimalNetworkDefaults),
+        Arguments.of(Eth2Network.PRATER, (NetworkDefinition) Builder::applyPraterNetworkDefaults),
         Arguments.of(
-            Eth2Network.KINTSUGI, (NetworkDefinition) b -> b.applyKintsugiNetworkDefaults()),
-        Arguments.of(Eth2Network.SWIFT, (NetworkDefinition) b -> b.applySwiftNetworkDefaults()),
+            Eth2Network.KINTSUGI, (NetworkDefinition) Builder::applyKintsugiNetworkDefaults),
+        Arguments.of(Eth2Network.KILN, (NetworkDefinition) Builder::applyKilnNetworkDefaults),
+        Arguments.of(Eth2Network.SWIFT, (NetworkDefinition) Builder::applySwiftNetworkDefaults),
         Arguments.of(
-            Eth2Network.LESS_SWIFT, (NetworkDefinition) b -> b.applyLessSwiftNetworkDefaults()));
+            Eth2Network.LESS_SWIFT, (NetworkDefinition) Builder::applyLessSwiftNetworkDefaults));
   }
 
   private List<NodeRecord> parseBootnodes(List<String> bootnodes) {
