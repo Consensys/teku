570d079a5add02911435f239f3768ce09c10a0f9
Dmitrii Shmatko <leodex23@gmail.com>
2022-03-14 18:58:12 +0300

    Add more detailed logging

--- networking/p2p/src/main/java/tech/pegasys/teku/networking/p2p/libp2p/LibP2PPeer.java
+++ networking/p2p/src/main/java/tech/pegasys/teku/networking/p2p/libp2p/LibP2PPeer.java
@@ -89,11 +89,13 @@ public class LibP2PPeer implements Peer {
     getAgentVersionFromIdentity()
         .thenAccept(
             maybeAgent -> {
-              LOG.debug("Connected peer has agent string: {}", maybeAgent.orElse("Unknown"));
+              LOG.debug(
+                  "Connected peer id {} ({}) has agent string: {}",
+                  peerId,
+                  connection.remoteAddress(),
+                  maybeAgent.orElse("Unknown"));
               maybeAgentString = maybeAgent;
-              if (maybeAgent.isPresent()) {
-                peerClientType = getPeerTypeFromAgentString(maybeAgent.get());
-              }
+              maybeAgent.ifPresent(s -> peerClientType = getPeerTypeFromAgentString(s));
             })
         .finish(error -> LOG.debug("Failed to retrieve client identity", error));
   }
