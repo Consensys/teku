c90c7828ffa4653ab85e3644ccbfbeca38c1f1e6
courtneyeh <courtney.hunter.nz@gmail.com>
2022-03-16 09:44:33 +1300

    Migrate GetPeers endpoint to MigratingEndpointAdapter

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/node/GetPeers.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/node/GetPeers.java
@@ -13,55 +13,143 @@
 
 package tech.pegasys.teku.beaconrestapi.handlers.v1.node;
 
-import static tech.pegasys.teku.infrastructure.http.RestApiConstants.CACHE_NONE;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_OK;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.RES_INTERNAL_ERROR;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.RES_OK;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.TAG_NODE;
+import static tech.pegasys.teku.infrastructure.json.types.CoreTypes.string;
+import static tech.pegasys.teku.infrastructure.json.types.SerializableTypeDefinition.listOf;
+import static tech.pegasys.teku.infrastructure.restapi.endpoints.CacheLength.NO_CACHE;
 
-import io.javalin.core.util.Header;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.javalin.http.Context;
-import io.javalin.http.Handler;
 import io.javalin.plugin.openapi.annotations.HttpMethod;
 import io.javalin.plugin.openapi.annotations.OpenApi;
 import io.javalin.plugin.openapi.annotations.OpenApiContent;
 import io.javalin.plugin.openapi.annotations.OpenApiResponse;
 import java.util.List;
+import java.util.Optional;
 import org.jetbrains.annotations.NotNull;
 import tech.pegasys.teku.api.DataProvider;
 import tech.pegasys.teku.api.NetworkDataProvider;
-import tech.pegasys.teku.api.response.v1.node.Peer;
+import tech.pegasys.teku.api.response.v1.node.Direction;
 import tech.pegasys.teku.api.response.v1.node.PeersResponse;
-import tech.pegasys.teku.provider.JsonProvider;
+import tech.pegasys.teku.api.response.v1.node.State;
+import tech.pegasys.teku.beaconrestapi.MigratingEndpointAdapter;
+import tech.pegasys.teku.infrastructure.json.types.DeserializableTypeDefinition;
+import tech.pegasys.teku.infrastructure.json.types.SerializableTypeDefinition;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.EndpointMetadata;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiRequest;
+import tech.pegasys.teku.networking.eth2.peers.Eth2Peer;
 
-public class GetPeers implements Handler {
+public class GetPeers extends MigratingEndpointAdapter {
   public static final String ROUTE = "/eth/v1/node/peers";
-  private final JsonProvider jsonProvider;
+
+  private static final DeserializableTypeDefinition<State> STATE_TYPE =
+      DeserializableTypeDefinition.string(State.class)
+          .name("State")
+          .formatter(State::toString)
+          .parser(State::valueOf)
+          .build();
+
+  private static final DeserializableTypeDefinition<Direction> DIRECTION_TYPE =
+      DeserializableTypeDefinition.string(Direction.class)
+          .name("Direction")
+          .formatter(Direction::toString)
+          .parser(Direction::valueOf)
+          .build();
+
+  private static final SerializableTypeDefinition<Eth2Peer> PEER_DATA_TYPE =
+      SerializableTypeDefinition.object(Eth2Peer.class)
+          .name("Peer")
+          .withField(
+              "peer_id",
+              string(
+                  "Cryptographic hash of a peer’s public key. "
+                      + "'[Read more](https://docs.libp2p.io/concepts/peer-id/)",
+                  "QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N"),
+              eth2Peer -> eth2Peer.getId().toBase58())
+          .withOptionalField(
+              "enr",
+              string(
+                  "Ethereum node record. Not currently populated. "
+                      + "[Read more](https://eips.ethereum.org/EIPS/eip-778)",
+                  "example: enr:-IS4QHCYrYZbAKWCBRlAy5zzaDZXJBGkcnh4MHcBFZntXNFrdvJjX04jRzjzCBOonrk"
+                      + "Tfj499SZuOh8R33Ls8RRcy5wBgmlkgnY0gmlwhH8AAAGJc2VjcDI1NmsxoQPKY0yuDUmstAHYp"
+                      + "Ma2_oxVtw0RW_QAdpzBQA8yWM0xOIN1ZHCCdl8"),
+              eth2Peer -> Optional.empty())
+          .withField(
+              "address",
+              string(
+                  "[Read more](https://docs.libp2p.io/reference/glossary/#multiaddr)",
+                  "/ip4/7.7.7.7/tcp/4242/p2p/QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N"),
+              eth2Peer -> eth2Peer.getAddress().toExternalForm())
+          .withField(
+              "state",
+              STATE_TYPE,
+              eth2Peer -> eth2Peer.isConnected() ? State.connected : State.disconnected)
+          .withField(
+              "direction",
+              DIRECTION_TYPE,
+              eth2Peer ->
+                  eth2Peer.connectionInitiatedLocally() ? Direction.outbound : Direction.inbound)
+          .build();
+
+  private static final SerializableTypeDefinition<PeersData> PEERS_RESPONSE_TYPE =
+      SerializableTypeDefinition.object(PeersData.class)
+          .name("GetNodePeersResponse")
+          .withField("data", listOf(PEER_DATA_TYPE), PeersData::getData)
+          .build();
+
   private final NetworkDataProvider network;
 
-  public GetPeers(final DataProvider provider, final JsonProvider jsonProvider) {
-    this.jsonProvider = jsonProvider;
-    this.network = provider.getNetworkDataProvider();
+  public GetPeers(final DataProvider provider) {
+    this(provider.getNetworkDataProvider());
   }
 
-  GetPeers(final NetworkDataProvider network, final JsonProvider jsonProvider) {
+  GetPeers(final NetworkDataProvider network) {
+    super(
+        EndpointMetadata.get(ROUTE)
+            .operationId("getNodePeers")
+            .summary("Get node peers")
+            .description("Retrieves data about the node's network peers.")
+            .tags(TAG_NODE)
+            .response(SC_OK, "Request successful", PEERS_RESPONSE_TYPE)
+            .build());
+
     this.network = network;
-    this.jsonProvider = jsonProvider;
   }
 
   @OpenApi(
       path = ROUTE,
       method = HttpMethod.GET,
       summary = "Get node peers",
-      tags = {TAG_NODE},
       description = "Retrieves data about the node's network peers.",
+      tags = {TAG_NODE},
       responses = {
         @OpenApiResponse(status = RES_OK, content = @OpenApiContent(from = PeersResponse.class)),
         @OpenApiResponse(status = RES_INTERNAL_ERROR)
       })
   @Override
   public void handle(@NotNull final Context ctx) throws Exception {
-    ctx.header(Header.CACHE_CONTROL, CACHE_NONE);
-    List<Peer> peers = network.getPeers();
-    ctx.json(jsonProvider.objectToJSON(new PeersResponse(peers)));
+    adapt(ctx);
+  }
+
+  @Override
+  public void handleRequest(RestApiRequest request) throws JsonProcessingException {
+    PeersData peersData = new PeersData(network.getEth2Peers());
+    request.respondOk(peersData, NO_CACHE);
+  }
+
+  static class PeersData {
+    public final List<Eth2Peer> data;
+
+    PeersData(List<Eth2Peer> data) {
+      this.data = data;
+    }
+
+    public List<Eth2Peer> getData() {
+      return data;
+    }
   }
 }
