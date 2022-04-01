8cf7c84a37f6bc12dfd39558d247e0b0f6caee15
courtneyeh <courtney.hunter.nz@gmail.com>
2022-04-01 17:48:10 +1300

    Simplify VoluntaryExitEvent

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/events/VoluntaryExitEvent.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/events/VoluntaryExitEvent.java
@@ -13,47 +13,11 @@
 
 package tech.pegasys.teku.beaconrestapi.handlers.v1.events;
 
-import static tech.pegasys.teku.infrastructure.json.types.CoreTypes.BYTES_TYPE;
-import static tech.pegasys.teku.infrastructure.json.types.CoreTypes.UINT64_TYPE;
-
-import tech.pegasys.teku.bls.BLSSignature;
-import tech.pegasys.teku.infrastructure.json.types.SerializableTypeDefinition;
 import tech.pegasys.teku.spec.datastructures.operations.SignedVoluntaryExit;
-import tech.pegasys.teku.spec.datastructures.operations.VoluntaryExit;
 
-public class VoluntaryExitEvent extends Event<VoluntaryExitEvent.VoluntaryExitData> {
-
-  private static final SerializableTypeDefinition<VoluntaryExit> VOL_EXIT_TYPE =
-      SerializableTypeDefinition.object(VoluntaryExit.class)
-          .withField("epoch", UINT64_TYPE, VoluntaryExit::getEpoch)
-          .withField("validator_index", UINT64_TYPE, VoluntaryExit::getValidatorIndex)
-          .build();
-
-  private static final SerializableTypeDefinition<VoluntaryExitData> EVENT_TYPE =
-      SerializableTypeDefinition.object(VoluntaryExitData.class)
-          .withField("message", VOL_EXIT_TYPE, VoluntaryExitData::getMessage)
-          .withField("signature", BYTES_TYPE, data -> data.getSignature().toBytesCompressed())
-          .build();
+public class VoluntaryExitEvent extends Event<SignedVoluntaryExit> {
 
   VoluntaryExitEvent(final SignedVoluntaryExit exit) {
-    super(EVENT_TYPE, new VoluntaryExitData(exit.getMessage(), exit.getSignature()));
-  }
-
-  public static class VoluntaryExitData {
-    private final VoluntaryExit message;
-    private final BLSSignature signature;
-
-    VoluntaryExitData(final VoluntaryExit message, final BLSSignature signature) {
-      this.message = message;
-      this.signature = signature;
-    }
-
-    private VoluntaryExit getMessage() {
-      return message;
-    }
-
-    private BLSSignature getSignature() {
-      return signature;
-    }
+    super(SignedVoluntaryExit.SSZ_SCHEMA.getJsonTypeDefinition(), exit);
   }
 }
