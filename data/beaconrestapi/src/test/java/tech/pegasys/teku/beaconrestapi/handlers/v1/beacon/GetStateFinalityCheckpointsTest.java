ea672d9d14219b06938ac9028c911be7a0cd948e
Courtney <45641759+courtneyeh@users.noreply.github.com>
2022-04-14 20:18:56 +1200

    Migrate GetStateFinalityCheckpoints to MigratingEndpointAdapter (#5338)
    * Migrate GetStateFinalityCheckpoints to MigratingEndpointAdapter
    
    Co-authored-by: Paul Harris <paul.harris@consensys.net>

--- data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/GetStateFinalityCheckpointsTest.java
+++ data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/GetStateFinalityCheckpointsTest.java
@@ -13,53 +13,46 @@
 
 package tech.pegasys.teku.beaconrestapi.handlers.v1.beacon;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import java.util.Map;
 import java.util.Optional;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Test;
-import tech.pegasys.teku.api.response.v1.beacon.FinalityCheckpointsResponse;
-import tech.pegasys.teku.api.response.v1.beacon.GetStateFinalityCheckpointsResponse;
-import tech.pegasys.teku.api.schema.Checkpoint;
 import tech.pegasys.teku.beaconrestapi.AbstractBeaconHandlerTest;
 import tech.pegasys.teku.infrastructure.async.SafeFuture;
-import tech.pegasys.teku.spec.SpecMilestone;
-import tech.pegasys.teku.spec.datastructures.metadata.ObjectAndMetaData;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiRequest;
+import tech.pegasys.teku.spec.datastructures.metadata.StateAndMetaData;
 import tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconState;
 import tech.pegasys.teku.spec.util.DataStructureUtil;
 
 public class GetStateFinalityCheckpointsTest extends AbstractBeaconHandlerTest {
   private final DataStructureUtil dataStructureUtil = new DataStructureUtil(spec);
-  private final BeaconState state = dataStructureUtil.randomBeaconState();
 
   @Test
   public void shouldReturnFinalityCheckpointsInfo() throws Exception {
-    final GetStateFinalityCheckpoints handler =
-        new GetStateFinalityCheckpoints(chainDataProvider, jsonProvider);
+    final GetStateFinalityCheckpoints handler = new GetStateFinalityCheckpoints(chainDataProvider);
+    final BeaconState beaconState = dataStructureUtil.randomBeaconState();
+    final StateAndMetaData stateAndMetaData =
+        new StateAndMetaData(beaconState, spec.getGenesisSpec().getMilestone(), false, false, true);
+    when(chainDataProvider.getBeaconStateAndMetadata(eq("head")))
+        .thenReturn(SafeFuture.completedFuture(Optional.of(stateAndMetaData)));
     when(context.pathParamMap()).thenReturn(Map.of("state_id", "head"));
-    when(chainDataProvider.getStateFinalityCheckpoints("head"))
-        .thenReturn(
-            SafeFuture.completedFuture(
-                Optional.of(
-                    new ObjectAndMetaData<>(
-                        FinalityCheckpointsResponse.fromState(state),
-                        spec.getGenesisSpec().getMilestone(),
-                        false,
-                        spec.isMilestoneSupported(SpecMilestone.BELLATRIX),
-                        true))));
+    RestApiRequest request = new RestApiRequest(context, handler.getMetadata());
 
-    handler.handle(context);
+    handler.handleRequest(request);
 
-    final FinalityCheckpointsResponse expectedResponse =
-        new FinalityCheckpointsResponse(
-            new Checkpoint(state.getPreviousJustifiedCheckpoint()),
-            new Checkpoint(state.getCurrentJustifiedCheckpoint()),
-            new Checkpoint(state.getFinalizedCheckpoint()));
-
-    final GetStateFinalityCheckpointsResponse response =
-        getResponseFromFuture(GetStateFinalityCheckpointsResponse.class);
-
-    assertThat(response.data).isEqualTo(expectedResponse);
+    String expected =
+        String.format(
+            "{\"data\":{\"previous_justified\":{\"epoch\":\"%s\",\"root\":\"%s\"},\"current_justified\":"
+                + "{\"epoch\":\"%s\",\"root\":\"%s\"},\"finalized\":{\"epoch\":\"%s\",\"root\":\"%s\"}}}",
+            beaconState.getPreviousJustifiedCheckpoint().getEpoch(),
+            beaconState.getPreviousJustifiedCheckpoint().getRoot(),
+            beaconState.getCurrentJustifiedCheckpoint().getEpoch(),
+            beaconState.getCurrentJustifiedCheckpoint().getRoot(),
+            beaconState.getFinalizedCheckpoint().getEpoch(),
+            beaconState.getFinalizedCheckpoint().getRoot());
+    AssertionsForClassTypes.assertThat(getResultString()).isEqualTo(expected);
   }
 }
