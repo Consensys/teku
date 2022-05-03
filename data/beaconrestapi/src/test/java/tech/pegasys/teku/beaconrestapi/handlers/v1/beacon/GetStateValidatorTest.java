4f22fd8f2d755b4814238903a1a799c07c377ec6
courtneyeh <courtney.hunter.nz@gmail.com>
2022-05-03 15:12:16 +1200

    Fix test

--- data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/GetStateValidatorTest.java
+++ data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/GetStateValidatorTest.java
@@ -20,34 +20,22 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static tech.pegasys.teku.infrastructure.unsigned.UInt64.ONE;
 
 import com.google.common.io.Resources;
+import java.io.ByteArrayInputStream;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
-import tech.pegasys.teku.api.response.v1.beacon.ValidatorStatus;
-import tech.pegasys.teku.api.schema.Validator;
-import tech.pegasys.teku.beaconrestapi.AbstractBeaconHandlerTest;
+import tech.pegasys.teku.beaconrestapi.AbstractMigratedBeaconHandlerTest;
 import tech.pegasys.teku.infrastructure.async.SafeFuture;
 import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiRequest;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.spec.datastructures.metadata.StateAndMetaData;
 import tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconState;
-import tech.pegasys.teku.spec.util.DataStructureUtil;
 
-public class GetStateValidatorTest extends AbstractBeaconHandlerTest {
-  private final DataStructureUtil dataStructureUtil = new DataStructureUtil(spec);
+public class GetStateValidatorTest extends AbstractMigratedBeaconHandlerTest {
   private final GetStateValidator handler = new GetStateValidator(chainDataProvider);
-  private final Validator validator = new Validator(dataStructureUtil.randomValidator());
-
-  private final tech.pegasys.teku.spec.datastructures.state.Validator randomValidator =
-      dataStructureUtil.randomValidator();
-
-  private final GetStateValidator.StateValidatorData validatorResponse =
-      new GetStateValidator.StateValidatorData(
-          ONE, UInt64.valueOf("32000000000"), ValidatorStatus.active_ongoing, randomValidator);
 
   @Test
   public void shouldGetValidatorFromState() throws Exception {
@@ -63,12 +51,13 @@ public class GetStateValidatorTest extends AbstractBeaconHandlerTest {
     RestApiRequest request = new RestApiRequest(context, handler.getMetadata());
     handler.handleRequest(request);
 
-    SafeFuture<String> future = getResultFuture();
+    SafeFuture<ByteArrayInputStream> future = getResultFuture();
     assertThat(future).isCompleted();
     String expected =
         Resources.toString(
             Resources.getResource(GetStateValidatorTest.class, "validatorState.json"), UTF_8);
-    assertThat(future.get()).isEqualTo(expected);
+    String actual = new String(future.get().readAllBytes(), UTF_8);
+    assertThat(actual).isEqualTo(expected);
   }
 
   @Test
