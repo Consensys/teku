7f0053d26f2b10e6a48d0f643d63a3310854cc7d
Paul Harris <paul.harris@consensys.net>
2022-05-05 06:46:31 +1000

    Initial implementation of stub RestApiRequest (#5425)
    * Initial implementation of stub RestApiRequest
    
    added to a couple of tests to show functionality.
    
    partially addresses #5372
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/GetStateForkTest.java
+++ data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/GetStateForkTest.java
@@ -14,26 +14,27 @@
 package tech.pegasys.teku.beaconrestapi.handlers.v1.beacon;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_BAD_REQUEST;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_INTERNAL_SERVER_ERROR;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_NOT_FOUND;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_OK;
 
-import java.util.Map;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.Optional;
-import org.junit.jupiter.api.BeforeEach;
+import javax.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
 import tech.pegasys.teku.beaconrestapi.AbstractMigratedBeaconHandlerTest;
 import tech.pegasys.teku.infrastructure.async.SafeFuture;
-import tech.pegasys.teku.infrastructure.restapi.endpoints.JavalinRestApiRequest;
-import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiRequest;
+import tech.pegasys.teku.infrastructure.http.HttpErrorResponse;
+import tech.pegasys.teku.infrastructure.restapi.StubRestApiRequest;
 import tech.pegasys.teku.spec.datastructures.metadata.StateAndMetaData;
 
 public class GetStateForkTest extends AbstractMigratedBeaconHandlerTest {
   private final GetStateFork handler = new GetStateFork(chainDataProvider);
-  private RestApiRequest request;
+  private final StubRestApiRequest request =
+      StubRestApiRequest.builder().pathParameter("state_id", "head").build();
   private final StateAndMetaData stateAndMetaData =
       new StateAndMetaData(
           dataStructureUtil.randomBeaconState(),
@@ -42,34 +43,48 @@ public class GetStateForkTest extends AbstractMigratedBeaconHandlerTest {
           false,
           true);
 
-  @BeforeEach
-  void setUp() {
-    when(context.pathParamMap()).thenReturn(Map.of("state_id", "head"));
-  }
-
   @Test
   public void shouldReturnForkInfo() throws Exception {
     when(chainDataProvider.getBeaconStateAndMetadata(eq("head")))
         .thenReturn(SafeFuture.completedFuture(Optional.of(stateAndMetaData)));
-    request = new JavalinRestApiRequest(context, handler.getMetadata());
-
     handler.handleRequest(request);
 
-    assertThat(getFutureResultString())
-        .isEqualTo(
-            "{\"data\":{\"previous_version\":\"0x103ac940\",\"current_version\":\"0x6fdfab40\",\"epoch\":\"4658411424342975020\"}}");
-    verify(context, never()).status(any());
+    assertThat(request.getResponseCode()).isEqualTo(SC_OK);
+    assertThat(request.getResponseBody()).isInstanceOf(StateAndMetaData.class);
+    assertThat(request.getResponseBody()).isEqualTo(stateAndMetaData);
   }
 
   @Test
   public void shouldReturnNotFound() throws Exception {
     when(chainDataProvider.getBeaconStateAndMetadata(eq("head")))
         .thenReturn(SafeFuture.completedFuture(Optional.empty()));
-    request = new JavalinRestApiRequest(context, handler.getMetadata());
-
     handler.handleRequest(request);
 
-    assertThat(getFutureResultString()).isEqualTo("{\"code\":404,\"message\":\"Not found\"}");
-    verify(context).status(SC_NOT_FOUND);
+    assertThat(request.getResponseCode()).isEqualTo(SC_NOT_FOUND);
+    assertThat(request.getResponseBody())
+        .isEqualTo(new HttpErrorResponse(SC_NOT_FOUND, "Not found"));
+  }
+
+  @Test
+  void metadata_shouldHandle400() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, SC_BAD_REQUEST);
+  }
+
+  @Test
+  void metadata_shouldHandle404() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, HttpServletResponse.SC_NOT_FOUND);
+  }
+
+  @Test
+  void metadata_shouldHandle500() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, SC_INTERNAL_SERVER_ERROR);
+  }
+
+  @Test
+  void metadata_shouldHandle200() throws JsonProcessingException {
+    final String data = getResponseStringFromMetadata(handler, SC_OK, stateAndMetaData);
+    assertThat(data)
+        .isEqualTo(
+            "{\"data\":{\"previous_version\":\"0x103ac940\",\"current_version\":\"0x6fdfab40\",\"epoch\":\"4658411424342975020\"}}");
   }
 }
