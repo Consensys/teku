936077510f0480b06925a308e23bfb4e44c6e7e4
Courtney <45641759+courtneyeh@users.noreply.github.com>
2022-05-09 10:50:47 +1200

    Update GetStateRootTest (#5442)

--- data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/GetStateRootTest.java
+++ data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/GetStateRootTest.java
@@ -13,42 +13,67 @@
 
 package tech.pegasys.teku.beaconrestapi.handlers.v1.beacon;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_BAD_REQUEST;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_INTERNAL_SERVER_ERROR;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_OK;
 
-import java.util.Map;
-import org.assertj.core.api.AssertionsForClassTypes;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.IOException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tech.pegasys.teku.beaconrestapi.AbstractMigratedBeaconHandlerWithChainDataProviderTest;
-import tech.pegasys.teku.infrastructure.restapi.endpoints.JavalinRestApiRequest;
-import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiRequest;
+import tech.pegasys.teku.infrastructure.restapi.StubRestApiRequest;
 import tech.pegasys.teku.spec.SpecMilestone;
+import tech.pegasys.teku.spec.datastructures.metadata.StateAndMetaData;
 import tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconState;
 
 public class GetStateRootTest extends AbstractMigratedBeaconHandlerWithChainDataProviderTest {
+  private GetStateRoot handler;
+  private final StubRestApiRequest request =
+      StubRestApiRequest.builder().pathParameter("state_id", "head").build();
 
   @BeforeEach
   public void setup() {
     initialise(SpecMilestone.PHASE0);
     genesis();
+    handler = new GetStateRoot(chainDataProvider);
   }
 
   @Test
   public void shouldReturnRootInfo() throws Exception {
     final BeaconState state = recentChainData.getBestState().orElseThrow().get();
-    final GetStateRoot handler = new GetStateRoot(chainDataProvider);
-
-    when(context.pathParamMap()).thenReturn(Map.of("state_id", "head"));
-    RestApiRequest request = new JavalinRestApiRequest(context, handler.getMetadata());
 
     handler.handleRequest(request);
 
+    assertThat(request.getResponseCode()).isEqualTo(SC_OK);
+    StateAndMetaData expectedBody =
+        new StateAndMetaData(state, spec.getGenesisSpec().getMilestone(), false, false, true);
+    assertThat(request.getResponseBody()).isEqualTo(expectedBody);
+  }
+
+  @Test
+  void metadata_shouldHandle400() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, SC_BAD_REQUEST);
+  }
+
+  @Test
+  void metadata_shouldHandle500() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, SC_INTERNAL_SERVER_ERROR);
+  }
+
+  @Test
+  void metadata_shouldHandle200() throws IOException {
+    final StateAndMetaData responseData =
+        new StateAndMetaData(
+            dataStructureUtil.randomBeaconState(),
+            spec.getGenesisSpec().getMilestone(),
+            false,
+            false,
+            true);
+    final String data = getResponseStringFromMetadata(handler, SC_OK, responseData);
     String expected =
-        String.format("{\"data\":{\"root\":\"%s\"}}", state.hashTreeRoot().toHexString());
-    AssertionsForClassTypes.assertThat(getFutureResultString()).isEqualTo(expected);
-    verify(context, never()).status(any());
+        String.format("{\"data\":{\"root\":\"%s\"}}", responseData.getData().hashTreeRoot());
+    assertThat(data).isEqualTo(expected);
   }
 }
