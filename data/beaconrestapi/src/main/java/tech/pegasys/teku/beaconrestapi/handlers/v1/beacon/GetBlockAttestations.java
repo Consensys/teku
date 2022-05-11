c6ed39fdb28de1988c9f1da928dad3200a9c2061
Courtney <45641759+courtneyeh@users.noreply.github.com>
2022-05-12 11:35:38 +1200

    Migrate GetBlockAttestations to MigratingEndpointAdapter (#5474)

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/GetBlockAttestations.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/GetBlockAttestations.java
@@ -13,7 +13,9 @@
 
 package tech.pegasys.teku.beaconrestapi.handlers.v1.beacon;
 
-import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_NOT_FOUND;
+import static tech.pegasys.teku.beaconrestapi.BeaconRestApiTypes.PARAMETER_BLOCK_ID;
+import static tech.pegasys.teku.beaconrestapi.handlers.AbstractHandler.routeWithBracedParameters;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_OK;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.PARAM_BLOCK_ID;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.PARAM_BLOCK_ID_DESCRIPTION;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.RES_BAD_REQUEST;
@@ -21,40 +23,52 @@ import static tech.pegasys.teku.infrastructure.http.RestApiConstants.RES_INTERNA
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.RES_NOT_FOUND;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.RES_OK;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.TAG_BEACON;
+import static tech.pegasys.teku.infrastructure.json.types.CoreTypes.BOOLEAN_TYPE;
+import static tech.pegasys.teku.infrastructure.json.types.SerializableTypeDefinition.listOf;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.javalin.http.Context;
-import io.javalin.http.Handler;
 import io.javalin.plugin.openapi.annotations.HttpMethod;
 import io.javalin.plugin.openapi.annotations.OpenApi;
 import io.javalin.plugin.openapi.annotations.OpenApiContent;
 import io.javalin.plugin.openapi.annotations.OpenApiParam;
 import io.javalin.plugin.openapi.annotations.OpenApiResponse;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import org.jetbrains.annotations.NotNull;
 import tech.pegasys.teku.api.ChainDataProvider;
 import tech.pegasys.teku.api.DataProvider;
 import tech.pegasys.teku.api.response.v1.beacon.GetBlockAttestationsResponse;
-import tech.pegasys.teku.api.schema.Attestation;
-import tech.pegasys.teku.beaconrestapi.handlers.AbstractHandler;
+import tech.pegasys.teku.beaconrestapi.MigratingEndpointAdapter;
 import tech.pegasys.teku.infrastructure.async.SafeFuture;
-import tech.pegasys.teku.provider.JsonProvider;
+import tech.pegasys.teku.infrastructure.json.types.SerializableTypeDefinition;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.AsyncApiResponse;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.EndpointMetadata;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiRequest;
+import tech.pegasys.teku.spec.Spec;
 import tech.pegasys.teku.spec.datastructures.metadata.ObjectAndMetaData;
+import tech.pegasys.teku.spec.datastructures.operations.Attestation;
 
-public class GetBlockAttestations extends AbstractHandler implements Handler {
+public class GetBlockAttestations extends MigratingEndpointAdapter {
   private static final String OAPI_ROUTE = "/eth/v1/beacon/blocks/:block_id/attestations";
   public static final String ROUTE = routeWithBracedParameters(OAPI_ROUTE);
   private final ChainDataProvider chainDataProvider;
 
-  public GetBlockAttestations(final DataProvider dataProvider, final JsonProvider jsonProvider) {
-    this(dataProvider.getChainDataProvider(), jsonProvider);
+  public GetBlockAttestations(final DataProvider dataProvider, Spec spec) {
+    this(dataProvider.getChainDataProvider(), spec);
   }
 
-  public GetBlockAttestations(
-      final ChainDataProvider chainDataProvider, final JsonProvider jsonProvider) {
-    super(jsonProvider);
+  public GetBlockAttestations(final ChainDataProvider chainDataProvider, Spec spec) {
+    super(
+        EndpointMetadata.get(ROUTE)
+            .operationId("getBlockAttestations")
+            .summary("Get block attestations")
+            .description("Retrieves attestations included in requested block.")
+            .tags(TAG_BEACON)
+            .pathParam(PARAMETER_BLOCK_ID)
+            .response(SC_OK, "Request successful", getResponseType(spec))
+            .withNotFoundResponse()
+            .build());
     this.chainDataProvider = chainDataProvider;
   }
 
@@ -75,19 +89,35 @@ public class GetBlockAttestations extends AbstractHandler implements Handler {
       })
   @Override
   public void handle(@NotNull final Context ctx) throws Exception {
-
-    final Map<String, String> pathParams = ctx.pathParamMap();
-    final SafeFuture<Optional<ObjectAndMetaData<List<Attestation>>>> future =
-        chainDataProvider.getBlockAttestations(pathParams.get(PARAM_BLOCK_ID));
-    handleOptionalResult(ctx, future, this::handleResult, SC_NOT_FOUND);
+    adapt(ctx);
   }
 
-  private Optional<String> handleResult(
-      Context ctx, final ObjectAndMetaData<List<Attestation>> response)
-      throws JsonProcessingException {
-    return Optional.of(
-        jsonProvider.objectToJSON(
-            new GetBlockAttestationsResponse(
-                response.isExecutionOptimisticForApi(), response.getData())));
+  @Override
+  public void handleRequest(RestApiRequest request) throws JsonProcessingException {
+    final SafeFuture<Optional<ObjectAndMetaData<List<Attestation>>>> future =
+        chainDataProvider.getBlockAttestations(request.getPathParameter(PARAMETER_BLOCK_ID));
+
+    request.respondAsync(
+        future.thenApply(
+            maybeObjectAndMetaData -> {
+              if (maybeObjectAndMetaData.isEmpty()) {
+                return AsyncApiResponse.respondNotFound();
+              }
+
+              ObjectAndMetaData<List<Attestation>> response = maybeObjectAndMetaData.get();
+              return AsyncApiResponse.respondOk(response);
+            }));
+  }
+
+  private static SerializableTypeDefinition<ObjectAndMetaData<List<Attestation>>> getResponseType(
+      Spec spec) {
+    Attestation.AttestationSchema dataSchema =
+        new Attestation.AttestationSchema(spec.getGenesisSpecConfig());
+
+    return SerializableTypeDefinition.<ObjectAndMetaData<List<Attestation>>>object()
+        .name("GetBlockAttestationsResponse")
+        .withField("execution_optimistic", BOOLEAN_TYPE, ObjectAndMetaData::isExecutionOptimistic)
+        .withField("data", listOf(dataSchema.getJsonTypeDefinition()), ObjectAndMetaData::getData)
+        .build();
   }
 }
