db86838495ebd95e9a2e299a0fa822b2fa2e8fa2
courtneyeh <courtney.hunter.nz@gmail.com>
2022-05-11 17:02:10 +1200

    Add tests for GetBlockAttestations

---
+++ data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/GetBlockAttestationsTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2022 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.beaconrestapi.handlers.v1.beacon;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.common.io.Resources;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tech.pegasys.teku.beaconrestapi.AbstractMigratedBeaconHandlerTest;
+import tech.pegasys.teku.beaconrestapi.AbstractMigratedBeaconHandlerWithChainDataProviderTest;
+import tech.pegasys.teku.infrastructure.restapi.StubRestApiRequest;
+import tech.pegasys.teku.spec.SpecMilestone;
+import tech.pegasys.teku.spec.datastructures.metadata.ObjectAndMetaData;
+import tech.pegasys.teku.spec.datastructures.metadata.StateAndMetaData;
+import tech.pegasys.teku.spec.datastructures.operations.Attestation;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_BAD_REQUEST;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_INTERNAL_SERVER_ERROR;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_OK;
+
+class GetBlockAttestationsTest extends AbstractMigratedBeaconHandlerWithChainDataProviderTest {
+  private StubRestApiRequest request;
+  private GetBlockAttestations handler;
+
+  @BeforeEach
+  void setup() {
+    initialise(SpecMilestone.PHASE0);
+    genesis();
+
+    request = StubRestApiRequest.builder().pathParameter("block_id", "head").build();
+    handler = new GetBlockAttestations(chainDataProvider, spec);
+  }
+
+  @Test
+  public void shouldReturnBlockAttestationsInformation() throws JsonProcessingException, ExecutionException, InterruptedException {
+    final Optional<ObjectAndMetaData<List<Attestation>>> stateAndMetaData =
+            chainDataProvider.getBlockAttestations("head").get();
+
+    handler.handleRequest(request);
+
+    assertThat(request.getResponseCode()).isEqualTo(SC_OK);
+    assertThat(stateAndMetaData.isPresent()).isTrue();
+    assertThat(request.getResponseBody()).isEqualTo(stateAndMetaData.get());
+  }
+
+  @Test
+  void metadata_shouldHandle400() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, SC_BAD_REQUEST);
+  }
+
+  @Test
+  void metadata_shouldHandle500() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, SC_INTERNAL_SERVER_ERROR);
+  }
+
+  @Test
+  void metadata_shouldHandle200() throws IOException {
+    final List<Attestation> attestations =
+            List.of(dataStructureUtil.randomAttestation(), dataStructureUtil.randomAttestation());
+    final ObjectAndMetaData<List<Attestation>> responseData =
+            new ObjectAndMetaData<>(attestations, spec.getGenesisSpec().getMilestone(), false, false, true);
+
+    final String data = getResponseStringFromMetadata(handler, SC_OK, responseData);
+    final String expected =
+            Resources.toString(
+                    Resources.getResource(GetBlockAttestationsTest.class, "getBlockAttestations.json"),
+                    UTF_8);
+    assertThat(data).isEqualTo(expected);
+  }
+}
