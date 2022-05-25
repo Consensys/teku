8dde333bff7d0dcd4eae53fee3122648cec96dab
courtneyeh <courtney.hunter.nz@gmail.com>
2022-05-25 16:22:12 +1200

    Migrate GetBlock V2 to MigratingEndpointAdapter

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v2/beacon/GetBlock.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v2/beacon/GetBlock.java
@@ -13,10 +13,14 @@
 
 package tech.pegasys.teku.beaconrestapi.handlers.v2.beacon;
 
+import static tech.pegasys.teku.beaconrestapi.BeaconRestApiTypes.PARAMETER_BLOCK_ID;
+import static tech.pegasys.teku.beaconrestapi.EthereumTypes.MILESTONE_TYPE;
+import static tech.pegasys.teku.beaconrestapi.EthereumTypes.sszResponseType;
+import static tech.pegasys.teku.beaconrestapi.handlers.AbstractHandler.routeWithBracedParameters;
+import static tech.pegasys.teku.beaconrestapi.handlers.v1.beacon.MilestoneDependentTypesUtil.getSchemaDefinitionForAllMilestones;
 import static tech.pegasys.teku.infrastructure.http.ContentTypes.JSON;
 import static tech.pegasys.teku.infrastructure.http.ContentTypes.OCTET_STREAM;
-import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_NOT_FOUND;
-import static tech.pegasys.teku.infrastructure.http.RestApiConstants.HEADER_ACCEPT;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_OK;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.HEADER_CONSENSUS_VERSION;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.PARAM_BLOCK_ID;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.PARAM_BLOCK_ID_DESCRIPTION;
@@ -25,41 +29,58 @@ import static tech.pegasys.teku.infrastructure.http.RestApiConstants.RES_INTERNA
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.RES_NOT_FOUND;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.RES_OK;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.TAG_BEACON;
+import static tech.pegasys.teku.infrastructure.json.types.CoreTypes.BOOLEAN_TYPE;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.javalin.http.Context;
-import io.javalin.http.Handler;
 import io.javalin.plugin.openapi.annotations.HttpMethod;
 import io.javalin.plugin.openapi.annotations.OpenApi;
 import io.javalin.plugin.openapi.annotations.OpenApiContent;
 import io.javalin.plugin.openapi.annotations.OpenApiParam;
 import io.javalin.plugin.openapi.annotations.OpenApiResponse;
-import java.io.ByteArrayInputStream;
-import java.util.Map;
 import java.util.Optional;
 import org.jetbrains.annotations.NotNull;
 import tech.pegasys.teku.api.ChainDataProvider;
 import tech.pegasys.teku.api.DataProvider;
-import tech.pegasys.teku.api.response.SszResponse;
 import tech.pegasys.teku.api.response.v2.beacon.GetBlockResponseV2;
-import tech.pegasys.teku.api.schema.SignedBeaconBlock;
 import tech.pegasys.teku.api.schema.Version;
-import tech.pegasys.teku.beaconrestapi.handlers.AbstractHandler;
+import tech.pegasys.teku.beaconrestapi.MigratingEndpointAdapter;
 import tech.pegasys.teku.infrastructure.async.SafeFuture;
-import tech.pegasys.teku.provider.JsonProvider;
+import tech.pegasys.teku.infrastructure.json.types.SerializableTypeDefinition;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.AsyncApiResponse;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.EndpointMetadata;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiRequest;
+import tech.pegasys.teku.spec.datastructures.blocks.SignedBeaconBlock;
 import tech.pegasys.teku.spec.datastructures.metadata.ObjectAndMetaData;
+import tech.pegasys.teku.spec.schemas.SchemaDefinitionCache;
+import tech.pegasys.teku.spec.schemas.SchemaDefinitions;
 
-public class GetBlock extends AbstractHandler implements Handler {
+public class GetBlock extends MigratingEndpointAdapter {
   private static final String OAPI_ROUTE = "/eth/v2/beacon/blocks/:block_id";
   public static final String ROUTE = routeWithBracedParameters(OAPI_ROUTE);
   private final ChainDataProvider chainDataProvider;
 
-  public GetBlock(final DataProvider dataProvider, final JsonProvider jsonProvider) {
-    this(dataProvider.getChainDataProvider(), jsonProvider);
+  public GetBlock(
+      final DataProvider dataProvider, final SchemaDefinitionCache schemaDefinitionCache) {
+    this(dataProvider.getChainDataProvider(), schemaDefinitionCache);
   }
 
-  public GetBlock(final ChainDataProvider chainDataProvider, final JsonProvider jsonProvider) {
-    super(jsonProvider);
+  public GetBlock(
+      final ChainDataProvider chainDataProvider,
+      final SchemaDefinitionCache schemaDefinitionCache) {
+    super(
+        EndpointMetadata.get(ROUTE)
+            .operationId("getBlockV2")
+            .summary("Retrieves block details for given block id.")
+            .description("Retrieves block details for given block id.")
+            .tags(TAG_BEACON)
+            .pathParam(PARAMETER_BLOCK_ID)
+            .response(
+                SC_OK,
+                "Request successful",
+                getResponseType(schemaDefinitionCache),
+                sszResponseType())
+            .build());
     this.chainDataProvider = chainDataProvider;
   }
 
@@ -83,41 +104,45 @@ public class GetBlock extends AbstractHandler implements Handler {
       })
   @Override
   public void handle(@NotNull final Context ctx) throws Exception {
-    final Map<String, String> pathParams = ctx.pathParamMap();
-    final Optional<String> maybeAcceptHeader = Optional.ofNullable(ctx.header(HEADER_ACCEPT));
-    final String blockIdentifier = pathParams.get(PARAM_BLOCK_ID);
-
-    if (getContentType(SSZ_OR_JSON_CONTENT_TYPES, maybeAcceptHeader)
-        .equalsIgnoreCase(OCTET_STREAM)) {
-      final SafeFuture<Optional<SszResponse>> future =
-          chainDataProvider.getBlockSsz(blockIdentifier);
-      handleOptionalSszResult(
-          ctx, future, this::handleSszResult, this::resultFilename, SC_NOT_FOUND);
-
-    } else {
-      final SafeFuture<Optional<ObjectAndMetaData<SignedBeaconBlock>>> future =
-          chainDataProvider.getBlock(blockIdentifier);
-      handleOptionalResult(ctx, future, this::handleJsonResult, SC_NOT_FOUND);
-    }
+    adapt(ctx);
   }
 
-  private Optional<String> handleJsonResult(
-      Context ctx, final ObjectAndMetaData<SignedBeaconBlock> response)
-      throws JsonProcessingException {
-    final Version version = Version.fromMilestone(response.getMilestone());
-    ctx.header(HEADER_CONSENSUS_VERSION, version.name());
-    return Optional.of(
-        jsonProvider.objectToJSON(
-            new GetBlockResponseV2(version, response.isExecutionOptimistic(), response.getData())));
+  @Override
+  public void handleRequest(RestApiRequest request) throws JsonProcessingException {
+    final SafeFuture<Optional<ObjectAndMetaData<SignedBeaconBlock>>> future =
+        chainDataProvider.getBlock(request.getPathParameter(PARAMETER_BLOCK_ID));
+
+    request.respondAsync(
+        future.thenApply(
+            maybebeaconBlockAndMetaData ->
+                maybebeaconBlockAndMetaData
+                    .map(
+                        beaconBlockAndMetaData -> {
+                          request.header(
+                              HEADER_CONSENSUS_VERSION,
+                              Version.fromMilestone(beaconBlockAndMetaData.getMilestone()).name());
+                          return AsyncApiResponse.respondOk(beaconBlockAndMetaData);
+                        })
+                    .orElseGet(AsyncApiResponse::respondNotFound)));
   }
 
-  private String resultFilename(final SszResponse response) {
-    return response.abbreviatedHash + ".ssz";
-  }
+  private static SerializableTypeDefinition<ObjectAndMetaData<SignedBeaconBlock>> getResponseType(
+      SchemaDefinitionCache schemaDefinitionCache) {
+    final SerializableTypeDefinition<SignedBeaconBlock> signedBeaconBlockType =
+        getSchemaDefinitionForAllMilestones(
+            schemaDefinitionCache,
+            "SignedBeaconBlock",
+            SchemaDefinitions::getSignedBeaconBlockSchema,
+            (signedBeaconBlock, milestone) ->
+                schemaDefinitionCache
+                    .milestoneAtSlot(signedBeaconBlock.getSlot())
+                    .equals(milestone));
 
-  private Optional<ByteArrayInputStream> handleSszResult(
-      final Context ctx, final SszResponse response) {
-    ctx.header(HEADER_CONSENSUS_VERSION, response.version.name());
-    return Optional.of(response.byteStream);
+    return SerializableTypeDefinition.<ObjectAndMetaData<SignedBeaconBlock>>object()
+        .name("GetStateV2Response")
+        .withField("version", MILESTONE_TYPE, ObjectAndMetaData::getMilestone)
+        .withField("execution_optimistic", BOOLEAN_TYPE, ObjectAndMetaData::isExecutionOptimistic)
+        .withField("data", signedBeaconBlockType, ObjectAndMetaData::getData)
+        .build();
   }
 }
