89f27fdb7b8994b58e5e49a9773642f6f71a79a4
Stefan Bratanov <stefan.bratanov93@gmail.com>
2022-05-30 10:12:04 +0300

    iniitialise validator registrator

--- validator/client/src/main/java/tech/pegasys/teku/validator/client/ValidatorClientService.java
+++ validator/client/src/main/java/tech/pegasys/teku/validator/client/ValidatorClientService.java
@@ -72,6 +72,7 @@ public class ValidatorClientService extends Service {
   private ValidatorIndexProvider validatorIndexProvider;
   private final Optional<ProposerConfigProvider> proposerConfigProvider;
   private final Optional<BeaconProposerPreparer> beaconProposerPreparer;
+  private final Optional<ValidatorRegistrator> validatorRegistrator;
 
   private final SafeFuture<Void> initializationComplete = new SafeFuture<>();
 
@@ -85,6 +86,7 @@ public class ValidatorClientService extends Service {
       final ForkProvider forkProvider,
       final Optional<ProposerConfigProvider> proposerConfigProvider,
       final Optional<BeaconProposerPreparer> beaconProposerPreparer,
+      final Optional<ValidatorRegistrator> validatorRegistrator,
       final Spec spec,
       final MetricsSystem metricsSystem) {
     this.eventChannels = eventChannels;
@@ -94,6 +96,7 @@ public class ValidatorClientService extends Service {
     this.forkProvider = forkProvider;
     this.proposerConfigProvider = proposerConfigProvider;
     this.beaconProposerPreparer = beaconProposerPreparer;
+    this.validatorRegistrator = validatorRegistrator;
     this.spec = spec;
     this.metricsSystem = metricsSystem;
   }
@@ -135,6 +138,7 @@ public class ValidatorClientService extends Service {
     Optional<RestApi> validatorRestApi = Optional.empty();
     Optional<ProposerConfigProvider> proposerConfigProvider = Optional.empty();
     Optional<BeaconProposerPreparer> beaconProposerPreparer = Optional.empty();
+    Optional<ValidatorRegistrator> validatorRegistrator = Optional.empty();
     if (config.getSpec().isMilestoneSupported(SpecMilestone.BELLATRIX)) {
       proposerConfigProvider =
           Optional.of(
@@ -155,6 +159,15 @@ public class ValidatorClientService extends Service {
                   Optional.of(
                       ValidatorClientService.getKeyManagerPath(services.getDataDirLayout())
                           .resolve("api-proposer-config.json"))));
+
+      validatorRegistrator =
+          Optional.of(
+              new ValidatorRegistrator(
+                  forkProvider,
+                  validatorLoader.getOwnedValidators(),
+                  validatorApiChannel,
+                  config.getSpec(),
+                  services.getTimeProvider()));
     }
     if (validatorApiConfig.isRestApiEnabled()) {
       validatorRestApi =
@@ -178,6 +191,7 @@ public class ValidatorClientService extends Service {
             forkProvider,
             proposerConfigProvider,
             beaconProposerPreparer,
+            validatorRegistrator,
             config.getSpec(),
             services.getMetricsSystem());
 
