8ad6391da97ab1e1834eb8d1f97c728898237124
Stefan Bratanov <stefan.bratanov93@gmail.com>
2022-05-31 16:27:22 +0100

    [MEV Boost\Builder] Retrieve the feeRecipient for a validator registration (#5642)

--- validator/client/src/main/java/tech/pegasys/teku/validator/client/ValidatorRegistrator.java
+++ validator/client/src/main/java/tech/pegasys/teku/validator/client/ValidatorRegistrator.java
@@ -18,18 +18,23 @@ import static tech.pegasys.teku.infrastructure.logging.ValidatorLogger.VALIDATOR
 import com.google.common.collect.Maps;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.tuweni.bytes.Bytes32;
+import tech.pegasys.teku.bls.BLSPublicKey;
 import tech.pegasys.teku.core.signatures.Signer;
 import tech.pegasys.teku.infrastructure.async.SafeFuture;
-import tech.pegasys.teku.infrastructure.bytes.Bytes20;
 import tech.pegasys.teku.infrastructure.ssz.impl.SszUtils;
 import tech.pegasys.teku.infrastructure.time.TimeProvider;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.spec.Spec;
+import tech.pegasys.teku.spec.datastructures.eth1.Eth1Address;
 import tech.pegasys.teku.spec.datastructures.execution.SignedValidatorRegistration;
 import tech.pegasys.teku.spec.datastructures.execution.ValidatorRegistration;
 import tech.pegasys.teku.spec.schemas.ApiSchemas;
@@ -38,6 +43,7 @@ import tech.pegasys.teku.validator.api.ValidatorTimingChannel;
 import tech.pegasys.teku.validator.client.loader.OwnedValidators;
 
 public class ValidatorRegistrator implements ValidatorTimingChannel {
+  private static final Logger LOG = LogManager.getLogger();
 
   private final Map<ValidatorRegistrationIdentity, SignedValidatorRegistration>
       cachedValidatorRegistrations = Maps.newConcurrentMap();
@@ -48,16 +54,19 @@ public class ValidatorRegistrator implements ValidatorTimingChannel {
   private final Spec spec;
   private final TimeProvider timeProvider;
   private final OwnedValidators ownedValidators;
+  private final FeeRecipientProvider feeRecipientProvider;
   private final ValidatorApiChannel validatorApiChannel;
 
   public ValidatorRegistrator(
-      final OwnedValidators ownedValidators,
-      final ValidatorApiChannel validatorApiChannel,
       final Spec spec,
-      final TimeProvider timeProvider) {
+      final TimeProvider timeProvider,
+      final OwnedValidators ownedValidators,
+      final FeeRecipientProvider feeRecipientProvider,
+      final ValidatorApiChannel validatorApiChannel) {
     this.spec = spec;
     this.timeProvider = timeProvider;
     this.ownedValidators = ownedValidators;
+    this.feeRecipientProvider = feeRecipientProvider;
     this.validatorApiChannel = validatorApiChannel;
   }
 
@@ -87,14 +96,15 @@ public class ValidatorRegistrator implements ValidatorTimingChannel {
       return;
     }
 
+    final Set<BLSPublicKey> alreadyAddedValidatorsPublicKeys =
+        cachedValidatorRegistrations.keySet().stream()
+            .map(ValidatorRegistrationIdentity::getPublicKey)
+            .collect(Collectors.toSet());
+
     final List<Validator> newlyAddedValidators =
         ownedValidators.getActiveValidators().stream()
             .filter(
-                validator -> {
-                  final ValidatorRegistrationIdentity validatorRegistrationIdentity =
-                      createValidatorRegistrationIdentity(validator);
-                  return !cachedValidatorRegistrations.containsKey(validatorRegistrationIdentity);
-                })
+                validator -> !alreadyAddedValidatorsPublicKeys.contains(validator.getPublicKey()))
             .collect(Collectors.toList());
 
     registerValidators(newlyAddedValidators, lastProcessedEpoch.get())
@@ -122,24 +132,37 @@ public class ValidatorRegistrator implements ValidatorTimingChannel {
 
     final Stream<SafeFuture<SignedValidatorRegistration>> validatorRegistrationsFutures =
         validators.stream()
-            .map(
+            .flatMap(
                 validator -> {
+                  final BLSPublicKey publicKey = validator.getPublicKey();
+                  final Optional<Eth1Address> maybeFeeRecipient =
+                      feeRecipientProvider.getFeeRecipient(publicKey);
+
+                  if (maybeFeeRecipient.isEmpty()) {
+                    LOG.warn(
+                        "There is no fee recipient configured for {}. Can't register.", validator);
+                    return Stream.empty();
+                  }
+
                   final ValidatorRegistrationIdentity validatorRegistrationIdentity =
-                      createValidatorRegistrationIdentity(validator);
+                      createValidatorRegistrationIdentity(validator, maybeFeeRecipient.get());
 
                   if (cachedValidatorRegistrations.containsKey(validatorRegistrationIdentity)) {
-                    return SafeFuture.completedFuture(
-                        cachedValidatorRegistrations.get(validatorRegistrationIdentity));
+                    final SignedValidatorRegistration cachedRegistration =
+                        cachedValidatorRegistrations.get(validatorRegistrationIdentity);
+                    return Stream.of(SafeFuture.completedFuture(cachedRegistration));
                   }
 
                   final ValidatorRegistration validatorRegistration =
                       createValidatorRegistration(validatorRegistrationIdentity);
                   final Signer signer = validator.getSigner();
-                  return signValidatorRegistration(validatorRegistration, signer, epoch)
-                      .thenPeek(
-                          signedValidatorRegistration ->
-                              cachedValidatorRegistrations.put(
-                                  validatorRegistrationIdentity, signedValidatorRegistration));
+                  final SafeFuture<SignedValidatorRegistration> registrationFuture =
+                      signValidatorRegistration(validatorRegistration, signer, epoch)
+                          .thenPeek(
+                              signedValidatorRegistration ->
+                                  cachedValidatorRegistrations.put(
+                                      validatorRegistrationIdentity, signedValidatorRegistration));
+                  return Stream.of(registrationFuture);
                 });
 
     return SafeFuture.collectAll(validatorRegistrationsFutures)
@@ -151,10 +174,10 @@ public class ValidatorRegistrator implements ValidatorTimingChannel {
   }
 
   private ValidatorRegistrationIdentity createValidatorRegistrationIdentity(
-      final Validator validator) {
-    // hardcoding fee_recipient and gas_limit to ZERO for now. The real values will be
+      final Validator validator, final Eth1Address feeRecipient) {
+    // hardcoding gas_limit to ZERO for now. The real value will be
     // taken from the proposer config in a future PR.
-    return new ValidatorRegistrationIdentity(Bytes20.ZERO, UInt64.ZERO, validator.getPublicKey());
+    return new ValidatorRegistrationIdentity(feeRecipient, UInt64.ZERO, validator.getPublicKey());
   }
 
   private ValidatorRegistration createValidatorRegistration(
