70c20ed2fa952e58e4591a3996b4d6f920cc34dc
Justin Traglia <jtraglia@pm.me>
2022-06-02 13:51:48 -0500

    Add roundTrip tests for operations structs

---
+++ ethereum/spec/src/property-test/java/tech/pegasys/teku/spec/datastructures/operations/SignedContributionAndProofPropertyTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2022 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.spec.datastructures.operations;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import net.jqwik.api.Arbitraries;
+import net.jqwik.api.Arbitrary;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.Provide;
+import net.jqwik.api.constraints.Size;
+import org.apache.tuweni.bytes.Bytes32;
+import tech.pegasys.teku.infrastructure.json.JsonUtil;
+import tech.pegasys.teku.infrastructure.json.types.DeserializableTypeDefinition;
+import tech.pegasys.teku.infrastructure.unsigned.UInt64;
+import tech.pegasys.teku.spec.Spec;
+import tech.pegasys.teku.spec.SpecMilestone;
+import tech.pegasys.teku.spec.TestSpecFactory;
+import tech.pegasys.teku.spec.datastructures.operations.versions.altair.SignedContributionAndProof;
+import tech.pegasys.teku.spec.networks.Eth2Network;
+import tech.pegasys.teku.spec.util.DataStructureUtil;
+
+public class SignedContributionAndProofPropertyTest {
+  @Property
+  void roundTrip(
+      @ForAll final int seed,
+      @ForAll("milestone") final SpecMilestone specMilestone,
+      @ForAll final Eth2Network network,
+      @ForAll final long slot,
+      @ForAll @Size(32) final byte[] beaconBlockRoot)
+      throws JsonProcessingException {
+    final Spec spec = TestSpecFactory.create(specMilestone, network);
+    final DataStructureUtil dataStructureUtil = new DataStructureUtil(seed, spec);
+    final SignedContributionAndProof signedContributionAndProof =
+        dataStructureUtil.randomSignedContributionAndProof(
+            UInt64.fromLongBits(slot), Bytes32.wrap(beaconBlockRoot));
+    final DeserializableTypeDefinition<SignedContributionAndProof> typeDefinition =
+        signedContributionAndProof.getSchema().getJsonTypeDefinition();
+    final String json = JsonUtil.serialize(signedContributionAndProof, typeDefinition);
+    final SignedContributionAndProof result = JsonUtil.parse(json, typeDefinition);
+    assertThat(result).isEqualTo(signedContributionAndProof);
+  }
+
+  @Provide
+  Arbitrary<SpecMilestone> milestone() {
+    return Arbitraries.of(SpecMilestone.class)
+        .filter(m -> m.isGreaterThanOrEqualTo(SpecMilestone.ALTAIR));
+  }
+}
