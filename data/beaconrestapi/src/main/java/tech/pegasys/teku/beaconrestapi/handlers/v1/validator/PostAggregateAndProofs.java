382ad318da01c4bcceb7c8e850ff42d2f8735f65
Courtney <45641759+courtneyeh@users.noreply.github.com>
2022-06-03 13:27:23 +1200

    Migrate PostAggregateAndProofs to MigratingEndpointAdapter (#5673)

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/validator/PostAggregateAndProofs.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/validator/PostAggregateAndProofs.java
@@ -13,44 +13,80 @@
 
 package tech.pegasys.teku.beaconrestapi.handlers.v1.validator;
 
-import static java.util.Arrays.asList;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_BAD_REQUEST;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_OK;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.RES_BAD_REQUEST;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.RES_INTERNAL_ERROR;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.RES_OK;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.TAG_VALIDATOR;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.TAG_VALIDATOR_REQUIRED;
+import static tech.pegasys.teku.infrastructure.json.types.CoreTypes.INTEGER_TYPE;
+import static tech.pegasys.teku.infrastructure.json.types.CoreTypes.STRING_TYPE;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.javalin.http.Context;
-import io.javalin.http.Handler;
 import io.javalin.plugin.openapi.annotations.HttpMethod;
 import io.javalin.plugin.openapi.annotations.OpenApi;
 import io.javalin.plugin.openapi.annotations.OpenApiContent;
 import io.javalin.plugin.openapi.annotations.OpenApiRequestBody;
 import io.javalin.plugin.openapi.annotations.OpenApiResponse;
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.jetbrains.annotations.NotNull;
 import tech.pegasys.teku.api.DataProvider;
 import tech.pegasys.teku.api.ValidatorDataProvider;
-import tech.pegasys.teku.api.response.v1.beacon.PostDataFailureResponse;
-import tech.pegasys.teku.api.schema.SignedAggregateAndProof;
-import tech.pegasys.teku.beaconrestapi.handlers.AbstractHandler;
-import tech.pegasys.teku.beaconrestapi.schema.BadRequest;
+import tech.pegasys.teku.beaconrestapi.MigratingEndpointAdapter;
+import tech.pegasys.teku.beaconrestapi.schema.ErrorListBadRequest;
 import tech.pegasys.teku.infrastructure.async.SafeFuture;
-import tech.pegasys.teku.provider.JsonProvider;
+import tech.pegasys.teku.infrastructure.json.types.DeserializableTypeDefinition;
+import tech.pegasys.teku.infrastructure.json.types.SerializableTypeDefinition;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.AsyncApiResponse;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.EndpointMetadata;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiRequest;
+import tech.pegasys.teku.spec.datastructures.operations.SignedAggregateAndProof;
+import tech.pegasys.teku.spec.schemas.SchemaDefinitions;
+import tech.pegasys.teku.validator.api.SubmitDataError;
 
-public class PostAggregateAndProofs extends AbstractHandler implements Handler {
-
+public class PostAggregateAndProofs extends MigratingEndpointAdapter {
   public static final String ROUTE = "/eth/v1/validator/aggregate_and_proofs";
-
   private final ValidatorDataProvider provider;
 
-  public PostAggregateAndProofs(final DataProvider provider, final JsonProvider jsonProvider) {
-    this(provider.getValidatorDataProvider(), jsonProvider);
+  static final String PARTIAL_PUBLISH_FAILURE_MESSAGE =
+      "Some items failed to publish, refer to errors for details";
+
+  private static final SerializableTypeDefinition<ErrorListBadRequest> BAD_REQUEST_RESPONSE =
+      SerializableTypeDefinition.object(ErrorListBadRequest.class)
+          .withField("code", INTEGER_TYPE, ErrorListBadRequest::getCode)
+          .withField("message", STRING_TYPE, ErrorListBadRequest::getMessage)
+          .withField(
+              "stacktraces",
+              SerializableTypeDefinition.listOf(STRING_TYPE),
+              data ->
+                  data.getErrors().stream()
+                      .map(SubmitDataError::getMessage)
+                      .collect(Collectors.toList()))
+          .build();
+
+  public PostAggregateAndProofs(
+      final DataProvider provider, final SchemaDefinitions schemaDefinitions) {
+    this(provider.getValidatorDataProvider(), schemaDefinitions);
   }
 
   public PostAggregateAndProofs(
-      final ValidatorDataProvider provider, final JsonProvider jsonProvider) {
-    super(jsonProvider);
+      final ValidatorDataProvider provider, final SchemaDefinitions schemaDefinitions) {
+    super(
+        EndpointMetadata.post(ROUTE)
+            .operationId("postAggregateAndProofs")
+            .summary("Publish aggregate and proofs")
+            .description(
+                "Verifies given aggregate and proofs and publishes it on appropriate gossipsub topic.")
+            .tags(TAG_VALIDATOR, TAG_VALIDATOR_REQUIRED)
+            .requestBodyType(
+                DeserializableTypeDefinition.listOf(
+                    schemaDefinitions.getSignedAggregateAndProofSchema().getJsonTypeDefinition()))
+            .response(SC_OK, "Successfully published aggregate.")
+            .response(SC_BAD_REQUEST, "Invalid request syntax", BAD_REQUEST_RESPONSE)
+            .build());
     this.provider = provider;
   }
 
@@ -61,7 +97,11 @@ public class PostAggregateAndProofs extends AbstractHandler implements Handler {
       tags = {TAG_VALIDATOR, TAG_VALIDATOR_REQUIRED},
       requestBody =
           @OpenApiRequestBody(
-              content = {@OpenApiContent(from = SignedAggregateAndProof.class, isArray = true)}),
+              content = {
+                @OpenApiContent(
+                    from = tech.pegasys.teku.api.schema.SignedAggregateAndProof.class,
+                    isArray = true)
+              }),
       description =
           "Verifies given aggregate and proofs and publishes it on appropriate gossipsub topic.",
       responses = {
@@ -70,18 +110,26 @@ public class PostAggregateAndProofs extends AbstractHandler implements Handler {
         @OpenApiResponse(status = RES_INTERNAL_ERROR, description = "Beacon node internal error.")
       })
   @Override
-  public void handle(Context ctx) throws Exception {
-    try {
-      final SignedAggregateAndProof[] signedAggregateAndProofs =
-          parseRequestBody(ctx.body(), SignedAggregateAndProof[].class);
+  public void handle(@NotNull Context ctx) throws Exception {
+    adapt(ctx);
+  }
 
-      final SafeFuture<Optional<PostDataFailureResponse>> future =
-          provider.sendAggregateAndProofs(asList(signedAggregateAndProofs));
+  @Override
+  public void handleRequest(RestApiRequest request) throws JsonProcessingException {
+    final List<SignedAggregateAndProof> signedAggregateAndProofs = request.getRequestBody();
+    final SafeFuture<List<SubmitDataError>> future =
+        provider.sendAggregateAndProofs(signedAggregateAndProofs);
 
-      handlePostDataResult(ctx, future);
-    } catch (IllegalArgumentException e) {
-      ctx.json(BadRequest.badRequest(jsonProvider, e.getMessage()));
-      ctx.status(SC_BAD_REQUEST);
-    }
+    request.respondAsync(
+        future.thenApply(
+            errors -> {
+              if (errors.isEmpty()) {
+                return AsyncApiResponse.respondWithCode(SC_OK);
+              }
+
+              final ErrorListBadRequest data =
+                  ErrorListBadRequest.convert(PARTIAL_PUBLISH_FAILURE_MESSAGE, errors);
+              return AsyncApiResponse.respondWithObject(SC_BAD_REQUEST, data);
+            }));
   }
 }
