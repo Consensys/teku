f5c97fbdaebfd11477029be23b727246e90d781f
Paul Harris <paul.harris@consensys.net>
2022-06-13 07:26:54 +1000

    Fix 503 response with no body defined
    When getting duties, if the node is syncing a 503 gets returned.
    
    After migration, because no body was specified, we ended up generating a 500.
    
    fixes #5761
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/validator/GetProposerDuties.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/validator/GetProposerDuties.java
@@ -141,7 +141,7 @@ public class GetProposerDuties extends MigratingEndpointAdapter {
   @Override
   public void handleRequest(RestApiRequest request) throws JsonProcessingException {
     if (!validatorDataProvider.isStoreAvailable() || syncDataProvider.isSyncing()) {
-      request.respondWithCode(SC_SERVICE_UNAVAILABLE);
+      request.respondError(SC_SERVICE_UNAVAILABLE, SERVICE_UNAVAILABLE);
       return;
     }
 
