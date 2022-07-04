f4a5bd83016241dda44d8b043152c34e8e2d9cc8
Stefan Bratanov <stefan.bratanov93@gmail.com>
2022-07-04 17:00:14 +0100

    fix spotless

--- validator/client/src/test/java/tech/pegasys/teku/validator/client/ValidatorRegistratorTest.java
+++ validator/client/src/test/java/tech/pegasys/teku/validator/client/ValidatorRegistratorTest.java
@@ -93,8 +93,7 @@ class ValidatorRegistratorTest {
             validatorConfig,
             feeRecipientProvider,
             validatorRegistrationBatchSender);
-    when(validatorRegistrationBatchSender.sendInBatches(any()))
-        .thenReturn(SafeFuture.COMPLETE);
+    when(validatorRegistrationBatchSender.sendInBatches(any())).thenReturn(SafeFuture.COMPLETE);
 
     when(proposerConfigProvider.getProposerConfig())
         .thenReturn(SafeFuture.completedFuture(Optional.of(proposerConfig)));
