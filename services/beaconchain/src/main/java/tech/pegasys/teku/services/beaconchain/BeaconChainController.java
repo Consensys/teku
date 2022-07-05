5ba64482e3ff1f5717b1eae784e84a811c9745e5
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2022-07-05 17:58:38 +0400

    Extract SyncServiceFactory interface

--- services/beaconchain/src/main/java/tech/pegasys/teku/services/beaconchain/BeaconChainController.java
+++ services/beaconchain/src/main/java/tech/pegasys/teku/services/beaconchain/BeaconChainController.java
@@ -38,6 +38,7 @@ import org.apache.tuweni.bytes.Bytes;
 import org.hyperledger.besu.plugin.services.MetricsSystem;
 import tech.pegasys.teku.api.ChainDataProvider;
 import tech.pegasys.teku.api.DataProvider;
+import tech.pegasys.teku.beacon.sync.DefaultSyncServiceFactory;
 import tech.pegasys.teku.beacon.sync.SyncService;
 import tech.pegasys.teku.beacon.sync.SyncServiceFactory;
 import tech.pegasys.teku.beacon.sync.events.CoalescingChainHeadChannel;
@@ -919,26 +920,29 @@ public class BeaconChainController extends Service implements BeaconChainControl
         .subscribe(BlockImportNotifications.class, blockManager);
   }
 
+  protected SyncServiceFactory getSyncServiceFactory() {
+    return new DefaultSyncServiceFactory(
+        beaconConfig.syncConfig(),
+        beaconConfig.eth2NetworkConfig().getGenesisState(),
+        metricsSystem,
+        asyncRunnerFactory,
+        beaconAsyncRunner,
+        timeProvider,
+        recentChainData,
+        combinedChainDataClient,
+        eventChannels.getPublisher(StorageUpdateChannel.class, beaconAsyncRunner),
+        p2pNetwork,
+        blockImporter,
+        pendingBlocks,
+        beaconConfig.eth2NetworkConfig().getStartupTargetPeerCount(),
+        signatureVerificationService,
+        Duration.ofSeconds(beaconConfig.eth2NetworkConfig().getStartupTimeoutSeconds()),
+        spec);
+  }
+
   public void initSyncService() {
     LOG.debug("BeaconChainController.initSyncService()");
-    syncService =
-        SyncServiceFactory.createSyncService(
-            beaconConfig.syncConfig(),
-            beaconConfig.eth2NetworkConfig().getGenesisState(),
-            metricsSystem,
-            asyncRunnerFactory,
-            beaconAsyncRunner,
-            timeProvider,
-            recentChainData,
-            combinedChainDataClient,
-            eventChannels.getPublisher(StorageUpdateChannel.class, beaconAsyncRunner),
-            p2pNetwork,
-            blockImporter,
-            pendingBlocks,
-            beaconConfig.eth2NetworkConfig().getStartupTargetPeerCount(),
-            signatureVerificationService,
-            Duration.ofSeconds(beaconConfig.eth2NetworkConfig().getStartupTimeoutSeconds()),
-            spec);
+    syncService = getSyncServiceFactory().create();
 
     // chainHeadChannel subscription
     syncService.getForwardSync().subscribeToSyncChanges(coalescingChainHeadChannel);
