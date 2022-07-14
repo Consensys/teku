4000f466d1e5873a38bc038446fe252c478cbf7a
Enrico Del Fante <enrico.delfante@consensys.net>
2022-07-14 15:35:58 +0200

    ExecutionLayerManagerImpl Message Unwrapping throw RuntimeException

--- ethereum/executionlayer/src/main/java/tech/pegasys/teku/ethereum/executionlayer/ExecutionLayerManagerImpl.java
+++ ethereum/executionlayer/src/main/java/tech/pegasys/teku/ethereum/executionlayer/ExecutionLayerManagerImpl.java
@@ -491,8 +491,11 @@ public class ExecutionLayerManagerImpl implements ExecutionLayerManager {
   }
 
   private static <K> K unwrapResponseOrThrow(final Response<K> response) {
-    checkArgument(response.isSuccess(), "Invalid remote response: %s", response.getErrorMessage());
-    return response.getPayload();
+    if (response.isSuccess()) {
+      return response.getPayload();
+    }
+    throw new RuntimeException(
+        String.format("Invalid remote response: %s", response.getErrorMessage()));
   }
 
   private void updateBuilderAvailability() {
