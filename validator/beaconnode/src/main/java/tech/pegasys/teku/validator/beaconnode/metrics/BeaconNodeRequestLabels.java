f0329d7e9912502388f7c664593702e34811bfd1
Stefan Bratanov <stefan.bratanov93@gmail.com>
2022-07-27 18:00:53 +0100

    Add metrics for failover BN requests

--- validator/beaconnode/src/main/java/tech/pegasys/teku/validator/beaconnode/metrics/BeaconNodeRequestLabels.java
+++ validator/beaconnode/src/main/java/tech/pegasys/teku/validator/beaconnode/metrics/BeaconNodeRequestLabels.java
@@ -15,31 +15,27 @@ package tech.pegasys.teku.validator.beaconnode.metrics;
 
 public class BeaconNodeRequestLabels {
 
-  public static final String GET_GENESIS_REQUEST_METHOD = "get_genesis";
-  public static final String GET_VALIDATOR_INDICES_REQUEST_METHOD = "get_validator_indices";
-  public static final String GET_VALIDATOR_STATUSES_REQUEST_METHOD = "get_validator_statuses";
-  public static final String GET_ATTESTATION_DUTIES_REQUEST_METHOD = "get_attestation_duties";
+  public static final String GET_GENESIS_METHOD = "get_genesis";
+  public static final String GET_VALIDATOR_INDICES_METHOD = "get_validator_indices";
+  public static final String GET_VALIDATOR_STATUSES_METHOD = "get_validator_statuses";
+  public static final String GET_ATTESTATION_DUTIES_METHOD = "get_attestation_duties";
   public static final String GET_PROPOSER_DUTIES_REQUESTS_METHOD = "get_proposer_duties";
-  public static final String GET_SYNC_COMMITTEE_DUTIES_REQUEST_METHOD = "get_sync_committee_duties";
-  public static final String CREATE_UNSIGNED_BLOCK_REQUEST_METHOD = "create_unsigned_block";
-  public static final String CREATE_ATTESTATION_REQUEST_METHOD = "create_attestation";
-  public static final String CREATE_AGGREGATE_REQUEST_METHOD = "create_aggregate";
-  public static final String CREATE_SYNC_COMMITTEE_CONTRIBUTION_REQUEST_METHOD =
+  public static final String GET_SYNC_COMMITTEE_DUTIES_METHOD = "get_sync_committee_duties";
+  public static final String CREATE_UNSIGNED_BLOCK_METHOD = "create_unsigned_block";
+  public static final String CREATE_ATTESTATION_METHOD = "create_attestation";
+  public static final String CREATE_AGGREGATE_METHOD = "create_aggregate";
+  public static final String CREATE_SYNC_COMMITTEE_CONTRIBUTION_METHOD =
       "create_sync_committee_contribution";
-  public static final String BEACON_COMMITTEE_SUBSCRIPTION_REQUEST_METHOD =
-      "beacon_committee_subscription";
-  public static final String SYNC_COMMITTEE_SUBNET_SUBSCRIPTION_REQUEST_METHOD =
+  public static final String BEACON_COMMITTEE_SUBSCRIPTION_METHOD = "beacon_committee_subscription";
+  public static final String SYNC_COMMITTEE_SUBNET_SUBSCRIPTION_METHOD =
       "sync_committee_subnet_subscription";
-  public static final String PERSISTENT_SUBNETS_SUBSCRIPTION_REQUEST_METHOD =
+  public static final String PERSISTENT_SUBNETS_SUBSCRIPTION_METHOD =
       "persistent_subnets_subscription";
-  public static final String PUBLISH_ATTESTATION_REQUEST_METHOD = "publish_attestation";
-  public static final String PUBLISH_AGGREGATE_AND_PROOFS_REQUEST_METHOD =
-      "publish_aggregate_and_proofs";
-  public static final String PUBLISH_BLOCK_REQUEST_METHOD = "publish_block";
-  public static final String SEND_SYNC_COMMITTEE_MESSAGES_REQUEST_METHOD =
-      "send_sync_committee_messages";
-  public static final String SEND_CONTRIBUTIONS_AND_PROOFS_REQUEST_METHOD =
-      "send_contributions_and_proofs";
-  public static final String PREPARE_BEACON_PROPOSERS_REQUEST_METHOD = "prepare_beacon_proposers";
-  public static final String REGISTER_VALIDATORS_REQUEST_METHOD = "register_validators";
+  public static final String PUBLISH_ATTESTATION_METHOD = "publish_attestation";
+  public static final String PUBLISH_AGGREGATE_AND_PROOFS_METHOD = "publish_aggregate_and_proofs";
+  public static final String PUBLISH_BLOCK_METHOD = "publish_block";
+  public static final String SEND_SYNC_COMMITTEE_MESSAGES_METHOD = "send_sync_committee_messages";
+  public static final String SEND_CONTRIBUTIONS_AND_PROOFS_METHOD = "send_contributions_and_proofs";
+  public static final String PREPARE_BEACON_PROPOSERS_METHOD = "prepare_beacon_proposers";
+  public static final String REGISTER_VALIDATORS_METHOD = "register_validators";
 }
--- validator/remote/src/test/java/tech/pegasys/teku/validator/remote/FailoverValidatorApiHandlerTest.java
+++ validator/remote/src/test/java/tech/pegasys/teku/validator/remote/FailoverValidatorApiHandlerTest.java
@@ -46,6 +46,8 @@ import tech.pegasys.teku.bls.BLSSignature;
 import tech.pegasys.teku.infrastructure.async.SafeFuture;
 import tech.pegasys.teku.infrastructure.async.SafeFutureAssert;
 import tech.pegasys.teku.infrastructure.logging.ValidatorLogger;
+import tech.pegasys.teku.infrastructure.metrics.StubMetricsSystem;
+import tech.pegasys.teku.infrastructure.metrics.TekuMetricCategory;
 import tech.pegasys.teku.infrastructure.ssz.SszList;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.spec.Spec;
@@ -70,12 +72,16 @@ import tech.pegasys.teku.validator.api.SubmitDataError;
 import tech.pegasys.teku.validator.api.SyncCommitteeDuties;
 import tech.pegasys.teku.validator.api.SyncCommitteeSubnetSubscription;
 import tech.pegasys.teku.validator.api.ValidatorApiChannel;
+import tech.pegasys.teku.validator.beaconnode.metrics.BeaconNodeRequestLabels;
+import tech.pegasys.teku.validator.remote.FailoverValidatorApiHandler.RequestOutcome;
 import tech.pegasys.teku.validator.remote.FailoverValidatorApiHandler.ValidatorApiChannelRequest;
 
 class FailoverValidatorApiHandlerTest {
 
   private static final Spec SPEC = TestSpecFactory.createMinimalAltair();
   private static final DataStructureUtil DATA_STRUCTURE_UTIL = new DataStructureUtil(SPEC);
+
+  private final StubMetricsSystem stubMetricsSystem = new StubMetricsSystem();
 
   private RemoteValidatorApiChannel primaryApiChannel;
   private RemoteValidatorApiChannel failoverApiChannel1;
@@ -105,6 +111,7 @@ class FailoverValidatorApiHandlerTest {
             primaryApiChannel,
             List.of(failoverApiChannel1, failoverApiChannel2),
             true,
+            stubMetricsSystem,
             validatorLogger);
   }
 
@@ -115,13 +122,13 @@ class FailoverValidatorApiHandlerTest {
         () ->
             failoverApiHandler =
                 new FailoverValidatorApiHandler(
-                    primaryApiChannel, List.of(), true, validatorLogger));
+                    primaryApiChannel, List.of(), true, stubMetricsSystem, validatorLogger));
   }
 
   @ParameterizedTest(name = "{0}")
   @MethodSource("getRequestsUsingFailover")
   <T> void requestSucceedsWithoutFailover(
-      final ValidatorApiChannelRequest<T> request, final T response) {
+      final ValidatorApiChannelRequest<T> request, final String methodLabel, final T response) {
 
     setupSuccesses(request, response, primaryApiChannel);
 
@@ -130,12 +137,21 @@ class FailoverValidatorApiHandlerTest {
     assertThat(result).isCompletedWithValue(response);
 
     verifyNoInteractions(failoverApiChannel1, failoverApiChannel2, validatorLogger);
+
+    verifyFailoverCounters(
+        failoverApiChannel1,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 0L, RequestOutcome.ERROR, 0L));
+    verifyFailoverCounters(
+        failoverApiChannel2,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 0L, RequestOutcome.ERROR, 0L));
   }
 
   @ParameterizedTest(name = "{0}")
   @MethodSource("getRequestsUsingFailover")
   <T> void requestFailoversOnFailure(
-      final ValidatorApiChannelRequest<T> request, final T response) {
+      final ValidatorApiChannelRequest<T> request, final String methodLabel, final T response) {
 
     setupSuccesses(request, response, failoverApiChannel2);
     setupFailures(request, primaryApiChannel, failoverApiChannel1);
@@ -145,11 +161,21 @@ class FailoverValidatorApiHandlerTest {
     assertThat(result).isCompletedWithValue(response);
 
     verifyNoInteractions(validatorLogger);
+
+    verifyFailoverCounters(
+        failoverApiChannel1,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 0L, RequestOutcome.ERROR, 1L));
+    verifyFailoverCounters(
+        failoverApiChannel2,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 1L, RequestOutcome.ERROR, 0L));
   }
 
   @ParameterizedTest(name = "{0}")
   @MethodSource("getRequestsUsingFailover")
-  <T> void requestFailsOnAllFailovers(final ValidatorApiChannelRequest<T> request) {
+  <T> void requestFailsOnAllFailovers(
+      final ValidatorApiChannelRequest<T> request, final String methodLabel) {
 
     setupFailures(request, primaryApiChannel, failoverApiChannel1, failoverApiChannel2);
 
@@ -165,6 +191,15 @@ class FailoverValidatorApiHandlerTest {
             });
 
     verify(validatorLogger).remoteBeaconNodeRequestFailedOnAllConfiguredEndpoints();
+
+    verifyFailoverCounters(
+        failoverApiChannel1,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 0L, RequestOutcome.ERROR, 1L));
+    verifyFailoverCounters(
+        failoverApiChannel2,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 0L, RequestOutcome.ERROR, 1L));
   }
 
   @ParameterizedTest(name = "{0}")
@@ -172,6 +207,7 @@ class FailoverValidatorApiHandlerTest {
   <T> void requestIsNotRelayedToFailoversIfFailoversSendSubnetSubscriptionsIsDisabled(
       final ValidatorApiChannelRequest<T> request,
       final Consumer<ValidatorApiChannel> verifyCallIsMade,
+      final String methodLabel,
       final T response) {
 
     failoverApiHandler =
@@ -179,6 +215,7 @@ class FailoverValidatorApiHandlerTest {
             primaryApiChannel,
             List.of(failoverApiChannel1, failoverApiChannel2),
             false,
+            stubMetricsSystem,
             validatorLogger);
 
     setupSuccesses(request, response, primaryApiChannel);
@@ -189,6 +226,15 @@ class FailoverValidatorApiHandlerTest {
     verifyCallIsMade.accept(primaryApiChannel);
 
     verifyNoInteractions(validatorLogger, failoverApiChannel1, failoverApiChannel2);
+
+    verifyFailoverCounters(
+        failoverApiChannel1,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 0L, RequestOutcome.ERROR, 0L));
+    verifyFailoverCounters(
+        failoverApiChannel2,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 0L, RequestOutcome.ERROR, 0L));
   }
 
   @ParameterizedTest(name = "{0}")
@@ -196,6 +242,7 @@ class FailoverValidatorApiHandlerTest {
   <T> void requestIsRelayedToAllNodes(
       final ValidatorApiChannelRequest<T> request,
       final Consumer<ValidatorApiChannel> verifyCallIsMade,
+      final String methodLabel,
       final T response) {
 
     setupSuccesses(request, response, primaryApiChannel, failoverApiChannel1, failoverApiChannel2);
@@ -209,6 +256,15 @@ class FailoverValidatorApiHandlerTest {
     verifyCallIsMade.accept(failoverApiChannel2);
 
     verifyNoInteractions(validatorLogger);
+
+    verifyFailoverCounters(
+        failoverApiChannel1,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 1L, RequestOutcome.ERROR, 0L));
+    verifyFailoverCounters(
+        failoverApiChannel2,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 1L, RequestOutcome.ERROR, 0L));
   }
 
   @ParameterizedTest(name = "{0}")
@@ -216,6 +272,7 @@ class FailoverValidatorApiHandlerTest {
   <T> void requestIsRelayedToAllNodesButFailsOnOneFailover(
       final ValidatorApiChannelRequest<T> request,
       final Consumer<ValidatorApiChannel> verifyCallIsMade,
+      final String methodLabel,
       final T response) {
 
     setupSuccesses(request, response, primaryApiChannel, failoverApiChannel1);
@@ -230,6 +287,15 @@ class FailoverValidatorApiHandlerTest {
     verifyCallIsMade.accept(failoverApiChannel2);
 
     verifyNoInteractions(validatorLogger);
+
+    verifyFailoverCounters(
+        failoverApiChannel1,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 1L, RequestOutcome.ERROR, 0L));
+    verifyFailoverCounters(
+        failoverApiChannel2,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 0L, RequestOutcome.ERROR, 1L));
   }
 
   @ParameterizedTest(name = "{0}")
@@ -237,6 +303,7 @@ class FailoverValidatorApiHandlerTest {
   <T> void requestFailsOnPrimaryNodeButRelayedToFailoverNodes(
       final ValidatorApiChannelRequest<T> request,
       final Consumer<ValidatorApiChannel> verifyCallIsMade,
+      final String methodLabel,
       final T response) {
 
     setupFailures(request, primaryApiChannel);
@@ -251,13 +318,23 @@ class FailoverValidatorApiHandlerTest {
     verifyCallIsMade.accept(failoverApiChannel2);
 
     verifyNoInteractions(validatorLogger);
+
+    verifyFailoverCounters(
+        failoverApiChannel1,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 1L, RequestOutcome.ERROR, 0L));
+    verifyFailoverCounters(
+        failoverApiChannel2,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 1L, RequestOutcome.ERROR, 0L));
   }
 
   @ParameterizedTest(name = "{0}")
   @MethodSource("getRelayRequests")
-  <T> void requestFailsOnPrimaryNodeAndOneFailoverDoesNotResponse(
+  <T> void requestFailsOnPrimaryNodeAndOneFailoverDoesNotRespond(
       final ValidatorApiChannelRequest<T> request,
       final Consumer<ValidatorApiChannel> verifyCallIsMade,
+      final String methodLabel,
       final T response) {
 
     setupFailures(request, primaryApiChannel);
@@ -274,13 +351,23 @@ class FailoverValidatorApiHandlerTest {
     verifyCallIsMade.accept(failoverApiChannel2);
 
     verifyNoInteractions(validatorLogger);
+
+    verifyFailoverCounters(
+        failoverApiChannel1,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 0L, RequestOutcome.ERROR, 0L));
+    verifyFailoverCounters(
+        failoverApiChannel2,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 1L, RequestOutcome.ERROR, 0L));
   }
 
   @ParameterizedTest(name = "{0}")
   @MethodSource("getRelayRequests")
   <T> void requestFailsOnPrimaryNodeAndAllFailoverNodes(
       final ValidatorApiChannelRequest<T> request,
-      @NotNull final Consumer<ValidatorApiChannel> verifyCallIsMade) {
+      @NotNull final Consumer<ValidatorApiChannel> verifyCallIsMade,
+      final String methodLabel) {
 
     setupFailures(request, primaryApiChannel, failoverApiChannel1, failoverApiChannel2);
 
@@ -301,6 +388,15 @@ class FailoverValidatorApiHandlerTest {
     verifyCallIsMade.accept(failoverApiChannel2);
 
     verify(validatorLogger).remoteBeaconNodeRequestFailedOnAllConfiguredEndpoints();
+
+    verifyFailoverCounters(
+        failoverApiChannel1,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 0L, RequestOutcome.ERROR, 1L));
+    verifyFailoverCounters(
+        failoverApiChannel2,
+        methodLabel,
+        Map.of(RequestOutcome.SUCCESS, 0L, RequestOutcome.ERROR, 1L));
   }
 
   private <T> void setupSuccesses(
@@ -338,43 +434,55 @@ class FailoverValidatorApiHandlerTest {
 
     return Stream.of(
         getArguments(
-            "getGenesisData", ValidatorApiChannel::getGenesisData, Optional.of(genesisData)),
+            "getGenesisData",
+            ValidatorApiChannel::getGenesisData,
+            BeaconNodeRequestLabels.GET_GENESIS_METHOD,
+            Optional.of(genesisData)),
         getArguments(
             "getValidatorIndices",
             apiChannel -> apiChannel.getValidatorIndices(List.of(publicKey)),
+            BeaconNodeRequestLabels.GET_VALIDATOR_INDICES_METHOD,
             Map.of(publicKey, 0)),
         getArguments(
             "getValidatorStatuses",
             apiChannel -> apiChannel.getValidatorStatuses(List.of(publicKey)),
+            BeaconNodeRequestLabels.GET_VALIDATOR_STATUSES_METHOD,
             Optional.of(Map.of(publicKey, ValidatorStatus.active_ongoing))),
         getArguments(
             "getAttestationDuties",
             apiChannel -> apiChannel.getAttestationDuties(epoch, validatorIndices),
+            BeaconNodeRequestLabels.GET_ATTESTATION_DUTIES_METHOD,
             Optional.of(mock(AttesterDuties.class))),
         getArguments(
             "getSyncCommitteeDuties",
             apiChannel -> apiChannel.getSyncCommitteeDuties(epoch, validatorIndices),
+            BeaconNodeRequestLabels.GET_SYNC_COMMITTEE_DUTIES_METHOD,
             Optional.of(mock(SyncCommitteeDuties.class))),
         getArguments(
             "getProposerDuties",
             apiChannel -> apiChannel.getProposerDuties(epoch),
+            BeaconNodeRequestLabels.GET_PROPOSER_DUTIES_REQUESTS_METHOD,
             Optional.of(mock(ProposerDuties.class))),
         getArguments(
             "createUnsignedBlock",
             apiChannel ->
                 apiChannel.createUnsignedBlock(slot, randaoReveal, Optional.empty(), true),
+            BeaconNodeRequestLabels.CREATE_UNSIGNED_BLOCK_METHOD,
             Optional.of(mock(BeaconBlock.class))),
         getArguments(
             "createAttestationData",
             apiChannel -> apiChannel.createAttestationData(slot, 0),
+            BeaconNodeRequestLabels.CREATE_ATTESTATION_METHOD,
             Optional.of(mock(AttestationData.class))),
         getArguments(
             "createAggregate",
             apiChannel -> apiChannel.createAggregate(slot, randomBytes32),
+            BeaconNodeRequestLabels.CREATE_AGGREGATE_METHOD,
             Optional.of(attestation)),
         getArguments(
             "createSyncCommitteeContribution",
             apiChannel -> apiChannel.createSyncCommitteeContribution(slot, 0, randomBytes32),
+            BeaconNodeRequestLabels.CREATE_SYNC_COMMITTEE_CONTRIBUTION_METHOD,
             Optional.of(mock(SyncCommitteeContribution.class))));
   }
 
@@ -400,23 +508,27 @@ class FailoverValidatorApiHandlerTest {
                 "sendSignedAttestations",
                 apiChannel -> apiChannel.sendSignedAttestations(List.of(attestation)),
                 apiChannel -> verify(apiChannel).sendSignedAttestations(List.of(attestation)),
+                BeaconNodeRequestLabels.PUBLISH_ATTESTATION_METHOD,
                 List.of(submitDataError)),
             getArguments(
                 "sendAggregateAndProofs",
                 apiChannel -> apiChannel.sendAggregateAndProofs(List.of(signedAggregateAndProof)),
                 apiChannel ->
                     verify(apiChannel).sendAggregateAndProofs(List.of(signedAggregateAndProof)),
+                BeaconNodeRequestLabels.PUBLISH_AGGREGATE_AND_PROOFS_METHOD,
                 List.of(submitDataError)),
             getArguments(
                 "sendSignedBlock",
                 apiChannel -> apiChannel.sendSignedBlock(signedBeaconBlock),
                 apiChannel -> verify(apiChannel).sendSignedBlock(signedBeaconBlock),
+                BeaconNodeRequestLabels.PUBLISH_BLOCK_METHOD,
                 mock(SendSignedBlockResult.class)),
             getArguments(
                 "sendSyncCommitteeMessages",
                 apiChannel -> apiChannel.sendSyncCommitteeMessages(List.of(syncCommitteeMessage)),
                 apiChannel ->
                     verify(apiChannel).sendSyncCommitteeMessages(List.of(syncCommitteeMessage)),
+                BeaconNodeRequestLabels.SEND_SYNC_COMMITTEE_MESSAGES_METHOD,
                 List.of(submitDataError)),
             getArguments(
                 "sendSignedContributionAndProofs",
@@ -425,11 +537,13 @@ class FailoverValidatorApiHandlerTest {
                 apiChannel ->
                     verify(apiChannel)
                         .sendSignedContributionAndProofs(List.of(signedContributionAndProof)),
+                BeaconNodeRequestLabels.SEND_CONTRIBUTIONS_AND_PROOFS_METHOD,
                 null),
             getArguments(
                 "registerValidators",
                 apiChannel -> apiChannel.registerValidators(validatorRegistrations),
                 apiChannel -> verify(apiChannel).registerValidators(validatorRegistrations),
+                BeaconNodeRequestLabels.REGISTER_VALIDATORS_METHOD,
                 null)));
   }
 
@@ -448,6 +562,7 @@ class FailoverValidatorApiHandlerTest {
             apiChannel ->
                 verify(apiChannel)
                     .subscribeToBeaconCommittee(List.of(committeeSubscriptionRequest)),
+            BeaconNodeRequestLabels.BEACON_COMMITTEE_SUBSCRIPTION_METHOD,
             null),
         getArguments(
             "subscribeToSyncCommitteeSubnets",
@@ -457,25 +572,52 @@ class FailoverValidatorApiHandlerTest {
             apiChannel ->
                 verify(apiChannel)
                     .subscribeToSyncCommitteeSubnets(List.of(syncCommitteeSubnetSubscription)),
+            BeaconNodeRequestLabels.SYNC_COMMITTEE_SUBNET_SUBSCRIPTION_METHOD,
             null),
         getArguments(
             "subscribeToPersistentSubnets",
             apiChannel -> apiChannel.subscribeToPersistentSubnets(Set.of(subnetSubscription)),
             apiChannel ->
                 verify(apiChannel).subscribeToPersistentSubnets(Set.of(subnetSubscription)),
+            BeaconNodeRequestLabels.PERSISTENT_SUBNETS_SUBSCRIPTION_METHOD,
             null));
   }
 
   private static <T> Arguments getArguments(
-      final String name, final ValidatorApiChannelRequest<T> request, final T response) {
-    return Arguments.of(Named.of(name, request), response);
+      final String name,
+      final ValidatorApiChannelRequest<T> request,
+      final String methodLabel,
+      final T response) {
+    return Arguments.of(Named.of(name, request), methodLabel, response);
   }
 
   private static <T> Arguments getArguments(
       final String name,
       final ValidatorApiChannelRequest<T> request,
       final Consumer<ValidatorApiChannel> verifyCallIsMade,
+      final String methodLabel,
       final T response) {
-    return Arguments.of(Named.of(name, request), verifyCallIsMade, response);
+    return Arguments.of(Named.of(name, request), verifyCallIsMade, methodLabel, response);
+  }
+
+  private void verifyFailoverCounters(
+      final RemoteValidatorApiChannel failoverApiChannel,
+      final String methodLabel,
+      final Map<RequestOutcome, Long> expectedCountByRequestOutcome) {
+    expectedCountByRequestOutcome.forEach(
+        (key, value) ->
+            assertThat(getFailoverCounterValue(failoverApiChannel, methodLabel, key))
+                .isEqualTo(value));
+  }
+
+  private long getFailoverCounterValue(
+      final RemoteValidatorApiChannel apiChannel,
+      final String methodLabel,
+      final RequestOutcome outcome) {
+    return stubMetricsSystem
+        .getCounter(
+            TekuMetricCategory.VALIDATOR,
+            FailoverValidatorApiHandler.FAILOVER_BEACON_NODES_REQUESTS_COUNTER_NAME)
+        .getValue(apiChannel.getEndpoint().toString(), methodLabel, outcome.toString());
   }
 }
