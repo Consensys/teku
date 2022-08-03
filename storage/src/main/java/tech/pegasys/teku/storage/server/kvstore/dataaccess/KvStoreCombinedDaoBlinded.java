7eb95d1737b4761b46e70b362a92be8fbe32fb07
Paul Harris <paul.harris@consensys.net>
2022-08-03 14:53:38 +1000

    tweak validate-block-storage for more information (#6005)
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- storage/src/main/java/tech/pegasys/teku/storage/server/kvstore/dataaccess/KvStoreCombinedDaoBlinded.java
+++ storage/src/main/java/tech/pegasys/teku/storage/server/kvstore/dataaccess/KvStoreCombinedDaoBlinded.java
@@ -63,6 +63,8 @@ public interface KvStoreCombinedDaoBlinded extends KvStoreCombinedDaoCommon {
   @MustBeClosed
   Stream<Map.Entry<Bytes32, UInt64>> streamUnblindedFinalizedBlockRoots();
 
+  Stream<SignedBeaconBlock> streamBlindedBlocks();
+
   interface CombinedUpdaterBlinded
       extends HotUpdaterBlinded, FinalizedUpdaterBlinded, CombinedUpdaterCommon {}
 
