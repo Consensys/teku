27d564bf7adc409dc5a9c8ff8b4ff857990a0217
Adrian Sutton <adrian.sutton@consensys.net>
2022-08-12 08:45:24 +1000

    Revert "Make timeouts on the engine API less strict."
    This reverts commit a8dcd20a2702bc6433266bfeae21b4b744c4b259.

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/config/Constants.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/config/Constants.java
@@ -43,7 +43,7 @@ public class Constants {
   public static final Duration ETH1_INDIVIDUAL_BLOCK_RETRY_TIMEOUT = Duration.ofMillis(500);
   public static final Duration ETH1_DEPOSIT_REQUEST_RETRY_TIMEOUT = Duration.ofSeconds(2);
   public static final Duration EL_ENGINE_BLOCK_EXECUTION_TIMEOUT = Duration.ofSeconds(8);
-  public static final Duration EL_ENGINE_NON_BLOCK_EXECUTION_TIMEOUT = Duration.ofSeconds(5);
+  public static final Duration EL_ENGINE_NON_BLOCK_EXECUTION_TIMEOUT = Duration.ofSeconds(1);
   public static final Duration ETH1_ENDPOINT_MONITOR_SERVICE_POLL_INTERVAL = Duration.ofSeconds(10);
   public static final Duration ETH1_VALID_ENDPOINT_CHECK_INTERVAL =
       Duration.ofSeconds(60); // usable
