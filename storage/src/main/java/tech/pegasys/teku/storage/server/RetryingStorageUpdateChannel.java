6c99ee6f376daadc1787c4b665cd06c23a7454f9
Adrian Sutton <adrian.sutton@consensys.net>
2022-09-13 14:06:26 +1000

    Support async storage updates.
    Adds a feature toggle (default off) to allow updating the in-memory Store without first storing the update to disk.
    
    Currently, newly finalized data may be temporarily unavailable as it may be removed from the in-memory store before it is persisted to disk. Bufferring will be added in a follow up PR.

---
+++ storage/src/main/java/tech/pegasys/teku/storage/server/RetryingStorageUpdateChannel.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright ConsenSys Software Inc., 2022
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.storage.server;
+
+import java.time.Duration;
+import java.util.Collection;
+import java.util.function.Function;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.tuweni.bytes.Bytes32;
+import tech.pegasys.teku.infrastructure.async.SafeFuture;
+import tech.pegasys.teku.infrastructure.exceptions.FatalServiceFailureException;
+import tech.pegasys.teku.infrastructure.time.TimeProvider;
+import tech.pegasys.teku.infrastructure.unsigned.UInt64;
+import tech.pegasys.teku.spec.datastructures.blocks.SignedBeaconBlock;
+import tech.pegasys.teku.spec.datastructures.state.AnchorPoint;
+import tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconState;
+import tech.pegasys.teku.storage.api.StorageUpdate;
+import tech.pegasys.teku.storage.api.StorageUpdateChannel;
+import tech.pegasys.teku.storage.api.UpdateResult;
+import tech.pegasys.teku.storage.api.WeakSubjectivityUpdate;
+
+/**
+ * When async storage updates are used, the in-memory Store will be updated immediately without
+ * waiting for the database write to complete. If the database write fails for some reason, we can't
+ * just skip that update or the in-memory and disk stores will be out of sync.
+ *
+ * <p>So we keep retrying the update until it succeeds. During this time a queue of other events may
+ * build up behind the failing event. That queue has a limited length until it starts blocking the
+ * caller threads again, which ultimately will bring the whole node to a stop. That happens to be
+ * essentially the same result we'd get if the update failed with synchronous storage - the node
+ * couldn't make any progress.
+ *
+ * <p>The only way to get the in-memory and on-disk stores back in sync if the update never
+ * succeeds, would be to restart the node at which point it will revert back to the on-disk version
+ * and start again from there. If a retry fails for long enough, we give up and crash to trigger
+ * this revert.
+ */
+public class RetryingStorageUpdateChannel implements StorageUpdateChannel {
+  private static final Logger LOG = LogManager.getLogger();
+  static final Duration MAX_RETRY_TIME = Duration.ofMinutes(1);
+
+  private final StorageUpdateChannel delegate;
+  private final TimeProvider timeProvider;
+
+  public RetryingStorageUpdateChannel(
+      final StorageUpdateChannel delegate, final TimeProvider timeProvider) {
+    this.delegate = delegate;
+    this.timeProvider = timeProvider;
+  }
+
+  @Override
+  public SafeFuture<UpdateResult> onStorageUpdate(final StorageUpdate event) {
+    return retry(delegate::onStorageUpdate, event);
+  }
+
+  @Override
+  public SafeFuture<Void> onFinalizedBlocks(final Collection<SignedBeaconBlock> finalizedBlocks) {
+    return retry(delegate::onFinalizedBlocks, finalizedBlocks);
+  }
+
+  @Override
+  public SafeFuture<Void> onFinalizedState(
+      final BeaconState finalizedState, final Bytes32 blockRoot) {
+    return this.retry(__ -> delegate.onFinalizedState(finalizedState, blockRoot), null);
+  }
+
+  @Override
+  public SafeFuture<Void> onWeakSubjectivityUpdate(
+      final WeakSubjectivityUpdate weakSubjectivityUpdate) {
+    return retry(delegate::onWeakSubjectivityUpdate, weakSubjectivityUpdate);
+  }
+
+  @Override
+  public void onChainInitialized(final AnchorPoint initialAnchor) {
+    this.retry(
+            __ -> {
+              delegate.onChainInitialized(initialAnchor);
+              return SafeFuture.COMPLETE;
+            },
+            null)
+        .ifExceptionGetsHereRaiseABug();
+  }
+
+  private <I, O> SafeFuture<O> retry(final Function<I, SafeFuture<O>> method, final I arg) {
+    final UInt64 startTime = timeProvider.getTimeInMillis();
+    while (true) {
+      try {
+        final SafeFuture<O> result = method.apply(arg);
+        result.join();
+        return result;
+      } catch (final Throwable t) {
+        final UInt64 failureTime = timeProvider.getTimeInMillis();
+        if (failureTime.minusMinZero(startTime).isGreaterThan(MAX_RETRY_TIME.toMillis())) {
+          throw new FatalServiceFailureException(RetryingStorageUpdateChannel.class, t);
+        }
+        LOG.error("Storage update failed, retrying.", t);
+      }
+    }
+  }
+}
