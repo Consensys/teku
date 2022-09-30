977d80f2ca77230ee9f0144d03351f7dcb402fc1
Lucas Saldanha <lucascrsaldanha@gmail.com>
2022-09-30 17:09:38 +1300

    Updated ATs DSL with utility methods to check metrics

--- acceptance-tests/src/testFixtures/java/tech/pegasys/teku/test/acceptance/dsl/Node.java
+++ acceptance-tests/src/testFixtures/java/tech/pegasys/teku/test/acceptance/dsl/Node.java
@@ -16,15 +16,16 @@ package tech.pegasys.teku.test.acceptance.dsl;
 import static com.fasterxml.jackson.dataformat.yaml.YAMLGenerator.Feature.WRITE_DOC_START_MARKER;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
+import static tech.pegasys.teku.test.acceptance.dsl.metrics.MetricConditions.anyLabels;
+import static tech.pegasys.teku.test.acceptance.dsl.metrics.MetricConditions.withNameEqualsTo;
+import static tech.pegasys.teku.test.acceptance.dsl.metrics.MetricConditions.withValueEqualTo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringReader;
 import java.net.URI;
 import java.net.URL;
 import java.nio.file.Files;
@@ -43,8 +44,15 @@ import org.testcontainers.images.PullPolicy;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 import tech.pegasys.teku.infrastructure.async.Waiter;
 import tech.pegasys.teku.provider.JsonProvider;
+import tech.pegasys.teku.test.acceptance.dsl.metrics.MetricConditions.MetricLabelsCondition;
+import tech.pegasys.teku.test.acceptance.dsl.metrics.MetricConditions.MetricNameCondition;
+import tech.pegasys.teku.test.acceptance.dsl.metrics.MetricConditions.MetricValuesCondition;
+import tech.pegasys.teku.test.acceptance.dsl.metrics.MetricFetcher;
+import tech.pegasys.teku.test.acceptance.dsl.metrics.MetricMatcher;
+import tech.pegasys.teku.test.acceptance.dsl.metrics.MetricValue;
 
 public abstract class Node {
+
   private static final Logger LOG = LogManager.getLogger();
   public static final String TEKU_DOCKER_IMAGE_NAME = "consensys/teku";
   protected final SimpleHttpClient httpClient = new SimpleHttpClient();
@@ -120,24 +128,48 @@ public abstract class Node {
     waitForEpochAtOrAbove(currentEpoch + 1);
   }
 
-  public double getMetricValue(final String metricName) throws IOException {
-    final String allMetrics = httpClient.get(getMetricsUrl(), "/metrics");
-    final String prefix = metricName + " ";
-    try (BufferedReader reader = new BufferedReader(new StringReader(allMetrics))) {
-      for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-        if (line.startsWith(prefix)) {
-          final String value = line.substring(prefix.length());
-          LOG.debug("Metric {}: {}", metricName, value);
-          return Double.parseDouble(value);
-        }
-      }
-    }
-    throw new IllegalArgumentException(
-        "Did not find metric " + metricName + " in: \n" + allMetrics);
+  // gauge
+  public double getMetricValue(final String metricName) {
+    final MetricFetcher metricFetcher = new MetricFetcher(httpClient, getMetricsUrl());
+    final MetricValue metricValue = metricFetcher.getSingleMetricByName(metricName);
+
+    LOG.debug("Metric {}: {}", metricName, metricValue.getValue());
+    return metricValue.getValue();
   }
 
   public void waitForLogMessageContaining(final String filter) {
     waitFor(() -> assertThat(getFilteredOutput(filter)).isNotEmpty(), 2, TimeUnit.MINUTES);
+  }
+
+  public void waitForMetricWithValue(final String metricName, final double value) {
+    waitForMetric(withNameEqualsTo(metricName), anyLabels(), withValueEqualTo(value));
+  }
+
+  public void waitForMetric(
+      final MetricNameCondition nameCondition,
+      final MetricLabelsCondition labelsCondition,
+      final MetricValuesCondition valueCondition) {
+    waitForMetric(nameCondition, labelsCondition, valueCondition, 5, TimeUnit.MINUTES);
+  }
+
+  public void waitForMetric(
+      final MetricNameCondition nameCondition,
+      final MetricLabelsCondition labelsCondition,
+      final MetricValuesCondition valueCondition,
+      final int timeoutAmount,
+      final TimeUnit timeoutUnit) {
+    final MetricFetcher metricFetcher = new MetricFetcher(httpClient, getMetricsUrl());
+
+    waitFor(
+        () -> {
+          final List<MetricValue> metrics = metricFetcher.getAllMetrics();
+          assertThat(
+                  MetricMatcher.anyMatching(
+                      metrics, nameCondition, labelsCondition, valueCondition))
+              .isPresent();
+        },
+        timeoutAmount,
+        timeoutUnit);
   }
 
   protected void waitFor(
@@ -201,11 +233,7 @@ public abstract class Node {
     return tmpFile;
   }
 
-  /**
-   * Copies contents of the given directory into node's working directory.
-   *
-   * @param tarFile
-   */
+  /** Copies contents of the given directory into node's working directory. */
   public void copyContentsToWorkingDirectory(File tarFile) {
     container.withExpandedTarballToContainer(tarFile, WORKING_DIRECTORY);
   }
