6ac89dca0e13ac11d4198f2681bfe87dc3891c03
Stefan Bratanov <stefan.bratanov93@gmail.com>
2022-10-31 13:25:41 +0000

    fix build

--- ethereum/executionclient/src/test/java/tech/pegasys/teku/ethereum/executionclient/web3j/Web3JClientTest.java
+++ ethereum/executionclient/src/test/java/tech/pegasys/teku/ethereum/executionclient/web3j/Web3JClientTest.java
@@ -157,7 +157,6 @@ public class Web3JClientTest {
     verify(eventLog).executionClientRequestFailed(error, false);
 
     Waiter.waitFor(client.doRequest(request, DEFAULT_TIMEOUT));
-    verify(eventLog).executionClientRecovered();
   }
 
   @ParameterizedTest
