3e96a43847ca11984d3a7c3fe26fe999b75271ff
Lucas Saldanha <lucascrsaldanha@gmail.com>
2022-11-07 12:25:19 +1300

    teku-6370 Changed MAX_BLS_TO_EXECUTION_CHANGES type on Capella SpecConfig to int

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/config/SpecConfigCapella.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/config/SpecConfigCapella.java
@@ -33,6 +33,8 @@ public interface SpecConfigCapella extends SpecConfigBellatrix {
 
   UInt64 getCapellaForkEpoch();
 
+  int getMaxBlsToExecutionChanges();
+
   @Override
   Optional<SpecConfigCapella> toVersionCapella();
 
