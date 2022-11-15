80ccc3d96ef5668f27e50a46201deefbdbeee16d
Dmitrii Shmatko <zilm@zilm.net>
2022-11-15 23:03:30 +0400

    Add deposit snapshot download script

---
+++ scripts/fetch-deposit-snapshot.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -euo pipefail
+
+REST_API=${1:?Must specify Teku Rest API url}
+VERIFY=${2:?Whether to check that snapshot points to higher deposit count than existing}
+OUT=${3:?Out filename}
+
+echo "Checking that required software available"
+
+require_command() {
+  echo -n "Checking $1... "
+  if which "${1}" &> /dev/null; then
+    echo "OK"
+  else
+    echo "FAILED" && exit 127
+  fi
+}
+
+require_command "curl"
+require_command "jq"
+require_command "sed"
+
+if [[ REST_API != *deposit_snapshot ]]
+then
+  REST_API=$(echo "$REST_API" | sed 's:/*$::')
+  REST_API="${REST_API}/teku/v1/beacon/deposit_snapshot"
+fi
+temp_file=$(mktemp)
+trap "rm -f $temp_file" 0 2 3 15
+echo ${REST_API}
+curl --fail -H 'Accept: application/json' "${REST_API}" -o "${temp_file}"
+
+new_snapshot_deposit_count=$(jq -r '.data.deposit_count' ${temp_file})
+echo "Downloaded deposit snapshot with $new_snapshot_deposit_count deposit_count"
+if [ -f "$OUT" ]; then
+  old_snapshot_deposit_count=$(jq -r '.data.deposit_count' ${OUT})
+  echo "Found old deposit snapshot with $old_snapshot_deposit_count deposit_count"
+  if (( "$new_snapshot_deposit_count" > "$old_snapshot_deposit_count" )); then
+      more_deposits=true
+  else
+      more_deposits=false
+  fi
+  if [[ "$VERIFY" = false || ("$VERIFY" = true  && "$more_deposits" = true) ]]; then
+    echo "Replacing old file ${OUT} with new deposit snapshot"
+    mv -f "${temp_file}" "$OUT"
+  else
+    echo "Deposit snapshot is up-to-date";fi
+else
+  echo "Writing deposit snapshot to ${OUT}"
+  mv "${temp_file}" "$OUT"; fi
