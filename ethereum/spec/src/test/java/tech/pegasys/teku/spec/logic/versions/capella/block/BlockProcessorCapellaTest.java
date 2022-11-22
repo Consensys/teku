1736c6cbe3022d052b8578d900f5570958700c02
Lucas Saldanha <lucas.saldanha@consensys.net>
2022-11-22 21:13:31 +1300

    Check if block contains more than 1 BlsToExecutionChange for the same validator (#6488)

--- ethereum/spec/src/test/java/tech/pegasys/teku/spec/logic/versions/capella/block/BlockProcessorCapellaTest.java
+++ ethereum/spec/src/test/java/tech/pegasys/teku/spec/logic/versions/capella/block/BlockProcessorCapellaTest.java
@@ -20,20 +20,28 @@ import java.util.List;
 import java.util.Optional;
 import org.apache.tuweni.bytes.Bytes32;
 import org.junit.jupiter.api.Test;
+import tech.pegasys.teku.bls.BLSPublicKey;
 import tech.pegasys.teku.bls.BLSSignatureVerifier;
 import tech.pegasys.teku.infrastructure.bytes.Bytes20;
+import tech.pegasys.teku.infrastructure.ssz.SszList;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.spec.Spec;
 import tech.pegasys.teku.spec.TestSpecFactory;
 import tech.pegasys.teku.spec.datastructures.blocks.SignedBeaconBlock;
 import tech.pegasys.teku.spec.datastructures.execution.versions.capella.Withdrawal;
+import tech.pegasys.teku.spec.datastructures.operations.BlsToExecutionChange;
+import tech.pegasys.teku.spec.datastructures.operations.SignedBlsToExecutionChange;
 import tech.pegasys.teku.spec.datastructures.state.Validator;
 import tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconState;
+import tech.pegasys.teku.spec.generator.ChainBuilder;
+import tech.pegasys.teku.spec.logic.common.statetransition.blockvalidator.BlockValidationResult;
 import tech.pegasys.teku.spec.logic.common.statetransition.exceptions.StateTransitionException;
 import tech.pegasys.teku.spec.logic.versions.bellatrix.BlockProcessorBellatrixTest;
+import tech.pegasys.teku.spec.schemas.SchemaDefinitionsCapella;
 import tech.pegasys.teku.spec.util.DataStructureUtil;
 
 public class BlockProcessorCapellaTest extends BlockProcessorBellatrixTest {
+
   @Override
   protected Spec createSpec() {
     return TestSpecFactory.createMainnetCapella();
@@ -95,5 +103,63 @@ public class BlockProcessorCapellaTest extends BlockProcessorBellatrixTest {
     assertThat(BlockProcessorCapella.incrementValidatorIndex(9, 10)).isEqualTo(0);
     assertThat(BlockProcessorCapella.incrementValidatorIndex(8, 10)).isEqualTo(9);
     assertThat(BlockProcessorCapella.incrementValidatorIndex(0, 10)).isEqualTo(1);
+  }
+
+  @Test
+  public void shouldRejectBlockWithMoreThanOneBlsExecutionChangesForSameValidator() {
+    final int validatorIndex = 0;
+    final ChainBuilder chain = ChainBuilder.create(spec);
+    chain.generateGenesis();
+
+    final BeaconState state = chain.getGenesis().getState();
+    final BLSPublicKey validatorPubKey = chain.getValidatorKeys().get(0).getPublicKey();
+
+    final SignedBlsToExecutionChange signedBlsToExecutionChange1 =
+        createSignedBlsToExecutionChange(validatorIndex, validatorPubKey);
+    final SignedBlsToExecutionChange signedBlsToExecutionChange2 =
+        createSignedBlsToExecutionChange(validatorIndex, validatorPubKey);
+
+    final SszList<SignedBlsToExecutionChange> blsToExecutionChangesListWithDuplicate =
+        createBlsToExecutionChangeList(signedBlsToExecutionChange1, signedBlsToExecutionChange2);
+
+    final BlockProcessorCapella capellaBlockProcessor =
+        ((BlockProcessorCapella) spec.getGenesisSpec().getBlockProcessor());
+
+    final BlockValidationResult validationResult =
+        capellaBlockProcessor.verifyBlsToExecutionChangesPreProcessing(
+            state, blsToExecutionChangesListWithDuplicate, BLSSignatureVerifier.NO_OP);
+
+    assertThat(validationResult.isValid()).isFalse();
+    assertThat(validationResult.getFailureReason())
+        .contains("Duplicated BlsToExecutionChange for validator " + validatorIndex);
+  }
+
+  private SignedBlsToExecutionChange createSignedBlsToExecutionChange(
+      final int validatorIndex, final BLSPublicKey validatorPubKey) {
+    final SchemaDefinitionsCapella capellaSchema =
+        SchemaDefinitionsCapella.required(spec.getGenesisSchemaDefinitions());
+
+    final BlsToExecutionChange blsToExecutionChange =
+        capellaSchema
+            .getBlsToExecutionChangeSchema()
+            .create(
+                UInt64.valueOf(validatorIndex),
+                validatorPubKey,
+                dataStructureUtil.randomEth1Address());
+
+    return capellaSchema
+        .getSignedBlsToExecutionChangeSchema()
+        .create(blsToExecutionChange, dataStructureUtil.randomSignature());
+  }
+
+  private SszList<SignedBlsToExecutionChange> createBlsToExecutionChangeList(
+      final SignedBlsToExecutionChange... signedBlsToExecutionChanges) {
+    return SchemaDefinitionsCapella.required(spec.getGenesisSchemaDefinitions())
+        .getBeaconBlockSchema()
+        .getBodySchema()
+        .toVersionCapella()
+        .orElseThrow()
+        .getBlsToExecutionChangesSchema()
+        .of(signedBlsToExecutionChanges);
   }
 }
