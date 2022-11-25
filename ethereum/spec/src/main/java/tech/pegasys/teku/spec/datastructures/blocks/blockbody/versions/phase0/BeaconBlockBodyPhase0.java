72c4de6e9aba066f731c6d76627ce97cae4f6313
Paul Harris <paul.harris@consensys.net>
2022-11-25 17:34:58 +1000

    Fixed blinded block on remote VC in capella
     - needed to fix how we get blsToExecutionChanges
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/datastructures/blocks/blockbody/versions/phase0/BeaconBlockBodyPhase0.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/datastructures/blocks/blockbody/versions/phase0/BeaconBlockBodyPhase0.java
@@ -27,6 +27,7 @@ import tech.pegasys.teku.spec.datastructures.operations.Attestation;
 import tech.pegasys.teku.spec.datastructures.operations.AttesterSlashing;
 import tech.pegasys.teku.spec.datastructures.operations.Deposit;
 import tech.pegasys.teku.spec.datastructures.operations.ProposerSlashing;
+import tech.pegasys.teku.spec.datastructures.operations.SignedBlsToExecutionChange;
 import tech.pegasys.teku.spec.datastructures.operations.SignedVoluntaryExit;
 import tech.pegasys.teku.spec.datastructures.type.SszSignature;
 
@@ -122,6 +123,11 @@ public class BeaconBlockBodyPhase0
   @Override
   public SszList<SignedVoluntaryExit> getVoluntaryExits() {
     return getField7();
+  }
+
+  @Override
+  public Optional<SszList<SignedBlsToExecutionChange>> getOptionalBlsToExecutionChanges() {
+    return Optional.empty();
   }
 
   @Override
