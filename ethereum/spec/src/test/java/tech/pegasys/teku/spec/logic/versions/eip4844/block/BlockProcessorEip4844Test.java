47d319e069a5f0ab72c75f325453d27769727f2e
Enrico Del Fante <enrico.delfante@consensys.net>
2022-11-25 17:00:37 +0100

    spotlessly

--- ethereum/spec/src/test/java/tech/pegasys/teku/spec/logic/versions/eip4844/block/BlockProcessorEip4844Test.java
+++ ethereum/spec/src/test/java/tech/pegasys/teku/spec/logic/versions/eip4844/block/BlockProcessorEip4844Test.java
@@ -1,5 +1,21 @@
+/*
+ * Copyright ConsenSys Software Inc., 2022
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package tech.pegasys.teku.spec.logic.versions.eip4844.block;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import tech.pegasys.teku.bls.BLSSignatureVerifier;
 import tech.pegasys.teku.spec.Spec;
@@ -9,12 +25,7 @@ import tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconState;
 import tech.pegasys.teku.spec.logic.common.statetransition.exceptions.StateTransitionException;
 import tech.pegasys.teku.spec.logic.versions.capella.block.BlockProcessorCapellaTest;
 import tech.pegasys.teku.spec.logic.versions.eip4844.blobs.BlobsSidecarAvailabilityChecker;
-import tech.pegasys.teku.spec.logic.versions.eip4844.block.KzgCommitmentsProcessor;
 import tech.pegasys.teku.spec.util.DataStructureUtil;
-
-import java.util.Optional;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class BlockProcessorEip4844Test extends BlockProcessorCapellaTest {
   @Override
@@ -29,13 +40,13 @@ public class BlockProcessorEip4844Test extends BlockProcessorCapellaTest {
     final SignedBeaconBlock block = data.randomSignedBeaconBlock(preState.getSlot().increment());
     assertThatThrownBy(
             () ->
-                    spec.processBlock(
-                            preState,
-                            block,
-                            BLSSignatureVerifier.SIMPLE,
-                            Optional.empty(),
-                            KzgCommitmentsProcessor.NOOP,
-                            BlobsSidecarAvailabilityChecker.NOOP))
-            .isInstanceOf(StateTransitionException.class);
+                spec.processBlock(
+                    preState,
+                    block,
+                    BLSSignatureVerifier.SIMPLE,
+                    Optional.empty(),
+                    KzgCommitmentsProcessor.NOOP,
+                    BlobsSidecarAvailabilityChecker.NOOP))
+        .isInstanceOf(StateTransitionException.class);
   }
 }
