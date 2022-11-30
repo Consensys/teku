a5ef37897829dc511f47c07c6a8bb002900096b6
Dmitrii Shmatko <zilm@zilm.net>
2022-11-30 19:06:38 +0400

    Remove download of extra side data for snapshots as this approach doesn't work

--- scripts/update-snapshots.sh
+++ scripts/update-snapshots.sh
@@ -49,28 +49,18 @@ require_command "jq"
 require_command "sed"
 require_command "ssh"
 
-echo $'\nStarting download of deposit tree snapshots and verification data...'
+echo $'\nStarting download of deposit tree snapshots...'
 OUT=$(echo "$OUT" | sed 's:/*$::')
 
 downloader() {
-  echo "" >&2
-  echo "Downloading $1 snapshot, header, state" >&2
+  echo "Downloading $1 snapshot" >&2
   ssh -f -o ExitOnForwardFailure=yes -L "${LOCAL_REST_API_BINDING_PORT}":localhost:5051 "${2}" sleep 1
   curl -s --fail -H 'Accept: application/octet-stream' http://localhost:"${LOCAL_REST_API_BINDING_PORT}"/teku/v1/beacon/deposit_snapshot -o "${OUT}/${1}.ssz"
-  local result=$(curl -s --fail -H 'Accept: application/json' http://localhost:"${LOCAL_REST_API_BINDING_PORT}"/eth/v1/beacon/headers/finalized | jq '.data')
-  echo $result > "${OUT}/${1}_header.json"
-  curl -s --fail -H 'Accept: application/octet-stream' http://localhost:"${LOCAL_REST_API_BINDING_PORT}"/eth/v1/debug/beacon/states/finalized -o "${OUT}/${1}_state.ssz"
-  local block_root=$(echo $result | jq -r '.root')
-  echo "$block_root"
 }
 
-goerli_root=$(downloader "goerli" "${SERVER_GOERLI_URL}")
-echo "GOERLI latest finalized slot verification link: https://goerli.beaconcha.in/slot/$goerli_root"
-sepolia_root=$(downloader "sepolia" "${SERVER_SEPOLIA_URL}")
-echo "SEPOLIA latest finalized slot verification link: https://sepolia.beaconcha.in/slot/$sepolia_root"
-gnosis_root=$(downloader "gnosis" "${SERVER_GNOSIS_URL}")
-echo "GNOSIS latest finalized slot verification link: https://beacon.gnosischain.com/block/$gnosis_root"
-mainnet_root=$(downloader "mainnet" "${SERVER_MAINNET_URL}")
-echo "MAINNET latest finalized slot verification link: https://beaconcha.in/slot/$mainnet_root"
+downloader "goerli" "${SERVER_GOERLI_URL}"
+downloader "sepolia" "${SERVER_SEPOLIA_URL}"
+downloader "gnosis" "${SERVER_GNOSIS_URL}"
+downloader "mainnet" "${SERVER_MAINNET_URL}"
 
 echo $'\nAll done! Run verification tests and commit changes manually'
