f1d371b5becb049e0ab078ca034d5422fd05e560
Dmitrii Shmatko <zilm@zilm.net>
2022-12-02 15:39:10 +0400

    Cleanup AbstractSignedBeaconBlockBlinder childs

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/datastructures/blocks/blockbody/versions/eip4844/SignedBeaconBlockBlinderEip4844.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/datastructures/blocks/blockbody/versions/eip4844/SignedBeaconBlockBlinderEip4844.java
@@ -13,10 +13,6 @@
 
 package tech.pegasys.teku.spec.datastructures.blocks.blockbody.versions.eip4844;
 
-import static com.google.common.base.Preconditions.checkState;
-
-import tech.pegasys.teku.spec.datastructures.blocks.BeaconBlock;
-import tech.pegasys.teku.spec.datastructures.blocks.SignedBeaconBlock;
 import tech.pegasys.teku.spec.datastructures.blocks.blockbody.common.AbstractSignedBeaconBlockBlinder;
 import tech.pegasys.teku.spec.schemas.SchemaDefinitionsEip4844;
 
@@ -24,51 +20,5 @@ public class SignedBeaconBlockBlinderEip4844 extends AbstractSignedBeaconBlockBl
 
   public SignedBeaconBlockBlinderEip4844(final SchemaDefinitionsEip4844 schemaDefinitions) {
     super(schemaDefinitions);
-  }
-
-  @Override
-  public SignedBeaconBlock blind(final SignedBeaconBlock signedUnblindedBock) {
-    final BeaconBlockBodyEip4844Impl unblindedBlockBody =
-        BeaconBlockBodyEip4844Impl.required(signedUnblindedBock.getMessage().getBody());
-
-    final BlindedBeaconBlockBodySchemaEip4844Impl schema =
-        (BlindedBeaconBlockBodySchemaEip4844Impl)
-            schemaDefinitions.getBlindedBeaconBlockBodySchema();
-
-    final BlindedBeaconBlockBodyEip4844Impl blindedBody =
-        new BlindedBeaconBlockBodyEip4844Impl(
-            schema,
-            unblindedBlockBody.getRandaoRevealSsz(),
-            unblindedBlockBody.getEth1Data(),
-            unblindedBlockBody.getGraffitiSsz(),
-            unblindedBlockBody.getProposerSlashings(),
-            unblindedBlockBody.getAttesterSlashings(),
-            unblindedBlockBody.getAttestations(),
-            unblindedBlockBody.getDeposits(),
-            unblindedBlockBody.getVoluntaryExits(),
-            unblindedBlockBody.getSyncAggregate(),
-            schema
-                .getExecutionPayloadHeaderSchema()
-                .createFromExecutionPayload(unblindedBlockBody.getExecutionPayload()),
-            unblindedBlockBody.getBlsToExecutionChanges(),
-            unblindedBlockBody.getBlobKzgCommitments());
-
-    final BeaconBlock blindedBeaconBlock =
-        schemaDefinitions
-            .getBlindedBeaconBlockSchema()
-            .create(
-                signedUnblindedBock.getSlot(),
-                signedUnblindedBock.getProposerIndex(),
-                signedUnblindedBock.getParentRoot(),
-                signedUnblindedBock.getStateRoot(),
-                blindedBody);
-
-    checkState(
-        blindedBeaconBlock.hashTreeRoot().equals(signedUnblindedBock.getMessage().hashTreeRoot()),
-        "blinded block root do not match original unblinded block root");
-
-    return schemaDefinitions
-        .getSignedBlindedBeaconBlockSchema()
-        .create(blindedBeaconBlock, signedUnblindedBock.getSignature());
   }
 }
