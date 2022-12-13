d3a6d1dc9db775f4a9aef02da6b2e0542430a0a3
Justin Traglia <jtraglia@pm.me>
2022-12-13 14:46:55 -0600

    Add some CKZG property tests

---
+++ infrastructure/kzg/src/property-test/java/tech/pegasys/teku/kzg/ckzg4844/CKZG4844PropertyTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright ConsenSys Software Inc., 2022
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.kzg.ckzg4844;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ethereum.ckzg4844.CKZG4844JNI;
+import java.util.List;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.From;
+import net.jqwik.api.Property;
+import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.bytes.Bytes32;
+import tech.pegasys.teku.kzg.KZG;
+import tech.pegasys.teku.kzg.KZGCommitment;
+import tech.pegasys.teku.kzg.KZGException;
+import tech.pegasys.teku.kzg.KZGProof;
+import tech.pegasys.teku.kzg.suppliers.Bytes32Supplier;
+import tech.pegasys.teku.kzg.suppliers.BytesSupplier;
+import tech.pegasys.teku.kzg.suppliers.KZGCommitmentSupplier;
+import tech.pegasys.teku.kzg.suppliers.KZGProofSupplier;
+
+public class CKZG4844PropertyTest {
+  private static final String MAINNET_TRUSTED_SETUP_TEST = "../trusted_setups/test_mainnet.txt";
+  private static KZG kzg = CKZG4844.createInstance(CKZG4844JNI.Preset.MAINNET.fieldElementsPerBlob);
+
+  static {
+    final String trustedSetup =
+        CKZG4844PropertyTest.class.getResource(MAINNET_TRUSTED_SETUP_TEST).toExternalForm();
+    kzg.loadTrustedSetup(trustedSetup);
+  }
+
+  @Property
+  void computeAggregateKzgProofThrowsExpected(
+      @ForAll final List<@From(supplier = BytesSupplier.class) Bytes> blobs) {
+    try {
+      kzg.computeAggregateKzgProof(blobs);
+    } catch (Exception e) {
+      assertThat(e).isInstanceOf(KZGException.class);
+    }
+  }
+
+  @Property(tries = 100)
+  void verifyAggregateKzgProofThrowsExpected(
+      @ForAll final List<@From(supplier = BytesSupplier.class) Bytes> blobs,
+      @ForAll final List<@From(supplier = KZGCommitmentSupplier.class) KZGCommitment> commitments,
+      @ForAll(supplier = KZGProofSupplier.class) final KZGProof proof) {
+    try {
+      kzg.verifyAggregateKzgProof(blobs, commitments, proof);
+    } catch (Exception e) {
+      assertThat(e).isInstanceOf(KZGException.class);
+    }
+  }
+
+  @Property
+  void blobToKzgCommitmentThrowsExpected(@ForAll(supplier = BytesSupplier.class) final Bytes blob) {
+    try {
+      kzg.blobToKzgCommitment(blob);
+    } catch (Exception e) {
+      assertThat(e).isInstanceOf(KZGException.class);
+    }
+  }
+
+  @Property
+  void verifyKzgProofThrowsExpected(
+      @ForAll(supplier = KZGCommitmentSupplier.class) final KZGCommitment commitment,
+      @ForAll(supplier = Bytes32Supplier.class) final Bytes32 z,
+      @ForAll(supplier = Bytes32Supplier.class) final Bytes32 y,
+      @ForAll(supplier = KZGProofSupplier.class) final KZGProof proof) {
+    try {
+      kzg.verifyKzgProof(commitment, z, y, proof);
+    } catch (Exception e) {
+      assertThat(e).isInstanceOf(KZGException.class);
+    }
+  }
+}
---
+++ infrastructure/kzg/src/property-test/java/tech/pegasys/teku/kzg/suppliers/Bytes32Supplier.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright ConsenSys Software Inc., 2022
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.kzg.suppliers;
+
+import net.jqwik.api.Arbitraries;
+import net.jqwik.api.Arbitrary;
+import net.jqwik.api.ArbitrarySupplier;
+import org.apache.tuweni.bytes.Bytes32;
+
+public class Bytes32Supplier implements ArbitrarySupplier<Bytes32> {
+  @Override
+  public Arbitrary<Bytes32> get() {
+    return Arbitraries.bytes().array(byte[].class).ofSize(32).map(Bytes32::wrap);
+  }
+}
