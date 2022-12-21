4e92377c95aea40be227f49b07d87b1617f72a81
Stefan Bratanov <stefan.bratanov93@gmail.com>
2022-12-21 15:07:53 +0200

    use only blob bytes supplier

--- ethereum/spec/src/property-test/java/tech/pegasys/teku/spec/logic/versions/eip4844/helpers/CKZG4844PropertyTest.java
+++ ethereum/spec/src/property-test/java/tech/pegasys/teku/spec/logic/versions/eip4844/helpers/CKZG4844PropertyTest.java
@@ -16,8 +16,6 @@ package tech.pegasys.teku.spec.logic.versions.eip4844.helpers;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import net.jqwik.api.Arbitraries;
-import net.jqwik.api.Arbitrary;
 import net.jqwik.api.ForAll;
 import net.jqwik.api.From;
 import net.jqwik.api.Property;
@@ -27,11 +25,9 @@ import tech.pegasys.teku.kzg.KZG;
 import tech.pegasys.teku.kzg.KZGCommitment;
 import tech.pegasys.teku.kzg.KZGException;
 import tech.pegasys.teku.kzg.KZGProof;
-import tech.pegasys.teku.spec.SpecMilestone;
-import tech.pegasys.teku.spec.propertytest.suppliers.DataStructureUtilSupplier;
+import tech.pegasys.teku.spec.propertytest.suppliers.execution.versions.eip4844.BlobBytesSupplier;
 import tech.pegasys.teku.spec.propertytest.suppliers.type.KZGCommitmentSupplier;
 import tech.pegasys.teku.spec.propertytest.suppliers.type.KZGProofSupplier;
-import tech.pegasys.teku.spec.util.DataStructureUtil;
 
 @AddLifecycleHook(KzgResolver.class)
 public class CKZG4844PropertyTest {
@@ -66,19 +62,6 @@ public class CKZG4844PropertyTest {
       kzg.blobToKzgCommitment(blob);
     } catch (Exception e) {
       assertThat(e).isInstanceOf(KZGException.class);
-    }
-  }
-
-  public static class BlobBytesSupplier extends DataStructureUtilSupplier<Bytes> {
-
-    public BlobBytesSupplier() {
-      super(DataStructureUtil::randomBlobBytes, SpecMilestone.EIP4844);
-    }
-
-    @Override
-    public Arbitrary<Bytes> get() {
-      return Arbitraries.oneOf(
-          super.get(), Arbitraries.bytes().array(byte[].class).map(Bytes::wrap));
     }
   }
 }
