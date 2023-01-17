af232cd7dc70fe9965798706b78935041a63d006
Lucas Saldanha <lucascrsaldanha@gmail.com>
2023-01-17 17:06:21 +1300

    Fix formatting

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/events/EventSubscriber.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/events/EventSubscriber.java
@@ -156,8 +156,7 @@ public class EventSubscriber {
                 final Throwable rootException = Throwables.getRootCause(throwable);
                 if (rootException instanceof IllegalStateException) {
                   // this might fail due to https://github.com/eclipse/jetty.project/issues/4461 but
-                  // ultimately
-                  // isn't important because it has a retry mechanism
+                  // ultimately isn't important because it has a retry mechanism
                   LOG.warn(
                       "Error sending keep-alive SSE comment. Retrying in {} seconds.",
                       retryInSeconds);
