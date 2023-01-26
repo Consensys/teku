3a0f810f12e3317601165a415bc11209ca14c90d
Lucas Saldanha <lucascrsaldanha@gmail.com>
2023-01-26 16:21:00 +0100

    teku-6734 Batch verify BlsToExecutionChange signatures

--- services/beaconchain/src/main/java/tech/pegasys/teku/services/beaconchain/BeaconChainController.java
+++ services/beaconchain/src/main/java/tech/pegasys/teku/services/beaconchain/BeaconChainController.java
@@ -394,6 +394,7 @@ public class BeaconChainController extends Service implements BeaconChainControl
     initForkChoice();
     initBlockImporter();
     initCombinedChainDataClient();
+    initSignatureVerificationService();
     initAttestationPool();
     initAttesterSlashingPool();
     initProposerSlashingPool();
@@ -402,7 +403,6 @@ public class BeaconChainController extends Service implements BeaconChainControl
     initEth1DataCache();
     initDepositProvider();
     initGenesisHandler();
-    initSignatureVerificationService();
     initAttestationManager();
     initPendingBlocks();
     initBlockManager();
@@ -530,7 +530,8 @@ public class BeaconChainController extends Service implements BeaconChainControl
   protected void initSignedBlsToExecutionChangePool() {
     LOG.debug("BeaconChainController.initSignedBlsToExecutionChangePool()");
     final SignedBlsToExecutionChangeValidator validator =
-        new SignedBlsToExecutionChangeValidator(spec, timeProvider, recentChainData);
+        new SignedBlsToExecutionChangeValidator(
+            spec, timeProvider, recentChainData, signatureVerificationService);
 
     blsToExecutionChangePool =
         new OperationPool<>(
