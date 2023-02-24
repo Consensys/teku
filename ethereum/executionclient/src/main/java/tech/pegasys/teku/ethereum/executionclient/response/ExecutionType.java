8511819a4d9870770c65d1da7c8ec2ee479e366e
Lucas Saldanha <lucas.saldanha@consensys.net>
2023-02-24 16:33:49 +1300

    Migrated Response-related utility objects into `executionclient.reponse` package (#6861)

--- ethereum/executionclient/src/main/java/tech/pegasys/teku/ethereum/executionclient/response/ExecutionType.java
+++ ethereum/executionclient/src/main/java/tech/pegasys/teku/ethereum/executionclient/response/ExecutionType.java
@@ -11,7 +11,7 @@
  * specific language governing permissions and limitations under the License.
  */
 
-package tech.pegasys.teku.ethereum.executionlayer;
+package tech.pegasys.teku.ethereum.executionclient.response;
 
 enum ExecutionType {
   EXECUTION_CLIENT("execution client"),
