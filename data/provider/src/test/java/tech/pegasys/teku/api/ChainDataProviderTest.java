63ef309e1aeddaded982d328a7bde004e0d089d1
courtneyeh <courtney.hunter.nz@gmail.com>
2023-02-24 14:11:32 +1300

    Add getCommitteeIndices method to ChainDataProvider

--- data/provider/src/test/java/tech/pegasys/teku/api/ChainDataProviderTest.java
+++ data/provider/src/test/java/tech/pegasys/teku/api/ChainDataProviderTest.java
@@ -51,6 +51,7 @@ import tech.pegasys.teku.api.response.v1.beacon.GenesisData;
 import tech.pegasys.teku.api.response.v1.beacon.ValidatorStatus;
 import tech.pegasys.teku.api.schema.BeaconState;
 import tech.pegasys.teku.api.schema.Fork;
+import tech.pegasys.teku.bls.BLSPublicKey;
 import tech.pegasys.teku.infrastructure.async.SafeFuture;
 import tech.pegasys.teku.infrastructure.async.SafeFutureAssert;
 import tech.pegasys.teku.infrastructure.bytes.Bytes4;
@@ -388,6 +389,89 @@ public class ChainDataProviderTest {
         provider.getStateSyncCommittees("head", Optional.of(UInt64.valueOf("1024000")));
     SafeFutureAssert.assertThatSafeFuture(future)
         .isCompletedExceptionallyWith(IllegalArgumentException.class);
+  }
+
+  @Test
+  public void getCommitteeIndices_withSpecifiedValidators() {
+    final Spec spec = TestSpecFactory.createMinimalAltair();
+    final DataStructureUtil data = new DataStructureUtil(spec);
+    final ChainDataProvider provider = setupAltairState();
+    final tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconState state =
+        data.randomBeaconState(32);
+
+    final List<BLSPublicKey> stateValidators =
+        state.getValidators().stream().map(Validator::getPublicKey).collect(toList());
+    final List<BLSPublicKey> committeeKeys =
+        List.of(
+            stateValidators.get(1),
+            stateValidators.get(4),
+            stateValidators.get(6),
+            stateValidators.get(7),
+            stateValidators.get(9),
+            stateValidators.get(11));
+
+    final Set<String> validators =
+        Set.of(committeeKeys.get(0).toHexString(), committeeKeys.get(2).toHexString(), "4");
+    final Map<Integer, Integer> committeeIndices =
+        provider.getCommitteeIndices(committeeKeys, validators, state);
+
+    assertThat(committeeIndices).containsExactlyInAnyOrderEntriesOf(Map.of(0, 1, 1, 4, 2, 6));
+  }
+
+  @Test
+  public void getCommitteeIndices_withNoSpecifiedValidators() {
+    final Spec spec = TestSpecFactory.createMinimalAltair();
+    final DataStructureUtil data = new DataStructureUtil(spec);
+    final ChainDataProvider provider = setupAltairState();
+    final tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconState state =
+        data.randomBeaconState(32);
+
+    final List<BLSPublicKey> stateValidators =
+        state.getValidators().stream().map(Validator::getPublicKey).collect(toList());
+    final List<BLSPublicKey> committeeKeys =
+        List.of(
+            stateValidators.get(1),
+            stateValidators.get(4),
+            stateValidators.get(6),
+            stateValidators.get(7),
+            stateValidators.get(9),
+            stateValidators.get(11));
+
+    final Map<Integer, Integer> committeeIndices =
+        provider.getCommitteeIndices(committeeKeys, Set.of(), state);
+
+    assertThat(committeeIndices)
+        .containsExactlyInAnyOrderEntriesOf(Map.of(0, 1, 1, 4, 2, 6, 3, 7, 4, 9, 5, 11));
+  }
+
+  @Test
+  public void getCommitteeIndices_withSpecifiedValidatorsNotInCommittee() {
+    final Spec spec = TestSpecFactory.createMinimalAltair();
+    final DataStructureUtil data = new DataStructureUtil(spec);
+    final ChainDataProvider provider = setupAltairState();
+    final tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconState state =
+        data.randomBeaconState(32);
+
+    final List<BLSPublicKey> stateValidators =
+        state.getValidators().stream().map(Validator::getPublicKey).collect(toList());
+    final List<BLSPublicKey> committeeKeys =
+        List.of(
+            stateValidators.get(1),
+            stateValidators.get(4),
+            stateValidators.get(6),
+            stateValidators.get(7),
+            stateValidators.get(9),
+            stateValidators.get(11));
+
+    final Set<String> validators =
+        Set.of(
+            data.randomPublicKey().toHexString(), // Validator not in committee
+            "2", // Validator not in committee
+            committeeKeys.get(2).toHexString());
+    final Map<Integer, Integer> committeeIndices =
+        provider.getCommitteeIndices(committeeKeys, validators, state);
+
+    assertThat(committeeIndices).containsExactlyInAnyOrderEntriesOf(Map.of(2, 6));
   }
 
   @Test
