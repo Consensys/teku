1c812df20f05b0fc7b0cb6a806e3a0a8522047ca
Stefan Bratanov <stefan.bratanov93@gmail.com>
2023-03-01 13:24:42 +0000

    Add MAX_REQUEST_BLOB_SIDECARS to SpecConfigDeneb

--- networking/eth2/src/main/java/tech/pegasys/teku/networking/eth2/rpc/beaconchain/methods/BlobSidecarsByRangeMessageHandler.java
+++ networking/eth2/src/main/java/tech/pegasys/teku/networking/eth2/rpc/beaconchain/methods/BlobSidecarsByRangeMessageHandler.java
@@ -13,7 +13,6 @@
 
 package tech.pegasys.teku.networking.eth2.rpc.beaconchain.methods;
 
-import static tech.pegasys.teku.spec.config.Constants.MAX_REQUEST_BLOCKS_DENEB;
 import static tech.pegasys.teku.spec.config.Constants.MIN_EPOCHS_FOR_BLOB_SIDECARS_REQUESTS;
 
 import com.google.common.annotations.VisibleForTesting;
@@ -89,8 +88,10 @@ public class BlobSidecarsByRangeMessageHandler
         message.getCount(),
         startSlot);
 
-    final UInt64 maxBlobsPerBlock = getMaxBlobsPerBlock(startSlot);
-    final UInt64 maxRequestBlobSidecars = MAX_REQUEST_BLOCKS_DENEB.times(maxBlobsPerBlock);
+    final SpecConfigDeneb specConfig = SpecConfigDeneb.required(spec.atSlot(startSlot).getConfig());
+
+    final UInt64 maxBlobsPerBlock = UInt64.valueOf(specConfig.getMaxBlobsPerBlock());
+    final UInt64 maxRequestBlobSidecars = specConfig.getMaxRequestBlobSidecars();
 
     if (!peer.wantToMakeRequest()
         || !peer.wantToReceiveBlobSidecars(
@@ -140,13 +141,6 @@ public class BlobSidecarsByRangeMessageHandler
               callback.completeSuccessfully();
             },
             error -> handleProcessingRequestError(error, callback));
-  }
-
-  @VisibleForTesting
-  UInt64 getMaxBlobsPerBlock(final UInt64 slot) {
-    final int maxBlobsPerBlock =
-        SpecConfigDeneb.required(spec.atSlot(slot).getConfig()).getMaxBlobsPerBlock();
-    return UInt64.valueOf(maxBlobsPerBlock);
   }
 
   private boolean checkBlobSidecarsAreAvailable(
