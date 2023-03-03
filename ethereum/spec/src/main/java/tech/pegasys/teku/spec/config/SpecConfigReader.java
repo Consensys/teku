79567d9ea292f03f76483ec2a4737c9e9cc76ca3
Paul Harris <paul.harris@consensys.net>
2023-03-03 11:44:58 +1000

    Revert "Added Capella fork config for Goerli (#6898)"
    This reverts commit b0430680
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/config/SpecConfigReader.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/config/SpecConfigReader.java
@@ -58,6 +58,9 @@ public class SpecConfigReader {
       ImmutableSet.of(
           PRESET_KEY,
           CONFIG_NAME_KEY,
+          // Unsupported, upcoming fork-related keys
+          "SHARDING_FORK_VERSION",
+          "SHARDING_FORK_EPOCH",
           // Old merge config item which is no longer used, ignore for backwards compatibility
           "TRANSITION_TOTAL_DIFFICULTY");
   private static final ImmutableSet<String> CONSTANT_KEYS =
