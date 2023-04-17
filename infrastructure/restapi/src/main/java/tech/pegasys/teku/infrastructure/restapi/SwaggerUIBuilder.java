4a4d6ff4dbcca3200308753ecb210062ce3297eb
Paul Harris <paul.harris@consensys.net>
2023-04-17 12:25:58 +1000

    Update project dependencies (#7043)
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- infrastructure/restapi/src/main/java/tech/pegasys/teku/infrastructure/restapi/SwaggerUIBuilder.java
+++ infrastructure/restapi/src/main/java/tech/pegasys/teku/infrastructure/restapi/SwaggerUIBuilder.java
@@ -25,7 +25,7 @@ import tech.pegasys.teku.infrastructure.restapi.openapi.OpenApiDocBuilder;
 
 public class SwaggerUIBuilder {
   // Version here MUST match `swagger-ui` library version
-  private static final String SWAGGER_UI_VERSION = "4.18.1";
+  private static final String SWAGGER_UI_VERSION = "4.18.2";
 
   private static final String SWAGGER_UI_PATH = "/swagger-ui";
   private static final String SWAGGER_HOSTED_PATH = "/webjars/swagger-ui/" + SWAGGER_UI_VERSION;
