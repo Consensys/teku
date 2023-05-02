0b5e472e434d10c88987a23170b2d5d5f417d6d6
Paul Harris <paul.harris@consensys.net>
2023-05-02 17:44:03 +1000

    Added unit tests for metadata on validator api endpoints (#7093)

--- validator/client/src/test/java/tech/pegasys/teku/validator/client/restapi/apis/DeleteKeysTest.java
+++ validator/client/src/test/java/tech/pegasys/teku/validator/client/restapi/apis/DeleteKeysTest.java
@@ -13,6 +13,7 @@
 
 package tech.pegasys.teku.validator.client.restapi.apis;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
@@ -23,6 +24,13 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_BAD_REQUEST;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_FORBIDDEN;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_INTERNAL_SERVER_ERROR;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_OK;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_UNAUTHORIZED;
+import static tech.pegasys.teku.infrastructure.restapi.MetadataTestUtil.getResponseStringFromMetadata;
+import static tech.pegasys.teku.infrastructure.restapi.MetadataTestUtil.verifyMetadataErrorResponse;
 import static tech.pegasys.teku.validator.client.restapi.apis.schema.DeleteKeyResult.success;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -39,15 +47,16 @@ import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiRequest;
 import tech.pegasys.teku.spec.TestSpecFactory;
 import tech.pegasys.teku.spec.util.DataStructureUtil;
 import tech.pegasys.teku.validator.client.ActiveKeyManager;
+import tech.pegasys.teku.validator.client.restapi.apis.schema.DeleteKeyResult;
 import tech.pegasys.teku.validator.client.restapi.apis.schema.DeleteKeysRequest;
 import tech.pegasys.teku.validator.client.restapi.apis.schema.DeleteKeysResponse;
 
 public class DeleteKeysTest {
-  final DataStructureUtil dataStructureUtil =
+  private final DataStructureUtil dataStructureUtil =
       new DataStructureUtil(TestSpecFactory.createMinimalAltair());
   private final ActiveKeyManager keyManager = mock(ActiveKeyManager.class);
   private Path tempDir;
-  private DeleteKeys endpoint;
+  private DeleteKeys handler;
 
   private final DeleteKeysRequest requestData = new DeleteKeysRequest();
   private final RestApiRequest request = mock(RestApiRequest.class);
@@ -57,7 +66,7 @@ public class DeleteKeysTest {
     tempDir = Files.createTempDirectory("teku_unit_test");
     assertTrue(tempDir.toFile().mkdirs() || tempDir.toFile().isDirectory());
     tempDir.toFile().deleteOnExit();
-    endpoint = new DeleteKeys(keyManager, tempDir);
+    handler = new DeleteKeys(keyManager, tempDir);
   }
 
   @Test
@@ -69,7 +78,7 @@ public class DeleteKeysTest {
         .thenReturn(new DeleteKeysResponse(List.of(success(), success()), ""));
     when(request.getRequestBody()).thenReturn(requestData);
 
-    endpoint.handleRequest(request);
+    handler.handleRequest(request);
     verify(keyManager).deleteValidators(eq(keys), any());
     verify(request, never()).respondError(anyInt(), any());
     verify(request, times(1)).respondOk(any(DeleteKeysResponse.class));
@@ -81,7 +90,7 @@ public class DeleteKeysTest {
     when(keyManager.deleteValidators(any(), any()))
         .thenReturn(new DeleteKeysResponse(List.of(), ""));
 
-    endpoint.handleRequest(request);
+    handler.handleRequest(request);
     verify(keyManager).deleteValidators(eq(Collections.emptyList()), any());
     verify(request, never()).respondError(anyInt(), any());
     verify(request, times(1)).respondOk(any(DeleteKeysResponse.class));
@@ -91,9 +100,42 @@ public class DeleteKeysTest {
   void shouldReturnBadRequest() throws JsonProcessingException {
     when(request.getRequestBody()).thenThrow(new MissingRequestBodyException());
 
-    assertThatThrownBy(() -> endpoint.handleRequest(request))
+    assertThatThrownBy(() -> handler.handleRequest(request))
         .isInstanceOf(MissingRequestBodyException.class);
     verify(keyManager, never()).deleteValidators(any(), any());
     verify(request, never()).respondOk(any());
+  }
+
+  @Test
+  void metadata_shouldHandle200() throws JsonProcessingException {
+    final List<DeleteKeyResult> deleteKeyResults =
+        List.of(DeleteKeyResult.success(), DeleteKeyResult.success());
+    final String slashingData = "{slashingData}";
+    final String responseData =
+        getResponseStringFromMetadata(
+            handler, SC_OK, new DeleteKeysResponse(deleteKeyResults, slashingData));
+    assertThat(responseData)
+        .isEqualTo(
+            "{\"data\":[{\"status\":\"deleted\"},{\"status\":\"deleted\"}],\"slashing_protection\":\"{slashingData}\"}");
+  }
+
+  @Test
+  void metadata_shouldHandle400() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, SC_BAD_REQUEST);
+  }
+
+  @Test
+  void metadata_shouldHandle401() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, SC_UNAUTHORIZED);
+  }
+
+  @Test
+  void metadata_shouldHandle403() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, SC_FORBIDDEN);
+  }
+
+  @Test
+  void metadata_shouldHandle500() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, SC_INTERNAL_SERVER_ERROR);
   }
 }
