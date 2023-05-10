426213bcee88269280dd7159dd6e379595ba421e
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2023-05-10 11:31:45 +0200

    refactor shcema definitions retrieval

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/PostBlock.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/PostBlock.java
@@ -13,6 +13,7 @@
 
 package tech.pegasys.teku.beaconrestapi.handlers.v1.beacon;
 
+import static tech.pegasys.teku.beaconrestapi.handlers.v1.beacon.MilestoneDependentTypesUtil.getSchemaDefinitionForAllMilestones;
 import static tech.pegasys.teku.beaconrestapi.handlers.v1.beacon.MilestoneDependentTypesUtil.slotBasedSelector;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_ACCEPTED;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_INTERNAL_SERVER_ERROR;
@@ -73,7 +74,7 @@ public class PostBlock extends RestApiEndpoint {
                 + " The beacon node performs the required validation.")
         .tags(TAG_BEACON, TAG_VALIDATOR_REQUIRED)
         .requestBodyType(
-            MilestoneDependentTypesUtil.getSchemaDefinitionForAllMilestones(
+            getSchemaDefinitionForAllMilestones(
                 schemaDefinitionCache,
                 "SignedBeaconBlock",
                 SchemaDefinitions::getSignedBeaconBlockSchema,
