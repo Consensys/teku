31a225ad9933f5cccf7e2258a868688ffa83a77d
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2023-05-11 15:12:42 +0200

    7019 refactor deneb containers (#7131)
    * use BlockContainer in the post block api

--- ethereum/spec/src/testFixtures/java/tech/pegasys/teku/spec/propertytest/suppliers/blocks/versions/deneb/SignedBlindedBlockContentsSupplier.java
+++ ethereum/spec/src/testFixtures/java/tech/pegasys/teku/spec/propertytest/suppliers/blocks/versions/deneb/SignedBlindedBlockContentsSupplier.java
@@ -14,7 +14,7 @@
 package tech.pegasys.teku.spec.propertytest.suppliers.blocks.versions.deneb;
 
 import tech.pegasys.teku.spec.SpecMilestone;
-import tech.pegasys.teku.spec.datastructures.blocks.blockbody.versions.deneb.SignedBlindedBlockContents;
+import tech.pegasys.teku.spec.datastructures.blocks.versions.deneb.SignedBlindedBlockContents;
 import tech.pegasys.teku.spec.propertytest.suppliers.DataStructureUtilSupplier;
 import tech.pegasys.teku.spec.util.DataStructureUtil;
 
