856926fb658734612eac81b1715b6a2f6d8b5a7b
Dmitrii Shmatko <zilm@zilm.net>
2023-05-13 23:08:01 +0400

    Add test earliestBlobSidecar is not ever overridden on saving new blobSidecars

--- storage/src/test/java/tech/pegasys/teku/storage/client/RecentChainDataTest.java
+++ storage/src/test/java/tech/pegasys/teku/storage/client/RecentChainDataTest.java
@@ -806,7 +806,7 @@ class RecentChainDataTest {
   }
 
   @Test
-  void retrieveBlobSidecars_shouldReturnNoBlobSidecarsWhenNotExists() {
+  void retrieveBlobSidecars_shouldReturnEmptyBlobSidecarsWhenNotExists() {
     initPostGenesis();
 
     final SignedBlockAndState block = chainBuilder.generateBlockAtSlot(UInt64.valueOf(1));
@@ -817,6 +817,28 @@ class RecentChainDataTest {
     final SafeFuture<List<BlobSidecar>> blobSidecarsFuture =
         recentChainData.retrieveBlobSidecars(block.getSlotAndBlockRoot());
     assertThat(blobSidecarsFuture).isCompletedWithValue(blobSidecars);
+  }
+
+  @Test
+  void getEarliestBlobSidecarSlot_shouldBeSetOnlyOnce() {
+    initPostGenesis();
+
+    final SignedBlockAndState block1 =
+        chainBuilder.generateBlockAtSlot(
+            UInt64.valueOf(1), BlockOptions.create().setGenerateRandomBlobs(true));
+    final List<BlobSidecar> blobSidecars1 = chainBuilder.getBlobSidecars(block1.getRoot());
+    storageSystem.chainUpdater().saveBlock(block1, blobSidecars1, UInt64.valueOf(1));
+    assertThat(recentChainData.retrieveEarliestBlobSidecarSlot())
+        .isCompletedWithValue(Optional.of(UInt64.valueOf(1)));
+
+    final SignedBlockAndState block2 =
+        chainBuilder.generateBlockAtSlot(
+            UInt64.valueOf(2), BlockOptions.create().setGenerateRandomBlobs(true));
+    final List<BlobSidecar> blobSidecars2 = chainBuilder.getBlobSidecars(block2.getRoot());
+    storageSystem.chainUpdater().saveBlock(block2, blobSidecars2, UInt64.valueOf(2));
+    // Still the same
+    assertThat(recentChainData.retrieveEarliestBlobSidecarSlot())
+        .isCompletedWithValue(Optional.of(UInt64.valueOf(1)));
   }
 
   @Test
