929d399b0e62216a439e806bab47d55d8b20db18
Stefan Bratanov <stefan.bratanov93@gmail.com>
2023-07-11 14:59:33 +0100

    Fix description for `delete-hot-blocks` command

--- teku/src/main/java/tech/pegasys/teku/cli/subcommand/debug/DebugDbCommand.java
+++ teku/src/main/java/tech/pegasys/teku/cli/subcommand/debug/DebugDbCommand.java
@@ -427,7 +427,7 @@ public class DebugDbCommand implements Runnable {
 
   @Command(
       name = "delete-hot-blocks",
-      description = "Writes all non-justified blocks in the database as a zip of SSZ files",
+      description = "Deletes all non-justified blocks from an invalid hard fork in the database",
       mixinStandardHelpOptions = true,
       showDefaultValues = true,
       abbreviateSynopsis = true,
@@ -443,7 +443,7 @@ public class DebugDbCommand implements Runnable {
       @Option(
               names = {"--delete-all", "-a"},
               description =
-                  "Delete all blocks that have not been justified, not just those from an invalid hard fork",
+                  "Delete all non-justified blocks, not just those from an invalid hard fork",
               defaultValue = "false",
               fallbackValue = "true",
               showDefaultValue = Visibility.ALWAYS)
