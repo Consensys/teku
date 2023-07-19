dce4aed93e7b4afae38562215e43e53dc12a90aa
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2023-07-19 17:01:58 +0200

    test subnets are disctinct

--- networking/eth2/src/test/java/tech/pegasys/teku/networking/eth2/gossip/subnets/NodeBasedStableSubnetSubscriberTest.java
+++ networking/eth2/src/test/java/tech/pegasys/teku/networking/eth2/gossip/subnets/NodeBasedStableSubnetSubscriberTest.java
@@ -13,14 +13,19 @@
 
 package tech.pegasys.teku.networking.eth2.gossip.subnets;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.intThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
+import it.unimi.dsi.fastutil.ints.IntSet;
+import java.util.List;
 import java.util.Optional;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.networking.eth2.Eth2P2PNetwork;
 import tech.pegasys.teku.spec.Spec;
@@ -47,15 +52,21 @@ public class NodeBasedStableSubnetSubscriberTest {
   void shouldSubscribeToNewSubnetsAfterExpiration() {
     final NodeBasedStableSubnetSubscriber subscriber = createSubscriber();
     subscriber.onSlot(UInt64.ONE);
-    verify(network, times(spec.getNetworkingConfig().getSubnetsPerNode()))
-        .subscribeToAttestationSubnetId(intThat(i -> i >= 0 && i < attestationSubnetCount));
     subscriber.onSlot(
         UInt64.ONE.plus(
             UInt64.valueOf(
                 (long) spec.getNetworkingConfig().getEpochsPerSubnetSubscription()
                     * spec.getSlotsPerEpoch(UInt64.ONE))));
+
+    final ArgumentCaptor<Integer> subnetIdsCaptor = ArgumentCaptor.forClass(Integer.class);
+
     verify(network, times(spec.getNetworkingConfig().getSubnetsPerNode() * 2))
-        .subscribeToAttestationSubnetId(intThat(i -> i >= 0 && i < attestationSubnetCount));
+        .subscribeToAttestationSubnetId(subnetIdsCaptor.capture());
+    assertThat(subnetIdsCaptor.getAllValues())
+        .allMatch(subnetId -> subnetId >= 0 && subnetId < attestationSubnetCount);
+    assertThat(subnetIdsCaptor.getAllValues())
+        .hasSize(spec.getNetworkingConfig().getSubnetsPerNode() * 2);
+    assertSubnetsAreDistinct(subnetIdsCaptor.getAllValues());
   }
 
   @NotNull
@@ -64,5 +75,11 @@ public class NodeBasedStableSubnetSubscriberTest {
         new AttestationTopicSubscriber(spec, network),
         spec,
         Optional.of(dataStructureUtil.randomUInt256()));
+  }
+
+  private void assertSubnetsAreDistinct(List<Integer> subscribedSubnetIds) {
+    IntSet subnetIds =
+        IntOpenHashSet.toSet(subscribedSubnetIds.stream().mapToInt(Integer::intValue));
+    assertThat(subscribedSubnetIds).hasSameSizeAs(subnetIds);
   }
 }
