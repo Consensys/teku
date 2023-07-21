f45b101b8b650abb5a7abfc513846c95511a7d4d
Stefan Bratanov <stefan.bratanov93@gmail.com>
2023-07-21 12:24:43 +0100

    simplification

--- infrastructure/ssz/src/main/java/tech/pegasys/teku/infrastructure/ssz/primitive/SszUInt64.java
+++ infrastructure/ssz/src/main/java/tech/pegasys/teku/infrastructure/ssz/primitive/SszUInt64.java
@@ -19,9 +19,9 @@ import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 
 public class SszUInt64 extends AbstractSszPrimitive<UInt64, SszUInt64> {
 
-  public static final SszUInt64 ZERO = SszUInt64.valueOf(UInt64.ZERO);
-  public static final SszUInt64 THIRTY_TWO_ETH = SszUInt64.valueOf(UInt64.THIRTY_TWO_ETH);
-  public static final SszUInt64 MAX_VALUE = SszUInt64.valueOf(UInt64.MAX_VALUE);
+  public static final SszUInt64 ZERO = new SszUInt64(UInt64.ZERO);
+  public static final SszUInt64 THIRTY_TWO_ETH = new SszUInt64(UInt64.THIRTY_TWO_ETH);
+  public static final SszUInt64 MAX_VALUE = new SszUInt64(UInt64.MAX_VALUE);
 
   public static SszUInt64 of(final UInt64 val) {
     if (val.isZero()) {
@@ -33,10 +33,6 @@ public class SszUInt64 extends AbstractSszPrimitive<UInt64, SszUInt64> {
     if (val.isMaxValue()) {
       return MAX_VALUE;
     }
-    return new SszUInt64(val);
-  }
-
-  private static SszUInt64 valueOf(final UInt64 val) {
     return new SszUInt64(val);
   }
 
