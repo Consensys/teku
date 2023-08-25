a99a55abdc771cdc784139c9d48bcd44c246a083
Paul Harris <paul.harris@consensys.net>
2023-08-25 11:47:37 +1000

    Update library dependencies ahead of release (#7447)
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- networking/p2p/src/test/java/tech/pegasys/teku/networking/p2p/network/config/NetworkConfigTest.java
+++ networking/p2p/src/test/java/tech/pegasys/teku/networking/p2p/network/config/NetworkConfigTest.java
@@ -18,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings("AddressSelection")
 class NetworkConfigTest {
 
   private Optional<String> advertisedIp = Optional.empty();
