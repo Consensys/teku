8ac8ef5d7af93f89aaef0407ef7df71498127ba1
Stefan Bratanov <stefan.bratanov93@gmail.com>
2023-09-14 11:38:30 +0100

    Don't override max buffered connection writes

--- networking/p2p/src/main/java/tech/pegasys/teku/networking/p2p/libp2p/LibP2PNetworkBuilder.java
+++ networking/p2p/src/main/java/tech/pegasys/teku/networking/p2p/libp2p/LibP2PNetworkBuilder.java
@@ -159,10 +159,7 @@ public class LibP2PNetworkBuilder {
 
           // yamux MUST take precedence during negotiation
           if (config.isYamuxEnabled()) {
-            // as a temporary workaround setting maxBufferedConnectionWrites to 150 MiB to handle
-            // overflowing the write buffer when serving blocks by range requests
-            final int maxBufferedConnectionWrites = 150 * 1024 * 1024;
-            b.getMuxers().add(StreamMuxerProtocol.getYamux(maxBufferedConnectionWrites));
+            b.getMuxers().add(StreamMuxerProtocol.getYamux());
           }
           b.getMuxers().add(StreamMuxerProtocol.getMplex());
 
