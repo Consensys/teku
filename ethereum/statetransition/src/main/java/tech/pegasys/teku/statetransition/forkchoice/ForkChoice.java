6e9623235c4abf4d0a5cb050494406af42355e61
Stefan Bratanov <stefan.bratanov93@gmail.com>
2023-09-19 15:18:55 +0800

    add comment

--- ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/forkchoice/ForkChoice.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/forkchoice/ForkChoice.java
@@ -586,6 +586,7 @@ public class ForkChoice implements ForkChoiceUpdatedResultSubscriber {
 
       if (isBeforeAttestingInterval(millisPerSlot, timeIntoSlotMillis)) {
         if (forkChoiceProposerBoostUniquenessEnabled) {
+          // is_first_block
           return transaction.getProposerBoostRoot().isEmpty();
         }
         return true;
