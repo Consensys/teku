84d6c2670b628cbfda5d9b873eec32e9f7398ef9
Stefan Bratanov <stefan.bratanov93@gmail.com>
2023-09-21 14:17:42 +0800

    disable Deneb fork choice tests temporarily

--- eth-tests/src/main/java/tech/pegasys/teku/ethtests/finder/ReferenceTestFinder.java
+++ eth-tests/src/main/java/tech/pegasys/teku/ethtests/finder/ReferenceTestFinder.java
@@ -19,6 +19,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 import tech.pegasys.teku.ethtests.TestFork;
 import tech.pegasys.teku.infrastructure.async.ExceptionThrowingFunction;
@@ -31,6 +32,10 @@ public class ReferenceTestFinder {
   private static final List<String> SUPPORTED_FORKS =
       List.of(
           TestFork.PHASE0, TestFork.ALTAIR, TestFork.BELLATRIX, TestFork.CAPELLA, TestFork.DENEB);
+  // should only be used to temporarily disable specific reference tests,
+  // otherwise should return false
+  private static final Predicate<TestDefinition> IGNORE_TESTS =
+      test -> test.getFork().equals(TestFork.DENEB) && test.getTestType().startsWith("fork-choice");
 
   @MustBeClosed
   public static Stream<TestDefinition> findReferenceTests() throws IOException {
@@ -58,7 +63,8 @@ public class ReferenceTestFinder {
                       new SszTestFinder("ssz_static"),
                       new ShufflingTestFinder(),
                       new PyspecTestFinder())
-                  .flatMap(unchecked(finder -> finder.findTests(fork, spec, testsPath)));
+                  .flatMap(unchecked(finder -> finder.findTests(fork, spec, testsPath)))
+                  .filter(test -> !IGNORE_TESTS.test(test));
             });
   }
 
