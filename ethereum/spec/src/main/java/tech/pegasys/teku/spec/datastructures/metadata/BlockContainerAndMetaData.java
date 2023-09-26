387002bceef5a63801c701054bbda799ed629f0f
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2023-09-26 16:40:45 +0200

    fix serialisation

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/datastructures/metadata/BlockContainerAndMetaData.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/datastructures/metadata/BlockContainerAndMetaData.java
@@ -17,55 +17,5 @@ import org.apache.tuweni.units.bigints.UInt256;
 import tech.pegasys.teku.spec.SpecMilestone;
 import tech.pegasys.teku.spec.datastructures.blocks.BlockContainer;
 
-<<<<<<< HEAD
 public record BlockContainerAndMetaData<T extends BlockContainer>(
     T blockContainer, SpecMilestone specMilestone, UInt256 executionPayloadValue) {}
-=======
-public class BlockContainerAndMetaData extends ObjectAndMetaData<BlockContainer> {
-
-  private final UInt256 blockValue;
-
-  public BlockContainerAndMetaData(
-      final BlockContainer data,
-      final SpecMilestone milestone,
-      final boolean executionOptimistic,
-      final boolean canonical,
-      final boolean finalized,
-      final UInt256 blockValue) {
-    super(data, milestone, executionOptimistic, canonical, finalized);
-    this.blockValue = blockValue;
-  }
-
-  public UInt256 getBlockValue() {
-    return blockValue;
-  }
-
-  @Override
-  public boolean equals(final Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    final BlockContainerAndMetaData that = (BlockContainerAndMetaData) o;
-    return super.equals(o) && blockValue.equals(that.blockValue);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(super.hashCode(), blockValue);
-  }
-
-  @Override
-  public String toString() {
-    return MoreObjects.toStringHelper(this)
-        .add("data", getData())
-        .add("milestone", getMilestone())
-        .add("executionOptimistic", isExecutionOptimistic())
-        .add("canonical", isCanonical())
-        .add("blockValue", blockValue.toDecimalString())
-        .toString();
-  }
-}
->>>>>>> d9bcb3fd14 (Override equals)
