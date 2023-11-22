cdf0c7ac28884662a22e048a6053941aa40d96b6
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2023-11-22 11:52:10 +0100

    add Deneb block contents signing (#7723)
    * add deneb block contents signing

--- validator/client/src/main/java/tech/pegasys/teku/validator/client/signer/BlockContainerSignerDeneb.java
+++ validator/client/src/main/java/tech/pegasys/teku/validator/client/signer/BlockContainerSignerDeneb.java
@@ -13,14 +13,18 @@
 
 package tech.pegasys.teku.validator.client.signer;
 
-import java.util.Collections;
+import java.util.List;
 import tech.pegasys.teku.infrastructure.async.SafeFuture;
+import tech.pegasys.teku.infrastructure.ssz.SszCollection;
+import tech.pegasys.teku.kzg.KZGProof;
 import tech.pegasys.teku.spec.Spec;
+import tech.pegasys.teku.spec.datastructures.blobs.versions.deneb.Blob;
 import tech.pegasys.teku.spec.datastructures.blocks.BeaconBlock;
 import tech.pegasys.teku.spec.datastructures.blocks.BlockContainer;
 import tech.pegasys.teku.spec.datastructures.blocks.SignedBeaconBlock;
 import tech.pegasys.teku.spec.datastructures.blocks.SignedBlockContainer;
 import tech.pegasys.teku.spec.datastructures.state.ForkInfo;
+import tech.pegasys.teku.spec.datastructures.type.SszKZGProof;
 import tech.pegasys.teku.spec.schemas.SchemaDefinitionsDeneb;
 import tech.pegasys.teku.validator.client.Validator;
 
@@ -44,15 +48,37 @@ public class BlockContainerSignerDeneb implements BlockContainerSigner {
     return signBlock(unsignedBlock, validator, forkInfo)
         .thenApply(
             signedBlock -> {
-              // Blinded flow
               if (signedBlock.isBlinded()) {
                 return signedBlock;
+              } else {
+                final List<KZGProof> kzgProofs =
+                    unsignedBlockContainer
+                        .getKzgProofs()
+                        .map(
+                            sszKzgProofs ->
+                                sszKzgProofs.asList().stream()
+                                    .map(SszKZGProof::getKZGProof)
+                                    .toList())
+                        .orElseThrow(
+                            () ->
+                                new RuntimeException(
+                                    String.format(
+                                        "Unable to get KZG Proofs when signing Deneb block at slot %d",
+                                        unsignedBlockContainer.getSlot().longValue())));
+                final List<Blob> blobs =
+                    unsignedBlockContainer
+                        .getBlobs()
+                        .map(SszCollection::asList)
+                        .orElseThrow(
+                            () ->
+                                new RuntimeException(
+                                    String.format(
+                                        "Unable to get blobs when signing Deneb block at slot %d",
+                                        unsignedBlockContainer.getSlot().longValue())));
+                return schemaDefinitions
+                    .getSignedBlockContentsSchema()
+                    .create(signedBlock, kzgProofs, blobs);
               }
-              // Unblinded flow
-              // TODO: add proofs and blobs
-              return schemaDefinitions
-                  .getSignedBlockContentsSchema()
-                  .create(signedBlock, Collections.emptyList(), Collections.emptyList());
             });
   }
 
