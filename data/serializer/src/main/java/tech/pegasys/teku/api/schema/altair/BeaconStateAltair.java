be8c5cf4c830fcdcffcef1766db48a4be789a119
Paul Harris <paul.harris@consensys.net>
2024-02-05 14:30:38 +1000

    Third party updates
    Also updated errorprone, I'm not sure why it complained about beaconStateAltair in schema... I followed the advice and nothing seemed to break.
    
    The WeakSubjectivityConfigTest also pinged but that seemed like an easy fix.
    
    Avoided Javalin, being a major upgrade.
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- data/serializer/src/main/java/tech/pegasys/teku/api/schema/altair/BeaconStateAltair.java
+++ data/serializer/src/main/java/tech/pegasys/teku/api/schema/altair/BeaconStateAltair.java
@@ -181,7 +181,10 @@ public class BeaconStateAltair extends BeaconState implements State {
   public int hashCode() {
     int result =
         Objects.hash(
-            super.hashCode(), inactivity_scores, current_sync_committee, next_sync_committee);
+            System.identityHashCode(this),
+            inactivity_scores,
+            current_sync_committee,
+            next_sync_committee);
     result = 31 * result + Arrays.hashCode(previous_epoch_participation);
     result = 31 * result + Arrays.hashCode(current_epoch_participation);
     return result;
