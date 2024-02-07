b48fb715d4542d3a38a1fe788b5905e03ac23b42
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2024-02-07 16:09:14 +0100

    ATs split

---
+++ acceptance-tests/src/acceptance-test/java/tech/pegasys/teku/test/acceptance/ValidatorSlashingDetectionSinglePeerAcceptanceTest.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright Consensys Software Inc., 2024
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.test.acceptance;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import tech.pegasys.teku.bls.BLSKeyPair;
+import tech.pegasys.teku.infrastructure.unsigned.UInt64;
+import tech.pegasys.teku.spec.SpecMilestone;
+import tech.pegasys.teku.test.acceptance.dsl.TekuNode;
+import tech.pegasys.teku.test.acceptance.dsl.TekuValidatorNode;
+
+/**
+ * In order to cover all possible validator slashing scenarios, this acceptance test runs different
+ * combinations: <br>
+ * - Single Process: VC/BN running in a single process. In this case there is no SSE. <br>
+ * - Stand-Alone VC: VC/BN running in a separate processes and communicating through the REST APIs
+ * and SSE. In this case the slashing event is sent from the BN to the VC through SSE. <br>
+ * - Single Peer: No network, the slashing event is directly received by the running node. <br>
+ * - Multi Peers: Multiple running nodes, the slashing event is received by a first node through the
+ * PostAttesterSlashing or PostProposerSlashing REST APIs and then sent to the concerned node either
+ * through gossip or within a block. <br>
+ * - No Blocks: the slashing event is not received by the slashed node within a block but rather
+ * through the proposer_slashing or attester_slashing p2p gossip topics. <br>
+ * - No Gossip: the slashing event is not received by the slashed node through the proposer_slashing
+ * or attester_slashing p2p gossip topics but rather within a block. <br>
+ */
+public class ValidatorSlashingDetectionSinglePeerAcceptanceTest
+    extends ValidatorSlashingDetectionAcceptanceTest {
+
+  @ParameterizedTest
+  @MethodSource("getSlashingEventTypes")
+  void shouldShutDownWhenOwnedValidatorSlashed_SingleProcess_SinglePeer(
+      final ValidatorSlashingDetectionAcceptanceTest.SlashingEventType slashingEventType)
+      throws Exception {
+
+    final int genesisTime = timeProvider.getTimeInSeconds().plus(10).intValue();
+    final UInt64 altairEpoch = UInt64.valueOf(100);
+
+    final TekuNode tekuNode =
+        createTekuNode(
+            config ->
+                configureNode(config, genesisTime, network)
+                    .withAltairEpoch(altairEpoch)
+                    .withStopVcWhenValidatorSlashedEnabled()
+                    .withInteropValidators(0, 32));
+
+    tekuNode.start();
+
+    tekuNode.waitForEpochAtOrAbove(2);
+
+    final int slashedValidatorIndex = 3;
+    final BLSKeyPair slashedValidatorKeyPair = getBlsKeyPair(slashedValidatorIndex);
+    final int slotInFirstEpoch =
+        tekuNode.getSpec().forMilestone(SpecMilestone.ALTAIR).getSlotsPerEpoch() - 1;
+
+    postSlashing(
+        tekuNode,
+        UInt64.valueOf(slotInFirstEpoch),
+        UInt64.valueOf(slashedValidatorIndex),
+        slashedValidatorKeyPair.getSecretKey(),
+        slashingEventType);
+
+    tekuNode.waitForLogMessageContaining(
+        String.format(slashingActionLog, slashedValidatorKeyPair.getPublicKey().toHexString()));
+    tekuNode.waitForExit(shutdownWaitingSeconds);
+  }
+
+  @ParameterizedTest
+  @MethodSource("getSlashingEventTypes")
+  void shouldShutDownWhenOwnedValidatorSlashed_StandAloneVC_SinglePeer(
+      final SlashingEventType slashingEventType) throws Exception {
+
+    final int genesisTime = timeProvider.getTimeInSeconds().plus(10).intValue();
+    final UInt64 altairEpoch = UInt64.valueOf(100);
+
+    final TekuNode beaconNode =
+        createTekuNode(
+            config -> configureNode(config, genesisTime, network).withAltairEpoch(altairEpoch));
+
+    final TekuValidatorNode validatorClient =
+        createValidatorNode(
+            config ->
+                config
+                    .withNetwork("auto")
+                    .withStopVcWhenValidatorSlashedEnabled()
+                    .withInteropValidators(0, 32)
+                    .withBeaconNode(beaconNode));
+
+    beaconNode.start();
+    validatorClient.start();
+
+    beaconNode.waitForEpochAtOrAbove(2);
+
+    final int slashedValidatorIndex = 3;
+    final BLSKeyPair slashedValidatorKeyPair = getBlsKeyPair(slashedValidatorIndex);
+    final int slotInFirstEpoch =
+        beaconNode.getSpec().forMilestone(SpecMilestone.ALTAIR).getSlotsPerEpoch() - 1;
+
+    postSlashing(
+        beaconNode,
+        UInt64.valueOf(slotInFirstEpoch),
+        UInt64.valueOf(slashedValidatorIndex),
+        slashedValidatorKeyPair.getSecretKey(),
+        slashingEventType);
+
+    validatorClient.waitForLogMessageContaining(
+        String.format(slashingActionLog, slashedValidatorKeyPair.getPublicKey().toHexString()));
+    validatorClient.waitForExit(shutdownWaitingSeconds);
+    // Make sure the BN didn't shut down
+    beaconNode.waitForEpochAtOrAbove(4);
+    beaconNode.stop();
+  }
+}
