21aca063d6064afa350f464071d8fa04e4bba761
Dmitrii Shmatko <leodex23@gmail.com>
2024-02-13 20:08:35 +0400

    update message

--- infrastructure/logging/src/main/java/tech/pegasys/teku/infrastructure/logging/StatusLogger.java
+++ infrastructure/logging/src/main/java/tech/pegasys/teku/infrastructure/logging/StatusLogger.java
@@ -325,9 +325,9 @@ public class StatusLogger {
   public void errorIncompatibleInitialState(final UInt64 epoch) {
     log.error(
         "Cannot start with provided initial state for the epoch {}, "
-            + "checkpoint occurred on the empty slot, which is not yet supported. \n"
-            + "If you use remote checkpoint source, please, "
-            + "retry an epoch later (6 minutes for most networks).",
+            + "checkpoint occurred on the empty slot, which is not yet supported.\n"
+            + "If you are using remote checkpoint source, "
+            + "please wait next epoch finalization and retry.",
         epoch);
   }
 
