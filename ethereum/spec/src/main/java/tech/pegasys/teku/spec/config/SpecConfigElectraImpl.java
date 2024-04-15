23b37382b4965fe2790bc1b3435f8582884a98b1
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2024-04-15 14:36:22 +0200

    fix rebase conflicts

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/config/SpecConfigElectraImpl.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/config/SpecConfigElectraImpl.java
@@ -160,11 +160,6 @@ public class SpecConfigElectraImpl extends DelegatingSpecConfigDeneb implements 
   }
 
   @Override
-  public int getMaxValidatorsPerAttestation() {
-    return getMaxValidatorsPerCommittee() * getMaxCommitteesPerSlot();
-  }
-
-  @Override
   public Optional<SpecConfigElectra> toVersionElectra() {
     return Optional.of(this);
   }
