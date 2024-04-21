e12ab36a74a59bfa13a5faf101389f511c09311c
Gabriel Fukushima <gabrielfukushima@gmail.com>
2024-04-22 09:41:12 +1000

    rename variables after review
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/logic/common/helpers/Predicates.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/logic/common/helpers/Predicates.java
@@ -55,18 +55,18 @@ public class Predicates {
       final Bytes32 leaf,
       final SszBytes32Vector branch,
       final int depth,
-      final int index,
+      final int startingIndex,
       final Bytes32 root) {
     final Sha256 sha256 = getSha256Instance();
     Bytes32 value = leaf;
-    int indexCopy = index;
+    int index = startingIndex;
     for (int i = 0; i < depth; i++) {
-      if ((indexCopy & 1) == 1) {
+      if ((index & 1) == 1) {
         value = sha256.wrappedDigest(branch.getElement(i), value);
       } else {
         value = sha256.wrappedDigest(value, branch.getElement(i));
       }
-      indexCopy >>>= 1;
+      index >>>= 1;
     }
     return value.equals(root);
   }
