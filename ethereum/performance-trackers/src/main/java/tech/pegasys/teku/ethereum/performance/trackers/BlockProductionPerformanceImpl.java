fc7bc967d60baf6c2bdf602937a01d8a831c3f26
Stefan Bratanov <stefan.bratanov93@gmail.com>
2024-04-23 16:36:00 +0100

    Block Production\Publishing Performance threshold based on flow (#8242)

--- ethereum/performance-trackers/src/main/java/tech/pegasys/teku/ethereum/performance/trackers/BlockProductionPerformanceImpl.java
+++ ethereum/performance-trackers/src/main/java/tech/pegasys/teku/ethereum/performance/trackers/BlockProductionPerformanceImpl.java
@@ -13,24 +13,28 @@
 
 package tech.pegasys.teku.ethereum.performance.trackers;
 
+import java.util.Map;
 import tech.pegasys.teku.infrastructure.logging.EventLogger;
 import tech.pegasys.teku.infrastructure.time.PerformanceTracker;
 import tech.pegasys.teku.infrastructure.time.TimeProvider;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 
 public class BlockProductionPerformanceImpl implements BlockProductionPerformance {
+
   private final PerformanceTracker performanceTracker;
   private final UInt64 slot;
   private final UInt64 slotTime;
-  private final int lateThreshold;
+  private final Map<Flow, Integer> lateThresholds;
+
+  private volatile Flow flow = Flow.LOCAL;
 
   BlockProductionPerformanceImpl(
       final TimeProvider timeProvider,
       final UInt64 slot,
       final UInt64 slotTime,
-      final int lateThreshold) {
+      final Map<Flow, Integer> lateThresholds) {
     this.performanceTracker = new PerformanceTracker(timeProvider);
-    this.lateThreshold = lateThreshold;
+    this.lateThresholds = lateThresholds;
     this.slot = slot;
     this.slotTime = slotTime;
     performanceTracker.addEvent("start");
@@ -39,7 +43,8 @@ public class BlockProductionPerformanceImpl implements BlockProductionPerformanc
   @Override
   public void complete() {
     final UInt64 completionTime = performanceTracker.addEvent(COMPLETE_LABEL);
-    final boolean isLateEvent = completionTime.minusMinZero(slotTime).isGreaterThan(lateThreshold);
+    final boolean isLateEvent =
+        completionTime.minusMinZero(slotTime).isGreaterThan(lateThresholds.get(flow));
     performanceTracker.report(
         slotTime,
         isLateEvent,
@@ -82,6 +87,8 @@ public class BlockProductionPerformanceImpl implements BlockProductionPerformanc
   @Override
   public void builderGetHeader() {
     performanceTracker.addEvent("builder_get_header");
+    // set the flow to BUILDER when builderGetHeader has been called
+    flow = Flow.BUILDER;
   }
 
   @Override
