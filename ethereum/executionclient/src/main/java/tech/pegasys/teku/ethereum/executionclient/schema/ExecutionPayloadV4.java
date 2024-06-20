cc50fb12206112dc537e14140faee3807c074c86
Lucas Saldanha <lucascrsaldanha@gmail.com>
2024-06-21 11:08:49 +1200

    Correctly add new field to engine api object

--- ethereum/executionclient/src/main/java/tech/pegasys/teku/ethereum/executionclient/schema/ExecutionPayloadV4.java
+++ ethereum/executionclient/src/main/java/tech/pegasys/teku/ethereum/executionclient/schema/ExecutionPayloadV4.java
@@ -32,13 +32,16 @@ import tech.pegasys.teku.spec.datastructures.execution.ExecutionPayloadBuilder;
 import tech.pegasys.teku.spec.datastructures.execution.ExecutionPayloadSchema;
 import tech.pegasys.teku.spec.datastructures.execution.versions.capella.ExecutionPayloadCapella;
 import tech.pegasys.teku.spec.datastructures.execution.versions.deneb.ExecutionPayloadDeneb;
+import tech.pegasys.teku.spec.datastructures.execution.versions.electra.ConsolidationRequest;
 import tech.pegasys.teku.spec.datastructures.execution.versions.electra.DepositReceipt;
 import tech.pegasys.teku.spec.datastructures.execution.versions.electra.ExecutionLayerWithdrawalRequest;
 import tech.pegasys.teku.spec.datastructures.execution.versions.electra.ExecutionPayloadElectra;
 
 public class ExecutionPayloadV4 extends ExecutionPayloadV3 {
+
   public final List<DepositRequestV1> depositRequests;
   public final List<WithdrawalRequestV1> withdrawalRequests;
+  public final List<ConsolidationRequestV1> consolidationRequests;
 
   public ExecutionPayloadV4(
       final @JsonProperty("parentHash") Bytes32 parentHash,
@@ -59,7 +62,9 @@ public class ExecutionPayloadV4 extends ExecutionPayloadV3 {
       final @JsonProperty("blobGasUsed") UInt64 blobGasUsed,
       final @JsonProperty("excessBlobGas") UInt64 excessBlobGas,
       final @JsonProperty("depositRequests") List<DepositRequestV1> depositRequests,
-      final @JsonProperty("withdrawalRequests") List<WithdrawalRequestV1> withdrawalRequests) {
+      final @JsonProperty("withdrawalRequests") List<WithdrawalRequestV1> withdrawalRequests,
+      final @JsonProperty("consolidationRequests") List<ConsolidationRequestV1>
+              consolidationRequests) {
     super(
         parentHash,
         feeRecipient,
@@ -80,8 +85,10 @@ public class ExecutionPayloadV4 extends ExecutionPayloadV3 {
         excessBlobGas);
     checkNotNull(depositRequests, "depositRequests");
     checkNotNull(withdrawalRequests, "withdrawalRequests");
+    checkNotNull(consolidationRequests, "consolidationRequests");
     this.depositRequests = depositRequests;
     this.withdrawalRequests = withdrawalRequests;
+    this.consolidationRequests = consolidationRequests;
   }
 
   @Override
@@ -103,6 +110,14 @@ public class ExecutionPayloadV4 extends ExecutionPayloadV3 {
                         withdrawalRequestV1 ->
                             createInternalWithdrawalRequest(
                                 withdrawalRequestV1, executionPayloadSchema))
+                    .toList())
+        .consolidationRequests(
+            () ->
+                consolidationRequests.stream()
+                    .map(
+                        consolidationRequestV1 ->
+                            createInternalConsolidationRequest(
+                                consolidationRequestV1, executionPayloadSchema))
                     .toList());
   }
 
@@ -130,6 +145,17 @@ public class ExecutionPayloadV4 extends ExecutionPayloadV3 {
             withdrawalRequestV1.amount);
   }
 
+  private ConsolidationRequest createInternalConsolidationRequest(
+      final ConsolidationRequestV1 consolidationRequestV1,
+      final ExecutionPayloadSchema<?> executionPayloadSchema) {
+    return executionPayloadSchema
+        .getConsolidationSchemaRequired()
+        .create(
+            consolidationRequestV1.sourceAddress,
+            BLSPublicKey.fromBytesCompressed(consolidationRequestV1.sourcePubkey),
+            BLSPublicKey.fromBytesCompressed(consolidationRequestV1.targetPubkey));
+  }
+
   public static ExecutionPayloadV4 fromInternalExecutionPayload(
       final ExecutionPayload executionPayload) {
     return new ExecutionPayloadV4(
@@ -152,7 +178,9 @@ public class ExecutionPayloadV4 extends ExecutionPayloadV3 {
         ExecutionPayloadDeneb.required(executionPayload).getExcessBlobGas(),
         getDepositRequests(ExecutionPayloadElectra.required(executionPayload).getDepositReceipts()),
         getWithdrawalRequests(
-            ExecutionPayloadElectra.required(executionPayload).getWithdrawalRequests()));
+            ExecutionPayloadElectra.required(executionPayload).getWithdrawalRequests()),
+        getConsolidationRequests(
+            ExecutionPayloadElectra.required(executionPayload).getConsolidationRequests()));
   }
 
   public static List<DepositRequestV1> getDepositRequests(
@@ -181,6 +209,18 @@ public class ExecutionPayloadV4 extends ExecutionPayloadV3 {
         .toList();
   }
 
+  public static List<ConsolidationRequestV1> getConsolidationRequests(
+      final SszList<ConsolidationRequest> consolidationRequests) {
+    return consolidationRequests.stream()
+        .map(
+            consolidationRequest ->
+                new ConsolidationRequestV1(
+                    consolidationRequest.getSourceAddress(),
+                    consolidationRequest.getSourcePubkey().toBytesCompressed(),
+                    consolidationRequest.getTargetPubkey().toBytesCompressed()))
+        .toList();
+  }
+
   @Override
   public boolean equals(final Object o) {
     if (this == o) {
@@ -194,12 +234,14 @@ public class ExecutionPayloadV4 extends ExecutionPayloadV3 {
     }
     final ExecutionPayloadV4 that = (ExecutionPayloadV4) o;
     return Objects.equals(depositRequests, that.depositRequests)
-        && Objects.equals(withdrawalRequests, that.withdrawalRequests);
+        && Objects.equals(withdrawalRequests, that.withdrawalRequests)
+        && Objects.equals(consolidationRequests, that.consolidationRequests);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(super.hashCode(), depositRequests, withdrawalRequests);
+    return Objects.hash(
+        super.hashCode(), depositRequests, withdrawalRequests, consolidationRequests);
   }
 
   @Override
@@ -224,6 +266,7 @@ public class ExecutionPayloadV4 extends ExecutionPayloadV3 {
         .add("excessBlobGas", excessBlobGas)
         .add("depositRequests", depositRequests)
         .add("withdrawalRequests", withdrawalRequests)
+        .add("consolidationRequests", consolidationRequests)
         .toString();
   }
 }
