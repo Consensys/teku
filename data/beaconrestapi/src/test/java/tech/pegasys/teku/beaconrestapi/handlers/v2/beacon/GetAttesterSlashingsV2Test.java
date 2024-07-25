867564cea70dd6a4b69dba048faba00857dbbcb3
Gabriel Fukushima <gabrielfukushima@gmail.com>
2024-07-25 10:09:41 +1000

    added 200 test for phase0
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v2/beacon/GetAttesterSlashingsV2Test.java
+++ data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v2/beacon/GetAttesterSlashingsV2Test.java
@@ -13,20 +13,45 @@
 
 package tech.pegasys.teku.beaconrestapi.handlers.v2.beacon;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_BAD_REQUEST;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_INTERNAL_SERVER_ERROR;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_OK;
+import static tech.pegasys.teku.infrastructure.restapi.MetadataTestUtil.getResponseStringFromMetadata;
 import static tech.pegasys.teku.infrastructure.restapi.MetadataTestUtil.verifyMetadataErrorResponse;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.common.io.Resources;
+import java.io.IOException;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tech.pegasys.teku.beaconrestapi.AbstractMigratedBeaconHandlerTest;
+import tech.pegasys.teku.spec.datastructures.metadata.ObjectAndMetaData;
+import tech.pegasys.teku.spec.datastructures.operations.AttesterSlashing;
 
-class GetAttesterSlashingV2Test extends AbstractMigratedBeaconHandlerTest {
+class GetAttesterSlashingsV2Test extends AbstractMigratedBeaconHandlerTest {
 
   @BeforeEach
   void setUp() {
     setHandler(new GetAttesterSlashingsV2(nodeDataProvider, schemaDefinitionCache));
+  }
+
+  @Test
+  void metadata_shouldHandle200_phase0() throws IOException {
+    final ObjectAndMetaData<List<AttesterSlashing>> responseData =
+        withMetaData(
+            List.of(
+                dataStructureUtil.randomAttesterSlashing(),
+                dataStructureUtil.randomAttesterSlashing()));
+
+    final String data = getResponseStringFromMetadata(handler, SC_OK, responseData);
+    final String expected =
+        Resources.toString(
+            Resources.getResource(GetAttesterSlashingsV2Test.class, "getAttesterSlashings.json"),
+            UTF_8);
+    assertThat(data).isEqualTo(expected);
   }
 
   @Test
