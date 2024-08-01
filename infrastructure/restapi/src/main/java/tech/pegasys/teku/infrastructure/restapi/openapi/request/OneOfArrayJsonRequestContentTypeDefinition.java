38d2635da614c50c8d1051f3a909177ad8818951
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2024-08-01 10:34:28 +0200

    Add PostAttestationV2 API (#8448)
    * wip array type definition
    
    * tests added, fixed error
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>
    
    * Add post attestation v2 interface
    
    * add missing final
    
    * rename json files
    
    * refactoring
    
    * add integration tests
    
    * refactoring
    
    ---------
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>
    Co-authored-by: Paul Harris <paul.harris@consensys.net>

---
+++ infrastructure/restapi/src/main/java/tech/pegasys/teku/infrastructure/restapi/openapi/request/OneOfArrayJsonRequestContentTypeDefinition.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright Consensys Software Inc., 2024
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.infrastructure.restapi.openapi.request;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.io.IOUtils;
+import tech.pegasys.teku.infrastructure.json.JsonUtil;
+import tech.pegasys.teku.infrastructure.json.exceptions.MissingRequestBodyException;
+import tech.pegasys.teku.infrastructure.json.types.DelegatingOpenApiTypeDefinition;
+import tech.pegasys.teku.infrastructure.json.types.DeserializableTypeDefinition;
+import tech.pegasys.teku.infrastructure.json.types.SerializableTypeDefinition;
+
+public class OneOfArrayJsonRequestContentTypeDefinition<T> extends DelegatingOpenApiTypeDefinition
+    implements RequestContentTypeDefinition<List<? extends T>> {
+
+  private final BodyTypeSelector<T> bodyTypeSelector;
+
+  public OneOfArrayJsonRequestContentTypeDefinition(
+      final SerializableTypeDefinition<List<T>> typeDefinition,
+      final BodyTypeSelector<T> bodyTypeSelector) {
+    super(typeDefinition);
+    this.bodyTypeSelector = bodyTypeSelector;
+  }
+
+  @Override
+  public List<? extends T> deserialize(final InputStream in) throws IOException {
+    return deserialize(in, Map.of());
+  }
+
+  @Override
+  public List<? extends T> deserialize(final InputStream in, final Map<String, String> headers)
+      throws IOException {
+    final String json = IOUtils.toString(in, StandardCharsets.UTF_8);
+    final DeserializableTypeDefinition<? extends T> type =
+        bodyTypeSelector.selectType(new BodyTypeSelectorContext(json, headers));
+
+    if (type == null) {
+      throw new MissingRequestBodyException();
+    }
+
+    if (!openApiTypeDefinition.getReferencedTypeDefinitions().contains(type)) {
+      throw new IllegalStateException(
+          "Schema determined for parsing request body is not listed in requestBodyTypes");
+    }
+    return JsonUtil.parse(json, DeserializableTypeDefinition.listOf(type));
+  }
+
+  public interface BodyTypeSelector<T> {
+    DeserializableTypeDefinition<? extends T> selectType(final BodyTypeSelectorContext context);
+  }
+
+  public static class BodyTypeSelectorContext {
+    final String body;
+    final Map<String, String> headers;
+
+    public BodyTypeSelectorContext(final String body, final Map<String, String> headers) {
+      this.body = body;
+      this.headers = headers;
+    }
+
+    public String getBody() {
+      return body;
+    }
+
+    public Map<String, String> getHeaders() {
+      return headers;
+    }
+  }
+}
