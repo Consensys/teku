8974fdba1f1182a9dd34611553562d5963dfffad
Lucas Saldanha <lucascrsaldanha@gmail.com>
2024-08-30 08:58:55 +1200

    Running post-bellatrix merged network ATs with develop Besu docker

--- acceptance-tests/src/acceptance-test/java/tech/pegasys/teku/test/acceptance/ExecutionLayerTriggeredExitAcceptanceTest.java
+++ acceptance-tests/src/acceptance-test/java/tech/pegasys/teku/test/acceptance/ExecutionLayerTriggeredExitAcceptanceTest.java
@@ -16,7 +16,6 @@ package tech.pegasys.teku.test.acceptance;
 import com.google.common.io.Resources;
 import java.net.URL;
 import java.util.Map;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import tech.pegasys.teku.bls.BLSPublicKey;
 import tech.pegasys.teku.ethereum.execution.types.Eth1Address;
@@ -38,7 +37,6 @@ public class ExecutionLayerTriggeredExitAcceptanceTest extends AcceptanceTestBas
   private static final URL JWT_FILE = Resources.getResource("auth/ee-jwt-secret.hex");
 
   @Test
-  @Disabled("Waiting for Besu 24.9.0 release (https://github.com/Consensys/teku/issues/8535)")
   void triggerValidatorExitWithFullWithdrawal() throws Exception {
     final UInt64 currentTime = new SystemTimeProvider().getTimeInSeconds();
     final int genesisTime =
@@ -91,7 +89,8 @@ public class ExecutionLayerTriggeredExitAcceptanceTest extends AcceptanceTestBas
     final Map<String, String> genesisOverrides = Map.of("pragueTime", String.valueOf(genesisTime));
 
     return createBesuNode(
-        BesuDockerVersion.STABLE,
+        // "Waiting for Besu 24.9.0 release (https://github.com/Consensys/teku/issues/8535)"
+        BesuDockerVersion.DEVELOP,
         config ->
             config
                 .withMergeSupport()
