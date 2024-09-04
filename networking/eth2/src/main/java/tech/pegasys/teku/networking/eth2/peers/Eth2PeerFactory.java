15f91e49f1f7a8ec30ebe822d5e719f38a95d8a0
Stefan Bratanov <stefan.bratanov93@gmail.com>
2024-09-04 20:11:02 +0300

    Implement ExecutionPayloadEnvelopesByRoot RPC method (#8563)

--- networking/eth2/src/main/java/tech/pegasys/teku/networking/eth2/peers/Eth2PeerFactory.java
+++ networking/eth2/src/main/java/tech/pegasys/teku/networking/eth2/peers/Eth2PeerFactory.java
@@ -77,6 +77,7 @@ public class Eth2PeerFactory {
         RateTracker.create(peerRateLimit, TIME_OUT, timeProvider),
         RateTracker.create(
             peerRateLimit * spec.getMaxBlobsPerBlock().orElse(1), TIME_OUT, timeProvider),
+        RateTracker.create(peerRateLimit, TIME_OUT, timeProvider),
         RateTracker.create(peerRequestLimit, TIME_OUT, timeProvider),
         kzg);
   }
