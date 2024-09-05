3e6ea16b6ee7a4077e202cb1194a7b6f0854e88e
Stefan Bratanov <stefan.bratanov93@gmail.com>
2024-09-05 17:21:50 +0300

    Improve the p2p bid pool

--- ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/block/BlockImporter.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/block/BlockImporter.java
@@ -29,6 +29,7 @@ import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.spec.Spec;
 import tech.pegasys.teku.spec.datastructures.blocks.SignedBeaconBlock;
 import tech.pegasys.teku.spec.datastructures.blocks.blockbody.BeaconBlockBody;
+import tech.pegasys.teku.spec.datastructures.execution.SignedExecutionPayloadHeader;
 import tech.pegasys.teku.spec.datastructures.forkchoice.ReadOnlyForkChoiceStrategy;
 import tech.pegasys.teku.spec.datastructures.operations.Attestation;
 import tech.pegasys.teku.spec.datastructures.operations.AttesterSlashing;
@@ -62,6 +63,8 @@ public class BlockImporter {
       voluntaryExitSubscribers = Subscribers.create(true);
   private final Subscribers<VerifiedBlockOperationsListener<SignedBlsToExecutionChange>>
       blsToExecutionChangeSubscribers = Subscribers.create(true);
+  private final Subscribers<VerifiedBlockExecutionPayloadHeaderListener>
+      executionPayloadHeaderSubscribers = Subscribers.create(true);
 
   private final AtomicReference<CheckpointState> latestFinalizedCheckpointState =
       new AtomicReference<>(null);
@@ -213,6 +216,13 @@ public class BlockImporter {
             blsOperations ->
                 blsToExecutionChangeSubscribers.deliver(
                     VerifiedBlockOperationsListener::onOperationsFromBlock, blsOperations));
+    blockBody
+        .getOptionalSignedExecutionPayloadHeader()
+        .ifPresent(
+            signedExecutionPayloadHeader ->
+                executionPayloadHeaderSubscribers.deliver(
+                    VerifiedBlockExecutionPayloadHeaderListener::onOperationFromBlock,
+                    signedExecutionPayloadHeader));
   }
 
   public void subscribeToVerifiedBlockAttestations(
@@ -244,6 +254,12 @@ public class BlockImporter {
     blsToExecutionChangeSubscribers.subscribe(verifiedBlockBlsToExecutionChangeListener);
   }
 
+  public void subscribeToVerifiedExecutionPayloadHeader(
+      final VerifiedBlockExecutionPayloadHeaderListener
+          verifiedBlockExecutionPayloadHeaderListener) {
+    executionPayloadHeaderSubscribers.subscribe(verifiedBlockExecutionPayloadHeaderListener);
+  }
+
   private String getBlockContent(final SignedBeaconBlock block) {
     return block.sszSerialize().toHexString();
   }
@@ -259,5 +275,9 @@ public class BlockImporter {
 
   public interface VerifiedBlockAttestationListener {
     void onOperationsFromBlock(UInt64 slot, SszList<Attestation> attestations);
+  }
+
+  public interface VerifiedBlockExecutionPayloadHeaderListener {
+    void onOperationFromBlock(SignedExecutionPayloadHeader header);
   }
 }
--- ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/execution/ExecutionPayloadHeaderPool.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/execution/ExecutionPayloadHeaderPool.java
@@ -13,12 +13,8 @@
 
 package tech.pegasys.teku.statetransition.execution;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.Predicate;
 import tech.pegasys.teku.infrastructure.async.SafeFuture;
 import tech.pegasys.teku.infrastructure.collections.LimitedSet;
 import tech.pegasys.teku.infrastructure.subscribers.Subscribers;
@@ -26,85 +22,56 @@ import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.spec.datastructures.execution.SignedExecutionPayloadHeader;
 import tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconState;
 import tech.pegasys.teku.statetransition.OperationAddedSubscriber;
-import tech.pegasys.teku.statetransition.SingleItemOperationPool;
 import tech.pegasys.teku.statetransition.validation.InternalValidationResult;
 import tech.pegasys.teku.statetransition.validation.OperationValidator;
 import tech.pegasys.teku.statetransition.validation.ValidationResultCode;
 
-public class ExecutionPayloadHeaderPool
-    implements SingleItemOperationPool<SignedExecutionPayloadHeader> {
+public class ExecutionPayloadHeaderPool {
 
-  private static final int DEFAULT_SIGNED_BIDS_POOL_SIZE = 1000;
+  private static final int DEFAULT_SIGNED_BIDS_POOL_SIZE = 64;
 
   private final Set<SignedExecutionPayloadHeader> signedBids =
       LimitedSet.createSynchronizedIterable(DEFAULT_SIGNED_BIDS_POOL_SIZE);
 
   private final OperationValidator<SignedExecutionPayloadHeader> operationValidator;
-  private final Optional<Comparator<SignedExecutionPayloadHeader>> priorityOrderComparator;
 
   private final Subscribers<OperationAddedSubscriber<SignedExecutionPayloadHeader>> subscribers =
       Subscribers.create(true);
 
   public ExecutionPayloadHeaderPool(
-      final OperationValidator<SignedExecutionPayloadHeader> operationValidator,
-      final Optional<Comparator<SignedExecutionPayloadHeader>> priorityOrderComparator) {
+      final OperationValidator<SignedExecutionPayloadHeader> operationValidator) {
     this.operationValidator = operationValidator;
-    this.priorityOrderComparator = priorityOrderComparator;
   }
 
-  @Override
   public void subscribeOperationAdded(
       final OperationAddedSubscriber<SignedExecutionPayloadHeader> subscriber) {
     this.subscribers.subscribe(subscriber);
   }
 
-  @Override
-  public SignedExecutionPayloadHeader getItemForBlock(
-      final BeaconState stateAtBlockSlot, final Predicate<SignedExecutionPayloadHeader> filter) {
-    final List<SignedExecutionPayloadHeader> sortedBids =
-        priorityOrderComparator
-            .map(comparator -> signedBids.stream().sorted(comparator))
-            .orElseGet(signedBids::stream)
-            .toList();
-    for (SignedExecutionPayloadHeader bid : sortedBids) {
-      if (!filter.test(bid)) {
-        continue;
-      }
+  public Optional<SignedExecutionPayloadHeader> selectBidForBlock(
+      final BeaconState stateAtBlockSlot) {
+    for (SignedExecutionPayloadHeader bid : signedBids) {
       if (operationValidator.validateForBlockInclusion(stateAtBlockSlot, bid).isEmpty()) {
-        return bid;
+        return Optional.of(bid);
       } else {
         // The item is no longer valid to be included in a block so remove it from the pool.
         remove(bid);
       }
     }
-    throw new IllegalStateException(
-        "Couldn't find a valid signed bid in the pool for inclusion in a block");
+    return Optional.empty();
   }
 
-  @Override
   public SafeFuture<InternalValidationResult> addLocal(final SignedExecutionPayloadHeader item) {
     return add(item, false);
   }
 
-  @Override
   public SafeFuture<InternalValidationResult> addRemote(
       final SignedExecutionPayloadHeader item, final Optional<UInt64> arrivalTimestamp) {
     return add(item, true);
   }
 
-  @Override
   public void remove(final SignedExecutionPayloadHeader item) {
     signedBids.remove(item);
-  }
-
-  @Override
-  public Set<SignedExecutionPayloadHeader> getAll() {
-    return Collections.unmodifiableSet(signedBids);
-  }
-
-  @Override
-  public int size() {
-    return signedBids.size();
   }
 
   private SafeFuture<InternalValidationResult> add(
