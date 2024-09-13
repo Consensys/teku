36e0864c9648638d98acf6228d9541a95366e56f
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2024-09-13 23:12:32 +0200

    add payload attestation aggregation (#8599)

--- ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/attestation/AggregatingAttestationPool.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/attestation/AggregatingAttestationPool.java
@@ -193,8 +193,8 @@ public class AggregatingAttestationPool implements SlotEventsChannel {
     if (!dataHashesToRemove.isEmpty()) {
       LOG.trace(
           "firstValidAttestationSlot: {}, removing: {}",
-          () -> firstValidAttestationSlot,
-          dataHashesToRemove::size);
+          firstValidAttestationSlot.longValue(),
+          dataHashesToRemove.size());
     }
     dataHashesToRemove.clear();
   }
