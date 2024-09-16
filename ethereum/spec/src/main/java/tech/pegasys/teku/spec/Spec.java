ac07c9bd8ed93efa8307f97a86687b7f8245ece3
Stefan Bratanov <stefan.bratanov93@gmail.com>
2024-09-16 16:56:28 +0300

    [ePBS] "Naive" block production in VC (and fixed blobs creation in BN) (#8602)

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/Spec.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/Spec.java
@@ -935,9 +935,9 @@ public class Spec {
         .getValidatorIndexToCommitteeAssignmentMap(state, epoch);
   }
 
-  public Int2ObjectMap<UInt64> getValidatorIndexToPctAssignmentMap(
+  public Int2ObjectMap<UInt64> getValidatorIndexToPtcAssignmentMap(
       final BeaconState state, final UInt64 epoch) {
-    return atEpoch(epoch).getValidatorsUtil().getValidatorIndexToPctAssignmentMap(state, epoch);
+    return atEpoch(epoch).getValidatorsUtil().getValidatorIndexToPtcAssignmentMap(state, epoch);
   }
 
   // Attestation helpers
