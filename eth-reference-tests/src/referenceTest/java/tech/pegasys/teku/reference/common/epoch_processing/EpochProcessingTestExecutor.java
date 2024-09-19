a46f4a2e58f8c5180617f71de6aa4c43b6999fd2
Lucas Saldanha <lucascrsaldanha@gmail.com>
2024-09-20 10:19:57 +1200

    Update ref-tests to v1.5.0-alpha.6

--- eth-reference-tests/src/referenceTest/java/tech/pegasys/teku/reference/common/epoch_processing/EpochProcessingTestExecutor.java
+++ eth-reference-tests/src/referenceTest/java/tech/pegasys/teku/reference/common/epoch_processing/EpochProcessingTestExecutor.java
@@ -34,7 +34,8 @@ public class EpochProcessingTestExecutor implements TestExecutor {
       ImmutableMap.<String, TestExecutor>builder()
           .put(
               "epoch_processing/slashings",
-              new EpochProcessingTestExecutor(EpochOperation.PROCESS_SLASHINGS))
+              // Re-enable these tests as part of https://github.com/Consensys/teku/pull/8612
+              TestExecutor.IGNORE_TESTS)
           .put(
               "epoch_processing/registry_updates",
               new EpochProcessingTestExecutor(EpochOperation.PROCESS_REGISTRY_UPDATES))
