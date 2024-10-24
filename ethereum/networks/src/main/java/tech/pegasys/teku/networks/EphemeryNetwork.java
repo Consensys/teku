186fbb22f340e569ca0877404d77d380d0b68056
Paul Harris <paul.harris@consensys.net>
2024-10-24 11:57:34 +1000

    cleanup EphemeryNetwork and add max slot constant
    fixes #8777
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- ethereum/networks/src/main/java/tech/pegasys/teku/networks/EphemeryNetwork.java
+++ ethereum/networks/src/main/java/tech/pegasys/teku/networks/EphemeryNetwork.java
@@ -21,17 +21,17 @@ import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.spec.config.SpecConfig;
 import tech.pegasys.teku.spec.config.SpecConfigLoader;
 import tech.pegasys.teku.spec.config.builder.SpecConfigBuilder;
-import tech.pegasys.teku.spec.networks.Eth2Network;
 
 public class EphemeryNetwork {
   private static final long GENESIS_CHAINID = 39438135;
-  private static final long GENESIS_TIMESTAMP = 1720119600;
+  private static final long INITIAL_GENESIS_TIMESTAMP = 1720119600;
   private static final int PERIOD = 28;
   private static final long PERIOD_IN_SECONDS = (PERIOD * 24 * 60 * 60);
+  public static final long MAX_EPHEMERY_SLOT = (PERIOD_IN_SECONDS / 12) - 1;
 
   static long getPeriodsSinceGenesis(final TimeProvider timeProvider) {
     return ChronoUnit.DAYS.between(
-            Instant.ofEpochSecond(GENESIS_TIMESTAMP),
+            Instant.ofEpochSecond(INITIAL_GENESIS_TIMESTAMP),
             Instant.ofEpochMilli(timeProvider.getTimeInMillis().longValue()))
         / PERIOD;
   }
@@ -43,23 +43,19 @@ public class EphemeryNetwork {
   static void updateConfig(final SpecConfigBuilder builder, final TimeProvider timeProvider) {
     final SpecConfig config = SpecConfigLoader.loadConfig("ephemery");
     final SpecConfigBuilder rawConfigBuilder = builder.rawConfig(config.getRawConfig());
+    final long periodsSinceInitialGenesis = getPeriodsSinceGenesis(timeProvider);
 
-    if (Eth2Network.EPHEMERY.configName().equals("ephemery")) {
-      final long currentTimestamp = timeProvider.getTimeInMillis().longValue() / 1000;
-
-      final long updatedTimestamp =
-          GENESIS_TIMESTAMP + (getPeriodsSinceGenesis(timeProvider) * PERIOD_IN_SECONDS);
-      final long updatedChainId = GENESIS_CHAINID + getPeriodsSinceGenesis(timeProvider);
-
-      try {
-        if (currentTimestamp > (GENESIS_TIMESTAMP + PERIOD_IN_SECONDS)) {
-          rawConfigBuilder.depositNetworkId(updatedChainId);
-          rawConfigBuilder.depositChainId(updatedChainId);
-          rawConfigBuilder.minGenesisTime(UInt64.valueOf(updatedTimestamp));
-        }
-      } catch (RuntimeException e) {
-        throw new RuntimeException("Error updating genesis file: " + e.getMessage(), e);
+    try {
+      if (periodsSinceInitialGenesis > 0L) {
+        final long updatedChainId = GENESIS_CHAINID + periodsSinceInitialGenesis;
+        final long currentPeriodGenesis =
+            INITIAL_GENESIS_TIMESTAMP + (periodsSinceInitialGenesis * PERIOD_IN_SECONDS);
+        rawConfigBuilder.depositNetworkId(updatedChainId);
+        rawConfigBuilder.depositChainId(updatedChainId);
+        rawConfigBuilder.minGenesisTime(UInt64.valueOf(currentPeriodGenesis));
       }
+    } catch (RuntimeException e) {
+      throw new RuntimeException("Error updating genesis file: " + e.getMessage(), e);
     }
   }
 }
