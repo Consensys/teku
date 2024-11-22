164982ef5d77fa4fb168f5adf0b4dcbdd095f668
Enrico Del Fante <enrico.delfante@consensys.net>
2024-11-22 11:31:48 +0100

    mark locally fetched blobs as validated

--- ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/util/BlockBlobSidecarsTrackersPoolImpl.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/util/BlockBlobSidecarsTrackersPoolImpl.java
@@ -722,6 +722,10 @@ public class BlockBlobSidecarsTrackersPoolImpl extends AbstractIgnoringFutureHis
                         beaconBlockBodyDeneb,
                         signedBeaconBlockHeader);
 
+                blobSidecar.markSignatureAsValidated();
+                // assume kzg validation done by local EL
+                blobSidecar.markKzgAndInclusionProofAsValidated();
+
                 onNewBlobSidecar(blobSidecar, LOCAL_EL);
               }
             });
