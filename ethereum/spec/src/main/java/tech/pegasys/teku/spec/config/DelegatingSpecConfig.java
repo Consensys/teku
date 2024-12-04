78bf8178cc77278b37428607a5512b2c046182a1
Stefan Bratanov <stefan.bratanov93@gmail.com>
2024-12-04 16:08:56 +0000

    remove constants

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/config/DelegatingSpecConfig.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/config/DelegatingSpecConfig.java
@@ -328,16 +328,6 @@ public class DelegatingSpecConfig implements SpecConfig {
   }
 
   @Override
-  public int getTtfbTimeout() {
-    return specConfig.getTtfbTimeout();
-  }
-
-  @Override
-  public int getRespTimeout() {
-    return specConfig.getRespTimeout();
-  }
-
-  @Override
   public int getAttestationPropagationSlotRange() {
     return specConfig.getAttestationPropagationSlotRange();
   }
