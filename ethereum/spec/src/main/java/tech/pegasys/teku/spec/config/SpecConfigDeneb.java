f6875c895b493c8c5accdfb338d5da4337f9b471
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2024-12-17 10:34:27 +0100

    Add blobs increase params (#8883)
    * add blobs increase params
    
    Co-authored-by: Lucas Saldanha <lucas.saldanha@consensys.net>

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/config/SpecConfigDeneb.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/config/SpecConfigDeneb.java
@@ -54,8 +54,6 @@ public interface SpecConfigDeneb extends SpecConfigCapella, NetworkingSpecConfig
 
   int getMaxBlobsPerBlock();
 
-  int getTargetBlobsPerBlock();
-
   int getKzgCommitmentInclusionProofDepth();
 
   int getEpochsStoreBlobs();
