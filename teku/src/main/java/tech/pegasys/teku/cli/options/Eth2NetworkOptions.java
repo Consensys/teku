1eab8496155cdc1f2319c8d9cbe1819cd6ed1045
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2025-01-09 18:27:50 +0100

    8944 focil spec config (#8978)
    * fix acceptance tests

--- teku/src/main/java/tech/pegasys/teku/cli/options/Eth2NetworkOptions.java
+++ teku/src/main/java/tech/pegasys/teku/cli/options/Eth2NetworkOptions.java
@@ -159,6 +159,14 @@ public class Eth2NetworkOptions {
   private UInt64 electraForkEpoch;
 
   @Option(
+      names = {"--Xnetwork-eip7805-fork-epoch"},
+      hidden = true,
+      paramLabel = "<epoch>",
+      description = "Override the eip7805 fork activation epoch.",
+      arity = "1")
+  private UInt64 eip7805ForkEpoch;
+
+  @Option(
       names = {"--Xnetwork-total-terminal-difficulty-override"},
       hidden = true,
       paramLabel = "<uint256>",
@@ -330,6 +338,9 @@ public class Eth2NetworkOptions {
     }
     if (electraForkEpoch != null) {
       builder.electraForkEpoch(electraForkEpoch);
+    }
+    if (eip7805ForkEpoch != null) {
+      builder.eip7805ForkEpoch(eip7805ForkEpoch);
     }
     if (totalTerminalDifficultyOverride != null) {
       builder.totalTerminalDifficultyOverride(totalTerminalDifficultyOverride);
