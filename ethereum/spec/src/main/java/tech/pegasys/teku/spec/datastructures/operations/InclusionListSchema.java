8578b2577da51a2332e8b66a2ef3683b8b11d1d6
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2025-01-13 17:32:55 +0100

    add property tests (#8989)

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/datastructures/operations/InclusionListSchema.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/datastructures/operations/InclusionListSchema.java
@@ -13,13 +13,17 @@
 
 package tech.pegasys.teku.spec.datastructures.operations;
 
+import java.util.List;
+import org.apache.tuweni.bytes.Bytes32;
 import tech.pegasys.teku.infrastructure.ssz.SszList;
 import tech.pegasys.teku.infrastructure.ssz.containers.ContainerSchema4;
 import tech.pegasys.teku.infrastructure.ssz.primitive.SszBytes32;
 import tech.pegasys.teku.infrastructure.ssz.primitive.SszUInt64;
+import tech.pegasys.teku.infrastructure.ssz.schema.SszFieldName;
 import tech.pegasys.teku.infrastructure.ssz.schema.SszListSchema;
 import tech.pegasys.teku.infrastructure.ssz.schema.SszPrimitiveSchemas;
 import tech.pegasys.teku.infrastructure.ssz.tree.TreeNode;
+import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.spec.config.SpecConfigEip7805;
 import tech.pegasys.teku.spec.datastructures.execution.Transaction;
 import tech.pegasys.teku.spec.datastructures.execution.TransactionSchema;
@@ -28,6 +32,8 @@ public class InclusionListSchema
     extends ContainerSchema4<
         InclusionList, SszUInt64, SszUInt64, SszBytes32, SszList<Transaction>> {
 
+  static final SszFieldName FIELD_TRANSACTIONS = () -> "transactions";
+
   public InclusionListSchema(final SpecConfigEip7805 specConfigEip7805) {
     super(
         "InclusionList",
@@ -35,14 +41,27 @@ public class InclusionListSchema
         namedSchema("validator_index", SszPrimitiveSchemas.UINT64_SCHEMA),
         namedSchema("inclusion_list_committee_root", SszPrimitiveSchemas.BYTES32_SCHEMA),
         namedSchema(
-            "transactions",
+            FIELD_TRANSACTIONS,
             SszListSchema.create(
                 new TransactionSchema(specConfigEip7805),
                 specConfigEip7805.getMaxTransactionsPerInclusionList())));
   }
 
+  public InclusionList create(
+      final UInt64 slot,
+      final UInt64 validatorIndex,
+      final Bytes32 inclusionListCommitteeRoot,
+      final List<Transaction> transactions) {
+    return new InclusionList(this, slot, validatorIndex, inclusionListCommitteeRoot, transactions);
+  }
+
   @Override
   public InclusionList createFromBackingNode(final TreeNode node) {
     return new InclusionList(this, node);
+  }
+
+  @SuppressWarnings("unchecked")
+  public SszListSchema<Transaction, ?> getTransactionsSchema() {
+    return (SszListSchema<Transaction, ?>) getChildSchema(getFieldIndex(FIELD_TRANSACTIONS));
   }
 }
