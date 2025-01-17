18a328642aeb85430cd39e346c26a6b923d26d11
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2025-01-17 18:02:14 +0100

    8934 inclusion list pool (#9013)
    * add inclusion list pool skeleton

--- data/provider/src/test/java/tech/pegasys/teku/api/NodeDataProviderTest.java
+++ data/provider/src/test/java/tech/pegasys/teku/api/NodeDataProviderTest.java
@@ -44,6 +44,7 @@ import tech.pegasys.teku.statetransition.attestation.AttestationManager;
 import tech.pegasys.teku.statetransition.blobs.BlockBlobSidecarsTrackersPool;
 import tech.pegasys.teku.statetransition.forkchoice.ForkChoiceNotifier;
 import tech.pegasys.teku.statetransition.forkchoice.ProposersDataManager;
+import tech.pegasys.teku.statetransition.inclusionlist.InclusionListPool;
 import tech.pegasys.teku.statetransition.synccommittee.SyncCommitteeContributionPool;
 import tech.pegasys.teku.statetransition.validation.InternalValidationResult;
 import tech.pegasys.teku.statetransition.validatorcache.ActiveValidatorChannel;
@@ -55,6 +56,7 @@ public class NodeDataProviderTest {
   private final Spec spec = TestSpecFactory.createMinimalCapella();
   private final DataStructureUtil dataStructureUtil = new DataStructureUtil(spec);
   private final AggregatingAttestationPool attestationPool = mock(AggregatingAttestationPool.class);
+  private final InclusionListPool inclusionListPool = mock(InclusionListPool.class);
   private final BlockBlobSidecarsTrackersPool blockBlobSidecarsTrackersPool =
       mock(BlockBlobSidecarsTrackersPool.class);
   private final AttestationManager attestationManager = mock(AttestationManager.class);
@@ -81,6 +83,7 @@ public class NodeDataProviderTest {
     provider =
         new NodeDataProvider(
             attestationPool,
+            inclusionListPool,
             attesterSlashingPool,
             proposerSlashingPool,
             voluntaryExitPool,
@@ -204,6 +207,7 @@ public class NodeDataProviderTest {
     provider =
         new NodeDataProvider(
             attestationPool,
+            inclusionListPool,
             attesterSlashingPool,
             proposerSlashingPool,
             voluntaryExitPool,
