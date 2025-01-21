a90192b7b3cc7d511d97ae4ce11859ba1c3a8075
Gabriel Fukushima <gabrielfukushima@gmail.com>
2025-01-21 16:28:53 +1000

    spotless
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- storage/src/main/java/tech/pegasys/teku/storage/server/rocksdb/RocksDbInstance.java
+++ storage/src/main/java/tech/pegasys/teku/storage/server/rocksdb/RocksDbInstance.java
@@ -25,7 +25,6 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.tuweni.bytes.Bytes;
