53a0e0087c4ccf415d4103bb754432d5a0c7f9c0
Paul Harris <paul.harris@consensys.net>
2025-01-23 12:33:39 +1000

    libp2p gossip size setting
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- networking/p2p/src/test/java/tech/pegasys/teku/networking/p2p/libp2p/config/LibP2PParamsFactoryTest.java
+++ networking/p2p/src/test/java/tech/pegasys/teku/networking/p2p/libp2p/config/LibP2PParamsFactoryTest.java
@@ -43,7 +43,7 @@ public class LibP2PParamsFactoryTest {
   public void createGossipParams_setGossipMaxSizeFromNetworkSpecConfig() {
     final GossipConfig gossipConfig = GossipConfig.builder().build();
     final NetworkingSpecConfig networkingSpecConfig = spy(spec.getNetworkingConfig());
-    final int expectedGossipMaxSize = 10485760;
+    final int expectedGossipMaxSize = 12234442;
     reset(networkingSpecConfig);
 
     final GossipParams gossipParams =
