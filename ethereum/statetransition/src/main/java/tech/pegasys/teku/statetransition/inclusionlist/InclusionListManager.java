c7de223c244370e1cdeaba0d2e22402e29cdb318
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2025-01-24 18:29:13 +0100

    add inclusion list manager

---
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/inclusionlist/InclusionListManager.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright Consensys Software Inc., 2025
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.statetransition.inclusionlist;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import tech.pegasys.teku.ethereum.events.SlotEventsChannel;
+import tech.pegasys.teku.infrastructure.async.SafeFuture;
+import tech.pegasys.teku.infrastructure.unsigned.UInt64;
+import tech.pegasys.teku.spec.datastructures.operations.SignedInclusionList;
+import tech.pegasys.teku.statetransition.validation.InternalValidationResult;
+import tech.pegasys.teku.statetransition.validation.SignedInclusionListValidator;
+
+public class InclusionListManager implements SlotEventsChannel {
+
+  private final SignedInclusionListValidator signedInclusionListValidator;
+
+  private final Map<UInt64, List<SignedInclusionList>> validatorIndexToInclusionLists =
+      new HashMap<>();
+
+  public InclusionListManager(final SignedInclusionListValidator signedInclusionListValidator) {
+    this.signedInclusionListValidator = signedInclusionListValidator;
+  }
+
+  @Override
+  public synchronized void onSlot(final UInt64 slot) {
+    validatorIndexToInclusionLists.clear();
+  }
+
+  public void add(final SignedInclusionList signedInclusionList) {
+    final UInt64 validatorIndex = signedInclusionList.getMessage().getValidatorIndex();
+    if (validatorIndexToInclusionLists.containsKey(validatorIndex)) {
+      validatorIndexToInclusionLists.get(validatorIndex).add(signedInclusionList);
+    } else {
+      validatorIndexToInclusionLists.put(validatorIndex, List.of(signedInclusionList));
+    }
+  }
+
+  public SafeFuture<InternalValidationResult> addSignedInclusionList(
+      final SignedInclusionList signedInclusionList, final Optional<UInt64> arrivalTimestamp) {
+    final SafeFuture<InternalValidationResult> validationResult =
+        signedInclusionListValidator.validate(signedInclusionList, validatorIndexToInclusionLists);
+    return validationResult.thenApply(
+        result -> {
+          if (result.isAccept()) {
+            add(signedInclusionList);
+          }
+          return result;
+        });
+  }
+
+  public List<SignedInclusionList> getInclusionLists(final UInt64 slot) {
+    return Collections.emptyList();
+  }
+}
