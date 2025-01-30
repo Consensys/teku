eed1256f7d95d28f846c55e16f6f8924f1421799
Gabriel Fukushima <gabrielfukushima@gmail.com>
2025-01-30 11:31:56 +1000

    suppress warning we don't care about the return of this doStop
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- infrastructure/async/src/main/java/tech/pegasys/teku/infrastructure/async/ScheduledExecutorAsyncRunner.java
+++ infrastructure/async/src/main/java/tech/pegasys/teku/infrastructure/async/ScheduledExecutorAsyncRunner.java
@@ -109,6 +109,14 @@ public class ScheduledExecutorAsyncRunner implements AsyncRunner {
     shutdown.set(true);
     scheduler.shutdownNow();
     workerPool.shutdownNow();
+    // wait for terminantion to give some time for the current tasks to finish
+    // mainly to avoid long runnning queries on db to cause a sigsegv
+    try {
+      scheduler.awaitTermination(30, TimeUnit.SECONDS);
+      workerPool.awaitTermination(30, TimeUnit.SECONDS);
+    } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+    }
   }
 
   private <U> void handleExecutorError(final SafeFuture<U> result, final Throwable t) {
