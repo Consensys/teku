96c73da84480d908e7cfd698762c80f712c0bcac
Gabriel Fukushima <gabrielfukushima@gmail.com>
2025-02-03 14:11:16 +1000

    Rocks subcommand (#9067)
    * migrate RocksDbHelper from Besu
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    * create a RocksDb subcommand
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    * add RocksDbSubCommand to debug tool
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    * add rocksdb jni to teku module
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    * suppress JavaCAse
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    * add final
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    * spotless
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    * add a check for format exception
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>
    
    * address formatting comment
    
    Signed-off-by: Paul Harris <paul.harris@consensys.net>
    
    ---------
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    Signed-off-by: Paul Harris <paul.harris@consensys.net>
    Co-authored-by: Paul Harris <paul.harris@consensys.net>

---
+++ teku/src/main/java/tech/pegasys/teku/cli/subcommand/storage/RocksDbCommand.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright Consensys Software Inc., 2025
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.cli.subcommand.storage;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.rocksdb.RocksDBException;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Mixin;
+import tech.pegasys.teku.cli.converter.PicoCliVersionProvider;
+import tech.pegasys.teku.cli.options.BeaconNodeDataOptions;
+import tech.pegasys.teku.cli.options.Eth2NetworkOptions;
+import tech.pegasys.teku.infrastructure.logging.SubCommandLogger;
+import tech.pegasys.teku.networks.Eth2NetworkConfiguration;
+import tech.pegasys.teku.service.serviceutils.layout.DataDirLayout;
+import tech.pegasys.teku.storage.server.rocksdb.RocksDbHelper;
+
+@Command(
+    name = "rocksdb",
+    description = "Print RocksDB information",
+    mixinStandardHelpOptions = true,
+    versionProvider = PicoCliVersionProvider.class)
+public class RocksDbCommand implements Runnable {
+
+  @Override
+  public void run() {
+    CommandLine.usage(this, System.out);
+  }
+
+  public static final SubCommandLogger SUB_COMMAND_LOG = new SubCommandLogger();
+
+  @Command(
+      name = "usage",
+      description = "Print disk usage",
+      mixinStandardHelpOptions = true,
+      versionProvider = PicoCliVersionProvider.class)
+  public int usage(
+      @Mixin final BeaconNodeDataOptions beaconNodeDataOptions,
+      @Mixin final Eth2NetworkOptions eth2NetworkOptions) {
+
+    RocksDbHelper.printTableHeader(SUB_COMMAND_LOG);
+    final String dbPath =
+        DataDirLayout.createFrom(beaconNodeDataOptions.getDataConfig()).getBeaconDataDirectory()
+            + "/db";
+    final Eth2NetworkConfiguration networkConfiguration =
+        eth2NetworkOptions.getNetworkConfiguration();
+    final List<RocksDbHelper.ColumnFamilyUsage> columnFamilyUsages = new ArrayList<>();
+    RocksDbHelper.forEachColumnFamily(
+        dbPath,
+        networkConfiguration,
+        (rocksdb, cfHandle, spec) -> {
+          try {
+            columnFamilyUsages.add(
+                RocksDbHelper.getAndPrintUsageForColumnFamily(
+                    rocksdb, cfHandle, spec, SUB_COMMAND_LOG));
+          } catch (final RocksDBException e) {
+            throw new RuntimeException(e);
+          }
+        });
+    RocksDbHelper.printTotals(SUB_COMMAND_LOG, columnFamilyUsages);
+    return 0;
+  }
+
+  @Command(
+      name = "stats",
+      description = "Print rocksdb stats",
+      mixinStandardHelpOptions = true,
+      versionProvider = PicoCliVersionProvider.class)
+  public int stats(
+      @Mixin final BeaconNodeDataOptions beaconNodeDataOptions,
+      @Mixin final Eth2NetworkOptions eth2NetworkOptions) {
+
+    final String dbPath =
+        DataDirLayout.createFrom(beaconNodeDataOptions.getDataConfig()).getBeaconDataDirectory()
+            + "/db";
+    final Eth2NetworkConfiguration networkConfiguration =
+        eth2NetworkOptions.getNetworkConfiguration();
+    SUB_COMMAND_LOG.display("Column Family Stats...");
+    RocksDbHelper.forEachColumnFamily(
+        dbPath,
+        networkConfiguration,
+        (rocksdb, cfHandle, spec) -> {
+          try {
+            RocksDbHelper.printStatsForColumnFamily(rocksdb, cfHandle, spec, SUB_COMMAND_LOG);
+          } catch (final RocksDBException e) {
+            throw new RuntimeException(e);
+          }
+        });
+    return 0;
+  }
+}
