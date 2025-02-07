d38c53e3c810220be04d4d8e91fef708a377620e
Stefan Bratanov <stefan.bratanov93@gmail.com>
2025-02-07 18:19:29 +0000

    fix issue with adding payload attestations to be packed into a block

--- ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/attestation/PayloadAttestationPool.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/attestation/PayloadAttestationPool.java
@@ -13,6 +13,7 @@
 
 package tech.pegasys.teku.statetransition.attestation;
 
+import it.unimi.dsi.fastutil.ints.IntList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -26,6 +27,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.bytes.Bytes32;
 import org.hyperledger.besu.plugin.services.MetricsSystem;
 import tech.pegasys.teku.ethereum.events.SlotEventsChannel;
 import tech.pegasys.teku.infrastructure.metrics.SettableGauge;
@@ -43,6 +45,7 @@ import tech.pegasys.teku.spec.datastructures.operations.PayloadAttestationSchema
 import tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconState;
 import tech.pegasys.teku.spec.schemas.SchemaDefinitions;
 import tech.pegasys.teku.spec.schemas.SchemaDefinitionsEip7732;
+import tech.pegasys.teku.storage.client.RecentChainData;
 
 public class PayloadAttestationPool implements SlotEventsChannel {
   private static final Logger LOG = LogManager.getLogger();
@@ -53,14 +56,18 @@ public class PayloadAttestationPool implements SlotEventsChannel {
   private final Map<Bytes, MatchingDataPayloadAttestationGroup> payloadAttestationGroupByDataHash =
       new HashMap<>();
   private final NavigableMap<UInt64, Set<Bytes>> payloadAttestationDataHashBySlot = new TreeMap<>();
+
   private final Spec spec;
   private final SettableGauge sizeGauge;
-  private final AtomicInteger size = new AtomicInteger(0);
+  private final RecentChainData recentChainData;
   private final int maximumPayloadAttestationCount;
+
+  private final AtomicInteger size = new AtomicInteger(0);
 
   public PayloadAttestationPool(
       final Spec spec,
       final MetricsSystem metricsSystem,
+      final RecentChainData recentChainData,
       final int maximumPayloadAttestationCount) {
     this.spec = spec;
     this.sizeGauge =
@@ -69,23 +76,36 @@ public class PayloadAttestationPool implements SlotEventsChannel {
             TekuMetricCategory.BEACON,
             "payload_attestation_pool_size",
             "The number of payload attestations available to be included in proposed blocks");
+    this.recentChainData = recentChainData;
     this.maximumPayloadAttestationCount = maximumPayloadAttestationCount;
   }
 
   public synchronized void add(final PayloadAttestationMessage payloadAttestationMessage) {
-    final SpecVersion specVersion = spec.atSlot(payloadAttestationMessage.getData().getSlot());
+    final UInt64 slot = payloadAttestationMessage.getData().getSlot();
+    final SpecVersion specVersion = spec.atSlot(slot);
     final PayloadAttestationSchema payloadAttestationSchema =
         SchemaDefinitionsEip7732.required(specVersion.getSchemaDefinitions())
             .getPayloadAttestationSchema();
+    // EIP-7732 TODO: avoid this join
+    final Optional<BeaconState> state = recentChainData.retrieveStateInEffectAtSlot(slot).join();
+    if (state.isEmpty()) {
+      return;
+    }
     getOrCreatePayloadAttestationGroup(payloadAttestationMessage)
         .ifPresent(
             attestationGroup -> {
+              final IntList ptc = spec.getPtc(state.get(), slot);
+              final int ptcRelativePosition =
+                  ptc.indexOf(payloadAttestationMessage.getValidatorIndex().intValue());
+              if (ptcRelativePosition == -1) {
+                return;
+              }
               final boolean added =
                   attestationGroup.add(
                       payloadAttestationSchema.create(
                           payloadAttestationSchema
                               .getAggregationBitsSchema()
-                              .ofBits(payloadAttestationMessage.getValidatorIndex().intValue()),
+                              .ofBits(ptcRelativePosition),
                           payloadAttestationMessage.getData(),
                           payloadAttestationMessage.getSignature()));
               if (added) {
@@ -102,6 +122,11 @@ public class PayloadAttestationPool implements SlotEventsChannel {
       removePayloadAttestationsPriorToSlot(firstSlotToKeep);
       currentSize = getSize();
     }
+  }
+
+  private void removeGroup(final Bytes dataHash) {
+    final int removed = payloadAttestationGroupByDataHash.remove(dataHash).size();
+    updateSize(-removed);
   }
 
   private Optional<MatchingDataPayloadAttestationGroup> getOrCreatePayloadAttestationGroup(
@@ -134,23 +159,59 @@ public class PayloadAttestationPool implements SlotEventsChannel {
         BeaconBlockBodySchemaEip7732.required(schemaDefinitions.getBeaconBlockBodySchema())
             .getPayloadAttestationsSchema();
 
-    return payloadAttestationDataHashBySlot
-        .headMap(stateAtBlockSlot.getSlot(), false)
-        .descendingMap()
-        .values()
-        .stream()
-        .flatMap(Collection::stream)
-        .map(payloadAttestationGroupByDataHash::get)
-        .filter(Objects::nonNull)
-        .filter(group -> isValid(stateAtBlockSlot, group.getPayloadAttestationData()))
-        .flatMap(MatchingDataPayloadAttestationGroup::stream)
-        .limit(payloadAttestationsSchema.getMaxLength())
-        .collect(payloadAttestationsSchema.collector());
+    final Set<PayloadAttestationData> invalidAttestationsToRemove = new HashSet<>();
+
+    final SszList<PayloadAttestation> payloadAttestations =
+        payloadAttestationDataHashBySlot
+            .headMap(stateAtBlockSlot.getSlot(), false)
+            .descendingMap()
+            .values()
+            .stream()
+            .flatMap(Collection::stream)
+            .map(payloadAttestationGroupByDataHash::get)
+            .filter(Objects::nonNull)
+            .filter(
+                group -> {
+                  if (!isValid(stateAtBlockSlot, group.getPayloadAttestationData())) {
+                    invalidAttestationsToRemove.add(group.getPayloadAttestationData());
+                    return false;
+                  }
+                  return true;
+                })
+            .flatMap(MatchingDataPayloadAttestationGroup::stream)
+            .limit(payloadAttestationsSchema.getMaxLength())
+            .collect(payloadAttestationsSchema.collector());
+
+    // clean-up invalid attestations
+    invalidAttestationsToRemove.forEach(
+        invalidAttestation -> {
+          final Bytes32 dataHash = invalidAttestation.hashTreeRoot();
+          final UInt64 slot = invalidAttestation.getSlot();
+          removeGroup(dataHash);
+          final Set<Bytes> dataHashes = payloadAttestationDataHashBySlot.get(slot);
+          dataHashes.remove(dataHash);
+          if (dataHashes.isEmpty()) {
+            payloadAttestationDataHashBySlot.remove(slot);
+          }
+        });
+
+    return payloadAttestations;
   }
 
   private boolean isValid(
       final BeaconState stateAtBlockSlot, final PayloadAttestationData payloadAttestationData) {
-    return spec.validatePayloadAttestation(stateAtBlockSlot, payloadAttestationData).isEmpty();
+    return spec.validatePayloadAttestation(stateAtBlockSlot, payloadAttestationData)
+        .map(
+            invalidReason -> {
+              final UInt64 slot = payloadAttestationData.getSlot();
+              LOG.debug(
+                  "Aggregated payload attestation for slot {} and block root {} is invalid: {}",
+                  slot,
+                  payloadAttestationData.getBeaconBlockRoot(),
+                  invalidReason.describe());
+              return false;
+            })
+        .orElse(true);
   }
 
   @Override
@@ -165,14 +226,7 @@ public class PayloadAttestationPool implements SlotEventsChannel {
   private void removePayloadAttestationsPriorToSlot(final UInt64 firstValidPayloadAttestationSlot) {
     final Collection<Set<Bytes>> dataHashesToRemove =
         payloadAttestationDataHashBySlot.headMap(firstValidPayloadAttestationSlot, false).values();
-    dataHashesToRemove.stream()
-        .flatMap(Set::stream)
-        .forEach(
-            key -> {
-              final int removed = payloadAttestationGroupByDataHash.get(key).size();
-              payloadAttestationGroupByDataHash.remove(key);
-              updateSize(-removed);
-            });
+    dataHashesToRemove.stream().flatMap(Set::stream).forEach(this::removeGroup);
     if (!dataHashesToRemove.isEmpty()) {
       LOG.trace(
           "firstValidPayloadAttestationSlot: {}, removing: {}",
