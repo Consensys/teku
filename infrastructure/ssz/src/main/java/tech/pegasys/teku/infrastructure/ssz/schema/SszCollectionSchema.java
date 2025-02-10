6bb24ac407703309a5d660bf310ec2d4735d2e3a
VolodymyrBg <aqdrgg19@gmail.com>
2025-02-10 17:55:24 +0200

    Update SszCollectionSchema.java

--- infrastructure/ssz/src/main/java/tech/pegasys/teku/infrastructure/ssz/schema/SszCollectionSchema.java
+++ infrastructure/ssz/src/main/java/tech/pegasys/teku/infrastructure/ssz/schema/SszCollectionSchema.java
@@ -82,4 +82,4 @@ public interface SszCollectionSchema<
   default Collector<SszElementT, ?, SszCollectionT> collector() {
     return Collectors.collectingAndThen(Collectors.<SszElementT>toList(), this::createFromElements);
   }
-} 
+}
