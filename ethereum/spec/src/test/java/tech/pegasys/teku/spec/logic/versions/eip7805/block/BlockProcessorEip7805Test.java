02dfc0a4d45c76658c39b5293ceef92223c11485
Gabriel Fukushima <gabrielfukushima@gmail.com>
2025-02-13 19:28:48 +1000

    remove import *
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- ethereum/spec/src/test/java/tech/pegasys/teku/spec/logic/versions/eip7805/block/BlockProcessorEip7805Test.java
+++ ethereum/spec/src/test/java/tech/pegasys/teku/spec/logic/versions/eip7805/block/BlockProcessorEip7805Test.java
@@ -15,7 +15,6 @@ package tech.pegasys.teku.spec.logic.versions.eip7805.block;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import java.util.Optional;
