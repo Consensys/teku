f96bbb951ca85ee57bb7e430b0b066a49c693fb8
Stefan Bratanov <stefan.bratanov93@gmail.com>
2025-02-21 14:33:52 +0000

    Fix payload attestation packing

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/logic/versions/eip7732/operations/validation/PayloadAttestationValidator.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/logic/versions/eip7732/operations/validation/PayloadAttestationValidator.java
@@ -17,6 +17,7 @@ import static tech.pegasys.teku.spec.logic.common.operations.validation.Operatio
 import static tech.pegasys.teku.spec.logic.common.operations.validation.OperationInvalidReason.firstOf;
 
 import java.util.Optional;
+import org.apache.tuweni.bytes.Bytes32;
 import tech.pegasys.teku.spec.datastructures.operations.PayloadAttestationData;
 import tech.pegasys.teku.spec.datastructures.state.Fork;
 import tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconState;
@@ -38,15 +39,15 @@ public class PayloadAttestationValidator
 
   private Optional<OperationInvalidReason> verifyAttestationIsForTheParentBeaconBlock(
       final BeaconState state, final PayloadAttestationData payloadAttestation) {
+    final Bytes32 latestBlockRoot = state.getLatestBlockHeader().getRoot();
     return check(
-        payloadAttestation
-            .getBeaconBlockRoot()
-            .equals(state.getLatestBlockHeader().getParentRoot()),
+        payloadAttestation.getBeaconBlockRoot().equals(latestBlockRoot),
+        // this check is used for validation during block production, so the latest block in the
+        // state should refer to the parent block of the current block being produced
         () ->
             String.format(
                 "Attestation with block root %s is not for the parent block %s",
-                payloadAttestation.getBeaconBlockRoot(),
-                state.getLatestBlockHeader().getParentRoot()));
+                payloadAttestation.getBeaconBlockRoot(), latestBlockRoot));
   }
 
   private Optional<OperationInvalidReason> verifyAttestationIsForThePreviousSlot(
