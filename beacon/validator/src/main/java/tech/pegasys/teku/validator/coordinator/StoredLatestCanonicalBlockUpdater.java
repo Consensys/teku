93cc8c1746766d38a916e14153df618ab9d98afa
Enrico Del Fante <enrico.delfante@consensys.net>
2025-03-05 19:32:42 +0100

    move to dedicated class

--- beacon/validator/src/main/java/tech/pegasys/teku/validator/coordinator/StoredLatestCanonicalBlockUpdater.java
+++ beacon/validator/src/main/java/tech/pegasys/teku/validator/coordinator/StoredLatestCanonicalBlockUpdater.java
@@ -1,4 +1,19 @@
+/*
+ * Copyright Consensys Software Inc., 2025
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package tech.pegasys.teku.validator.coordinator;
+
+import static tech.pegasys.teku.infrastructure.unsigned.UInt64.ONE;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -7,25 +22,24 @@ import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.storage.client.RecentChainData;
 import tech.pegasys.teku.storage.store.UpdatableStore.StoreTransaction;
 
-import static tech.pegasys.teku.infrastructure.unsigned.UInt64.ONE;
-
-
 public class StoredLatestCanonicalBlockUpdater implements SlotEventsChannel {
-    private static final Logger LOG = LogManager.getLogger();
+  private static final Logger LOG = LogManager.getLogger();
 
-    private final RecentChainData recentChainData;
+  private final RecentChainData recentChainData;
 
-    public StoredLatestCanonicalBlockUpdater(final RecentChainData recentChainData) {
-        this.recentChainData = recentChainData;
+  public StoredLatestCanonicalBlockUpdater(final RecentChainData recentChainData) {
+    this.recentChainData = recentChainData;
+  }
+
+  @Override
+  public void onSlot(UInt64 slot) {
+    if (!slot.mod(UInt64.valueOf(32)).equals(ONE)) {
+      return;
     }
-
-    @Override
-    public void onSlot(UInt64 slot) {
-        if(!slot.mod(UInt64.valueOf(32)).equals(ONE)) {
-            return;
-        }
-        final StoreTransaction transaction = recentChainData.startStoreTransaction();
-        recentChainData.getBestBlockRoot().ifPresent(transaction::setLatestCanonicalBlockRoot);
-        transaction.commit().finish(error -> LOG.error("Failed to store latest canonical block root", error));
-    }
+    final StoreTransaction transaction = recentChainData.startStoreTransaction();
+    recentChainData.getBestBlockRoot().ifPresent(transaction::setLatestCanonicalBlockRoot);
+    transaction
+        .commit()
+        .finish(error -> LOG.error("Failed to store latest canonical block root", error));
+  }
 }
--- beacon/validator/src/test/java/tech/pegasys/teku/validator/coordinator/StoredLatestCanonicalBlockUpdaterTest.java
+++ beacon/validator/src/test/java/tech/pegasys/teku/validator/coordinator/StoredLatestCanonicalBlockUpdaterTest.java
@@ -1,5 +1,24 @@
+/*
+ * Copyright Consensys Software Inc., 2025
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package tech.pegasys.teku.validator.coordinator;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 import org.apache.tuweni.bytes.Bytes32;
 import org.junit.jupiter.api.Test;
 import tech.pegasys.teku.infrastructure.async.SafeFuture;
@@ -7,37 +26,30 @@ import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.storage.client.RecentChainData;
 import tech.pegasys.teku.storage.store.UpdatableStore.StoreTransaction;
 
-import java.util.Optional;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.when;
-
 public class StoredLatestCanonicalBlockUpdaterTest {
-    private final RecentChainData recentChainData = mock(RecentChainData.class);
+  private final RecentChainData recentChainData = mock(RecentChainData.class);
 
-    private final StoredLatestCanonicalBlockUpdater updater = new StoredLatestCanonicalBlockUpdater(recentChainData);
+  private final StoredLatestCanonicalBlockUpdater updater =
+      new StoredLatestCanonicalBlockUpdater(recentChainData);
 
-    @Test
-    void onSlot_shouldUpdateLatestCanonicalBlockRoot() {
-        final Optional<Bytes32> blockRoot = Optional.of(Bytes32.fromHexString("0x01"));
-        final StoreTransaction storeTransaction = mock(StoreTransaction.class);
-        when(storeTransaction.commit()).thenReturn(SafeFuture.COMPLETE);
+  @Test
+  void onSlot_shouldUpdateLatestCanonicalBlockRoot() {
+    final Optional<Bytes32> blockRoot = Optional.of(Bytes32.fromHexString("0x01"));
+    final StoreTransaction storeTransaction = mock(StoreTransaction.class);
+    when(storeTransaction.commit()).thenReturn(SafeFuture.COMPLETE);
 
-        when(recentChainData.startStoreTransaction()).thenReturn(storeTransaction);
-        when(recentChainData.getBestBlockRoot()).thenReturn(blockRoot);
+    when(recentChainData.startStoreTransaction()).thenReturn(storeTransaction);
+    when(recentChainData.getBestBlockRoot()).thenReturn(blockRoot);
 
-        updater.onSlot(UInt64.valueOf(33));
+    updater.onSlot(UInt64.valueOf(33));
 
+    verify(storeTransaction).setLatestCanonicalBlockRoot(blockRoot.get());
+  }
 
-        verify(storeTransaction).setLatestCanonicalBlockRoot(blockRoot.get());
-    }
+  @Test
+  void onSlot_shouldNotUpdateLatestCanonicalBlockRoot() {
+    updater.onSlot(UInt64.valueOf(32));
 
-    @Test
-    void onSlot_shouldNotUpdateLatestCanonicalBlockRoot() {
-        updater.onSlot(UInt64.valueOf(32));
-
-        verifyNoInteractions(recentChainData);
-    }
+    verifyNoInteractions(recentChainData);
+  }
 }
