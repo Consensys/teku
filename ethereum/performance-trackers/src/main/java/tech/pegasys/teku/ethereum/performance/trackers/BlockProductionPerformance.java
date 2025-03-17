44d0de2126040fcbc34d88bccb27aacd0a2e88a1
Gabriel Fukushima <gabrielfukushima@gmail.com>
2025-03-17 23:15:42 +1000

    Report time to collect attestations in slow block production (#9233)
    * Add `get_attestations_for_block` to slow block production report
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    * spotless
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    * very important leftover from test
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    * add beacon_block_prep_started event to ensure attestation event don't take into account previous event duration time
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    * spotless
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    ---------
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- ethereum/performance-trackers/src/main/java/tech/pegasys/teku/ethereum/performance/trackers/BlockProductionPerformance.java
+++ ethereum/performance-trackers/src/main/java/tech/pegasys/teku/ethereum/performance/trackers/BlockProductionPerformance.java
@@ -32,7 +32,9 @@ package tech.pegasys.teku.ethereum.performance.trackers;
  *    (which set slotTime too)
  *         |
  *         v
- *    beaconBlockPrepared
+ *         |
+ *         v
+ *    beaconBlockPrepared (attestations_for_block is part of the process of beaconBlockPrepared)
  *    |                  |
  *    v                  v
  * engineGetPayload   builderGetHeader (maybe)
@@ -95,6 +97,12 @@ public interface BlockProductionPerformance {
 
         @Override
         public void stateHashing() {}
+
+        @Override
+        public void getAttestationsForBlock() {}
+
+        @Override
+        public void beaconBlockPreparationStarted() {}
       };
 
   void complete();
@@ -120,4 +128,8 @@ public interface BlockProductionPerformance {
   void stateTransition();
 
   void stateHashing();
+
+  void getAttestationsForBlock();
+
+  void beaconBlockPreparationStarted();
 }
