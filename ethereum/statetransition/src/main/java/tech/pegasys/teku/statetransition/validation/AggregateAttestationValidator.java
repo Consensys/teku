dc04e17d261ee8831490e844c1885064942feb4f
Paul Harris <paul.harris@consensys.net>
2025-03-20 09:24:26 +1000

    kept change minimal
    Signed-off-by: Paul Harris <paul.harris@consensys.net>

--- ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/validation/AggregateAttestationValidator.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/validation/AggregateAttestationValidator.java
@@ -180,7 +180,7 @@ public class AggregateAttestationValidator {
               if (!beaconCommittee.contains(aggregateAndProof.getIndex().intValue())) {
                 return SafeFuture.completedFuture(
                     reject(
-                        "Rejecting aggregate because validator index %s is not in committee. Should have been one of %s",
+                        "Rejecting aggregate because attester %s is not in committee. Should have been one of %s",
                         aggregateAndProof.getIndex(), beaconCommittee));
               }
 
