dacd29ff1f2abeaa7319879c6b8f931833f0cd7f
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2025-04-03 19:09:00 +0200

    simplify schema definitions

--- ethereum/executionlayer/src/main/java/tech/pegasys/teku/ethereum/executionlayer/ExecutionClientHandlerImpl.java
+++ ethereum/executionlayer/src/main/java/tech/pegasys/teku/ethereum/executionlayer/ExecutionClientHandlerImpl.java
@@ -46,6 +46,7 @@ import tech.pegasys.teku.spec.executionlayer.PayloadStatus;
 import tech.pegasys.teku.spec.logic.versions.deneb.types.VersionedHash;
 import tech.pegasys.teku.spec.schemas.SchemaDefinitions;
 import tech.pegasys.teku.spec.schemas.SchemaDefinitionsDeneb;
+import tech.pegasys.teku.spec.schemas.SchemaDefinitionsEip7805;
 
 public class ExecutionClientHandlerImpl implements ExecutionClientHandler {
 
@@ -198,10 +199,7 @@ public class ExecutionClientHandlerImpl implements ExecutionClientHandler {
         .thenApply(
             response -> {
               final TransactionSchema transactionSchema =
-                  spec.atSlot(slot)
-                      .getSchemaDefinitions()
-                      .toVersionEip7805()
-                      .orElseThrow()
+                  SchemaDefinitionsEip7805.required(spec.atSlot(slot).getSchemaDefinitions())
                       .getInclusionListSchema()
                       .getTransactionSchema();
               return response.stream()
