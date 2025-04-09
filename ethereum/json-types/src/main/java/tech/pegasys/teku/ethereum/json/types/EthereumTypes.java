22648015cc006908c81aeeb7c4c7bb22cd15380a
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2025-04-09 16:43:59 +0200

    revert headers (#9333)

--- ethereum/json-types/src/main/java/tech/pegasys/teku/ethereum/json/types/EthereumTypes.java
+++ ethereum/json-types/src/main/java/tech/pegasys/teku/ethereum/json/types/EthereumTypes.java
@@ -160,15 +160,9 @@ public class EthereumTypes {
   @NotNull
   private static <T extends SszData> Map<String, String> getSszHeaders(
       final Function<T, SpecMilestone> milestoneSelector, final T value) {
-    // TODO EIP7805 this should be removed once eip7805 data types are supported
-    final SpecMilestone specMilestone = milestoneSelector.apply(value);
-    final SpecMilestone selectedMilestone =
-        specMilestone.isGreaterThanOrEqualTo(SpecMilestone.EIP7805)
-            ? SpecMilestone.ELECTRA
-            : specMilestone;
     return Map.of(
         HEADER_CONSENSUS_VERSION,
-        Version.fromMilestone(selectedMilestone).name(),
+        Version.fromMilestone(milestoneSelector.apply(value)).name(),
         RestApiConstants.HEADER_CONTENT_DISPOSITION,
         getSszFilename(value));
   }
