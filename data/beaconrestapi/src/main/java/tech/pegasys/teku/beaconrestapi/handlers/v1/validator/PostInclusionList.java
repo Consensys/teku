71685c9f097d7928a4a13bdf2094f829cde84dce
Gabriel Fukushima <gabrielfukushima@gmail.com>
2025-04-16 12:46:31 +1000

    add implementation
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/validator/PostInclusionList.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/validator/PostInclusionList.java
@@ -5,24 +5,25 @@ import org.apache.commons.lang3.NotImplementedException;
 import tech.pegasys.teku.api.DataProvider;
 import tech.pegasys.teku.api.SyncDataProvider;
 import tech.pegasys.teku.api.ValidatorDataProvider;
+import tech.pegasys.teku.infrastructure.async.SafeFuture;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.AsyncApiResponse;
 import tech.pegasys.teku.spec.datastructures.operations.InclusionList;
 import tech.pegasys.teku.infrastructure.json.types.DeserializableTypeDefinition;
 import tech.pegasys.teku.infrastructure.json.types.SerializableTypeDefinition;
 import tech.pegasys.teku.infrastructure.restapi.endpoints.EndpointMetadata;
 import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiEndpoint;
 import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiRequest;
-import tech.pegasys.teku.spec.Spec;
 import tech.pegasys.teku.spec.SpecMilestone;
-import tech.pegasys.teku.spec.datastructures.operations.AttestationData;
 import tech.pegasys.teku.spec.datastructures.operations.SignedInclusionList;
-import tech.pegasys.teku.spec.datastructures.operations.SignedInclusionListSchema;
 import tech.pegasys.teku.spec.schemas.SchemaDefinitionCache;
+import tech.pegasys.teku.validator.api.SubmitDataError;
 
+import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 
-import static tech.pegasys.teku.beaconrestapi.BeaconRestApiTypes.EPOCH_PARAMETER;
+import static tech.pegasys.teku.api.ValidatorDataProvider.PARTIAL_PUBLISH_FAILURE_MESSAGE;
 import static tech.pegasys.teku.beaconrestapi.BeaconRestApiTypes.ETH_CONSENSUS_VERSION_TYPE;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_BAD_REQUEST;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_OK;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.TAG_VALIDATOR;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.TAG_VALIDATOR_REQUIRED;
@@ -31,7 +32,6 @@ public class PostInclusionList extends RestApiEndpoint {
 
     public static final String ROUTE = "/eth/v1/validator/inclusion_list";
     private final ValidatorDataProvider validatorDataProvider;
-    private final SchemaDefinitionCache schemaDefinitionCache;
 
     public PostInclusionList(DataProvider dataProvider, final SchemaDefinitionCache schemaDefinitionCache) {
         this(dataProvider.getValidatorDataProvider(), schemaDefinitionCache);
@@ -51,19 +51,29 @@ public class PostInclusionList extends RestApiEndpoint {
                         .withInternalErrorResponse()
                         .build());
         this.validatorDataProvider = validatorDataProvider;
-        this.schemaDefinitionCache = schemaDefinitionCache;
     }
 
 
     @Override
     public void handleRequest(final RestApiRequest request) throws JsonProcessingException {
-        throw new NotImplementedException();
+        SignedInclusionList signedInclusionList = request.getRequestBody();
+
+        final SafeFuture<List<SubmitDataError>> future = validatorDataProvider.sendSignedInclusionList(List.of(signedInclusionList));
+        request.respondAsync(
+                future.thenApply(
+                        errors -> {
+                            if (errors.isEmpty()) {
+                                return AsyncApiResponse.respondWithCode(SC_OK);
+                            }
+                            return AsyncApiResponse.respondWithError(
+                                    SC_BAD_REQUEST, PARTIAL_PUBLISH_FAILURE_MESSAGE);
+                        }));
     }
 
-    private static DeserializableTypeDefinition<InclusionList> getRequestType(final SchemaDefinitionCache schemaDefinitionCache) {
+    private static DeserializableTypeDefinition<SignedInclusionList> getRequestType(final SchemaDefinitionCache schemaDefinitionCache) {
         return schemaDefinitionCache.getSchemaDefinition(SpecMilestone.EIP7805)
                 .toVersionEip7805().orElseThrow()
-                .getInclusionListSchema()
+                .getSignedInclusionListSchema()
                 .getJsonTypeDefinition();
     }
 
