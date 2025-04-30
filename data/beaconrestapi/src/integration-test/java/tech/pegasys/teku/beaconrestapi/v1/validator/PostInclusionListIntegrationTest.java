fb28d25725c21bf4ed02c5233088c25377f24393
Gabriel Fukushima <gabrielfukushima@gmail.com>
2025-04-30 14:27:49 +1000

    spotless
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- data/beaconrestapi/src/integration-test/java/tech/pegasys/teku/beaconrestapi/v1/validator/PostInclusionListIntegrationTest.java
+++ data/beaconrestapi/src/integration-test/java/tech/pegasys/teku/beaconrestapi/v1/validator/PostInclusionListIntegrationTest.java
@@ -1,88 +1,83 @@
+/*
+ * Copyright Consensys Software Inc., 2025
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package tech.pegasys.teku.beaconrestapi.v1.validator;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import okhttp3.Response;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import tech.pegasys.teku.beacon.sync.events.SyncState;
-import tech.pegasys.teku.beaconrestapi.AbstractDataBackedRestAPIIntegrationTest;
-import tech.pegasys.teku.beaconrestapi.handlers.v1.validator.GetInclusionListCommitteeDuties;
-import tech.pegasys.teku.beaconrestapi.handlers.v1.validator.PostInclusionList;
-import tech.pegasys.teku.ethereum.json.types.validator.InclusionListDuties;
-import tech.pegasys.teku.infrastructure.async.SafeFuture;
-import tech.pegasys.teku.infrastructure.json.JsonUtil;
-import tech.pegasys.teku.infrastructure.json.types.DeserializableTypeDefinition;
-import tech.pegasys.teku.infrastructure.unsigned.UInt64;
-import tech.pegasys.teku.spec.SpecMilestone;
-import tech.pegasys.teku.spec.datastructures.operations.SignedInclusionList;
-import tech.pegasys.teku.spec.util.DataStructureUtil;
-import tech.pegasys.teku.validator.api.SubmitDataError;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_BAD_REQUEST;
-import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_INTERNAL_SERVER_ERROR;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_OK;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import okhttp3.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tech.pegasys.teku.beaconrestapi.AbstractDataBackedRestAPIIntegrationTest;
+import tech.pegasys.teku.beaconrestapi.handlers.v1.validator.PostInclusionList;
+import tech.pegasys.teku.infrastructure.async.SafeFuture;
+import tech.pegasys.teku.infrastructure.json.JsonUtil;
+import tech.pegasys.teku.spec.SpecMilestone;
+import tech.pegasys.teku.spec.datastructures.operations.SignedInclusionList;
+import tech.pegasys.teku.spec.util.DataStructureUtil;
+
 public class PostInclusionListIntegrationTest extends AbstractDataBackedRestAPIIntegrationTest {
-    private DataStructureUtil dataStructureUtil;
+  private DataStructureUtil dataStructureUtil;
 
-    @BeforeEach
-    void setup() {
-        startRestAPIAtGenesis(SpecMilestone.EIP7805);
-        dataStructureUtil = new DataStructureUtil(spec);
+  @BeforeEach
+  void setup() {
+    startRestAPIAtGenesis(SpecMilestone.EIP7805);
+    dataStructureUtil = new DataStructureUtil(spec);
+  }
+
+  @Test
+  void shouldReturnOk() throws IOException {
+    final SignedInclusionList signedInclusionList = dataStructureUtil.randomSignedInclusionList();
+    when(validatorApiChannel.sendSignedInclusionLists(any()))
+        .thenReturn(SafeFuture.completedFuture(List.of()));
+
+    final SignedInclusionList request = signedInclusionList;
+
+    try (Response response =
+        post(
+            PostInclusionList.ROUTE,
+            JsonUtil.serialize(request, signedInclusionList.getSchema().getJsonTypeDefinition()),
+            Optional.of("eip7805"))) {
+
+      assertThat(response.code()).isEqualTo(SC_OK);
     }
+  }
 
-    @Test
-    void shouldReturnOk() throws IOException {
-        final SignedInclusionList signedInclusionList = dataStructureUtil.randomSignedInclusionList();
-        when(validatorApiChannel.sendSignedInclusionLists(any()))
-                .thenReturn(SafeFuture.completedFuture(List.of()));
+  @Test
+  void postEmptyBodyShouldReturnBadRequest() throws IOException {
 
-        final SignedInclusionList request = signedInclusionList;
+    try (Response response = post(PostInclusionList.ROUTE, "", Optional.of("eip7805"))) {
 
-        try (Response response =
-                     post(
-                             PostInclusionList.ROUTE,
-                             JsonUtil.serialize(request,
-                                     signedInclusionList.getSchema().getJsonTypeDefinition()),
-                             Optional.of("eip7805"))) {
-
-            assertThat(response.code()).isEqualTo(SC_OK);
-        }
+      assertThat(response.code()).isEqualTo(SC_BAD_REQUEST);
+      verifyNoInteractions(validatorApiChannel);
     }
+  }
 
-    @Test
-    void postEmptyBodyShouldReturnBadRequest() throws IOException {
+  @Test
+  void postInvalidBodyShouldReturnBadRequest() throws IOException {
+    try (Response response =
+        post(PostInclusionList.ROUTE, "invalid body", Optional.of("eip7805"))) {
 
-        try (Response response =
-                     post(
-                             PostInclusionList.ROUTE,
-                             "",
-                             Optional.of("eip7805"))) {
-
-            assertThat(response.code()).isEqualTo(SC_BAD_REQUEST);
-            verifyNoInteractions(validatorApiChannel);
-        }
+      assertThat(response.code()).isEqualTo(SC_BAD_REQUEST);
+      verifyNoInteractions(validatorApiChannel);
     }
-
-    @Test
-    void postInvalidBodyShouldReturnBadRequest() throws IOException {
-        try (Response response =
-                     post(
-                             PostInclusionList.ROUTE,
-                             "invalid body",
-                             Optional.of("eip7805"))) {
-
-            assertThat(response.code()).isEqualTo(SC_BAD_REQUEST);
-            verifyNoInteractions(validatorApiChannel);
-        }
-    }
-
+  }
 }
