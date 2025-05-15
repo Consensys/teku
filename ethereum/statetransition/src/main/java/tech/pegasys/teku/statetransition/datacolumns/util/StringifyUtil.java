f95d7394cc9743b0b80b79818eae4d866539850f
Lucas Saldanha <lucascrsaldanha@gmail.com>
2025-05-15 11:22:25 +1000

    Added GossipLogger and DAS related changes

---
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/util/StringifyUtil.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright Consensys Software Inc., 2025
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.statetransition.datacolumns.util;
+
+import static java.lang.Integer.max;
+import static java.lang.Integer.min;
+
+import java.util.BitSet;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.apache.tuweni.bytes.Bytes;
+
+public class StringifyUtil {
+
+  public static String columnIndexesToString(
+      final Collection<Integer> indexes, final int maxColumns) {
+    final String lenStr = "(len: " + indexes.size() + ") ";
+    if (indexes.isEmpty()) {
+      return lenStr + "[]";
+    } else if (indexes.size() == maxColumns) {
+      return lenStr + "[all]";
+    } else if (maxColumns - indexes.size() <= 16) {
+      final Set<Integer> exceptIndexes =
+          IntStream.range(0, maxColumns).boxed().collect(Collectors.toSet());
+      exceptIndexes.removeAll(indexes);
+      return lenStr + "[all except " + sortAndJoin(exceptIndexes) + "]";
+    } else {
+      List<IntRange> ranges = reduceToIntRanges(indexes);
+      if (ranges.size() <= 16) {
+        return lenStr
+            + "["
+            + ranges.stream().map(Objects::toString).collect(Collectors.joining(","))
+            + "]";
+      } else {
+        BitSet bitSet = new BitSet(maxColumns);
+        indexes.forEach(bitSet::set);
+        return lenStr + "[bitmap: " + Bytes.of(bitSet.toByteArray()) + "]";
+      }
+    }
+  }
+
+  public static String toIntRangeStringWithSize(final Collection<Integer> ints) {
+    return "(size: " + ints.size() + ") " + toIntRangeString(ints);
+  }
+
+  public static String toIntRangeString(final Collection<Integer> ints) {
+    List<IntRange> ranges = reduceToIntRanges(ints);
+    return "[" + ranges.stream().map(Objects::toString).collect(Collectors.joining(",")) + "]";
+  }
+
+  private record IntRange(int first, int last) {
+
+    static IntRange of(final int i) {
+      return new IntRange(i, i);
+    }
+
+    static List<IntRange> union(final List<IntRange> left, final List<IntRange> right) {
+      if (left.isEmpty()) {
+        return right;
+      } else if (right.isEmpty()) {
+        return right;
+      } else {
+        return Stream.of(
+                left.stream().limit(left.size() - 1),
+                left.getLast().union(right.getFirst()).stream(),
+                right.stream().skip(1))
+            .flatMap(s -> s)
+            .toList();
+      }
+    }
+
+    boolean isEmpty() {
+      return first > last;
+    }
+
+    @SuppressWarnings("UnusedMethod")
+    boolean isSingle() {
+      return first == last;
+    }
+
+    int size() {
+      return Integer.max(0, last() - first() + 1);
+    }
+
+    List<IntRange> union(final IntRange other) {
+      if (this.isEmpty()) {
+        return List.of(other);
+      } else if (other.isEmpty()) {
+        return List.of(this);
+      } else if (other.first() > this.last() + 1) {
+        return List.of(this, other);
+      } else if (this.first() > other.last() + 1) {
+        return List.of(other, this);
+      } else {
+        return List.of(
+            new IntRange(min(this.first(), other.first()), max(this.last(), other.last())));
+      }
+    }
+
+    @Override
+    public String toString() {
+      return switch (size()) {
+        case 0 -> "<empty>";
+        case 1 -> Integer.toString(first());
+        case 2 -> first() + "," + last();
+        default -> first() + ".." + last();
+      };
+    }
+  }
+
+  private static List<IntRange> reduceToIntRanges(final Collection<Integer> nums) {
+    return nums.stream()
+        .sorted()
+        .map(i -> List.of(IntRange.of(i)))
+        .reduce(IntRange::union)
+        .orElse(Collections.emptyList());
+  }
+
+  private static String sortAndJoin(final Collection<Integer> nums) {
+    return nums.stream().sorted().map(Objects::toString).collect(Collectors.joining(","));
+  }
+}
---
+++ ethereum/statetransition/src/test/java/tech/pegasys/teku/statetransition/datacolumns/util/StringifyUtilTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright Consensys Software Inc., 2025
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.statetransition.datacolumns.util;
+
+import static java.util.stream.IntStream.range;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class StringifyUtilTest {
+
+  record TestCase(IntStream indexes, String expectedString) {
+    @Override
+    public String toString() {
+      return expectedString;
+    }
+  }
+
+  static final int MAX_INDEXES_LEN = 128;
+
+  static final List<TestCase> TEST_CASES =
+      List.of(
+          new TestCase(IntStream.empty(), "[]"),
+          new TestCase(range(0, 128), "[all]"),
+          new TestCase(range(0, 128).skip(1), "[all except 0]"),
+          new TestCase(range(0, 128).limit(127), "[all except 127]"),
+          new TestCase(
+              range(0, 128).filter(i -> !Set.of(14, 16, 18, 98).contains(i)),
+              "[all except 14,16,18,98]"),
+          new TestCase(IntStream.of(14, 16, 18, 98), "[14,16,18,98]"),
+          new TestCase(IntStream.of(14, 15, 18, 98), "[14,15,18,98]"),
+          new TestCase(IntStream.of(14, 15, 16, 98), "[14..16,98]"),
+          new TestCase(range(0, 128).skip(64), "[64..127]"),
+          new TestCase(range(0, 128).limit(64), "[0..63]"),
+          new TestCase(
+              range(0, 128).filter(i -> i % 3 != 0),
+              "[bitmap: 0xb66ddbb66ddbb66ddbb66ddbb66ddbb6]"),
+          new TestCase(
+              range(10, 100).filter(i -> !Set.of(14, 16, 18, 98).contains(i)),
+              "[10..13,15,17,19..97,99]"));
+
+  private static Stream<Arguments> provideTestCaseParameters() {
+    return TEST_CASES.stream().map(Arguments::of);
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideTestCaseParameters")
+  void columnIndexesToString_test(final TestCase testCase) {
+    List<Integer> idxList = testCase.indexes.boxed().toList();
+    String s = StringifyUtil.columnIndexesToString(idxList, MAX_INDEXES_LEN);
+    System.out.println(s);
+    assertThat(s).isEqualTo("(len: " + idxList.size() + ") " + testCase.expectedString);
+  }
+}
