c11a4bf2385adb7880d8955c7a675f4c43d552ca
Gabriel Fukushima <gabrielfukushima@gmail.com>
2025-05-15 15:33:08 +1000

    spotless
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/tekuv1/admin/AddPeer.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/tekuv1/admin/AddPeer.java
@@ -1,69 +1,72 @@
+/*
+ * Copyright Consensys Software Inc., 2025
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package tech.pegasys.teku.beaconrestapi.handlers.tekuv1.admin;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.apache.logging.log4j.Level;
-import tech.pegasys.teku.api.DataProvider;
-import tech.pegasys.teku.api.NetworkDataProvider;
-import tech.pegasys.teku.api.provider.Peer;
-import tech.pegasys.teku.beaconrestapi.BeaconRestApi;
-import tech.pegasys.teku.beaconrestapi.JsonTypeDefinitionBeaconRestApi;
-import tech.pegasys.teku.infrastructure.json.types.DeserializableTypeDefinition;
-import tech.pegasys.teku.infrastructure.json.types.StringBasedPrimitiveTypeDefinition;
-import tech.pegasys.teku.infrastructure.restapi.endpoints.EndpointMetadata;
-import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiEndpoint;
-import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiRequest;
-import tech.pegasys.teku.networking.p2p.discovery.DiscoveryNetwork;
-
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-
-import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_ACCEPTED;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_BAD_REQUEST;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_INTERNAL_SERVER_ERROR;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_OK;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.TAG_TEKU;
 import static tech.pegasys.teku.infrastructure.json.types.CoreTypes.STRING_TYPE;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.List;
+import java.util.Optional;
+import tech.pegasys.teku.api.DataProvider;
+import tech.pegasys.teku.api.NetworkDataProvider;
+import tech.pegasys.teku.infrastructure.json.types.DeserializableTypeDefinition;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.EndpointMetadata;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiEndpoint;
+import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiRequest;
+import tech.pegasys.teku.networking.p2p.discovery.DiscoveryNetwork;
+
 public class AddPeer extends RestApiEndpoint {
 
-    public static final String ROUTE = "/teku/v1/admin/add_peer";
-    final NetworkDataProvider networkDataProvider;
+  public static final String ROUTE = "/teku/v1/admin/add_peer";
+  final NetworkDataProvider networkDataProvider;
 
-    public AddPeer(final DataProvider dataProvider) {
-        this(dataProvider.getNetworkDataProvider());
-    }
+  public AddPeer(final DataProvider dataProvider) {
+    this(dataProvider.getNetworkDataProvider());
+  }
 
-    AddPeer(final NetworkDataProvider networkDataProvider) {
-        super(
-                EndpointMetadata.post(ROUTE)
-                        .operationId("AddPeer")
-                        .summary("Add a peer to the node")
-                        .description("Adds a peer to the node.")
-                        .tags(TAG_TEKU)
-                        .requestBodyType(DeserializableTypeDefinition.listOf(STRING_TYPE))
-                        .response(SC_OK, "Peer added successfully")
-                        .withBadRequestResponse(Optional.of("Invalid peer address"))
-                        .withInternalErrorResponse()
-                        .build());
-        this.networkDataProvider = networkDataProvider;
-    }
+  AddPeer(final NetworkDataProvider networkDataProvider) {
+    super(
+        EndpointMetadata.post(ROUTE)
+            .operationId("AddPeer")
+            .summary("Add a peer to the node")
+            .description("Adds a peer to the node.")
+            .tags(TAG_TEKU)
+            .requestBodyType(DeserializableTypeDefinition.listOf(STRING_TYPE))
+            .response(SC_OK, "Peer added successfully")
+            .withBadRequestResponse(Optional.of("Invalid peer address"))
+            .withInternalErrorResponse()
+            .build());
+    this.networkDataProvider = networkDataProvider;
+  }
 
-    @Override
-    public void handleRequest(final RestApiRequest request) throws JsonProcessingException {
-        DiscoveryNetwork<?> discoveryNetwork =
-                networkDataProvider.getDiscoveryNetwork().orElseThrow();
-        final List<String> peerAddress = request.getRequestBody();
-        if(peerAddress.isEmpty()) {
-            request.respondError(SC_BAD_REQUEST, "No peer address provided");
-            return;
-        }
-        try {
-            peerAddress.forEach(discoveryNetwork::addStaticPeer);
-            request.respondWithCode(SC_OK);
-        }
-        catch (final IllegalArgumentException e) {
-            request.respondError(SC_INTERNAL_SERVER_ERROR,e.getMessage());
-        }
+  @Override
+  public void handleRequest(final RestApiRequest request) throws JsonProcessingException {
+    DiscoveryNetwork<?> discoveryNetwork = networkDataProvider.getDiscoveryNetwork().orElseThrow();
+    final List<String> peerAddress = request.getRequestBody();
+    if (peerAddress.isEmpty()) {
+      request.respondError(SC_BAD_REQUEST, "No peer address provided");
+      return;
     }
+    try {
+      peerAddress.forEach(discoveryNetwork::addStaticPeer);
+      request.respondWithCode(SC_OK);
+    } catch (final IllegalArgumentException e) {
+      request.respondError(SC_INTERNAL_SERVER_ERROR, e.getMessage());
+    }
+  }
 }
--- data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/tekuv1/admin/AddPeerTest.java
+++ data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/tekuv1/admin/AddPeerTest.java
@@ -1,15 +1,17 @@
+/*
+ * Copyright Consensys Software Inc., 2025
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package tech.pegasys.teku.beaconrestapi.handlers.tekuv1.admin;
-
-import org.assertj.core.api.AssertionsForInterfaceTypes;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import tech.pegasys.teku.beaconrestapi.AbstractMigratedBeaconHandlerTest;
-import tech.pegasys.teku.infrastructure.restapi.endpoints.CacheLength;
-import tech.pegasys.teku.networking.p2p.discovery.DiscoveryNetwork;
-
-import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -19,44 +21,52 @@ import static org.mockito.Mockito.when;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_BAD_REQUEST;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_INTERNAL_SERVER_ERROR;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_OK;
-import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_SERVICE_UNAVAILABLE;
+
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tech.pegasys.teku.beaconrestapi.AbstractMigratedBeaconHandlerTest;
+import tech.pegasys.teku.networking.p2p.discovery.DiscoveryNetwork;
 
 class AddPeerTest extends AbstractMigratedBeaconHandlerTest {
 
-    private final DiscoveryNetwork<?> discoveryNetwork = mock(DiscoveryNetwork.class);
+  private final DiscoveryNetwork<?> discoveryNetwork = mock(DiscoveryNetwork.class);
 
-    @BeforeEach
-    public void setup() {
-        setHandler(new AddPeer(network));
-    }
+  @BeforeEach
+  public void setup() {
+    setHandler(new AddPeer(network));
+  }
 
-    @Test
-    public void shouldReturnOkWhenAValidListIsProvided() throws Exception {
-        final List<String> peers = List.of("/ip4/178.128.136.233/udp/9001/p2p/16Uiu2HAmNSjEXNqaFjfLePKk87WZ7QwuTPd1HPEfJEeYnaC3bGZ1");
-        request.setRequestBody(peers);
-        when(network.getDiscoveryNetwork()).thenReturn(Optional.of(discoveryNetwork));
-        handler.handleRequest(request);
-        assertThat(request.getResponseCode()).isEqualTo(SC_OK);
-    }
+  @Test
+  public void shouldReturnOkWhenAValidListIsProvided() throws Exception {
+    final List<String> peers =
+        List.of(
+            "/ip4/178.128.136.233/udp/9001/p2p/16Uiu2HAmNSjEXNqaFjfLePKk87WZ7QwuTPd1HPEfJEeYnaC3bGZ1");
+    request.setRequestBody(peers);
+    when(network.getDiscoveryNetwork()).thenReturn(Optional.of(discoveryNetwork));
+    handler.handleRequest(request);
+    assertThat(request.getResponseCode()).isEqualTo(SC_OK);
+  }
 
-    @Test
-    public void shouldThrowExceptionForInvalidPeerAddress() throws Exception {
-        final List<String> peers = List.of("invalid-peer-address");
-        request.setRequestBody(peers);
-        when(network.getDiscoveryNetwork()).thenReturn(Optional.of(discoveryNetwork));
-        doThrow(new IllegalArgumentException("Invalid peer address")).when(discoveryNetwork).addStaticPeer(any());
-        handler.handleRequest(request);
-        assertThat(request.getResponseCode()).isEqualTo(SC_INTERNAL_SERVER_ERROR);
-    }
+  @Test
+  public void shouldThrowExceptionForInvalidPeerAddress() throws Exception {
+    final List<String> peers = List.of("invalid-peer-address");
+    request.setRequestBody(peers);
+    when(network.getDiscoveryNetwork()).thenReturn(Optional.of(discoveryNetwork));
+    doThrow(new IllegalArgumentException("Invalid peer address"))
+        .when(discoveryNetwork)
+        .addStaticPeer(any());
+    handler.handleRequest(request);
+    assertThat(request.getResponseCode()).isEqualTo(SC_INTERNAL_SERVER_ERROR);
+  }
 
-    @Test
-    public void shouldReturnBadRequestWhenRequestBodyIsEmpty() throws Exception {
-        final List<String> peers = List.of();
-        request.setRequestBody(peers);
-        when(network.getDiscoveryNetwork()).thenReturn(Optional.of(discoveryNetwork));
-        handler.handleRequest(request);
-        assertThat(request.getResponseCode()).isEqualTo(SC_BAD_REQUEST);
-    }
-
-
+  @Test
+  public void shouldReturnBadRequestWhenRequestBodyIsEmpty() throws Exception {
+    final List<String> peers = List.of();
+    request.setRequestBody(peers);
+    when(network.getDiscoveryNetwork()).thenReturn(Optional.of(discoveryNetwork));
+    handler.handleRequest(request);
+    assertThat(request.getResponseCode()).isEqualTo(SC_BAD_REQUEST);
+  }
 }
