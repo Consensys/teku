ec76ec5c6e053dbe2dca3412bdc5205645337607
Gabriel Fukushima <gabrielfukushima@gmail.com>
2025-05-23 14:30:02 +1000

    Add new and modified beacon accessor methods for fulu
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/logic/versions/fulu/helpers/BeaconStateAccessorsFulu.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/logic/versions/fulu/helpers/BeaconStateAccessorsFulu.java
@@ -11,12 +11,14 @@ import tech.pegasys.teku.spec.constants.Domain;
 import tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconState;
 import tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconStateCache;
 import tech.pegasys.teku.spec.datastructures.state.beaconstate.versions.fulu.BeaconStateFulu;
+import tech.pegasys.teku.spec.logic.common.helpers.BeaconStateAccessors;
 import tech.pegasys.teku.spec.logic.versions.electra.helpers.BeaconStateAccessorsElectra;
 import tech.pegasys.teku.spec.logic.versions.electra.helpers.MiscHelpersElectra;
 import tech.pegasys.teku.spec.logic.versions.electra.helpers.PredicatesElectra;
 
 import java.util.List;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static tech.pegasys.teku.spec.logic.common.helpers.MathHelpers.uint64ToBytes;
 
 public class BeaconStateAccessorsFulu extends BeaconStateAccessorsElectra {
@@ -25,28 +27,34 @@ public class BeaconStateAccessorsFulu extends BeaconStateAccessorsElectra {
     public BeaconStateAccessorsFulu(
             final SpecConfig config,
             final PredicatesElectra predicatesElectra,
-            final MiscHelpersElectra miscHelpers) {
+            final MiscHelpersFulu miscHelpers) {
         super(SpecConfigFulu.required(config), predicatesElectra, miscHelpers);
         configFulu = config.toVersionFulu().orElseThrow();
     }
 
-
-    public List<UInt64> getBeaconProposerIndex(final BeaconStateFulu state) {
-        return state.getProposerLookahead().asListUnboxed();
+    @Override
+    public int getBeaconProposerIndex(final BeaconState state, final UInt64 requestedSlot) {
+        final int lookAheadIndex = state.getSlot().mod(configFulu.getSlotsPerEpoch()).intValue();
+        return state.toVersionFulu().orElseThrow().getProposerLookahead().asListUnboxed().get(lookAheadIndex).intValue();
     }
 
-    public List<UInt64> computeProposerIndeces(final BeaconState state, final UInt64 requestedEpoch) {
-        //validateStateCanCalculateProposerIndexAtSlot(state, requestedSlot); might be useful to add similar check later
-        return BeaconStateCache.getTransitionCaches(state)
-                .getBeaconProposerIndex()
-                .get(
-                        requestedEpoch,
-                        slot -> {
-                            final UInt64 startSlot = miscHelpers.computeStartSlotAtEpoch(requestedEpoch);
-                            final Bytes32 epochSeed =
-                                    Hash.sha256(getSeed(state, epoch, Domain.BEACON_PROPOSER), uint64ToBytes(slot));
-                            IntList indices = getActiveValidatorIndices(state, epoch);
-                            return miscHelpers.computeProposerIndex(state, indices, epochSeed);
-                        });
+
+    public List<Integer> getBeaconProposerIndices(final BeaconState state, final UInt64 epoch) {
+        //validateStateCanCalculateProposerIndexAtEpoch(state, requestedSlot);
+        final Bytes32 seed = Hash.sha256(getSeed(state, epoch, Domain.BEACON_PROPOSER));
+        IntList indices = getActiveValidatorIndices(state, epoch);
+        return miscHelpers.computeProposerIndices(state,epoch, seed, indices);
+
     }
+
+    public static BeaconStateAccessorsFulu required(
+            final BeaconStateAccessors beaconStateAccessors) {
+        checkArgument(
+                beaconStateAccessors instanceof BeaconStateAccessorsElectra,
+                "Expected %s but it was %s",
+                BeaconStateAccessorsFulu.class,
+                beaconStateAccessors.getClass());
+        return (BeaconStateAccessorsFulu) beaconStateAccessors;
+    }
+
 }
