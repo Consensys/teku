b2c99d77c696f69b4de3f6e0c7f8bb2f414a3414
StefanBratanov <stefan.bratanov93@gmail.com>
2025-05-28 15:28:59 +0100

    fix spotless

--- teku/src/main/java/tech/pegasys/teku/cli/options/P2POptions.java
+++ teku/src/main/java/tech/pegasys/teku/cli/options/P2POptions.java
@@ -28,7 +28,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalInt;
-import java.util.stream.Collectors;
 import picocli.CommandLine.Help.Visibility;
 import picocli.CommandLine.Mixin;
 import picocli.CommandLine.Option;
