dd4ccd478628fc5cac3075288203bbecba7399b5
StefanBratanov <stefan.bratanov93@gmail.com>
2025-06-05 08:54:04 +0300

    remove from ref test

--- eth-reference-tests/src/referenceTest/java/tech/pegasys/teku/reference/phase0/sanity/SanityTests.java
+++ eth-reference-tests/src/referenceTest/java/tech/pegasys/teku/reference/phase0/sanity/SanityTests.java
@@ -24,7 +24,5 @@ public class SanityTests {
           .put("random/random", new SanityBlocksTestExecutor())
           .put("sanity/slots", new SanitySlotsTestExecutor())
           .put("finality/finality", new SanityBlocksTestExecutor())
-          .put(
-              "sanity/effective_balance_increase_changes_lookahead", new SanityBlocksTestExecutor())
           .build();
 }
