6a33dd2c82d40ee43eaa44b7ab5433cfb3a78f21
Katya Ryazantseva <sibkatya@gmail.com>
2025-06-10 11:23:29 +0200

    Rename getBlobsV2 runtime metric

--- ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/util/DataColumnSidecarELRecoveryManagerImpl.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/util/DataColumnSidecarELRecoveryManagerImpl.java
@@ -139,11 +139,10 @@ public class DataColumnSidecarELRecoveryManagerImpl extends AbstractIgnoringFutu
             metricsSystem,
             timeProvider,
             TekuMetricCategory.BEACON,
-            "engine_getBlobsV2_runtime_seconds",
-            "Full runtime of engine_getBlobsV2 requests",
+            "engine_getBlobsV2_request_duration_seconds",
+            "Duration of engine_getBlobsV2 requests",
             new double[] {
-              0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 5.0,
-              7.5, 10.0
+              0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 1.0, 2.0, 5.0, 10.0
             });
     this.miscHelpersFuluSupplier =
         () -> MiscHelpersFulu.required(spec.forMilestone(SpecMilestone.FULU).miscHelpers());
