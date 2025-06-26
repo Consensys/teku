c1270a36061509e687d502226a551b30a840da58
Gabriel Fukushima <gabrielfukushima@gmail.com>
2025-06-26 15:26:27 +1000

    add dataColumnSidecarsPublishingInitiated to the BlockPublishingPerformance interface
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- beacon/validator/src/main/java/tech/pegasys/teku/validator/coordinator/publisher/BlockPublisherFulu.java
+++ beacon/validator/src/main/java/tech/pegasys/teku/validator/coordinator/publisher/BlockPublisherFulu.java
@@ -58,6 +58,7 @@ public class BlockPublisherFulu extends BlockPublisherPhase0 {
   void publishDataColumnSidecars(
       final List<DataColumnSidecar> dataColumnSidecars,
       final BlockPublishingPerformance blockPublishingPerformance) {
+    blockPublishingPerformance.dataColumnSidecarsPublishingInitiated();
     // TODO-fulu blockPublishingPerformance (https://github.com/Consensys/teku/issues/9473)
     dataColumnSidecarGossipChannel.publishDataColumnSidecars(
         dataColumnSidecars, RemoteOrigin.LOCAL_PROPOSAL);
