b545d74a2441258aac996272955f23ff20700c38
Gabriel Fukushima <gabrielfukushima@gmail.com>
2025-07-29 14:42:26 +1000

    add dataColumnPruner options
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

---
+++ storage/src/main/java/tech/pegasys/teku/storage/server/pruner/DataColumnSidecarPruner.java
@@ -0,0 +1,151 @@
+package tech.pegasys.teku.storage.server.pruner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hyperledger.besu.plugin.services.MetricsSystem;
+import org.hyperledger.besu.plugin.services.metrics.LabelledSuppliedMetric;
+import tech.pegasys.teku.infrastructure.async.AsyncRunner;
+import tech.pegasys.teku.infrastructure.async.Cancellable;
+import tech.pegasys.teku.infrastructure.async.SafeFuture;
+import tech.pegasys.teku.infrastructure.metrics.SettableLabelledGauge;
+import tech.pegasys.teku.infrastructure.metrics.TekuMetricCategory;
+import tech.pegasys.teku.infrastructure.time.TimeProvider;
+import tech.pegasys.teku.infrastructure.unsigned.UInt64;
+import tech.pegasys.teku.service.serviceutils.Service;
+import tech.pegasys.teku.spec.Spec;
+import tech.pegasys.teku.spec.config.SpecConfigFulu;
+import tech.pegasys.teku.storage.archive.BlobSidecarsArchiver;
+import tech.pegasys.teku.storage.server.Database;
+
+import java.time.Duration;
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class DataColumnSidecarPruner extends Service {
+
+    private static final Logger LOG = LogManager.getLogger();
+
+    private final Spec spec;
+    private final Database database;
+    private final AsyncRunner asyncRunner;
+    private final Duration pruneInterval;
+    private final long pruneLimit;
+    private final TimeProvider timeProvider;
+    private final boolean dataColumnSidecarsStorageCountersEnabled;
+    private final SettableLabelledGauge pruningTimingsLabelledGauge;
+    private final SettableLabelledGauge pruningActiveLabelledGauge;
+    private final String pruningMetricsType;
+
+    private final AtomicLong dataColumnSize = new AtomicLong(0);
+    private final AtomicLong earliestDataColumnSidecarSlot = new AtomicLong(-1);
+    private Optional<UInt64> genesisTime = Optional.empty();
+
+    private Optional<Cancellable> scheduledPruner = Optional.empty();
+
+    public DataColumnSidecarPruner(
+            final Spec spec,
+            final Database database,
+            final MetricsSystem metricsSystem,
+            final AsyncRunner asyncRunner,
+            final TimeProvider timeProvider,
+            final Duration pruneInterval,
+            final int pruneLimit,
+            final boolean dataColumnSidecarsStorageCountersEnabled,
+            final String pruningMetricsType,
+            final SettableLabelledGauge pruningTimingsLabelledGauge,
+            final SettableLabelledGauge pruningActiveLabelledGauge) {
+        this.spec = spec;
+        this.database = database;
+        this.asyncRunner = asyncRunner;
+        this.pruneInterval = pruneInterval;
+        this.pruneLimit = pruneLimit;
+        this.timeProvider = timeProvider;
+        this.dataColumnSidecarsStorageCountersEnabled = dataColumnSidecarsStorageCountersEnabled;
+        this.pruningMetricsType = pruningMetricsType;
+        this.pruningTimingsLabelledGauge = pruningTimingsLabelledGauge;
+        this.pruningActiveLabelledGauge = pruningActiveLabelledGauge;
+        if (dataColumnSidecarsStorageCountersEnabled) {
+            LabelledSuppliedMetric labelledGauge =
+                    metricsSystem.createLabelledSuppliedGauge(
+                            TekuMetricCategory.STORAGE,
+                            "dataColumn_sidecars",
+                            "Statistics for Data columns stored",
+                            "type");
+
+            labelledGauge.labels(dataColumnSize::get, "total");
+            labelledGauge.labels(earliestDataColumnSidecarSlot::get, "earliest_slot");
+    }
+        }
+
+    @Override
+    protected SafeFuture<?> doStart() {
+            scheduledPruner =
+                    Optional.of(
+                            asyncRunner.runWithFixedDelay(
+                                    this::pruneDataColumnSidecars,
+                                    Duration.ZERO,
+                                    pruneInterval,
+                                    error -> LOG.error("Failed to prune old blobs", error)));
+            return SafeFuture.COMPLETE;
+    }
+
+    @Override
+    protected SafeFuture<?> doStop() {
+            scheduledPruner.ifPresent(Cancellable::cancel);
+            return SafeFuture.COMPLETE;
+    }
+
+
+    private void pruneDataColumnSidecars() {
+
+        final Optional<UInt64> genesisTime = getGenesisTime();
+        if (genesisTime.isEmpty()) {
+            LOG.debug("Not pruning as no genesis time is available.");
+            return;
+        }
+
+        pruningActiveLabelledGauge.set(1, pruningMetricsType);
+        final long start = System.currentTimeMillis();
+        final UInt64 minCustodySlot = calculatePruneSlot();
+        database.pruneAllSidecars(minCustodySlot);
+
+        pruningTimingsLabelledGauge.set(System.currentTimeMillis() - start, pruningMetricsType);
+        pruningActiveLabelledGauge.set(0, pruningMetricsType);
+
+        if (dataColumnSidecarsStorageCountersEnabled) {
+            dataColumnSize.set(database.getSidecarColumnCount());
+            earliestDataColumnSidecarSlot.set(
+                    database.getEarliestDataColumnSidecarSlot().map(UInt64::longValue).orElse(-1L));
+        }
+    }
+
+    private UInt64 calculatePruneSlot() {
+
+        final UInt64 currentSlot =
+                spec.getCurrentSlot(timeProvider.getTimeInSeconds(), genesisTime.get());
+        final UInt64 currentEpoch = spec.computeEpochAtSlot(currentSlot);
+        final int custodyPeriodEpochs =
+        spec.getSpecConfig(currentEpoch)
+                .toVersionFulu()
+                .map(SpecConfigFulu::getMinEpochsForDataColumnSidecarsRequests)
+                .orElse(0);
+      if (custodyPeriodEpochs == 0) {
+        return currentSlot;
+      } else{
+        final UInt64 minCustodyEpoch = currentEpoch.minusMinZero(custodyPeriodEpochs);
+        return spec.computeStartSlotAtEpoch(minCustodyEpoch);
+      }
+    }
+
+    private Optional<UInt64> getGenesisTime() {
+    if (genesisTime.isPresent()) {
+        return genesisTime;
+    }
+    genesisTime = database.getGenesisTime();
+    return genesisTime;
+    }
+}
