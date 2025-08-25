043b512269fe1948a21c9bb49dda90018901d31b
Mehdi AOUADI <mehdi.aouadi@consensys.net>
2025-08-25 23:08:01 +0200

    update test with non synced custody group count

--- ethereum/statetransition/src/test/java/tech/pegasys/teku/statetransition/datacolumns/DataColumnSidecarRecoveryCustodyTest.java
+++ ethereum/statetransition/src/test/java/tech/pegasys/teku/statetransition/datacolumns/DataColumnSidecarRecoveryCustodyTest.java
@@ -121,9 +121,7 @@ public class DataColumnSidecarRecoveryCustodyTest {
             miscHelpersFulu,
             NoOpKZG.INSTANCE,
             dataColumnSidecarPublisher,
-            () ->
-                createCustodyGroupCountManager(
-                    config.getNumberOfCustodyGroups(), config.getSamplesPerSlot()),
+            () -> createCustodyGroupCountManager(0, config.getSamplesPerSlot()),
             config.getNumberOfColumns(),
             config.getNumberOfCustodyGroups(),
             __ -> Duration.ofSeconds(2),
