20655488f91d8a9dde5bba1fc0bc4c1fe70f2b26
Gabriel Fukushima <gabrielfukushima@gmail.com>
2025-09-18 13:01:07 +1000

    fix executionProof container and schema
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/datastructures/execution/ExecutionProof.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/datastructures/execution/ExecutionProof.java
@@ -13,41 +13,73 @@
 
 package tech.pegasys.teku.spec.datastructures.execution;
 
+import org.apache.tuweni.bytes.Bytes32;
 import tech.pegasys.teku.infrastructure.ssz.collections.SszByteList;
+import tech.pegasys.teku.infrastructure.ssz.collections.impl.SszByteListImpl;
 import tech.pegasys.teku.infrastructure.ssz.containers.Container4;
+import tech.pegasys.teku.infrastructure.ssz.containers.Container5;
 import tech.pegasys.teku.infrastructure.ssz.primitive.SszBytes32;
 import tech.pegasys.teku.infrastructure.ssz.primitive.SszUInt64;
+import tech.pegasys.teku.infrastructure.ssz.tree.LeafNode;
 import tech.pegasys.teku.infrastructure.ssz.tree.TreeNode;
+import tech.pegasys.teku.infrastructure.ssz.tree.TreeUtil;
+import tech.pegasys.teku.infrastructure.unsigned.UInt64;
+
+import java.util.List;
+
+import static java.lang.Long.toHexString;
+import static tech.pegasys.teku.spec.datastructures.execution.ExecutionProofSchema.SSZ_SCHEMA;
 
 public class ExecutionProof
-    extends Container4<ExecutionProof, SszBytes32, SszUInt64, SszUInt64, SszByteList> {
+    extends Container5<ExecutionProof, SszBytes32, SszBytes32, SszUInt64, SszUInt64, SszByteList> {
 
   public ExecutionProof(final ExecutionProofSchema schema, final TreeNode node) {
     super(schema, node);
   }
 
+  public static ExecutionProof create(final UInt64 slot) {
+      return SSZ_SCHEMA.create(
+              SszBytes32.of(Bytes32.fromHexString(toHexString(slot.longValue()))),
+              SszBytes32.of(Bytes32.fromHexString(toHexString(slot.longValue()))),
+              SszUInt64.of(UInt64.ONE),
+              SszUInt64.ZERO,
+              SSZ_SCHEMA.getProofDataSchema().fromBytes(Bytes32.ZERO));
+  }
+
+  public static final ExecutionProof DUMMY_PROOF = SSZ_SCHEMA.create(
+      SszBytes32.of(Bytes32.ZERO),
+      SszBytes32.of(Bytes32.ZERO),
+      SszUInt64.of(UInt64.ONE),
+      SszUInt64.ZERO,
+          SSZ_SCHEMA.getProofDataSchema().fromBytes(Bytes32.ZERO));
+
   public ExecutionProof(
       final ExecutionProofSchema schema,
+      final SszBytes32 blockRoot,
       final SszBytes32 blockHash,
       final SszUInt64 subnetId,
       final SszUInt64 version,
       final SszByteList proofData) {
-    super(schema, blockHash, subnetId, version, proofData);
+    super(schema, blockRoot, blockHash, subnetId, version, proofData);
   }
 
-  public SszBytes32 getBlockHash() {
+  public SszBytes32 getBlockRoot() {
     return getField0();
   }
 
-  public SszUInt64 getSubnetId() {
-    return getField1();
-  }
+    public SszBytes32 getBlockHash() {
+        return getField1();
+    }
 
-  public SszUInt64 getVersion() {
+  public SszUInt64 getSubnetId() {
     return getField2();
   }
 
-  public SszByteList getProofData() {
+  public SszUInt64 getVersion() {
     return getField3();
+  }
+
+  public SszByteList getProofData() {
+    return getField4();
   }
 }
