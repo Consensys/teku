61c58bf35a7031bfe527a12604788e1d5e6e762f
Gabriel Fukushima <gabrielfukushima@gmail.com>
2025-09-27 22:38:41 +1000

    Add EP Availability checker
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

---
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/forkchoice/ExecutionProofsAvailabilityChecker.java
@@ -0,0 +1,33 @@
+package tech.pegasys.teku.statetransition.forkchoice;
+
+import tech.pegasys.teku.infrastructure.async.SafeFuture;
+import tech.pegasys.teku.spec.datastructures.blobs.versions.deneb.BlobSidecar;
+import tech.pegasys.teku.spec.datastructures.blocks.SignedBeaconBlock;
+import tech.pegasys.teku.spec.datastructures.execution.ExecutionProof;
+import tech.pegasys.teku.spec.logic.common.statetransition.availability.AvailabilityChecker;
+import tech.pegasys.teku.spec.logic.common.statetransition.availability.DataAndValidationResult;
+import tech.pegasys.teku.statetransition.executionproofs.ExecutionProofManager;
+
+public class ExecutionProofsAvailabilityChecker implements AvailabilityChecker<ExecutionProof> {
+    private final ExecutionProofManager executionProofManager;
+    private final SafeFuture<DataAndValidationResult<ExecutionProof>> validationResult =
+            new SafeFuture<>();
+    private final SignedBeaconBlock block;
+
+    public ExecutionProofsAvailabilityChecker(final ExecutionProofManager executionProofManager, final SignedBeaconBlock block) {
+        this.executionProofManager = executionProofManager;
+        this.block=block;
+    }
+
+
+    @Override
+    public boolean initiateDataAvailabilityCheck() {
+        executionProofManager.validateBlockWithExecutionProofs(block).propagateTo(validationResult);
+        return true;
+    }
+
+    @Override
+    public SafeFuture<DataAndValidationResult<ExecutionProof>> getAvailabilityCheckResult() {
+        return validationResult;
+    }
+}
