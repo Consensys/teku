71f35d553e5e1e1ba0e8b99b05d3bd1038a367a9
StefanBratanov <stefan.bratanov93@gmail.com>
2025-10-02 18:51:04 +0300

    fix acceptance test

--- acceptance-tests/src/testFixtures/java/tech/pegasys/teku/test/acceptance/dsl/TekuBeaconNode.java
+++ acceptance-tests/src/testFixtures/java/tech/pegasys/teku/test/acceptance/dsl/TekuBeaconNode.java
@@ -477,10 +477,27 @@ public class TekuBeaconNode extends TekuNode {
         () -> {
           final Optional<SignedBeaconBlock> block = fetchHeadBlock();
           assertThat(block).isPresent();
-          checkExecutionPayloadInBlock(block.get());
+          final SignedBeaconBlock signedBlock = block.get();
+          if (spec.atSlot(signedBlock.getSlot())
+              .getMilestone()
+              .isGreaterThanOrEqualTo(SpecMilestone.GLOAS)) {
+            // block with bid means non default execution payload
+            checkExecutionPayloadBidInBlock(signedBlock);
+          } else {
+            checkExecutionPayloadInBlock(signedBlock);
+          }
         },
         5,
         MINUTES);
+  }
+
+  private void checkExecutionPayloadBidInBlock(final SignedBeaconBlock signedBlock) {
+    final BeaconBlock beaconBlock = signedBlock.getMessage();
+    final UInt64 slot = beaconBlock.getSlot();
+    assertThat(beaconBlock.getBody().getOptionalSignedExecutionPayloadBid())
+        .describedAs("Block body should contain a signed execution payload bid")
+        .isPresent();
+    LOG.debug("Signed execution payload bid found at slot " + slot);
   }
 
   private void checkExecutionPayloadInBlock(final SignedBeaconBlock signedBlock) {
