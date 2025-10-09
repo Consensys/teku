6afaebcb23218dab68868b38e997136c10192790
Paul Harris <paul.harris@consensys.net>
2025-10-09 14:15:21 +1000

    started a new implementation of the recovering retriever. (#9910)
    Co-authored-by: Enrico Del Fante <enrico.delfante@consensys.net>

---
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/retriever/recovering/SidecarRetriever.java
@@ -0,0 +1,222 @@
+/*
+ * Copyright Consensys Software Inc., 2025
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.statetransition.datacolumns.retriever.recovering;
+
+import com.google.common.annotations.VisibleForTesting;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.tuweni.bytes.Bytes32;
+import org.hyperledger.besu.plugin.services.MetricsSystem;
+import org.hyperledger.besu.plugin.services.metrics.Counter;
+import org.hyperledger.besu.plugin.services.metrics.LabelledMetric;
+import tech.pegasys.teku.infrastructure.async.AsyncRunner;
+import tech.pegasys.teku.infrastructure.async.Cancellable;
+import tech.pegasys.teku.infrastructure.async.SafeFuture;
+import tech.pegasys.teku.infrastructure.metrics.TekuMetricCategory;
+import tech.pegasys.teku.infrastructure.time.TimeProvider;
+import tech.pegasys.teku.infrastructure.unsigned.UInt64;
+import tech.pegasys.teku.kzg.KZG;
+import tech.pegasys.teku.spec.datastructures.blobs.DataColumnSidecar;
+import tech.pegasys.teku.spec.datastructures.util.DataColumnSlotAndIdentifier;
+import tech.pegasys.teku.spec.logic.versions.fulu.helpers.MiscHelpersFulu;
+import tech.pegasys.teku.statetransition.datacolumns.db.DataColumnSidecarDbAccessor;
+import tech.pegasys.teku.statetransition.datacolumns.retriever.DataColumnSidecarRetriever;
+
+public class SidecarRetriever implements DataColumnSidecarRetriever {
+  private static final Logger LOG = LogManager.getLogger();
+
+  private final DataColumnSidecarRetriever downloader;
+  private final KZG kzg;
+  private final MiscHelpersFulu miscHelpersFulu;
+  private final DataColumnSidecarDbAccessor sidecarDB;
+  private final AsyncRunner asyncRunner;
+  private final TimeProvider timeProvider;
+  private final Duration recoveryTimeout;
+  private final Duration recoveryCheckInterval;
+  private final int numberOfColumnsRequiredToReconstruct;
+  private Cancellable pendingRequestsChecker;
+
+  private final Map<DataColumnSlotAndIdentifier, PendingRecoveryRequest> requests =
+      new ConcurrentHashMap<>();
+  private final Map<Bytes32, RebuildColumnsTask> rebuildTasks = new ConcurrentHashMap<>();
+
+  private final LabelledMetric<Counter> sidecarRecoveryMetric;
+
+  static final String DOWNLOADED = "DOWNLOADED";
+  static final String DOWNLOAD_TIMEOUT = "DOWNLOAD_TIMEOUT";
+  static final String RECOVERED = "RECOVERED";
+  static final String CANCELLED = "CANCELLED";
+  static final String RECOVERY_METRIC_NAME = "data_column_sidecar_recovery_requests";
+
+  public SidecarRetriever(
+      final DataColumnSidecarRetriever delegate,
+      final KZG kzg,
+      final MiscHelpersFulu miscHelpersFulu,
+      final DataColumnSidecarDbAccessor sidecarDB,
+      final AsyncRunner asyncRunner,
+      final Duration recoveryTimeout,
+      final Duration recoveryCheckInterval,
+      final TimeProvider timeProvider,
+      final int numberOfColumns,
+      final MetricsSystem metricsSystem) {
+    downloader = delegate;
+    this.kzg = kzg;
+    this.miscHelpersFulu = miscHelpersFulu;
+    this.sidecarDB = sidecarDB;
+    this.asyncRunner = asyncRunner;
+    this.recoveryTimeout = recoveryTimeout;
+    this.recoveryCheckInterval = recoveryCheckInterval;
+    this.timeProvider = timeProvider;
+    // reconstruction of all columns is possible with >= 50% of the column data
+    this.numberOfColumnsRequiredToReconstruct = Math.ceilDiv(numberOfColumns, 2);
+    sidecarRecoveryMetric =
+        metricsSystem.createLabelledCounter(
+            TekuMetricCategory.BEACON,
+            RECOVERY_METRIC_NAME,
+            "The number of sidecar recovery requests performed",
+            "result");
+  }
+
+  @VisibleForTesting
+  int pendingRequestCount() {
+    return requests.size();
+  }
+
+  @Override
+  public synchronized void start() {
+    if (pendingRequestsChecker != null) {
+      return;
+    }
+    pendingRequestsChecker =
+        asyncRunner.runWithFixedDelay(
+            this::checkPendingRequests,
+            recoveryCheckInterval,
+            recoveryCheckInterval,
+            error ->
+                LOG.error(
+                    "Failed to check if {} pending data column sidecars requests require recovery to be run",
+                    requests.size(),
+                    error));
+  }
+
+  @Override
+  public synchronized void stop() {
+    if (pendingRequestsChecker != null) {
+      pendingRequestsChecker.cancel();
+      pendingRequestsChecker = null;
+    }
+
+    rebuildTasks.values().forEach(RebuildColumnsTask::cancel);
+    rebuildTasks.clear();
+
+    requests.values().forEach(PendingRecoveryRequest::cancel);
+    requests.clear();
+  }
+
+  @Override
+  @SuppressWarnings("FutureReturnValueIgnored")
+  public SafeFuture<DataColumnSidecar> retrieve(final DataColumnSlotAndIdentifier columnId) {
+    final PendingRecoveryRequest pendingRecoveryRequest =
+        requests.computeIfAbsent(
+            columnId,
+            __ -> {
+              final PendingRecoveryRequest request =
+                  new PendingRecoveryRequest(
+                      columnId,
+                      downloader.retrieve(columnId),
+                      timeProvider.getTimeInMillis(),
+                      recoveryTimeout,
+                      recoveryTimeout.dividedBy(2));
+              request.getFuture().always(() -> requests.remove(columnId));
+              request
+                  .getDownloadFuture()
+                  .finish((err) -> sidecarRecoveryMetric.labels(DOWNLOAD_TIMEOUT).inc());
+              request.getFuture().finish((err) -> sidecarRecoveryMetric.labels(CANCELLED).inc());
+              request
+                  .getFuture()
+                  .thenPeek((result) -> sidecarRecoveryMetric.labels(DOWNLOADED).inc());
+              return request;
+            });
+    return pendingRecoveryRequest.getFuture();
+  }
+
+  @Override
+  public void flush() {
+    downloader.flush();
+  }
+
+  @Override
+  public void onNewValidatedSidecar(final DataColumnSidecar sidecar) {
+    downloader.onNewValidatedSidecar(sidecar);
+  }
+
+  @VisibleForTesting
+  Cancellable getPendingRequestsChecker() {
+    return pendingRequestsChecker;
+  }
+
+  @VisibleForTesting
+  Map<DataColumnSlotAndIdentifier, PendingRecoveryRequest> getPendingRequests() {
+    return requests;
+  }
+
+  @SuppressWarnings("FutureReturnValueIgnored")
+  private void checkPendingRequests() {
+    if (requests.isEmpty() && rebuildTasks.isEmpty()) {
+      return;
+    }
+    final UInt64 currentTime = timeProvider.getTimeInMillis();
+    LOG.debug(
+        "Checking pending requests ({}): {} requests, {} rebuild tasks",
+        currentTime,
+        requests.size(),
+        rebuildTasks.size());
+
+    // make sure requests are within their timeout
+    requests.values().forEach(request -> request.checkTimeout(currentTime));
+
+    // update state of any requests that are active
+    requests.values().stream()
+        .filter(PendingRecoveryRequest::isFailedDownloading)
+        .forEach(
+            request -> {
+              final RebuildColumnsTask rebuildColumnsTask =
+                  rebuildTasks.computeIfAbsent(
+                      request.getBlockRoot(),
+                      __ ->
+                          new RebuildColumnsTask(
+                              request.getSlotAndBlockRoot(),
+                              currentTime,
+                              recoveryTimeout.dividedBy(2),
+                              numberOfColumnsRequiredToReconstruct,
+                              sidecarDB,
+                              miscHelpersFulu,
+                              kzg));
+              LOG.debug(
+                  "Rebuilding columns for slot {} root {}",
+                  request.getSlot(),
+                  request.getBlockRoot());
+              rebuildColumnsTask.addTask(request);
+              request
+                  .getFuture()
+                  .thenPeek((ignored) -> sidecarRecoveryMetric.labels(RECOVERED).inc());
+            });
+    rebuildTasks.entrySet().removeIf(entry -> entry.getValue().isDone(currentTime));
+
+    rebuildTasks.forEach((key, value) -> value.checkQueryResult());
+  }
+}
---
+++ ethereum/statetransition/src/test/java/tech/pegasys/teku/statetransition/datacolumns/retriever/recovering/RebuildColumnsTaskTest.java
@@ -0,0 +1,331 @@
+/*
+ * Copyright Consensys Software Inc., 2025
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.statetransition.datacolumns.retriever.recovering;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static tech.pegasys.teku.statetransition.datacolumns.retriever.recovering.SidecarRetrieverTest.RECOVERY_TIMEOUT;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.junit.jupiter.api.Test;
+import tech.pegasys.teku.infrastructure.async.SafeFuture;
+import tech.pegasys.teku.infrastructure.time.StubTimeProvider;
+import tech.pegasys.teku.infrastructure.unsigned.UInt64;
+import tech.pegasys.teku.kzg.KZG;
+import tech.pegasys.teku.spec.Spec;
+import tech.pegasys.teku.spec.TestSpecFactory;
+import tech.pegasys.teku.spec.datastructures.blobs.DataColumnSidecar;
+import tech.pegasys.teku.spec.datastructures.blocks.SlotAndBlockRoot;
+import tech.pegasys.teku.spec.datastructures.util.DataColumnSlotAndIdentifier;
+import tech.pegasys.teku.spec.logic.versions.fulu.helpers.MiscHelpersFulu;
+import tech.pegasys.teku.spec.util.DataStructureUtil;
+import tech.pegasys.teku.statetransition.datacolumns.DataColumnSidecarDBStub;
+import tech.pegasys.teku.statetransition.datacolumns.db.DataColumnSidecarDB;
+import tech.pegasys.teku.statetransition.datacolumns.db.DataColumnSidecarDbAccessor;
+
+class RebuildColumnsTaskTest {
+
+  private final Spec spec = TestSpecFactory.createMinimalFulu();
+  private final DataStructureUtil dataStructureUtil = new DataStructureUtil(spec);
+  private final StubTimeProvider timeProvider = StubTimeProvider.withTimeInMillis(1_000_000);
+  private final DataColumnSidecarDB db = new DataColumnSidecarDBStub();
+  private final DataColumnSidecarDbAccessor dbAccessor =
+      DataColumnSidecarDbAccessor.builder(db).spec(spec).build();
+  private final KZG kzg = mock(KZG.class);
+  private final PendingRecoveryRequest request = mock(PendingRecoveryRequest.class);
+  private final SafeFuture<DataColumnSidecar> future = new SafeFuture<>();
+
+  @Test
+  void addTask_acceptsValidPendingRecoveryRequest() {
+    final UInt64 timestampMillis = timeProvider.getTimeInMillis();
+    final SlotAndBlockRoot slotAndBlockRoot = dataStructureUtil.randomSlotAndBlockRoot();
+    final RebuildColumnsTask task =
+        new RebuildColumnsTask(
+            slotAndBlockRoot,
+            timestampMillis,
+            RECOVERY_TIMEOUT,
+            1,
+            dbAccessor,
+            spec.getGenesisSpec().miscHelpers().toVersionFulu().orElseThrow(),
+            kzg);
+
+    final PendingRecoveryRequestTestArticle pendingRequest =
+        createPendingRecovery(slotAndBlockRoot, UInt64.ONE, true);
+
+    assertThat(task.isDone(timestampMillis)).isFalse();
+    assertThat(task.addTask(pendingRequest)).isTrue();
+    assertThat(pendingRequest.getFuture()).isNotDone();
+  }
+
+  @Test
+  void addTask_rejectsIfBlockrootMismatch() {
+    final UInt64 timestampMillis = timeProvider.getTimeInMillis();
+    final SlotAndBlockRoot slotAndBlockRoot = dataStructureUtil.randomSlotAndBlockRoot(UInt64.ZERO);
+    final RebuildColumnsTask task =
+        new RebuildColumnsTask(
+            slotAndBlockRoot,
+            timestampMillis,
+            RECOVERY_TIMEOUT,
+            1,
+            dbAccessor,
+            spec.getGenesisSpec().miscHelpers().toVersionFulu().orElseThrow(),
+            kzg);
+
+    final PendingRecoveryRequestTestArticle pendingRequest =
+        createPendingRecovery(slotAndBlockRoot, UInt64.ONE, false);
+
+    assertThat(task.addTask(pendingRequest)).isFalse();
+    assertThat(pendingRequest.getFuture()).isCancelled();
+  }
+
+  @Test
+  void addTask_completesIfRootMismatches() {
+    final UInt64 timestampMillis = timeProvider.getTimeInMillis();
+    final SlotAndBlockRoot slotAndBlockRoot = dataStructureUtil.randomSlotAndBlockRoot(UInt64.ZERO);
+    final RebuildColumnsTaskTestArticle task =
+        new RebuildColumnsTaskTestArticle(
+            slotAndBlockRoot,
+            timestampMillis,
+            RECOVERY_TIMEOUT,
+            1,
+            dbAccessor,
+            spec.getGenesisSpec().miscHelpers().toVersionFulu().orElseThrow(),
+            kzg);
+
+    final PendingRecoveryRequestTestArticle pendingRequest =
+        createPendingRecovery(slotAndBlockRoot, UInt64.ONE, false);
+
+    assertThat(task.addTask(pendingRequest)).isFalse();
+    assertThat(pendingRequest.getFuture()).isCancelled();
+  }
+
+  @Test
+  void addTask_completesIfRebuildIsDoneAndColumnMissing() {
+    final UInt64 timestampMillis = timeProvider.getTimeInMillis();
+    final SlotAndBlockRoot slotAndBlockRoot = dataStructureUtil.randomSlotAndBlockRoot(UInt64.ZERO);
+    final RebuildColumnsTaskTestArticle task =
+        new RebuildColumnsTaskTestArticle(
+            slotAndBlockRoot,
+            timestampMillis,
+            RECOVERY_TIMEOUT,
+            1,
+            dbAccessor,
+            spec.getGenesisSpec().miscHelpers().toVersionFulu().orElseThrow(),
+            kzg);
+
+    final PendingRecoveryRequestTestArticle pendingRequest =
+        createPendingRecovery(slotAndBlockRoot, UInt64.ONE, true);
+    task.done.set(true);
+
+    assertThat(task.addTask(pendingRequest)).isFalse();
+    assertThat(pendingRequest.getFuture()).isCancelled();
+  }
+
+  @Test
+  void addTask_completesIfColumnIsAvailableOnIncompleteRebuildTask() {
+    final UInt64 column = UInt64.ONE;
+    final UInt64 timestampMillis = timeProvider.getTimeInMillis();
+    final SlotAndBlockRoot slotAndBlockRoot = dataStructureUtil.randomSlotAndBlockRoot(UInt64.ZERO);
+    final RebuildColumnsTaskTestArticle task =
+        new RebuildColumnsTaskTestArticle(
+            slotAndBlockRoot,
+            timestampMillis,
+            RECOVERY_TIMEOUT,
+            2,
+            dbAccessor,
+            spec.getGenesisSpec().miscHelpers().toVersionFulu().orElseThrow(),
+            kzg);
+
+    final DataColumnSidecar sidecar = mock(DataColumnSidecar.class);
+    when(sidecar.getSlot()).thenReturn(slotAndBlockRoot.getSlot());
+    task.getSidecarMap().put(column.intValue(), sidecar);
+
+    final PendingRecoveryRequestTestArticle pendingRequest =
+        createPendingRecovery(slotAndBlockRoot, UInt64.ONE, true);
+
+    assertThat(task.addTask(pendingRequest)).isTrue();
+    assertThat(pendingRequest.getFuture()).isCompletedWithValue(sidecar);
+    assertThat(task.isDone(timestampMillis)).isFalse();
+
+    verifyNoMoreInteractions(request);
+  }
+
+  @Test
+  void addTask_completesIfFutureIsDoneAndEntryFound() {
+    final UInt64 column = UInt64.ONE;
+    final UInt64 timestampMillis = timeProvider.getTimeInMillis();
+    final SlotAndBlockRoot slotAndBlockRoot = dataStructureUtil.randomSlotAndBlockRoot(UInt64.ZERO);
+    final RebuildColumnsTaskTestArticle task =
+        new RebuildColumnsTaskTestArticle(
+            slotAndBlockRoot,
+            timestampMillis,
+            RECOVERY_TIMEOUT,
+            1,
+            dbAccessor,
+            spec.getGenesisSpec().miscHelpers().toVersionFulu().orElseThrow(),
+            kzg);
+
+    // when done and sidecar present
+    final DataColumnSidecar sidecar = mock(DataColumnSidecar.class);
+    task.getDone().set(true);
+    task.getSidecarMap().put(column.intValue(), sidecar);
+
+    final PendingRecoveryRequestTestArticle pendingRequest =
+        createPendingRecovery(slotAndBlockRoot, column, true);
+
+    when(sidecar.getSlotAndBlockRoot()).thenReturn(slotAndBlockRoot);
+
+    assertThat(task.addTask(pendingRequest)).isTrue();
+    assertThat(pendingRequest.getFuture()).isCompletedWithValue(sidecar);
+  }
+
+  @Test
+  void cancel_worksOnDoneTask() {
+    final UInt64 timestampMillis = timeProvider.getTimeInMillis();
+    final SlotAndBlockRoot slotAndBlockRoot = dataStructureUtil.randomSlotAndBlockRoot(UInt64.ZERO);
+    final RebuildColumnsTaskTestArticle task =
+        new RebuildColumnsTaskTestArticle(
+            slotAndBlockRoot,
+            timestampMillis,
+            RECOVERY_TIMEOUT,
+            1,
+            dbAccessor,
+            spec.getGenesisSpec().miscHelpers().toVersionFulu().orElseThrow(),
+            kzg);
+
+    final PendingRecoveryRequestTestArticle pendingRequest =
+        createPendingRecovery(slotAndBlockRoot, UInt64.ONE, true);
+
+    task.done.set(true);
+    task.cancel();
+
+    assertThat(task.isDone(timestampMillis)).isTrue();
+    // we hacked done, so this task wasn't cleaned properly.
+    assertThat(pendingRequest.getFuture()).isNotDone();
+  }
+
+  @Test
+  void cancel_shouldMarkAsDone() {
+
+    final UInt64 timestampMillis = timeProvider.getTimeInMillis();
+    final SlotAndBlockRoot slotAndBlockRoot = dataStructureUtil.randomSlotAndBlockRoot(UInt64.ZERO);
+    final RebuildColumnsTaskTestArticle task =
+        new RebuildColumnsTaskTestArticle(
+            slotAndBlockRoot,
+            timestampMillis,
+            RECOVERY_TIMEOUT,
+            1,
+            dbAccessor,
+            spec.getGenesisSpec().miscHelpers().toVersionFulu().orElseThrow(),
+            kzg);
+    final PendingRecoveryRequestTestArticle pendingRequest =
+        createPendingRecovery(slotAndBlockRoot, UInt64.ONE, true);
+
+    task.getPendingTasks().add(pendingRequest);
+    task.cancel();
+
+    assertThat(task.isDone(timestampMillis)).isTrue();
+    assertThat(pendingRequest.getFuture()).isCancelled();
+  }
+
+  @Test
+  void isDone_handlesTimeout() {
+    final UInt64 timestampMillis = timeProvider.getTimeInMillis();
+    final SlotAndBlockRoot slotAndBlockRoot = dataStructureUtil.randomSlotAndBlockRoot(UInt64.ZERO);
+    final RebuildColumnsTaskTestArticle task =
+        new RebuildColumnsTaskTestArticle(
+            slotAndBlockRoot,
+            timestampMillis,
+            RECOVERY_TIMEOUT,
+            1,
+            dbAccessor,
+            spec.getGenesisSpec().miscHelpers().toVersionFulu().orElseThrow(),
+            kzg);
+    final PendingRecoveryRequestTestArticle pendingRequest =
+        createPendingRecovery(slotAndBlockRoot, UInt64.ONE, true);
+
+    task.getPendingTasks().add(pendingRequest);
+    timeProvider.advanceTimeBy(RECOVERY_TIMEOUT);
+
+    assertThat(task.isDone(timeProvider.getTimeInMillis())).isTrue();
+    assertThat(pendingRequest.getFuture()).isCancelled();
+  }
+
+  final PendingRecoveryRequestTestArticle createPendingRecovery(
+      final SlotAndBlockRoot slotAndBlockRoot, final UInt64 column, final boolean isMatchingRoot) {
+    final DataColumnSlotAndIdentifier columnId =
+        new DataColumnSlotAndIdentifier(
+            slotAndBlockRoot.getSlot(),
+            isMatchingRoot ? slotAndBlockRoot.getBlockRoot() : dataStructureUtil.randomBytes32(),
+            column);
+    return new PendingRecoveryRequestTestArticle(future, columnId, timeProvider.getTimeInMillis());
+  }
+
+  static class PendingRecoveryRequestTestArticle extends PendingRecoveryRequest {
+
+    PendingRecoveryRequestTestArticle(
+        final SafeFuture<DataColumnSidecar> downloadFuture,
+        final DataColumnSlotAndIdentifier columnId,
+        final UInt64 timestamp) {
+      super(columnId, downloadFuture, timestamp, RECOVERY_TIMEOUT.dividedBy(2), RECOVERY_TIMEOUT);
+    }
+
+    PendingRecoveryRequestTestArticle(
+        final DataColumnSlotAndIdentifier columnId,
+        final SafeFuture<DataColumnSidecar> downloadFuture,
+        final UInt64 timestamp,
+        final Duration timeout,
+        final Duration downloadTimeout) {
+      super(columnId, downloadFuture, timestamp, timeout, downloadTimeout);
+    }
+  }
+
+  static class RebuildColumnsTaskTestArticle extends RebuildColumnsTask {
+
+    RebuildColumnsTaskTestArticle(
+        final SlotAndBlockRoot slotAndBlockRoot,
+        final UInt64 timestampMillis,
+        final Duration timeout,
+        final int minimumColumnsForRebuild,
+        final DataColumnSidecarDbAccessor sidecarDB,
+        final MiscHelpersFulu miscHelpers,
+        final KZG kzg) {
+      super(
+          slotAndBlockRoot,
+          timestampMillis,
+          timeout,
+          minimumColumnsForRebuild,
+          sidecarDB,
+          miscHelpers,
+          kzg);
+    }
+
+    AtomicBoolean getDone() {
+      return done;
+    }
+
+    Map<Integer, DataColumnSidecar> getSidecarMap() {
+      return sidecarMap;
+    }
+
+    List<PendingRecoveryRequest> getPendingTasks() {
+      return tasks;
+    }
+  }
+}
