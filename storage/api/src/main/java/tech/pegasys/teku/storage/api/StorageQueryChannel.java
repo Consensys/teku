ab7ad6b089afbd588e5e7636cb0336a5b731a38b
StefanBratanov <stefan.bratanov93@gmail.com>
2025-11-17 17:51:03 +0000

    fix a nit

--- storage/api/src/main/java/tech/pegasys/teku/storage/api/StorageQueryChannel.java
+++ storage/api/src/main/java/tech/pegasys/teku/storage/api/StorageQueryChannel.java
@@ -63,7 +63,8 @@ public interface StorageQueryChannel extends ChannelInterface {
       Bytes32 blockRoot);
 
   /**
-   * Returns "hot" blocks - the latest block or blocks that descend from the latest finalized block
+   * Returns "hot" blocks - the latest finalized block or blocks that descend from the latest
+   * finalized block
    *
    * @param blockRoots The roots of blocks to look up
    * @return A map from root to block of any found blocks
