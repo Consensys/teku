36d7b6a4a93d31ebfb0c061ef2977c4e502ccea5
Enrico Del Fante <enrico.delfante@consensys.net>
2025-11-20 23:02:24 +0100

    tmp

--- services/beaconchain/src/main/java/tech/pegasys/teku/services/beaconchain/BeaconChainController.java
+++ services/beaconchain/src/main/java/tech/pegasys/teku/services/beaconchain/BeaconChainController.java
@@ -998,7 +998,7 @@ public class BeaconChainController extends Service implements BeaconChainControl
     final DasCustodyBackfiller s =
         new DasCustodyBackfiller(
             combinedChainDataClient,
-            Duration.ofSeconds(30),
+            Duration.ofSeconds(5),
             dataColumnSidecarRecoveringCustody,
             custodyGroupCountManager,
             recoveringSidecarRetriever,
