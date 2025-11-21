13c17f3b13ca58fdae96d1319b87ce6a85956e0c
Enrico Del Fante <enrico.delfante@consensys.net>
2025-11-21 13:54:07 +0100

    new version

--- services/beaconchain/src/main/java/tech/pegasys/teku/services/beaconchain/BeaconChainController.java
+++ services/beaconchain/src/main/java/tech/pegasys/teku/services/beaconchain/BeaconChainController.java
@@ -1016,6 +1016,7 @@ public class BeaconChainController extends Service implements BeaconChainControl
             sidecarDB::setFirstCustodyIncompleteSlot,
             BATCH_SIZE_IN_SLOTS);
     eventChannels.subscribe(CustodyGroupCountChannel.class, s);
+    eventChannels.subscribe(FinalizedCheckpointChannel.class, s);
     dasCustodyBackfiller = Optional.of(s);
 
     final CurrentSlotProvider currentSlotProvider =
