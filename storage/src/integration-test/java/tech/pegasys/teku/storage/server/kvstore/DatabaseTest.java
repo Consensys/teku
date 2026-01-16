af9bc3bb083f363540d409a7fd51299cb5487da8
Gabriel Fukushima <gabrielfukushima@gmail.com>
2026-01-16 11:46:07 +1000

    Update earliest available data column slot after pruning (#10265)
    * update earliestAvailableDataColumnSlot after pruning and add test
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    * spotless
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    * fix test
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    * Fix constant name
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>
    
    ---------
    
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- storage/src/integration-test/java/tech/pegasys/teku/storage/server/kvstore/DatabaseTest.java
+++ storage/src/integration-test/java/tech/pegasys/teku/storage/server/kvstore/DatabaseTest.java
@@ -376,6 +376,48 @@ public class DatabaseTest {
     assertThat(getSlotBlobsArchiveFile(block5Sidecar0)).exists();
   }
 
+  @TestTemplate
+  public void verifyDataColumnSidecarsPruningSetsDbVariable(final DatabaseContext context)
+      throws IOException {
+    setupWithSpec(TestSpecFactory.createMinimalFulu());
+    initialize(context);
+
+    // no sidecars, earliest slot 0, because of genesis Fulu
+    assertThat(database.getEarliestDataColumnSidecarSlot()).isEmpty();
+
+    final DataColumnSidecar dataColumnSidecarSlot0 =
+        dataStructureUtil.randomDataColumnSidecar(
+            dataStructureUtil.randomSignedBeaconBlockHeader(UInt64.valueOf(0)), UInt64.valueOf(0));
+    final DataColumnSidecar dataColumnSidecarSlot1 =
+        dataStructureUtil.randomDataColumnSidecar(
+            dataStructureUtil.randomSignedBeaconBlockHeader(UInt64.valueOf(1)), UInt64.valueOf(1));
+    final DataColumnSidecar dataColumnSidecarSlot2 =
+        dataStructureUtil.randomDataColumnSidecar(
+            dataStructureUtil.randomSignedBeaconBlockHeader(UInt64.valueOf(2)), UInt64.valueOf(2));
+    final DataColumnSidecar dataColumnSidecarSlot3 =
+        dataStructureUtil.randomDataColumnSidecar(
+            dataStructureUtil.randomSignedBeaconBlockHeader(UInt64.valueOf(3)), UInt64.valueOf(3));
+
+    // add blobs out of order
+    database.addSidecar(dataColumnSidecarSlot1);
+    database.addSidecar(dataColumnSidecarSlot0);
+    database.addSidecar(dataColumnSidecarSlot3);
+    database.addSidecar(dataColumnSidecarSlot2);
+
+    assertThat(database.getSidecarColumnCount()).isEqualTo(4L);
+
+    // This variable only gets set by pruner or backfiller
+    assertThat(database.getEarliestAvailableDataColumnSlot()).isEmpty();
+
+    // let's prune with limit to 1, will prune slot 0
+    database.pruneAllSidecars(UInt64.MAX_VALUE, 1);
+
+    assertThat(database.getEarliestAvailableDataColumnSlot()).contains(ONE);
+
+    database.pruneAllSidecars(UInt64.MAX_VALUE, 2);
+    assertThat(database.getEarliestAvailableDataColumnSlot()).contains(UInt64.valueOf(3));
+  }
+
   private Path getSlotBlobsArchiveFile(final BlobSidecar blobSidecar) {
     return blobSidecarsArchiver.resolveArchivePath(
         blobSidecar.getSlotAndBlockRoot().getBlockRoot());
