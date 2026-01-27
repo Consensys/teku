a16ac1ce16f1f5ac9dfbfd86a5500b9c8396d2d0
Dmitrii Shmatko <zilm13@users.noreply.github.com>
2026-01-27 17:57:41 +0100

    Add getBlobs blob reconstruction from any 50% (#10274)
    * Add getBlobs blob reconstruction from any 50%
    
    * Refactor BlobReconstructor to abstract class

--- services/beaconchain/src/main/java/tech/pegasys/teku/services/beaconchain/BeaconChainController.java
+++ services/beaconchain/src/main/java/tech/pegasys/teku/services/beaconchain/BeaconChainController.java
@@ -1425,7 +1425,7 @@ public class BeaconChainController extends Service implements BeaconChainControl
     }
 
     this.blobReconstructionProvider =
-        new BlobReconstructionProvider(combinedChainDataClient, networkRetriever, spec);
+        BlobReconstructionProvider.create(combinedChainDataClient, networkRetriever, spec);
   }
 
   protected void initDataProvider() {
