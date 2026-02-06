045f904a7d3a791e96024bbef4f629164eb7cafa
Dmitrii Shmatko <zilm@zilm.net>
2026-02-06 11:49:19 +0100

    move according to the pathspec

--- ethereum/spec/src/property-test/java/tech/pegasys/teku/spec/datastructures/blobs/versions/fulu/CellPropertyTest.java
+++ ethereum/spec/src/property-test/java/tech/pegasys/teku/spec/datastructures/blobs/versions/fulu/CellPropertyTest.java
@@ -11,7 +11,7 @@
  * specific language governing permissions and limitations under the License.
  */
 
-package tech.pegasys.teku.spec.datastructures.datacolumn.versions.fulu;
+package tech.pegasys.teku.spec.datastructures.blobs.versions.fulu;
 
 import static tech.pegasys.teku.spec.propertytest.util.PropertyTestHelper.assertDeserializeMutatedThrowsExpected;
 import static tech.pegasys.teku.spec.propertytest.util.PropertyTestHelper.assertRoundTrip;
@@ -19,7 +19,6 @@ import static tech.pegasys.teku.spec.propertytest.util.PropertyTestHelper.assert
 import com.fasterxml.jackson.core.JsonProcessingException;
 import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
-import tech.pegasys.teku.spec.datastructures.blobs.versions.fulu.Cell;
 import tech.pegasys.teku.spec.propertytest.suppliers.datacolumn.versions.fulu.CellSupplier;
 
 public class CellPropertyTest {
