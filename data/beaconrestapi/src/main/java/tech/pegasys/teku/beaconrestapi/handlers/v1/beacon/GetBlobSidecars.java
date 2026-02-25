2e961b0f70440060652dcd77bb6b052cb62329d4
Paul Harris <paul.harris@consensys.net>
2026-02-25 15:20:01 +1000

    Marked blob_sidecar events deprecated
    also marked GetBlobSidecars endpoint deprecated.
    
    fixes #10412

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/GetBlobSidecars.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/GetBlobSidecars.java
@@ -65,6 +65,7 @@ public class GetBlobSidecars extends RestApiEndpoint {
     return EndpointMetadata.get(ROUTE)
         .operationId("getBlobSidecars")
         .summary("Get blob sidecars")
+        .deprecated(true)
         .description(
             """
                 Retrieves blob sidecars for a given block id.
