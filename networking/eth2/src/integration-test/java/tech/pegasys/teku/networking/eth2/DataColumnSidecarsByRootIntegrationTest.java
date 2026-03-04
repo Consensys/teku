d19f5fbc81c48e5d98d76a7e0aa51d462f73d5d9
Mehdi AOUADI <1208687+mehdi-aouadi@users.noreply.github.com>
2026-03-04 15:32:36 +0100

    rergoup by root tests

--- networking/eth2/src/integration-test/java/tech/pegasys/teku/networking/eth2/DataColumnSidecarsByRootIntegrationTest.java
+++ networking/eth2/src/integration-test/java/tech/pegasys/teku/networking/eth2/DataColumnSidecarsByRootIntegrationTest.java
@@ -14,6 +14,11 @@
 package tech.pegasys.teku.networking.eth2;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assumptions.assumeThat;
+import static tech.pegasys.teku.spec.SpecMilestone.ELECTRA;
+import static tech.pegasys.teku.spec.SpecMilestone.FULU;
+import static tech.pegasys.teku.spec.SpecMilestone.GLOAS;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,43 +27,67 @@ import java.util.concurrent.TimeoutException;
 import java.util.stream.Stream;
 import org.apache.tuweni.bytes.Bytes32;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
+import tech.pegasys.teku.kzg.NoOpKZG;
 import tech.pegasys.teku.networking.eth2.peers.Eth2Peer;
-import tech.pegasys.teku.spec.Spec;
+import tech.pegasys.teku.spec.SpecMilestone;
+import tech.pegasys.teku.spec.TestSpecContext;
+import tech.pegasys.teku.spec.TestSpecInvocationContextProvider;
 import tech.pegasys.teku.spec.datastructures.blobs.DataColumnSidecar;
 import tech.pegasys.teku.spec.datastructures.networking.libp2p.rpc.DataColumnsByRootIdentifier;
 import tech.pegasys.teku.spec.datastructures.networking.libp2p.rpc.DataColumnsByRootIdentifierSchema;
+import tech.pegasys.teku.spec.logic.common.statetransition.availability.AvailabilityCheckerFactory;
+import tech.pegasys.teku.spec.schemas.SchemaDefinitionsFulu;
 
-public abstract class DataColumnSidecarsByRootIntegrationTest
-    extends AbstractRpcMethodIntegrationTest {
+@TestSpecContext(milestone = {ELECTRA, FULU, GLOAS})
+public class DataColumnSidecarsByRootIntegrationTest extends AbstractRpcMethodIntegrationTest {
 
   private Eth2Peer peer;
   private DataColumnsByRootIdentifierSchema dataColumnsIdentifierSchema;
-
-  protected abstract Spec createSpec();
-
-  protected abstract DataColumnsByRootIdentifierSchema createDataColumnsIdentifierSchema(Spec spec);
+  private SpecMilestone specMilestone;
 
   @BeforeEach
-  public void setUp() {
-    final Spec spec = createSpec();
-    peer = createPeer(spec);
-    dataColumnsIdentifierSchema = createDataColumnsIdentifierSchema(spec);
+  public void setUp(final TestSpecInvocationContextProvider.SpecContext specContext) {
+    specContext
+        .getSpec()
+        .reinitializeForTesting(
+            AvailabilityCheckerFactory.NOOP_BLOB_SIDECAR,
+            AvailabilityCheckerFactory.NOOP_DATACOLUMN_SIDECAR,
+            NoOpKZG.INSTANCE);
+    specMilestone = specContext.getSpecMilestone();
+    peer = createPeer(specContext.getSpec());
+    if (specMilestone.isGreaterThanOrEqualTo(FULU)) {
+      dataColumnsIdentifierSchema =
+          SchemaDefinitionsFulu.required(
+                  specContext.getSpec().forMilestone(SpecMilestone.FULU).getSchemaDefinitions())
+              .getDataColumnsByRootIdentifierSchema();
+    }
   }
 
-  @Test
+  @TestTemplate
+  public void requestDataColumnSidecars_shouldFailBeforeFuluMilestone() {
+    assumeThat(specMilestone).isLessThan(FULU);
+    assertThatThrownBy(() -> requestDataColumnSidecarsByRoot(peer, List.of()))
+        .hasRootCauseInstanceOf(UnsupportedOperationException.class)
+        .hasMessageContaining("DataColumnSidecarsByRoot method is not supported");
+  }
+
+  @TestTemplate
   public void requestDataColumnSidecars_shouldReturnEmptyDataColumnSidecars()
       throws ExecutionException, InterruptedException, TimeoutException {
+    assumeThat(specMilestone).isGreaterThanOrEqualTo(FULU);
     final List<DataColumnSidecar> dataColumnSidecars =
         requestDataColumnSidecarsByRoot(
             peer, List.of(dataColumnsIdentifierSchema.create(Bytes32.ZERO, UInt64.ZERO)));
     assertThat(dataColumnSidecars).isEmpty();
   }
 
-  @Test
+  @TestTemplate
   public void requestDataColumnSidecars_shouldReturnDataColumnSidecars()
       throws ExecutionException, InterruptedException, TimeoutException {
+    assumeThat(specMilestone).isGreaterThanOrEqualTo(FULU);
+
     // generate 2 blobs per block
     peerStorage.chainUpdater().blockOptions.setGenerateRandomBlobs(true);
     peerStorage.chainUpdater().blockOptions.setGenerateRandomBlobsCount(Optional.of(2));
