469a10e2ecd9b866c48f054b1615306f21036d65
Paul Harris <paul.harris@consensys.net>
2026-03-10 12:02:22 +1000

    review feedback

--- eth-benchmark-tests/src/jmh/java/tech/pegasys/teku/benchmarks/util/backing/LargeSszStateBenchmark.java
+++ eth-benchmark-tests/src/jmh/java/tech/pegasys/teku/benchmarks/util/backing/LargeSszStateBenchmark.java
@@ -11,7 +11,7 @@
  * specific language governing permissions and limitations under the License.
  */
 
-package tech.pegasys.teku.benchmarks;
+package tech.pegasys.teku.benchmarks.util.backing;
 
 import java.util.concurrent.TimeUnit;
 import org.apache.tuweni.bytes.Bytes48;
