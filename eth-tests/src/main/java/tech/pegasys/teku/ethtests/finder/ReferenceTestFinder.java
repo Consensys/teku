e4cf3d6a4ad8f3f91c0cc554fae2e999d28dba67
StefanBratanov <stefan.bratanov93@gmail.com>
2026-04-17 14:40:37 +0100

    fix codespell

--- eth-tests/src/main/java/tech/pegasys/teku/ethtests/finder/ReferenceTestFinder.java
+++ eth-tests/src/main/java/tech/pegasys/teku/ethtests/finder/ReferenceTestFinder.java
@@ -61,7 +61,7 @@ public class ReferenceTestFinder {
               if (!testsPath.toFile().exists()) {
                 return Stream.empty();
               }
-              // TODO-GLOAS: temporarily don't run Gloas refence tests until we merge all
+              // TODO-GLOAS: temporarily don't run Gloas reference tests until we merge all
               // v1.7.0-alpha.5 changes
               if (fork.equals(TestFork.GLOAS)) {
                 return Stream.empty();
