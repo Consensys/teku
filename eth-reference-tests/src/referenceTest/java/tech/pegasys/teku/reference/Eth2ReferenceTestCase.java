33e3bee56dbe55fdca74082035724106b3f9a94f
Dmitrii Shmatko <zilm@zilm.net>
2026-05-29 14:19:54 +0200

    disable gloas reference tests in a correct way\=

--- eth-reference-tests/src/referenceTest/java/tech/pegasys/teku/reference/Eth2ReferenceTestCase.java
+++ eth-reference-tests/src/referenceTest/java/tech/pegasys/teku/reference/Eth2ReferenceTestCase.java
@@ -123,12 +123,11 @@ public abstract class Eth2ReferenceTestCase {
 
   private static final ImmutableMap<String, TestExecutor> GLOAS_TEST_TYPES =
       ImmutableMap.<String, TestExecutor>builder()
-          // TODO: unignore when all alpha.8 is done
-          //          .putAll(TransitionTestExecutor.TRANSITION_TEST_TYPES)
-          //          .putAll(ForkUpgradeTestExecutor.FORK_UPGRADE_TEST_TYPES)
-          //          .putAll(RewardsTestExecutorBellatrix.REWARDS_TEST_TYPES)
-          //          .putAll(MerkleProofTests.MERKLE_PROOF_TEST_TYPES)
-          //          .putAll(NetworkingTests.NETWORKING_TEST_TYPES)
+          .putAll(TransitionTestExecutor.TRANSITION_TEST_TYPES)
+          .putAll(ForkUpgradeTestExecutor.FORK_UPGRADE_TEST_TYPES)
+          .putAll(RewardsTestExecutorBellatrix.REWARDS_TEST_TYPES)
+          .putAll(MerkleProofTests.MERKLE_PROOF_TEST_TYPES)
+          .putAll(NetworkingTests.NETWORKING_TEST_TYPES)
           .build();
 
   protected void runReferenceTest(final TestDefinition testDefinition) throws Throwable {
