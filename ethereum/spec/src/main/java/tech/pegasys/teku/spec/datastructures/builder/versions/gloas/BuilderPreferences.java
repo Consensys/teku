812014d576a6c8d0cb0e4761499733a1160eba88
Stefan Bratanov <stefan.bratanov93@gmail.com>
2026-06-12 08:58:18 +0100

    Add containers for Staked Builder API (#10824)

---
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/datastructures/builder/versions/gloas/BuilderPreferences.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright Consensys Software Inc., 2026
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package tech.pegasys.teku.spec.datastructures.builder.versions.gloas;
+
+import tech.pegasys.teku.infrastructure.ssz.containers.Container1;
+import tech.pegasys.teku.infrastructure.ssz.primitive.SszUInt64;
+import tech.pegasys.teku.infrastructure.ssz.tree.TreeNode;
+import tech.pegasys.teku.infrastructure.unsigned.UInt64;
+
+public class BuilderPreferences extends Container1<BuilderPreferences, SszUInt64> {
+
+  protected BuilderPreferences(
+      final BuilderPreferencesSchema schema, final UInt64 maxExecutionPayment) {
+    super(schema, SszUInt64.of(maxExecutionPayment));
+  }
+
+  protected BuilderPreferences(final BuilderPreferencesSchema schema, final TreeNode backingTree) {
+    super(schema, backingTree);
+  }
+
+  public UInt64 getMaxExecutionPayment() {
+    return getField0().get();
+  }
+
+  @Override
+  public BuilderPreferencesSchema getSchema() {
+    return (BuilderPreferencesSchema) super.getSchema();
+  }
+}
