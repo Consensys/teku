512593196a078bb2aee72751acd931b9005aaf13
Lucas Saldanha <lucascrsaldanha@gmail.com>
2026-06-25 12:20:37 +1200

    Fix BootnodeService package name
    Move BootnodeService from teku.pegasys.teku.services.bootnode to the
    correct tech.pegasys.teku.services.bootnode package and update its
    importer.

--- services/bootnode/src/main/java/tech/pegasys/teku/services/bootnode/BootnodeService.java
+++ services/bootnode/src/main/java/tech/pegasys/teku/services/bootnode/BootnodeService.java
@@ -11,7 +11,7 @@
  * specific language governing permissions and limitations under the License.
  */
 
-package teku.pegasys.teku.services.bootnode;
+package tech.pegasys.teku.services.bootnode;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
