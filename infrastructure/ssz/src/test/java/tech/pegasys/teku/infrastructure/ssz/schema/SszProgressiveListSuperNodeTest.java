f5a325c5487cdccbb422c9a263fffb3e267e4df0
Enrico Del Fante <enrico.delfante@consensys.net>
2026-07-03 18:24:16 +0200

    Add store/load roundtrip coverage for hinted progressive lists

--- infrastructure/ssz/src/test/java/tech/pegasys/teku/infrastructure/ssz/schema/SszProgressiveListSuperNodeTest.java
+++ infrastructure/ssz/src/test/java/tech/pegasys/teku/infrastructure/ssz/schema/SszProgressiveListSuperNodeTest.java
@@ -204,4 +204,39 @@ public class SszProgressiveListSuperNodeTest {
       assertThat(node).describedAs("level %s bottom node", level).isInstanceOf(SszSuperNode.class);
     }
   }
+
+  @ParameterizedTest
+  @ValueSource(ints = {1, 2, 5, 21, 85, 341})
+  void storeLoadRoundtrip_hinted(final int size) {
+    final SszList<SszContainer> hinted = hintedFromSsz(size);
+    final TreeNode node = hinted.getBackingNode();
+    final InMemoryStoringTreeNodeStore nodeStore = new InMemoryStoringTreeNodeStore();
+    final long rootGIndex = GIndexUtil.SELF_G_INDEX;
+
+    HINTED_SCHEMA.storeBackingNodes(nodeStore, 100, rootGIndex, node);
+    final TreeNode loaded =
+        HINTED_SCHEMA.loadBackingNodes(nodeStore, node.hashTreeRoot(), rootGIndex);
+
+    assertThat(loaded.hashTreeRoot()).isEqualTo(node.hashTreeRoot());
+    final SszList<SszContainer> reloaded = HINTED_SCHEMA.createFromBackingNode(loaded);
+    assertThat(reloaded.size()).isEqualTo(size);
+    for (int i = 0; i < size; i++) {
+      assertThat(reloaded.get(i).hashTreeRoot()).isEqualTo(hinted.get(i).hashTreeRoot());
+    }
+    assertLevelsAreSuperNodeBacked(loaded, size);
+  }
+
+  @Test
+  void storeLoadRoundtrip_hintedAfterMutation() {
+    final var mutable = hintedFromSsz(21).createWritableCopy();
+    mutable.set(21, element(21)); // grow into a new level before storing
+    final var list = mutable.commitChanges();
+    final TreeNode node = list.getBackingNode();
+    final InMemoryStoringTreeNodeStore nodeStore = new InMemoryStoringTreeNodeStore();
+
+    HINTED_SCHEMA.storeBackingNodes(nodeStore, 100, GIndexUtil.SELF_G_INDEX, node);
+    final TreeNode loaded =
+        HINTED_SCHEMA.loadBackingNodes(nodeStore, node.hashTreeRoot(), GIndexUtil.SELF_G_INDEX);
+    assertThat(loaded.hashTreeRoot()).isEqualTo(node.hashTreeRoot());
+  }
 }
