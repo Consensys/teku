d6004ef50f0893a43c228a0b809d1759e927e262
Gabriel Fukushima <gabrielfukushima@gmail.com>
2026-07-06 13:00:10 +1000

    Migrate acceptance-test deposit senders off web3j
    Reworks TekuDepositSender to submit validator deposits without the web3j-based
    DepositContract wrapper, and removes the now-unused DepositGenerator,
    DepositSenderService, and DepositTransactionSender acceptance-test utilities.
    
    Part of #10876.

--- acceptance-tests/src/testFixtures/java/tech/pegasys/teku/test/acceptance/dsl/TekuDepositSender.java
+++ acceptance-tests/src/testFixtures/java/tech/pegasys/teku/test/acceptance/dsl/TekuDepositSender.java
@@ -13,23 +13,13 @@
 
 package tech.pegasys.teku.test.acceptance.dsl;
 
-import java.time.Duration;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testcontainers.containers.Network;
-import org.web3j.crypto.Credentials;
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import tech.pegasys.teku.ethereum.execution.types.Eth1Address;
-import tech.pegasys.teku.infrastructure.async.SafeFuture;
-import tech.pegasys.teku.infrastructure.async.Waiter;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.spec.Spec;
-import tech.pegasys.teku.test.acceptance.dsl.tools.deposits.DepositGenerator;
-import tech.pegasys.teku.test.acceptance.dsl.tools.deposits.DepositSenderService;
 import tech.pegasys.teku.test.acceptance.dsl.tools.deposits.ValidatorKeyGenerator;
 import tech.pegasys.teku.test.acceptance.dsl.tools.deposits.ValidatorKeystores;
 
@@ -41,44 +31,6 @@ public class TekuDepositSender extends Node {
   public TekuDepositSender(final Network network, final Spec spec) {
     super(network, TekuBeaconNode.TEKU_DOCKER_IMAGE_NAME, TekuDockerVersion.LOCAL_BUILD, LOG);
     this.spec = spec;
-  }
-
-  public ValidatorKeystores sendValidatorDeposits(
-      final BesuNode eth1Node, final int numberOfValidators)
-      throws InterruptedException, ExecutionException, TimeoutException {
-    return sendValidatorDeposits(
-        eth1Node, numberOfValidators, spec.getGenesisSpecConfig().getMaxEffectiveBalance());
-  }
-
-  public ValidatorKeystores sendValidatorDeposits(
-      final BesuNode eth1Node, final int numberOfValidators, final UInt64 amount)
-      throws InterruptedException, ExecutionException, TimeoutException {
-    final Eth1Address eth1Address = eth1Node.getDepositContractAddress();
-    final Credentials eth1Credentials = Credentials.create(eth1Node.getRichBenefactorKey());
-    try (final DepositGenerator depositGenerator =
-        new DepositGenerator(
-            spec, eth1Node.getExternalJsonRpcUrl(), eth1Address, eth1Credentials, amount)) {
-      final ValidatorKeystores validatorKeystores = generateValidatorKeys(numberOfValidators);
-      final SafeFuture<Void> future = depositGenerator.sendDeposits(validatorKeystores);
-      Waiter.waitFor(future, Duration.ofMinutes(2));
-      return validatorKeystores;
-    }
-  }
-
-  public void sendValidatorDeposits(
-      final BesuNode eth1Node, final ValidatorKeystores validatorKeys, final UInt64 amount)
-      throws InterruptedException, ExecutionException, TimeoutException {
-    final Eth1Address eth1Address = eth1Node.getDepositContractAddress();
-    final Credentials eth1Credentials = Credentials.create(eth1Node.getRichBenefactorKey());
-    try (final DepositSenderService depositSenderService =
-        new DepositSenderService(
-            spec, eth1Node.getExternalJsonRpcUrl(), eth1Credentials, eth1Address, amount)) {
-      final List<SafeFuture<TransactionReceipt>> transactionReceipts =
-          validatorKeys.getValidatorKeys().stream().map(depositSenderService::sendDeposit).toList();
-      final SafeFuture<Void> future =
-          SafeFuture.allOf(transactionReceipts.toArray(SafeFuture[]::new));
-      Waiter.waitFor(future, Duration.ofMinutes(2));
-    }
   }
 
   public ValidatorKeystores generateValidatorKeys(
