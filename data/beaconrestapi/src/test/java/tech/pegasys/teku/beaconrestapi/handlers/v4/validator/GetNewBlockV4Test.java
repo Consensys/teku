8648527e54c9f789a4fdd6a24c26816b51d03736
Matilda Clerke <matilda.clerke@consensys.net>
2026-07-07 15:06:19 +1000

    Fix GetNewBlockV4Test

--- data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v4/validator/GetNewBlockV4Test.java
+++ data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v4/validator/GetNewBlockV4Test.java
@@ -13,140 +13,39 @@
 
 package tech.pegasys.teku.beaconrestapi.handlers.v4.validator;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assumptions.assumeThat;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_BAD_REQUEST;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_INTERNAL_SERVER_ERROR;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_NOT_ACCEPTABLE;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_NOT_IMPLEMENTED;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_SERVICE_UNAVAILABLE;
-import static tech.pegasys.teku.infrastructure.http.RestApiConstants.HEADER_CONSENSUS_BLOCK_VALUE;
-import static tech.pegasys.teku.infrastructure.http.RestApiConstants.HEADER_CONSENSUS_VERSION;
-import static tech.pegasys.teku.infrastructure.http.RestApiConstants.HEADER_EXECUTION_PAYLOAD_BLINDED;
-import static tech.pegasys.teku.infrastructure.http.RestApiConstants.HEADER_EXECUTION_PAYLOAD_VALUE;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.RANDAO_REVEAL;
 import static tech.pegasys.teku.infrastructure.http.RestApiConstants.SLOT;
 import static tech.pegasys.teku.infrastructure.restapi.MetadataTestUtil.verifyMetadataErrorResponse;
-import static tech.pegasys.teku.infrastructure.unsigned.UInt64.ONE;
-import static tech.pegasys.teku.spec.SpecMilestone.BELLATRIX;
-import static tech.pegasys.teku.spec.SpecMilestone.DENEB;
-import static tech.pegasys.teku.spec.SpecMilestone.FULU;
-import static tech.pegasys.teku.spec.SpecMilestone.GLOAS;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import tech.pegasys.teku.beaconrestapi.AbstractMigratedBeaconHandlerTest;
-import tech.pegasys.teku.beaconrestapi.handlers.v3.validator.GetNewBlockV3;
 import tech.pegasys.teku.bls.BLSSignature;
 import tech.pegasys.teku.bls.BLSTestUtil;
-import tech.pegasys.teku.infrastructure.async.SafeFuture;
-import tech.pegasys.teku.infrastructure.http.HttpStatusCodes;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.spec.SpecMilestone;
 import tech.pegasys.teku.spec.TestSpecContext;
 import tech.pegasys.teku.spec.TestSpecInvocationContextProvider;
-import tech.pegasys.teku.spec.datastructures.blocks.BlockContainer;
-import tech.pegasys.teku.spec.datastructures.metadata.BlockContainerAndMetaData;
 
 @TestSpecContext(allMilestones = true)
 public class GetNewBlockV4Test extends AbstractMigratedBeaconHandlerTest {
 
-  private SpecMilestone specMilestone;
   protected final BLSSignature signature = BLSTestUtil.randomSignature(1234);
 
   @BeforeEach
   public void setup(final TestSpecInvocationContextProvider.SpecContext specContext) {
     setSpec(specContext.getSpec());
-    specMilestone = specContext.getSpecMilestone();
-    setHandler(new GetNewBlockV3(validatorDataProvider, schemaDefinitionCache));
+    setHandler(new GetNewBlockV4(validatorDataProvider, schemaDefinitionCache));
     request.setPathParameter(SLOT, "1");
     request.setQueryParameter(RANDAO_REVEAL, signature.toBytesCompressed().toHexString());
     when(validatorDataProvider.getMilestoneAtSlot(UInt64.ONE)).thenReturn(SpecMilestone.ALTAIR);
-  }
-
-  @TestTemplate
-  void shouldHandleBlindedBeaconBlocks() throws Exception {
-    assumeThat(specMilestone).isBetween(BELLATRIX, FULU);
-    final BlockContainerAndMetaData blockContainerAndMetaData =
-        dataStructureUtil.randomBlindedBlockContainerAndMetaData(ONE);
-
-    doReturn(SafeFuture.completedFuture(Optional.of(blockContainerAndMetaData)))
-        .when(validatorDataProvider)
-        .produceBlock(ONE, signature, Optional.empty(), Optional.empty());
-
-    handler.handleRequest(request);
-
-    assertThat(request.getResponseCode()).isEqualTo(HttpStatusCodes.SC_OK);
-    assertThat(request.getResponseBody()).isEqualTo(blockContainerAndMetaData);
-    assertThat(request.getResponseHeaders(HEADER_CONSENSUS_VERSION))
-        .isEqualTo(blockContainerAndMetaData.specMilestone().lowerCaseName());
-    assertThat(request.getResponseHeaders(HEADER_EXECUTION_PAYLOAD_BLINDED))
-        .isEqualTo(Boolean.toString(true));
-    assertThat(request.getResponseHeaders(HEADER_EXECUTION_PAYLOAD_VALUE))
-        .isEqualTo(blockContainerAndMetaData.executionPayloadValue().toDecimalString());
-    assertThat(request.getResponseHeaders(HEADER_CONSENSUS_BLOCK_VALUE))
-        .isEqualTo(blockContainerAndMetaData.consensusBlockValue().toDecimalString());
-  }
-
-  @TestTemplate
-  void shouldHandleUnBlindedBeaconBlocks() throws Exception {
-    assumeThat(specMilestone.isLessThan(DENEB) || specMilestone.isGreaterThanOrEqualTo(GLOAS))
-        .isTrue();
-    BlockContainerAndMetaData blockContainerAndMetaData =
-        dataStructureUtil.randomBlockContainerAndMetaData(ONE);
-    doReturn(SafeFuture.completedFuture(Optional.of(blockContainerAndMetaData)))
-        .when(validatorDataProvider)
-        .produceBlock(ONE, signature, Optional.empty(), Optional.empty());
-
-    handler.handleRequest(request);
-
-    assertThat(request.getResponseCode()).isEqualTo(HttpStatusCodes.SC_OK);
-    assertThat(request.getResponseBody()).isEqualTo(blockContainerAndMetaData);
-    assertThat(request.getResponseHeaders(HEADER_CONSENSUS_VERSION))
-        .isEqualTo(blockContainerAndMetaData.specMilestone().lowerCaseName());
-    assertThat(request.getResponseHeaders(HEADER_EXECUTION_PAYLOAD_BLINDED))
-        .isEqualTo(Boolean.toString(false));
-    assertThat(request.getResponseHeaders(HEADER_EXECUTION_PAYLOAD_VALUE))
-        .isEqualTo(blockContainerAndMetaData.executionPayloadValue().toDecimalString());
-    assertThat(request.getResponseHeaders(HEADER_CONSENSUS_BLOCK_VALUE))
-        .isEqualTo(blockContainerAndMetaData.consensusBlockValue().toDecimalString());
-  }
-
-  @TestTemplate
-  void shouldHandleUnBlindedBlockContentsPostDeneb() throws Exception {
-    assumeThat(specMilestone).isBetween(DENEB, FULU);
-    final BlockContainer blockContents = dataStructureUtil.randomBlockContents(ONE);
-    final BlockContainerAndMetaData blockContainerAndMetaData =
-        dataStructureUtil.randomBlockContainerAndMetaData(blockContents, ONE);
-    doReturn(SafeFuture.completedFuture(Optional.of(blockContainerAndMetaData)))
-        .when(validatorDataProvider)
-        .produceBlock(ONE, signature, Optional.empty(), Optional.empty());
-
-    handler.handleRequest(request);
-
-    assertThat(request.getResponseCode()).isEqualTo(HttpStatusCodes.SC_OK);
-    assertThat(request.getResponseBody()).isEqualTo(blockContainerAndMetaData);
-    assertThat(request.getResponseHeaders(HEADER_CONSENSUS_VERSION))
-        .isEqualTo(blockContainerAndMetaData.specMilestone().lowerCaseName());
-    assertThat(request.getResponseHeaders(HEADER_EXECUTION_PAYLOAD_BLINDED)).isEqualTo("false");
-    assertThat(request.getResponseHeaders(HEADER_EXECUTION_PAYLOAD_VALUE))
-        .isEqualTo(blockContainerAndMetaData.executionPayloadValue().toDecimalString());
-    assertThat(request.getResponseHeaders(HEADER_CONSENSUS_BLOCK_VALUE))
-        .isEqualTo(blockContainerAndMetaData.consensusBlockValue().toDecimalString());
-  }
-
-  @TestTemplate
-  void shouldThrowExceptionWhenEmptyBlock() throws Exception {
-    doReturn(SafeFuture.completedFuture(Optional.empty()))
-        .when(validatorDataProvider)
-        .produceBlock(ONE, signature, Optional.empty(), Optional.empty());
-
-    handler.handleRequest(request);
-    assertThat(request.getResponseCode()).isEqualTo(SC_INTERNAL_SERVER_ERROR);
-    assertThat(request.getResponseBody().toString()).contains("Unable to produce a block");
   }
 
   @TestTemplate
@@ -162,6 +61,11 @@ public class GetNewBlockV4Test extends AbstractMigratedBeaconHandlerTest {
   @TestTemplate
   void metadata_shouldHandle500() throws JsonProcessingException {
     verifyMetadataErrorResponse(handler, SC_INTERNAL_SERVER_ERROR);
+  }
+
+  @TestTemplate
+  void metadata_shouldHandle501() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, SC_NOT_IMPLEMENTED);
   }
 
   @TestTemplate
