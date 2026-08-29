78908cf86a8a045a05082348f4f638562ac49ec9
Abhivansh <31abhivanshj@gmail.com>
2026-08-29 16:21:13 +0530

    test: add tests

--- data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/lightclient/GetLightClientUpdatesByRangeTest.java
+++ data/beaconrestapi/src/test/java/tech/pegasys/teku/beaconrestapi/handlers/v1/beacon/lightclient/GetLightClientUpdatesByRangeTest.java
@@ -14,47 +14,76 @@
 package tech.pegasys.teku.beaconrestapi.handlers.v1.beacon.lightclient;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_BAD_REQUEST;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_INTERNAL_SERVER_ERROR;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_NOT_ACCEPTABLE;
-import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_NOT_IMPLEMENTED;
 import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_OK;
+import static tech.pegasys.teku.infrastructure.http.HttpStatusCodes.SC_SERVICE_UNAVAILABLE;
 import static tech.pegasys.teku.infrastructure.restapi.MetadataTestUtil.getResponseSszFromMetadata;
 import static tech.pegasys.teku.infrastructure.restapi.MetadataTestUtil.getResponseStringFromMetadata;
 import static tech.pegasys.teku.infrastructure.restapi.MetadataTestUtil.verifyMetadataErrorResponse;
+import static tech.pegasys.teku.spec.constants.NetworkConstants.MAX_REQUEST_LIGHT_CLIENT_UPDATES;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.io.Resources;
 import java.io.IOException;
+import java.nio.ByteOrder;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import org.apache.tuweni.bytes.Bytes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tech.pegasys.teku.beaconrestapi.AbstractMigratedBeaconHandlerTest;
+import tech.pegasys.teku.infrastructure.bytes.Bytes4;
 import tech.pegasys.teku.infrastructure.unsigned.UInt64;
-import tech.pegasys.teku.spec.SpecMilestone;
 import tech.pegasys.teku.spec.TestSpecFactory;
 import tech.pegasys.teku.spec.datastructures.lightclient.LightClientUpdate;
-import tech.pegasys.teku.spec.datastructures.lightclient.LightClientUpdateResponse;
-import tech.pegasys.teku.spec.datastructures.metadata.ObjectAndMetaData;
-import tech.pegasys.teku.spec.util.DataStructureUtil;
+import tech.pegasys.teku.spec.datastructures.metadata.LightClientUpdateWithContext;
 
 public class GetLightClientUpdatesByRangeTest extends AbstractMigratedBeaconHandlerTest {
 
   @BeforeEach
   void setup() {
-    setHandler(new GetLightClientUpdatesByRange(schemaDefinitionCache));
-    dataStructureUtil = new DataStructureUtil(TestSpecFactory.createMinimalAltair());
+    setSpec(TestSpecFactory.createMinimalAltair());
+    setHandler(new GetLightClientUpdatesByRange(chainDataProvider, schemaDefinitionCache));
     request.setQueryParameter("start_period", "1");
     request.setQueryParameter("count", "1");
   }
 
   @Test
+  void shouldReturnBestUpdates() throws Exception {
+    final List<LightClientUpdateWithContext> updates =
+        List.of(dataStructureUtil.randomLightClientUpdateWithContext(UInt64.ONE));
+    when(chainDataProvider.getBestLightClientUpdates(UInt64.ONE, 1)).thenReturn(updates);
+
+    handler.handleRequest(request);
+
+    assertThat(request.getResponseCode()).isEqualTo(SC_OK);
+    assertThat(request.getResponseBody()).isEqualTo(updates);
+  }
+
+  @Test
+  void shouldCapCountAtMaxRequestLightClientUpdates() throws Exception {
+    setHandler(new GetLightClientUpdatesByRange(chainDataProvider, schemaDefinitionCache));
+    request.setQueryParameter("start_period", "1");
+    request.setQueryParameter("count", "1000");
+    when(chainDataProvider.getBestLightClientUpdates(UInt64.ONE, MAX_REQUEST_LIGHT_CLIENT_UPDATES))
+        .thenReturn(List.of());
+
+    handler.handleRequest(request);
+
+    assertThat(request.getResponseCode()).isEqualTo(SC_OK);
+    assertThat(request.getResponseBody()).isEqualTo(List.of());
+  }
+
+  @Test
   void metadata_shouldHandleJson200() throws IOException {
-    LightClientUpdate lightClientUpdate = dataStructureUtil.randomLightClientUpdate(UInt64.ONE);
-    ObjectAndMetaData<LightClientUpdate> responseData =
-        new ObjectAndMetaData<>(lightClientUpdate, SpecMilestone.ALTAIR, false, true, false);
-    List<ObjectAndMetaData<LightClientUpdate>> response = List.of(responseData);
+    final LightClientUpdate lightClientUpdate =
+        dataStructureUtil.randomLightClientUpdate(UInt64.ONE);
+    final List<LightClientUpdateWithContext> response =
+        List.of(
+            new LightClientUpdateWithContext(dataStructureUtil.randomBytes4(), lightClientUpdate));
 
     final String data = getResponseStringFromMetadata(handler, SC_OK, response);
     final String expected =
@@ -67,14 +96,23 @@ public class GetLightClientUpdatesByRangeTest extends AbstractMigratedBeaconHand
 
   @Test
   void metadata_shouldHandleSsz200() throws IOException {
-    LightClientUpdateResponse responseData =
-        dataStructureUtil.randomLightClientUpdateResponse(UInt64.ONE);
-    List<LightClientUpdateResponse> response = List.of(responseData);
+    final LightClientUpdateWithContext first =
+        dataStructureUtil.randomLightClientUpdateWithContext(UInt64.ONE);
+    final LightClientUpdateWithContext second =
+        dataStructureUtil.randomLightClientUpdateWithContext(UInt64.ONE);
 
-    final byte[] actual = getResponseSszFromMetadata(handler, SC_OK, response);
-    final byte[] expected = responseData.sszSerialize().toArray();
+    final byte[] actual = getResponseSszFromMetadata(handler, SC_OK, List.of(first, second));
 
-    assertThat(actual).isEqualTo(expected);
+    assertThat(Bytes.wrap(actual))
+        .isEqualTo(Bytes.wrap(responseChunk(first), responseChunk(second)));
+  }
+
+  private static Bytes responseChunk(final LightClientUpdateWithContext updateWithContext) {
+    final Bytes payload = updateWithContext.update().sszSerialize();
+    return Bytes.wrap(
+        Bytes.ofUnsignedLong(Bytes4.SIZE + payload.size(), ByteOrder.LITTLE_ENDIAN),
+        updateWithContext.context().getWrappedBytes(),
+        payload);
   }
 
   @Test
@@ -93,7 +131,7 @@ public class GetLightClientUpdatesByRangeTest extends AbstractMigratedBeaconHand
   }
 
   @Test
-  public void metadata_shouldHandle501() throws JsonProcessingException {
-    verifyMetadataErrorResponse(handler, SC_NOT_IMPLEMENTED);
+  void metadata_shouldHandle503() throws JsonProcessingException {
+    verifyMetadataErrorResponse(handler, SC_SERVICE_UNAVAILABLE);
   }
 }
